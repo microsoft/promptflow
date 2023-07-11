d33c4d3809bd76313542fba567f4f93622bf9ebb
Korin <0mza987@gmail.com>
2023-07-11 17:05:36 +0800

    fix comments

--- flows/standard/classification_accuracy_evaluation/grade.py
+++
@@ -1,8 +0,0 @@
-from promptflow import tool
-
-
-@tool
-def grade(groundtruth: str, prediction: str):
-    groundtruth = groundtruth.lower().strip('"')
-    prediction = prediction.lower().strip('"')
-    return "Correct" if  groundtruth== prediction else "Incorrect"
