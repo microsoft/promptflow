99803809c663706676a1f8ea3593020c8679a0d8
zhen <henu_rz@163.com>
2023-07-21 11:23:44 +0800

    fix comment

---
+++ examples/flows/standard/flow-with-symlinks/create_symlinks.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+saved_path = os.getcwd()
+os.chdir("./")
+
+source_folder = Path("../web-classification")
+for file_name in os.listdir(source_folder):
+    if not Path(file_name).exists():
+        os.symlink(
+            source_folder / file_name,
+            file_name
+        )
+
+os.chdir(saved_path)
