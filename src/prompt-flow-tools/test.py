7782c4975f58bf1611d21e8d4a1ec2e5b031373c
yalu4 <yalu4@microsoft.com>
2023-07-21 11:05:52 +0800

    update

--- src/prompt-flow-tools/test.py
+++
@@ -1,7 +0,0 @@
-def test():
-    # `collect_package_tools` gathers all tools info using the `package-tools` entry point. This ensures that your package is correctly packed and your tools are accurately collected. 
-    from promptflow.core.tools_manager import collect_package_tools
-    tools = collect_package_tools()
-    print(tools)
-if __name__ == "__main__":
-    test()
