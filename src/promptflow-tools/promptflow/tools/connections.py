ec6d93c9bb4fcf6c7343d5133a29107f64832b1e
Yao <46446115+16oeahr@users.noreply.github.com>
2023-07-28 10:19:06 +0800

    remove legacy connections (#33)
    Co-authored-by: yalu4 <yalu4@microsoft.com>

--- src/promptflow-tools/promptflow/tools/connections.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from promptflow.connections import *  # noqa
-
-logging.warning(
-    "Module 'promptflow.tools.connections' will be removed in the future, "
-    "please import from 'promptflow.connections' instead."
-)
