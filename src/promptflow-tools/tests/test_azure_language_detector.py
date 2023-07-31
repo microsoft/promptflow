ab7c2cb1002cda0997d96cc7966c8bb029fc15d6
Jincheng Chen <chjinche@microsoft.com>
2023-07-31 16:31:49 +0800

    test

--- src/promptflow-tools/tests/test_azure_language_detector.py
+++ src/promptflow-tools/tests/test_azure_language_detector.py
@@ -13,11 +13,13 @@ def translate_connection() -> CustomConnection:
     return ConnectionManager().get("translate_connection")
 
 
-def test_get_language(translate_connection):
-    input_text = "This is a long sentence that I want to translate."
-    result = get_language(translate_connection, input_text)
-    # builtin tool output should be json serializable
-    # make sure to run successfully in single node run mode.
-    utils.is_json_serializable(result, "detect()")
-    print(f"detect() :{json.dumps(result)}")
-    assert result == "en"
+@pytest.mark.usefixtures("use_secrets_config_file")
+class TestAzureLanguageDetector:
+    def test_get_language(self, translate_connection):
+        input_text = "This is a long sentence that I want to translate."
+        result = get_language(translate_connection, input_text)
+        # builtin tool output should be json serializable
+        # make sure to run successfully in single node run mode.
+        utils.is_json_serializable(result, "detect()")
+        print(f"detect() :{json.dumps(result)}")
+        assert result == "en"
