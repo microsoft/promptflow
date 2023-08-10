7aa65f69b85c59c4aec885742f1af144ef3c93b2
crazygao <yigao@microsoft.com>
2023-08-10 14:39:30 +0800

    Generate Workflow From Readme (#63)
    Generate Workfow from Readme file.
    Extract Readme file ```bash``` steps and generate workflow file.
    By the way, use jinja as template driver for workflow generation.

---
+++ scripts/ghactions_driver/extract_steps_from_readme.py
@@ -0,0 +1,77 @@
+import argparse
+import io
+import re
+from pathlib import Path
+
+import panflute
+import pypandoc
+from jinja2 import Environment, FileSystemLoader
+
+from readme_step import ReadmeStepsManage
+
+full_text = ""
+
+
+def strip_comments(code):
+    code = str(code)
+    code = re.sub(r"(?m)^ *#.*\n?", "", code)  # remove comments
+    splits = [ll.rstrip() for ll in code.splitlines() if ll.strip()]  # remove empty
+    splits_no_interactive = [
+        split for split in splits if "interactive" not in split
+    ]  # remove --interactive
+    text = "\n".join([ll.rstrip() for ll in splits_no_interactive])
+    # replacements
+    text = text.replace("<your_api_key>", "$1")
+    text = text.replace("<your_api_base>", "$2")
+    text = text.replace("<your_subscription_id>", "$3")
+    text = text.replace("<your_resource_group_id>", "$4")
+    text = text.replace("<your_workspace_name>", "$5")
+    return text
+
+
+def action(elem, doc):
+    global full_text
+    if isinstance(elem, panflute.CodeBlock) and "bash" in elem.classes:
+        full_text = "\n".join([full_text, strip_comments(elem.text)])
+
+
+def readme_parser(filename: str):
+    real_filename = Path(ReadmeStepsManage.git_base_dir()) / filename
+    data = pypandoc.convert_file(str(real_filename), "json")
+    f = io.StringIO(data)
+    doc = panflute.load(f)
+    panflute.run_filter(action, doc=doc)
+
+
+def write_readme_shell(readme_path: str, output_folder: str):
+    readme_parser(readme_path)
+    Path(ReadmeStepsManage.git_base_dir())
+    bash_script_path = (
+        Path(ReadmeStepsManage.git_base_dir()) / output_folder / "bash_script.sh"
+    )
+    template_env = Environment(
+        loader=FileSystemLoader(
+            Path(ReadmeStepsManage.git_base_dir())
+            / "scripts/ghactions_driver/bash_script"
+        )
+    )
+    bash_script_template = template_env.get_template("bash_script.sh.jinja2")
+    with open(bash_script_path, "w") as f:
+        f.write(bash_script_template.render({"command": full_text}))
+
+
+if __name__ == "__main__":
+    # setup argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--readme-file",
+        help="Input README.md example 'examples/flows/standard/basic/README.md'",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-folder",
+        help="Output folder for bash_script.sh example 'examples/flows/standard/basic/'",
+    )
+    args = parser.parse_args()
+    write_readme_shell(args.readme_file, args.output_folder)
---
+++ scripts/readme_generator.py
@@ -0,0 +1,33 @@
+import argparse
+from pathlib import Path
+
+from ghactions_driver.readme_workflow_generate import write_readme_workflow
+from ghactions_driver.readme_step import ReadmeStepsManage, ReadmeSteps
+
+
+def main(args):
+    globs = [
+        sorted(Path(ReadmeStepsManage.git_base_dir()).glob(p)) for p in args.input_glob
+    ]
+    readme_items = sorted([j for i in globs for j in i])
+
+    for readme in readme_items:
+        workflow_name = readme.parent.relative_to(ReadmeStepsManage.git_base_dir())
+        # Deal with readme
+        write_readme_workflow(workflow_name.resolve())
+        ReadmeSteps.cleanup()
+
+
+if __name__ == "__main__":
+    # setup argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-g",
+        "--input-glob",
+        nargs="+",
+        help="Input Readme.md glob example 'examples/flows/**/Readme.md'",
+    )
+    args = parser.parse_args()
+
+    # call main
+    main(args)
