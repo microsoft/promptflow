a51bb6338dacd0de6efe52b2051927dd49f215e8
chenslucky <75061414+chenslucky@users.noreply.github.com>
2023-08-10 11:15:46 +0800

    Remove form recognizer tool (#79)
    Remove form recognizer tool
    
    ---------
    
    Co-authored-by: cs_lucky <si.chen@microsoft.com>

--- src/promptflow-tools/promptflow/tools/__init__.py
+++ src/promptflow-tools/promptflow/tools/__init__.py
@@ -2,7 +2,6 @@ from .aoai import AzureOpenAI  # noqa: F401
 from .azure_content_safety import AzureContentSafety  # noqa: F401
 from .azure_detect import AzureDetect  # noqa: F401
 from .azure_language_detector import get_language  # noqa: F401
-from .azure_form_recognizer import AzureFormRecognizer  # noqa: F401
 from .azure_translator import get_translation, AzureTranslator  # noqa: F401
 from .openai import OpenAI  # noqa: F401
 from .serpapi import SerpAPI  # noqa: F401
