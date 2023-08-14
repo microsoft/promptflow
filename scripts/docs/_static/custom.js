b12c9e889ad8633b30cd5ee72254caef65dd5b34
Brynn Yin <biyi@microsoft.com>
2023-08-14 16:34:20 +0800

    Setup github pages
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

---
+++ scripts/docs/_static/custom.js
@@ -0,0 +1,54 @@
+// This is used to zoom in images when clicked on
+window.onload = () => {
+    if (document.getElementById("lightbox") === null){
+      // Append lightbox div to each page
+      let div = document.createElement('div');
+      div.innerHTML = '<div id="lightbox"></div>';
+      document.body.appendChild(div);
+    }
+
+    // (A) GET LIGHTBOX & ALL .ZOOMD IMAGES
+    let all = document.getElementsByClassName("bd-article")[0].getElementsByTagName("img"),
+        lightbox = document.getElementById("lightbox");
+
+    // (B) CLICK TO SHOW IMAGE IN LIGHTBOX
+    // * SIMPLY CLONE INTO LIGHTBOX & SHOW
+    if (all.length>0) { for (let i of all) {
+      i.onclick = () => {
+        let clone = i.cloneNode();
+        clone.className = "";
+        lightbox.innerHTML = "";
+        lightbox.appendChild(clone);
+        lightbox.className = "show";
+      };
+    }}
+
+    // (C) CLICK TO CLOSE LIGHTBOX
+    lightbox.onclick = () => {
+      lightbox.className = "";
+    };
+};
+
+// This is used to control homepage background
+if (document.URL === "https://promptflow.azurewebsites.net/" || document.URL.includes("index.html")) {
+  var observer = new MutationObserver(function(mutations) {
+    const dark = document.documentElement.dataset.theme == 'dark';
+    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundPositionY = "bottom";
+    document.body.style.backgroundRepeat = "no-repeat"
+    if (dark) {
+      if (window.screen.width <= 1280){
+        document.body.style.backgroundImage = "url('_static/bg_dark_small.png')"
+      } else {
+        document.body.style.backgroundImage = "url('_static/bg_dark_large.png')"
+      }
+    } else {
+      if (window.screen.width <= 1280){
+        document.body.style.backgroundImage = "url('_static/bg_light_small.png')"
+      } else {
+        document.body.style.backgroundImage = "url('_static/bg_light_large.png')"
+      }
+    }
+  })
+  observer.observe(document.documentElement, {attributes: true, attributeFilter: ['data-theme']});
+}
---
+++ scripts/docs/conf.py
@@ -0,0 +1,132 @@
+# -- Path setup --------------------------------------------------------------
+import sys
+
+# -- Project information -----------------------------------------------------
+
+project = 'Prompt flow'
+copyright = '2023, Microsoft'
+author = 'Microsoft'
+
+sys.path.append(".")
+from gallery_directive import GalleryDirective  # noqa: E402
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinxext.rediraffe",
+    "sphinx_design",
+    "sphinx_copybutton",
+    "matplotlib.sphinxext.plot_directive",
+    "numpydoc",
+    "sphinx_togglebutton",
+    'myst_parser',
+    "sphinx.builders.linkcheck",
+]
+
+# -- Internationalization ------------------------------------------------
+# specifying the natural language populates some key tags
+language = "en"
+
+# spcify charset as utf-8 to accept chinese punctuation
+charset_type = "utf-8"
+
+autosummary_generate = True
+
+# Add any paths that contain templates here, relative to this directory.
+# templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = [
+    "_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints",
+    "**.py", "**.yml", "**.ipynb", "**.sh", "**.zip", "**.skip"
+]
+
+# Options for the linkcheck builder
+linkcheck_ignore = [
+    r"https://(ms\.)?portal\.azure\.com/",
+    r"https://aka\.ms/acs-doc",
+    r"https://github\.com/microsoft/promptflow/",
+    r"https://github\.com/Azure/promptflow/",
+    r"https://platform\.openai\.com/",
+]
+
+linkcheck_exclude_documents = ["contributing"]
+
+# -- Extension options -------------------------------------------------------
+
+# This allows us to use ::: to denote directives, useful for admonitions
+myst_enable_extensions = ["colon_fence", "substitution"]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "pydata_sphinx_theme"
+html_logo = "_static/logo.svg"
+html_favicon = "_static/logo32.ico"
+html_sourcelink_suffix = ""
+html_show_sourcelink = False
+
+# Define the json_url for our version switcher.
+
+html_theme_options = {
+    "github_url": "https://github.com/Azure/promptflow",
+    "header_links_before_dropdown": 6,
+    "icon_links": [
+    ],
+    "logo": {
+        "text": "Prompt flow",
+        "alt_text": "Prompt flow",
+    },
+    "use_edit_page_button": True,
+    "show_toc_level": 1,
+    "navbar_align": "left",  # [left, content, right] For testing that the navbar items align properly
+    "navbar_center": ["navbar-nav"],
+    # "announcement": "Test our announcement here.",
+    "show_nav_level": 1,
+}
+
+html_sidebars = {
+    # "quick_start/README.md": ['localtoc.html', 'relations.html', 'searchbox.html'],
+    # "examples/persistent-search-field": ["search-field"],
+    # Blog sidebars
+    # ref: https://ablog.readthedocs.io/manual/ablog-configuration-options/#blog-sidebars
+    "features": ['localtoc.html', 'relations.html', 'searchbox.html'],
+    # "tutorials": ['localtoc.html', 'relations.html', 'searchbox.html'],
+}
+
+html_context = {
+    "default_mode": "light",
+    "github_user": "",
+    "github_repo": "Azure/promptflow",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+
+rediraffe_redirects = {
+}
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+html_js_files = ['custom.js']
+todo_include_todos = True
+
+
+# myst reference config
+myst_heading_anchors = 5
+
+
+def setup(app):
+    # Add the gallery directive
+    app.add_directive("gallery-grid", GalleryDirective)
