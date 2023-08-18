16158515d0716d0d2e66c6c0dff5e86c029fc4d7
Chenyang Zhang <chenyzhang@microsoft.com>
2023-08-18 13:48:33 +0800

    update code

--- examples/flows/auto_generate_docstring/divider.py
+++ examples/flows/auto_generate_docstring/divider.py
@@ -1,3 +1,4 @@
+import logging
 import re
 
 
@@ -69,21 +70,31 @@ class Divider:
         funcs2, pos2 = Divider.get_functions_and_pos(origin_code)
         pattern = r'""".*?"""'
         code = ""
-        pos1.append((pos1[-1][0] + 1, pos1[-1][0] + 1))  # avoid index out of range
-        pos2.append((pos2[-1][0] + 1, pos2[-1][0] + 1))  # avoid index out of range
+        pos1.append((len(docstring), len(docstring)))  # avoid index out of range
+        pos2.append((len(origin_code), len(origin_code)))  # avoid index out of range
         for i2 in range(len(funcs2)):  # add docstring for each function in origin_code
             try:
                 i1 = funcs1.index(funcs2[i2])
-                new_doc =re.findall(pattern, docstring[pos1[i1][1]:pos1[i1+1][0]], re.DOTALL)
-                if new_doc:
-                    code_doc = re.findall(pattern, origin_code[pos2[i2][1]:pos2[i2+1][0]], re.DOTALL)
-                    if code_doc:
-                        code += origin_code[pos2[i2][0]:pos2[i2+1][0]].replace(code_doc[0], new_doc[0])
-                    else:
-                        line = origin_code[pos2[i2][0]:pos2[i2][1]].replace('\n', '')
-                        space = (len(line) - len(line.lstrip()) + 4) * ' '
-                        code += origin_code[pos2[i2][0]:pos2[i2][1]] + '\n' + space + new_doc[0] + '\n' + \
-                                origin_code[pos2[i2][1]:pos2[i2+1][0]]
             except ValueError:
+                logging.warning(f"No docstring found for {funcs2[i2]}.")
+                code += origin_code[pos2[i2][0]:pos2[i2][1]]
                 continue
+            new_doc = re.findall(pattern, docstring[pos1[i1][1]:pos1[i1 + 1][0]], re.DOTALL)
+            if new_doc:
+                code_doc = re.findall(pattern, origin_code[pos2[i2][1]:pos2[i2 + 1][0]], re.DOTALL)
+                if code_doc:
+                    code += origin_code[pos2[i2][0]:pos2[i2 + 1][0]].replace(code_doc[0], new_doc[0])
+                else:
+                    line = origin_code[pos2[i2][0]:pos2[i2][1]].replace('\n', '')
+                    space = (len(line) - len(line.lstrip()) + 4) * ' '
+                    code += origin_code[pos2[i2][0]:pos2[i2][1]] + '\n' + \
+                            Divider.add_indentation(new_doc[0], space) + '\n' + \
+                            origin_code[pos2[i2][1]:pos2[i2 + 1][0]]
         return code
+
+    @classmethod
+    def add_indentation(cls, text, space):
+        lines = text.split('\n')
+        indented_lines = [space + line.strip() for line in lines]
+        indented_string = '\n'.join(indented_lines)
+        return indented_string
