2fec001326a2322f151e3db0cedd3cbeeb52fefd
Clement Wang <wangchao1230@outlook.com>
2023-08-18 20:17:34 +0800

    update export sample

--- examples/tutorials/flow-deploy/linux/start.sh
+++ examples/tutorials/flow-deploy/linux/start.sh
@@ -1,2 +1,6 @@
-python connections_setup.py --file connections.sqlite --migration-secret-file /run/secrets/MIGRATION_SECRET --clean --ignore-errors
+# Startup script for the flow container
+ls
+ls connections
+export USER_AGENT=promptflow-appservice
+pf connection create --file /connections/basic_custom_connection.yaml
 pf flow serve --source flow --host 0.0.0.0
