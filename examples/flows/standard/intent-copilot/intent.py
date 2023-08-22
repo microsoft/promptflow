bce16ae3d3f0d47d0cd34b9057be91ae04a7cd6a
Philip Gao <yigao@microsoft.com>
2023-08-22 10:11:07 +0000

    Remove Version specify

--- examples/flows/standard/intent-copilot/intent.py
+++ examples/flows/standard/intent-copilot/intent.py
@@ -26,7 +26,6 @@ def extract_intent(customer_info: str, history: list, user_prompt_template: str)
         openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
         openai_api_base=os.environ["AZURE_OPENAI_API_BASE"],
         openai_api_type="azure",
-        openai_api_version="2023-03-15-preview",
         temperature=0,
     )
 
@@ -55,7 +54,9 @@ if __name__ == "__main__":
 
     # each test
     for item in data:
-        reply = extract_intent(item["customer_info"], item["history"], user_prompt_template)
+        reply = extract_intent(
+            item["customer_info"], item["history"], user_prompt_template
+        )
         print("=====================================")
         # print("Customer info: ", item["customer_info"])
         # print("+++++++++++++++++++++++++++++++++++++")
