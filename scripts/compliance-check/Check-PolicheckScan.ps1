1e9e13198d12fe997e95f98217a46e93ea91074c
zhangxingzhi <zhangxingzhi@microsoft.com>
2023-08-28 13:02:33 +0800

    feat: enable policheck scan ci

---
+++ scripts/compliance-check/Check-PolicheckScan.ps1
@@ -0,0 +1,23 @@
+# Copyright (C) Microsoft Corporation.  All rights reserved.
+
+<#
+    .SYNOPSIS
+        Check Policheck Scan result.
+    .DESCRIPTION
+        Helper script to check the Policheck result.
+        If there is policheck failure, show the error and throw exception.
+#>
+
+[CmdLetbinding()]
+param (
+[string]$policheckResult
+)
+
+$result = Get-Content -Path $policheckResult | Measure-Object -Line;
+Write-Host("Number of errors found in this scan: " + ($result.Lines - 1));
+if ($raiseError -and ($result.Lines -gt 1))
+{
+Get-Content -Path $policheckResult;
+throw "Policheck scan completed successfully but there are issues to fix.";
+}
+# Read-Host "Press enter to finish the process and close this window";
