0f8cffb5f612d63ebbe3e4209ad6d7f3955360ec
Zhengfei Wang <zhengfei.wang@microsoft.com>
2023-08-28 14:54:57 +0800

    add /{name}/metadata

--- src/promptflow/promptflow/_sdk/_service/run.py
+++ src/promptflow/promptflow/_sdk/_service/run.py
@@ -2,10 +2,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+from dataclasses import asdict
+
 from flask import Blueprint, jsonify
 
 from promptflow._sdk.operations._local_storage_operations import LocalStorageOperations
 from promptflow._sdk.operations._run_operations import RunOperations
+from promptflow.contracts._run_management import RunMetadata
 
 run_bp = Blueprint("run", __name__, url_prefix="/run")
 
@@ -19,11 +22,30 @@ def list():
 
 
 @run_bp.route("/<string:name>", methods=["GET"])
-def get_metadata(name: str):
+def get(name: str):
     op = RunOperations()
     run = op.get(name=name)
-    metadata_dict = run._to_dict()
-    return jsonify(metadata_dict)
+    run_dict = run._to_dict()
+    return jsonify(run_dict)
+
+
+@run_bp.route("/<string:name>/metadata", methods=["GET"])
+def get_metadata(name: str):
+    run_op = RunOperations()
+    run = run_op.get(name=name)
+    local_storage_op = LocalStorageOperations(run=run)
+    metadata = RunMetadata(
+        name=run.name,
+        display_name=run.display_name,
+        tags=run.tags,
+        lineage=run.run,
+        metrics=local_storage_op.load_metrics(),
+        dag=local_storage_op.load_dag_as_string(),
+        # TODO(zhengfei): investigate why this will lead to API break
+        # flow_tools_json=local_storage_op.load_flow_tools_json(),
+        flow_tools_json=None,
+    )
+    return jsonify(asdict(metadata))
 
 
 @run_bp.route("/<string:name>/detail", methods=["GET"])
