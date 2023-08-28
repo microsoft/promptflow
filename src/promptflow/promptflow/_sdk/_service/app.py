6f9cb015fcac4411dd1c99eb5587eccdf7e92f7b
Zhengfei Wang <zhengfei.wang@microsoft.com>
2023-08-28 18:21:09 +0800

    add /heartbeat

--- src/promptflow/promptflow/_sdk/_service/app.py
+++ src/promptflow/promptflow/_sdk/_service/app.py
@@ -2,13 +2,18 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from flask import Flask
+from flask import Flask, Response
 
 from promptflow._sdk._service.run import run_bp
 
 
+def heartbeat():
+    return Response(status=204)
+
+
 def create_app():
     app = Flask(__name__)
+    app.add_url_rule("/heartbeat", view_func=heartbeat)
     app.register_blueprint(run_bp)
     return app
 
