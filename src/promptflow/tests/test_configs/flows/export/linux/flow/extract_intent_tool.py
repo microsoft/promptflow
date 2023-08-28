22ab0d934e99a3df8d4c6c952b0febf792c382b1
Brynn Yin <biyi@microsoft.com>
2023-08-28 14:30:29 +0800

    Update example
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- src/promptflow/tests/test_configs/flows/export/linux/flow/extract_intent_tool.py
+++ src/promptflow/tests/test_configs/flows/export/linux/flow/extract_intent_tool.py
@@ -10,7 +10,7 @@ from promptflow.connections import CustomConnection
 def extract_intent_tool(customer_info, history, user_prompt_template, connection: CustomConnection) -> str:
 
     # set environment variables
-    for key, value in connection.items():
+    for key, value in dict(connection).items():
         os.environ[key] = value
 
     # call the entry function
