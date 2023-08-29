f59edd6f18aececb785c74d2464ccb60cde409d6
Zhengfei Wang <zhengfei.wang@microsoft.com>
2023-08-29 17:29:21 +0800

    update GH action

--- src/promptflow/tests/sdk_local_service_test/e2etests/test_run_apis.py
+++ src/promptflow/tests/sdk_local_service_test/e2etests/test_run_apis.py
@@ -3,8 +3,11 @@
 # ---------------------------------------------------------
 
 import pytest
+import requests
 
 
 @pytest.mark.e2etest
 class TestRunAPIs:
-    ...
+    def test_heartbeat() -> None:
+        response = requests.get("localhost:5000/heartbeat")
+        assert response.status_code == 204
