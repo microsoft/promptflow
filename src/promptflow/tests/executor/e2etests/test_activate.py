e79340fccf8afa5d2b65ce85818b8745f9045d87
Peiwen Gao <peiwengao@microsoft.com>
2023-08-29 13:33:57 +0800

    Update activate flow test cases

--- src/promptflow/tests/executor/e2etests/test_activate.py
+++ src/promptflow/tests/executor/e2etests/test_activate.py
@@ -22,48 +22,22 @@ class TestExecutorActivate:
         # Validate the flow output
         assert isinstance(result.output, dict)
         expected_output = {
-            "first": "Skip job info extractor",
+            "investigation_method": {
+                "first": "Skip job info extractor",
+                "second": "Execute incident info extractor",
+            }
         }
-        assert expected_output.items() <= result.output["investigation_method"].items()
+        assert expected_output.items() <= result.output.items()
 
-        # Validate the flow node run infos
-        assert len(result.node_run_infos) == 8
-
-        node_run_info = result.node_run_infos["incident_id_extractor"]
-        assert node_run_info.status == Status.Completed
-        assert node_run_info.output == {
-            "has_incident_id": True,
-            "incident_id": 0,
-            "incident_content": "Incident 418856448 : Stale App Deployment for App promptflow"
-        }
-
-        node_run_info = result.node_run_infos["incident_info_extractor"]
-        assert node_run_info.status == Status.Completed
-        assert node_run_info.output == {
-            "retriever": "icm",
-            "incident_content": "Incident 418856448 : Stale App Deployment for App promptflow"
-        }
-
-        node_run_info = result.node_run_infos["icm_retriever"]
-        assert node_run_info.status == Status.Completed
-        assert node_run_info.output == "ICM: Incident 418856448 : Stale App Deployment for App promptflow"
-
-        node_run_info = result.node_run_infos["investigation_steps"]
-        assert node_run_info.status == Status.Completed
-
-        node_run_info = result.node_run_infos["investigation_method"]
-        assert node_run_info.status == Status.Completed
-        assert expected_output.items() <= node_run_info.output.items()
+        # Validate the flow node run infos for the completed nodes
+        assert len(result.node_run_infos) == 9
+        completed_nodes = ["incident_id_extractor", "incident_info_extractor", "investigation_steps",
+                           "retriever_summary", "tsg_retriever", "kql_tsg_retriever", "investigation_method"]
+        completed_nodes_run_infos = [result.node_run_infos[i] for i in completed_nodes]
+        assert all([node.status == Status.Completed for node in completed_nodes_run_infos])
 
         # Validate the flow node run infos for the skipped nodes
-        node_run_info = result.node_run_infos["job_info_extractor"]
-        assert node_run_info.status == Status.Skipped
-        assert node_run_info.output is None
-
-        node_run_info = result.node_run_infos["tsg_retriever"]
-        assert node_run_info.status == Status.Skipped
-        assert node_run_info.output is None
-
-        node_run_info = result.node_run_infos["kql_retriever"]
-        assert node_run_info.status == Status.Skipped
-        assert node_run_info.output is None
+        skipped_nodes = ["job_info_extractor", "icm_retriever"]
+        skipped_nodes_run_infos = [result.node_run_infos[i] for i in skipped_nodes]
+        assert all([node.status == Status.Skipped for node in skipped_nodes_run_infos])
+        assert all([node.output is None for node in skipped_nodes_run_infos])
--- src/promptflow/tests/test_configs/flows/conditional_flow_with_activate/job_info_extractor.py
+++ src/promptflow/tests/test_configs/flows/conditional_flow_with_activate/job_info_extractor.py
@@ -3,4 +3,5 @@ from promptflow import tool
 
 @tool
 def extract_job_info(incident_content: str) -> str:
-  return "Job_info: " + incident_content
+    print(f"Incident: {incident_content}")
+    return "Execute job info extractor"
