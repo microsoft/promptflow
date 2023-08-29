484a2c4506f0242c86fc12da5a9b71e029a179b6
Peiwen Gao <peiwengao@microsoft.com>
2023-08-29 18:05:31 +0800

    Fix tests

--- src/promptflow/tests/test_configs/flows/conditional_flow_with_activate/incident_id_extractor.py
+++ src/promptflow/tests/test_configs/flows/conditional_flow_with_activate/incident_id_extractor.py
@@ -3,9 +3,9 @@ from promptflow import tool
 
 @tool
 def extract_incident_id(incident_content: str, incident_id: int):
-    if incident_id >= 0 or incident_id < 3:
+    if incident_id >= 0 and incident_id < 3:
         return {
-            "has_incident_id": True, 
+            "has_incident_id": True,
             "incident_id": incident_id,
             "incident_content": incident_content
         }
