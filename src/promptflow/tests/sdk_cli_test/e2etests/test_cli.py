1f8066c2198cd1a5adb40f2b7a1733c6e9efd079
zhen <henu_rz@163.com>
2023-08-30 18:09:59 +0800

    [Promptflow SDK& CLI] Clean up the previous log for flow test (#235)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes]**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow/tests/sdk_cli_test/e2etests/test_cli.py
+++ src/promptflow/tests/sdk_cli_test/e2etests/test_cli.py
@@ -254,6 +254,9 @@ class TestCli:
         )
         output_path = Path(FLOWS_DIR) / "web_classification" / ".promptflow" / "flow.output.json"
         assert output_path.exists()
+        log_path = Path(FLOWS_DIR) / "web_classification" / ".promptflow" / "flow.log"
+        with open(log_path, "r") as f:
+            previous_log_content = f.read()
 
         # Test without input
         run_pf_command(
@@ -264,6 +267,10 @@ class TestCli:
         )
         output_path = Path(FLOWS_DIR) / "web_classification" / ".promptflow" / "flow.output.json"
         assert output_path.exists()
+        log_path = Path(FLOWS_DIR) / "web_classification" / ".promptflow" / "flow.log"
+        with open(log_path, "r") as f:
+            log_content = f.read()
+        assert previous_log_content not in log_content
 
     def test_pf_flow_with_variant(self, capsys):
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -525,6 +532,12 @@ class TestCli:
             outputs = json.load(f)
         assert outputs["output"] == env["API_BASE"]
         validate_stdout(Path(FLOWS_DIR) / "print_env_var" / ".promptflow" / "flow.detail.json")
+
+        # Test log contains user printed outputs
+        log_path = Path(FLOWS_DIR) / "print_env_var" / ".promptflow" / "flow.log"
+        with open(log_path, "r") as f:
+            log_content = f.read()
+        assert env["API_BASE"] in log_content
 
         run_pf_command(
             "flow",
