ec902907009a1e77edc1b077d0d68ddd12d8875b
Peiwen Gao <peiwengao@microsoft.com>
2023-08-30 14:33:21 +0800

    Update skip test cases

--- src/promptflow/tests/executor/e2etests/test_skip.py
+++ src/promptflow/tests/executor/e2etests/test_skip.py
@@ -19,9 +19,9 @@ class TestExecutorSkip:
         assert result.run_info.status == Status.Completed
         # Validate the flow output
         assert isinstance(result.output, dict)
-        assert result.output == {"string": "10 is even number, skip the next node"}
+        assert result.output == {"string": "Result: 10 is even number, skip the next node"}
         # Validate the flow node run infos
-        assert len(result.node_run_infos) == 2
+        assert len(result.node_run_infos) == 3
 
         node_run_info = result.node_run_infos["is_even"]
         assert node_run_info.status == Status.Completed
@@ -30,3 +30,7 @@ class TestExecutorSkip:
         node_run_info = result.node_run_infos["conditional_node"]
         assert node_run_info.status == Status.Skipped
         assert node_run_info.output == "10 is even number, skip the next node"
+
+        node_run_info = result.node_run_infos["print_result"]
+        assert node_run_info.status == Status.Completed
+        assert node_run_info.output == "Result: 10 is even number, skip the next node"
