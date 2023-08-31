4c10dc21437aae2575fa703634d8bc5d3cb5ce55
Philip Gao <yigao@microsoft.com>
2023-08-31 10:58:29 +0000

    Fix with black

--- scripts/building/run_coverage_tests.py
+++ scripts/building/run_coverage_tests.py
@@ -5,13 +5,9 @@ from pathlib import Path
 from utils import Color, run_command
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description=Color.RED + "Test Coverage for Promptflow!" + Color.END + "\n"
-    )
+    parser = argparse.ArgumentParser(description=Color.RED + "Test Coverage for Promptflow!" + Color.END + "\n")
 
-    parser.add_argument(
-        "-p", required=True, nargs="+", help="The paths to calculate code coverage"
-    )
+    parser.add_argument("-p", required=True, nargs="+", help="The paths to calculate code coverage")
     parser.add_argument("-t", required=True, nargs="+", help="The path to the tests")
     parser.add_argument("-l", required=True, help="Location to run tests in")
     parser.add_argument(
@@ -20,18 +16,14 @@ if __name__ == "__main__":
         help="Pytest marker to identify the tests to run",
         default="all",
     )
-    parser.add_argument(
-        "-n", help="Pytest number of process to run the tests", default="15"
-    )
+    parser.add_argument("-n", help="Pytest number of process to run the tests", default="15")
     parser.add_argument(
         "--model-name",
         help="The model file name to run the tests",
         type=str,
         default="",
     )
-    parser.add_argument(
-        "--timeout", help="Timeout for individual tests (seconds)", type=str, default=""
-    )
+    parser.add_argument("--timeout", help="Timeout for individual tests (seconds)", type=str, default="")
     parser.add_argument(
         "--coverage-config",
         help="The path of code coverage config file",
@@ -51,7 +43,7 @@ if __name__ == "__main__":
     print("Args.coverage-config: " + str(args.coverage_config))
 
     test_paths_list = [str(Path(path).absolute()) for path in args.t]
-    
+
     # display a list of all Python packages installed in the current Python environment
     run_command(["pip", "list"])
     run_command(["pip", "show", "promptflow", "promptflow-sdk"])
