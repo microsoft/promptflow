8dbdeacfb73b9d629947ab92d01eb6102c7b57b5
Brynn Yin <biyi@microsoft.com>
2023-08-31 20:26:27 +0800

    Add bing connection back
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- src/promptflow/promptflow/connections.py
+++ src/promptflow/promptflow/connections.py
@@ -1,6 +1,8 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+from dataclasses import dataclass
+
 from promptflow._core.tools_manager import register_connections
 from promptflow._sdk.entities import (
     AzureContentSafetyConnection,
@@ -12,6 +14,14 @@ from promptflow._sdk.entities import (
     SerpConnection,
 )
 from promptflow._sdk.entities._connection import _Connection
+from promptflow.contracts.types import Secret
+
+
+@dataclass
+class BingConnection:
+    api_key: Secret
+    url: str = "https://api.bing.microsoft.com/v7.0/search"
+
 
 # We should use unified connection class everywhere.
 # Do not add new connection class definition directly here.
