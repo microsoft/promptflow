48e616f3c0677ed04ca46a82c8c4e90b68be6275
Brynn Yin <24237253+brynn-code@users.noreply.github.com>
2023-09-01 10:54:52 +0800

    Support get connection name with flow variant (#252)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes]**
    - [x] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.
    
    ---------
    
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

---
+++ src/promptflow/tests/test_configs/flows/flow_with_dict_input_with_variant/print_val.py
@@ -0,0 +1,11 @@
+from promptflow import tool
+from promptflow.connections import CustomConnection
+
+
+@tool
+def get_val(key, conn: CustomConnection):
+    # get from env var
+    print(key)
+    if not isinstance(key, dict):
+        raise TypeError(f"key must be a dict, got {type(key)}")
+    return {"value": f"{key}: {type(key)}"}
