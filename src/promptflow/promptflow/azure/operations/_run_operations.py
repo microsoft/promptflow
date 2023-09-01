2a6bd4d677050d568b7b1d5e382428f70b822167
Han Wang <phoenix.seek@gmail.com>
2023-09-01 17:00:08 +0800

    [SDK]Limit session id length to avoid session creation stuck issue (#272)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes]**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow/promptflow/azure/operations/_run_operations.py
+++ src/promptflow/promptflow/azure/operations/_run_operations.py
@@ -538,7 +538,11 @@ class RunOperations(_ScopeDependentOperations):
             # fall back to unknown user when failed to get credential.
             user_alias = "unknown_user"
         flow_id = get_flow_lineage_id(flow_dir=flow)
-        return f"{user_alias}_{flow_id}"
+        session_id = f"{user_alias}_{flow_id}"
+        # hash and truncate to avoid the session id getting too long
+        # backend has a 64 bit limit for session id.
+        session_id = str(hash(session_id))[:48]
+        return session_id
 
     def _get_child_runs_from_pfs(self, run_id: str):
         """Get the child runs from the PFS."""
