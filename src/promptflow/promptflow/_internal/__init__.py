72a4166277c96aea562b9d237a75cb3cec9a82c0
Heyi Tang <heta@microsoft.com>
2023-09-07 16:48:54 +0800

    Remove CacheManager in init

--- src/promptflow/promptflow/_internal/__init__.py
+++ src/promptflow/promptflow/_internal/__init__.py
@@ -8,7 +8,7 @@ __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
 """Put some imports here for internal packages to minimize the effort of refactoring."""
 from promptflow._constants import PROMPTFLOW_CONNECTIONS
 from promptflow._core._errors import GenerateMetaUserError, PackageToolNotFoundError, ToolExecutionError
-from promptflow._core.cache_manager import AbstractCacheManager, CacheManager, enable_cache
+from promptflow._core.cache_manager import AbstractCacheManager, enable_cache
 from promptflow._core.connection_manager import ConnectionManager
 from promptflow._core.flow_execution_context import FlowExecutionContext
 from promptflow._core.log_manager import NodeLogManager, NodeLogWriter
