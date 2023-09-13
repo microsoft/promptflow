acb97b622bd1ddf564ef7798a40f629797140007
yalu4 <yavenika@163.com>
2023-09-13 15:51:58 +0800

    fix template not found

--- scripts/tool/generate_tool_package_template.py
+++ scripts/tool/generate_tool_package_template.py
@@ -32,7 +32,8 @@ def create_tool_project_structure(destination: str, package_name: str, tool_name
         class_name = convert_tool_name_to_class_name(tool_name)
 
     # Load templates
-    file_loader = FileSystemLoader('scripts\\tool\\templates')
+    templates_abs_path = os.path.abspath("scripts/tool/templates")
+    file_loader = FileSystemLoader(templates_abs_path)
     env = Environment(loader=file_loader)
 
     # Create new directory
