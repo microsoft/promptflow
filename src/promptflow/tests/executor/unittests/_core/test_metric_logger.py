e7b8bf6129a94e225f9dd0274f45a9cb6ad2b327
Peiwen Gao <peiwengao@microsoft.com>
2023-09-13 15:58:15 +0800

    Format tests

--- src/promptflow/tests/executor/unittests/_core/test_metric_logger.py
+++ src/promptflow/tests/executor/unittests/_core/test_metric_logger.py
@@ -6,6 +6,7 @@ from promptflow._core.metric_logger import MetricLoggerManager, add_metric_logge
 @pytest.mark.unittest
 class TestMetricLogger:
     def test_add_and_remove_metric_logger(self):
+        # define log metric function
         metrics = {}
 
         def _log_metric(key, value):
@@ -26,17 +27,18 @@ class TestMetricLogger:
         assert MetricLoggerManager.get_instance()._metric_loggers == []
 
     def test_log_metric(self):
+        # define log metric function
         metrics = {}
 
         def _log_metric(key, value):
             metrics[key] = value
 
+        def _log_metric_with_variant_id(key, value, variant_id):
+            metrics[key] = {variant_id: value}
+
         add_metric_logger(_log_metric)
         log_metric("test1", 1)
         assert metrics == {"test1": 1}
-
-        def _log_metric_with_variant_id(key, value, variant_id):
-            metrics[key] = {variant_id: value}
 
         add_metric_logger(_log_metric_with_variant_id)
         log_metric("test2", 1, "line_0")
