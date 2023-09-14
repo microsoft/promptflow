75a61a07b45db30c9491720304eb1146bf4030e2
Min Shi <minshi@microsoft.com>
2023-09-14 11:56:04 +0800

    add docstring for run_info and types

--- src/promptflow/promptflow/contracts/run_info.py
+++ src/promptflow/promptflow/contracts/run_info.py
@@ -9,6 +9,8 @@ from typing import Any, Dict, List, Mapping, Optional
 
 
 class Status(Enum):
+    """An enumeration class for different types of run status."""
+
     Running = "Running"
     Preparing = "Preparing"
     Completed = "Completed"
@@ -20,6 +22,12 @@ class Status(Enum):
 
     @staticmethod
     def is_terminated(status):
+        """Check if a given status is terminated.
+
+        :param status: The status to be checked
+        :return: True if the status is terminated, False otherwise
+        :rtype: bool
+        """
         if isinstance(status, Status):
             status = status.value
         return status in {s.value for s in {Status.Completed, Status.Failed, Status.Bypassed, Status.Canceled}}
@@ -27,9 +35,32 @@ class Status(Enum):
 
 @dataclass
 class RunInfo:
-    node: str  # Node name
-    flow_run_id: str  # This is equal to root_run_id
-    run_id: str  # flow_run_id:step_run_id
+    """A dataclass representing the run information.
+
+    :param node: Node name
+    :param flow_run_id: This is equal to root_run_id
+    :param run_id: flow_run_id:step_run_id
+    :param status: Status of the run, see :class:`Status`
+    :param inputs: List of inputs for the run
+    :param output: Output of the run
+    :param metrics: Metrics of the run
+    :param error: Errors occurred during the run
+    :param parent_run_id: Parent run id
+    :param start_time: Start time of the run
+    :param end_time: End time of the run
+    :param index: (Optional) Index of the run
+    :param api_calls: (Optional) API calls made during the run
+    :param variant_id: (Optional) Variant id of the run
+    :param cached_run_id: (Optional) Cached run id
+    :param cached_flow_run_id: (Optional) Cached flow run id
+    :param logs: (Optional) Logs of the run
+    :param system_metrics: (Optional) System metrics of the run
+    :param result: (Optional) Result of the run
+    """
+
+    node: str
+    flow_run_id: str
+    run_id: str
     status: Status
     inputs: list
     output: object
@@ -50,7 +81,32 @@ class RunInfo:
 
 @dataclass
 class FlowRunInfo:
-    run_id: str  # flow_run_id:child_flow_run_id
+    """A dataclass representing the run information.
+
+    :param run_id: flow_run_id:child_flow_run_id
+    :param status: Status of the flow run, see :class:`Status`
+    :param error: Errors occurred during the flow run
+    :param inputs: Inputs for the flow run
+    :param output: Output of the flow run
+    :param metrics: Metrics of the flow run
+    :param request: Request made for the flow run
+    :param parent_run_id: Parent run id of the flow run
+    :param root_run_id: Root run id of the flow run
+    :param source_run_id: The run id of the run that triggered the flow run
+    :param flow_id: Flow id of the flow run
+    :param start_time: Start time of the flow run
+    :param end_time: End time of the flow run
+    :param index: (Optional) Index of the flow run (used for bulk test mode)
+    :param api_calls: (Optional) API calls made during the flow run
+    :param variant_id: (Optional) Variant id of the flow run
+    :param name: (Optional) Name of the flow run
+    :param description: (Optional) Description of the flow run
+    :param tags: (Optional) Tags of the flow run
+    :param system_metrics: (Optional) System metrics of the flow run
+    :param result: (Optional) Result of the flow run
+    :param upload_metrics: (Optional) Flag indicating whether to upload metrics for the flow run
+    """
+    run_id: str
     status: Status
     error: object
     inputs: object
