6c2a850924f6b9e2e0a45e14ea13e93b594a6e0e
Peiwen Gao <vivian9795@163.com>
2023-09-14 18:40:04 +0800

    Add test cases for input parser

--- src/promptflow/tests/executor/unittests/executor/test_input_assignment_parser.py
+++ src/promptflow/tests/executor/unittests/executor/test_input_assignment_parser.py
@@ -4,7 +4,12 @@ import pytest
 
 from promptflow._core._errors import NotSupported
 from promptflow.contracts.flow import InputAssignment
-from promptflow.executor._errors import InputNotFound, InputNotFoundFromAncestorNodeOutput, UnsupportedReference
+from promptflow.executor._errors import (
+    InputNotFound,
+    InputNotFoundFromAncestorNodeOutput,
+    InvalidReferenceProperty,
+    UnsupportedReference,
+)
 from promptflow.executor._input_assignment_parser import parse_node_property, parse_value
 
 FLOW_INPUTS = {"text": "hello promptflow"}
@@ -16,6 +21,10 @@ class WrongInputAssignment:
     value_type: str = "wrong_type"
     section: str = ""
     property: str = ""
+
+
+class DummyObject:
+    value: str = "dummy"
 
 
 @pytest.mark.unittest
@@ -77,20 +86,79 @@ class TestInputAssignmentParser:
             assert exc_info.value.message == expected_error_message
 
     @pytest.mark.parametrize(
-        "node_name, node_val, property, expected_value",
-        [],
+        "node_val, property, expected_value",
+        [
+            (
+                {"output": "hello promptflow"},
+                "output",
+                "hello promptflow",
+            ),
+            (
+                {"output": "hello promptflow"},
+                "['output']",
+                "hello promptflow",
+            ),
+            (
+                {"output": "hello promptflow"},
+                '["output"]',
+                "hello promptflow",
+            ),
+            (
+                {"output": {"text": "hello promptflow"}},
+                '["output"]["text"]',
+                "hello promptflow",
+            ),
+            (
+                ["output1", "output2"],
+                "[1]",
+                "output2",
+            ),
+            (
+                DummyObject(),
+                "value",
+                "dummy",
+            ),
+        ],
     )
-    def test_parse_node_property(self, node_name, node_val, property, expected_value):
-        actual_value = parse_node_property(node_name, node_val, property)
+    def test_parse_node_property(self, node_val, property, expected_value):
+        actual_value = parse_node_property("node1", node_val, property)
         assert actual_value == expected_value
 
     @pytest.mark.parametrize(
-        "node_name, node_val, property, expected_error_class, expected_error_message",
-        [],
+        "node_val, property, expected_error_message",
+        [
+            (
+                {"output_str": ["output1", "output2"]},
+                "output_str[2]",
+                (
+                    "Invalid index '2' when accessing property 'output_str[2]' of the node 'node1'. "
+                    "Please check the index and try again."
+                ),
+            ),
+            (
+                {"word": "hello promptflow"},
+                "text",
+                (
+                    "Invalid property 'text' when accessing the node 'node1'. "
+                    "Please check the property and try again."
+                ),
+            ),
+            (
+                ["output1", "output2"],
+                "[2]",
+                ("Invalid property '[2]' when accessing the node 'node1'. " "Please check the property and try again."),
+            ),
+            (
+                DummyObject(),
+                "value_type",
+                (
+                    "Invalid property 'value_type' when accessing the node 'node1'. "
+                    "Please check the property and try again."
+                ),
+            ),
+        ],
     )
-    def test_parse_node_property_with_exception(
-        self, node_name, node_val, property, expected_error_class, expected_error_message
-    ):
-        with pytest.raises(expected_error_class) as exc_info:
-            parse_node_property(node_name, node_val, property)
+    def test_parse_node_property_with_exception(self, node_val, property, expected_error_message):
+        with pytest.raises(InvalidReferenceProperty) as exc_info:
+            parse_node_property("node1", node_val, property)
             assert exc_info.value.message == expected_error_message
