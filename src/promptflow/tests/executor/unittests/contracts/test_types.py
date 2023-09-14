cb9eaa0874647208ac39863e31f266c7d87ae785
Min Shi <minshi@microsoft.com>
2023-09-14 16:29:47 +0800

    add test for types and run_management

---
+++ src/promptflow/tests/executor/unittests/contracts/test_types.py
@@ -0,0 +1,13 @@
+import pytest
+from promptflow.contracts.types import Secret, PromptTemplate
+
+@pytest.mark.unittest
+def test_secret():
+    secret = Secret('my_secret')
+    secret.set_secret_name('secret_name')
+    assert secret.secret_name == 'secret_name'
+
+def test_prompt_template():
+    prompt = PromptTemplate('my_prompt')
+    assert isinstance(prompt, str)
+    assert str(prompt) == 'my_prompt'
