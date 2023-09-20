7f698b806d7830a1d96cad33ef51ca2b6841eabc
Lina Tang <linatang@microsoft.com>
2023-09-20 15:29:43 +0800

    Enhance tests

--- src/promptflow/tests/executor/unittests/integrations/test_langchain.py
+++ src/promptflow/tests/executor/unittests/integrations/test_langchain.py
@@ -24,9 +24,9 @@ class TestLangchain:
 
     def test_langchain_traces(self):
         handler = self.get_handler()
-        handler.on_agent_action(action=AgentAction("test", "test", "test"))
-        handler.on_tool_start(serialized={"test": "test"}, input_str="test")
-        handler.on_chain_start(serialized={"test": "test"}, inputs={"test": "test"})
+        handler.on_agent_action(action=AgentAction("test_agent_name", "test", "test"))
+        handler.on_tool_start(serialized={"name": "test_tool_name"}, input_str="test")
+        handler.on_chain_start(serialized={"id": ["test_chain_name"]}, inputs={"test": "test"})
         handler.on_llm_start(serialized={"test": "test"}, prompts=["test"])
         assert handler._events_stack == [
             LangChainEventType.AGENT,
@@ -35,9 +35,13 @@ class TestLangchain:
             LangChainEventType.LLM
         ]
         assert len(handler._tracer._trace_stack) == 4
-        handler.on_llm_end(response=None)
-        handler.on_chain_end(outputs={"test": "test"})
-        handler.on_tool_end(output="test")
+        assert handler._tracer._trace_stack[0].name == "test_agent_name"
+        assert handler._tracer._trace_stack[1].name == "test_tool_name"
+        assert handler._tracer._trace_stack[2].name == "test_chain_name"
+        assert handler._tracer._trace_stack[3].name == "LLM"  # The default name
+        handler.on_llm_error(error=None)
+        handler.on_chain_error(error=None)
+        handler.on_tool_error(error=None)
         handler.on_agent_finish(finish=AgentFinish({"test": "test"}, "test"))
         assert len(handler._events_stack) == 0
         assert len(handler._tracer._trace_stack) == 0
