4ab2af394d2503f30c1245c293ca4a153c6f93ac
Peiwen Gao <peiwengao@microsoft.com>
2023-09-20 11:44:47 +0800

    Add --disable-cov-branch

--- scripts/building/run_coverage_tests.py
+++ scripts/building/run_coverage_tests.py
@@ -31,10 +31,9 @@ if __name__ == "__main__":
         default="",
     )
     parser.add_argument(
-        "--enable-cov-branch",
+        "--disable-cov-branch",
+        action="store_true",
         help="Whether to enable branch coverage calculation",
-        type=bool,
-        default=True,
     )
 
     args = parser.parse_args()
@@ -47,7 +46,7 @@ if __name__ == "__main__":
     print("Args.model-name: " + str(args.model_name))
     print("Args.timeout: " + str(args.timeout))
     print("Args.coverage-config: " + str(args.coverage_config))
-    print("Args.enable-cov-branch: " + str(args.enable_cov_branch))
+    print("Args.disable-cov-branch: " + str(args.disable_cov_branch))
 
     test_paths_list = [str(Path(path).absolute()) for path in args.t]
 
@@ -61,7 +60,7 @@ if __name__ == "__main__":
         if args.p:
             cov_path_list = [f"--cov={path}" for path in args.p]
             pytest_command += cov_path_list
-        if args.enable_cov_branch:
+        if not args.disable_cov_branch:
             pytest_command += ["--cov-branch"]
         pytest_command += [  # noqa: W503
             "--cov-report=term",
