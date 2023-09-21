fad84648bde182f828802683a7976e04308bd8d4
Yucong Jin <yucongj@microsoft.com>
2023-09-21 19:15:31 +0800

    nx init connections package

---
+++ src/promptflow-js/connections/README.md
@@ -0,0 +1,11 @@
+# connections
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build connections` to build the library.
+
+## Running unit tests
+
+Run `nx test connections` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ src/promptflow-js/connections/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'connections',
+  preset: '../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/connections',
+};
