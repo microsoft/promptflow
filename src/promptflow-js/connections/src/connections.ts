633671413cd032dbeb66dc1b10b48c566ae5e6c0
Jin Yucong <jinyucong911@outlook.com>
2023-09-22 10:32:07 +0800

    refactor workspace structure

--- src/promptflow-js/connections/src/connections.ts
+++
@@ -1,7 +0,0 @@
-export const list = () => {
-  const testAbc = "abc";
-  
-  // todo
-
-  return testAbc;
-}
--- src/promptflow-js/packages/connections/vite.config.ts
+++ src/promptflow-js/packages/connections/vite.config.ts
@@ -1,17 +1,17 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import dts from 'vite-plugin-dts';
-import * as path from 'path';
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import dts from "vite-plugin-dts";
+import * as path from "path";
 
 export default defineConfig({
-  cacheDir: '../node_modules/.vite/connections',
+  cacheDir: "../../node_modules/.vite/connections",
 
   plugins: [
     dts({
-      entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
+      entryRoot: "src",
+      tsConfigFilePath: path.join(__dirname, "tsconfig.lib.json"),
       skipDiagnostics: true,
     }),
 
@@ -28,12 +28,12 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'connections',
-      fileName: 'index',
+      entry: "src/index.ts",
+      name: "connections",
+      fileName: "index",
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs'],
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
