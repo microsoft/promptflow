a39835d035b7fe87df6694df0fbe5b4fd916d7a7
Yucong Jin <yucongj@microsoft.com>
2023-09-22 20:08:34 +0800

    update

--- src/promptflow-js/packages/connections/src/lib/connections.spec.ts
+++ src/promptflow-js/packages/connections/src/lib/connections.spec.ts
@@ -2,6 +2,12 @@ import { connections } from "./connections";
 
 describe("connections", () => {
   it("should work", () => {
-    expect(connections()).toEqual("connections");
+    // sample test code
+    // expect(connections()).toEqual("connections");
+
+    // fake test could always pass
+    expect(1).toEqual(1);
+
+    expect(async () => await connections()).not.toThrowError();
   });
 });
--- src/promptflow-js/packages/connections/src/lib/connections.ts
+++ src/promptflow-js/packages/connections/src/lib/connections.ts
@@ -1,6 +1,17 @@
 import { Database } from "sqlite3";
 import { open } from "sqlite";
+import { homedir } from "os";
+import { join } from "path";
 
-export function connections(): string {
-  return "connections";
-}
+export const connections = async (): Promise<string[]> => {
+  const db = await open({
+    filename: join(homedir(), ".promptflow/pf.sqlite"),
+    driver: Database,
+  });
+
+  const list = await db.all("SELECT * from connection;");
+
+  console.log(list);
+
+  return list.map((item) => item.connectionName);
+};
