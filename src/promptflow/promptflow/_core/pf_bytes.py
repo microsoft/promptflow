b37c315cee69b47ef2998d1965bcdd043f340f18
Ming Gu <migu@microsoft.com>
2023-09-25 04:07:31 +0800

    Add support to base64 encode/decode

--- src/promptflow/promptflow/_core/pf_bytes.py
+++ src/promptflow/promptflow/_core/pf_bytes.py
@@ -1,3 +1,4 @@
+import base64
 import json
 
 
@@ -32,12 +33,28 @@ def pfbytes_decoder(dct):
     return dct
 
 
-# Sample usage
+def pfbytes_base64_encoder(obj):
+    """Encodes PFBytes to base64."""
+    if isinstance(obj, PFBytes):
+        encoded_bytes = base64.b64encode(obj.bytes).decode('utf-8')
+        return {"pf_mime_type": obj.mime_type, "base64": encoded_bytes}
+    raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
+
+
+def pfbytes_base64_decoder(dct):
+    """Decodes PFBytes from a base64 string."""
+    if "pf_mime_type" in dct and "base64" in dct:
+        decoded_bytes = base64.b64decode(dct["base64"])
+        return PFBytes(dct["pf_mime_type"], decoded_bytes)
+    return dct
+
+
 data = {
     "name": "John Doe",
-    "audioClip": PFBytes("image/png", b"Some mock binary data...")  # using mock bytes data for simplicity
+    "myImage": PFBytes("image/png", b"Some mock binary data...")  # using mock bytes data for simplicity
 }
 
+# Test file-based encode/decode
 # Dump data to JSON string
 json_str = json.dumps(data, default=pfbytes_encoder)
 print(json_str)
@@ -45,5 +62,16 @@ print(json_str)
 # Load data back from JSON string
 loaded_data = json.loads(json_str, object_hook=pfbytes_decoder)
 print(loaded_data)
-print(isinstance(loaded_data["audioClip"], PFBytes))  # This should print True
-print(loaded_data["audioClip"].bytes)  # Should print the mock binary data
+print(isinstance(loaded_data["myImage"], PFBytes))  # This should print True
+print(loaded_data["myImage"].bytes)  # Should print the mock binary data
+
+# Test base64 encode/decode
+# Dump data to base64 encoded JSON string
+json_base64_str = json.dumps(data, default=pfbytes_base64_encoder)
+print(json_base64_str)
+
+# Load data back from base64 encoded JSON string
+loaded_data_base64 = json.loads(json_base64_str, object_hook=pfbytes_base64_decoder)
+print(loaded_data_base64)
+print(isinstance(loaded_data_base64["myImage"], PFBytes))  # This should print True
+print(loaded_data_base64["myImage"].bytes)  # Should print the mock binary data
