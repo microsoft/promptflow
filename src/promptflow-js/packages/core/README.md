c2d4d4be9c4bb8abde321c56311f84491ac9329a
Yucong Jin <yucongj@microsoft.com>
2023-09-25 14:25:03 +0800

    init core package

---
+++ src/promptflow-js/packages/core/README.md
@@ -0,0 +1,19 @@
+# core
+
+This library was generated with [Nx](https://nx.dev).
+
+
+
+## Building
+
+Run `nx build core` to build the library.
+
+
+
+
+
+## Running unit tests
+
+Run `nx test core` to execute the unit tests via [Jest](https://jestjs.io).
+
+
---
+++ src/promptflow-js/packages/core/vite.config.ts
@@ -0,0 +1,49 @@
+
+      /// <reference types="vitest" />
+      import { defineConfig } from 'vite';
+      
+      import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+      import dts from 'vite-plugin-dts';
+import * as path from 'path';
+      
+      export default defineConfig({
+        cacheDir: '../../node_modules/.vite/core',
+        
+        
+        
+    plugins: [
+      dts({
+      entryRoot: 'src',
+      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
+      skipDiagnostics: true,
+    }),
+      
+      nxViteTsPaths(),
+    ],
+    
+        
+    // Uncomment this if you are using workers. 
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+        
+      // Configuration for building your library.
+      // See: https://vitejs.dev/guide/build.html#library-mode
+      build: {
+        lib: {
+          // Could also be a dictionary or array of multiple entry points.
+          entry: 'src/index.ts',
+          name: 'core',
+          fileName: 'index',
+          // Change this to the formats you want to support.
+          // Don't forget to update your package.json as well.
+          formats: ['es', 'cjs']
+        },
+        rollupOptions: {
+          // External packages that should not be bundled into your library.
+          external: []
+        }
+      },
+        
+        
+      });
