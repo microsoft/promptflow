2da56288ae9637428161d9cd21c4f8f8041800bc
Peiwen Gao <vivian9795@163.com>
2023-09-26 15:52:20 +0900

    Fix bug

--- src/promptflow/promptflow/executor/flow_validator.py
+++ src/promptflow/promptflow/executor/flow_validator.py
@@ -34,8 +34,14 @@ class FlowValidator:
             inputs_list = [i for i in n.inputs.values()]
             if n.skip:
                 if n.aggregation and (
-                    n.skip.condition.value not in aggregation_nodes
-                    or n.skip.return_value.value not in aggregation_nodes
+                    (
+                        n.skip.condition.value_type == InputValueType.NODE_REFERENCE
+                        and n.skip.condition.value not in aggregation_nodes
+                    )
+                    or (
+                        n.skip.return_value.value_type == InputValueType.NODE_REFERENCE
+                        and n.skip.return_value.value not in aggregation_nodes
+                    )
                 ):
                     msg_format = (
                         "Invalid node definitions found in the flow graph. Non-aggregation nodes cannot be "
@@ -45,7 +51,11 @@ class FlowValidator:
                     raise InvalidNodeReference(message_format=msg_format, node_name=n.name)
                 inputs_list.extend([n.skip.condition, n.skip.return_value])
             if n.activate:
-                if n.aggregation and n.activate.condition.value not in aggregation_nodes:
+                if (
+                    n.aggregation
+                    and n.activate.condition.value_type == InputValueType.NODE_REFERENCE
+                    and n.activate.condition.value not in aggregation_nodes
+                ):
                     msg_format = (
                         "Invalid node definitions found in the flow graph. Non-aggregation node '{invalid_reference}' "
                         "cannot be referenced in the activate config of the aggregation node '{node_name}'. Please "
