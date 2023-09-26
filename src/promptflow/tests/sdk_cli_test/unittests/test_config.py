62b15c32927d878e160cd92ae9838e815624ebd1
Han Wang <phoenix.seek@gmail.com>
2023-09-26 15:12:27 +0800

    remove dup test

--- src/promptflow/tests/sdk_cli_test/unittests/test_config.py
+++ src/promptflow/tests/sdk_cli_test/unittests/test_config.py
@@ -28,14 +28,6 @@ class TestConfig:
         config.set_config("test_key", "test_value")
         assert config.get_config("test_key") == "test_value"
 
-    def test_get_telemetry_consent(self, config):
-        config.set_telemetry_consent(True)
-        assert config.get_telemetry_consent() is True
-
-    def test_set_telemetry_consent(self, config):
-        config.set_telemetry_consent(True)
-        assert config.get_telemetry_consent() is True
-
     def test_get_or_set_installation_id(self, config):
         user_id = config.get_or_set_installation_id()
         assert user_id is not None
