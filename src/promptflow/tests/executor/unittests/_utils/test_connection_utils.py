446d48ef5d70d3db2ccf79c82e1ccd8356135e26
cs_lucky <si.chen@microsoft.com>
2023-09-27 16:13:52 +0800

    fix flake8

--- src/promptflow/tests/executor/unittests/_utils/test_connection_utils.py
+++ src/promptflow/tests/executor/unittests/_utils/test_connection_utils.py
@@ -23,7 +23,6 @@ class MyCustomConnectionWithDefaultValue(CustomStrongTypeConnection):
 
 class MyCustomConnectionWithInvalidComments(CustomStrongTypeConnection):
     """Custom connection.
-
     :param api_key: The api key.
     :type api_key: String
     :param api_base: The api base.
@@ -37,7 +36,6 @@ class MyCustomConnectionWithInvalidComments(CustomStrongTypeConnection):
 
 class MyCustomConnectionMissingTypeComments(CustomStrongTypeConnection):
     """Custom connection.
-
     :param api_key: The api key.
     """
     api_key: Secret
@@ -46,7 +44,6 @@ class MyCustomConnectionMissingTypeComments(CustomStrongTypeConnection):
 
 class MyCustomConnectionMissingParamComments(CustomStrongTypeConnection):
     """Custom connection.
-
     :type api_key: String
     """
     api_key: Secret
@@ -59,26 +56,39 @@ class TestConnectionUtils:
         "cls, expected_str_in_template",
         [
             (MyCustomConnectionWithNoComments,
-             ['api_base: "to_replace_with_api_base"\n', 'api_key: "to_replace_with_api_key"\n']),
-            (MyCustomConnectionWithInvalidComments, ["api_base: to_replace_with_api_base  # String, The api base.\n",
-                                                     "api_key: to_replace_with_api_key  # String, The api key.\n"]),
-            (MyCustomConnectionMissingTypeComments, ["api_key: to_replace_with_api_key  # The api key."]),
-            (MyCustomConnectionMissingParamComments, ["api_key: to_replace_with_api_key  # String"]),
+             ['api_base: "to_replace_with_api_base"\n',
+              'api_key: "to_replace_with_api_key"\n']),
+            (MyCustomConnectionWithInvalidComments,
+             ["api_base: to_replace_with_api_base  # String, The api base.\n",
+              "api_key: to_replace_with_api_key  # String, The api key.\n"]),
+            (MyCustomConnectionMissingTypeComments,
+             ["api_key: to_replace_with_api_key  # The api key."]),
+            (MyCustomConnectionMissingParamComments,
+             ["api_key: to_replace_with_api_key  # String"]),
         ]
     )
-    def test_generate_custom_strong_type_connection_template_with_comments(self, cls, expected_str_in_template):
+    def test_generate_custom_strong_type_connection_template_with_comments(
+            self,
+            cls,
+            expected_str_in_template):
         package = "test-package"
         package_version = "0.0.1"
         spec = generate_custom_strong_type_connection_spec(cls, package, package_version)
-        template = generate_custom_strong_type_connection_template(cls, spec, package, package_version)
+        template = \
+            generate_custom_strong_type_connection_template(cls, spec, package, package_version)
         for comment in expected_str_in_template:
             assert comment in template
 
     def test_generate_custom_strong_type_connection_template_with_default_value(self):
         package = "test-package"
         package_version = "0.0.1"
-        spec = generate_custom_strong_type_connection_spec(MyCustomConnectionWithDefaultValue, package, package_version)
-        template = generate_custom_strong_type_connection_template(MyCustomConnectionWithDefaultValue, spec, package, package_version)
-        print(template)
+        spec = generate_custom_strong_type_connection_spec(
+            MyCustomConnectionWithDefaultValue,
+            package,
+            package_version)
+        template = generate_custom_strong_type_connection_template(
+            MyCustomConnectionWithDefaultValue,
+            spec,
+            package,
+            package_version)
         assert 'api_base: "api-base"' in template
-
