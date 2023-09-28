683f17db0dbff3ed63990ec158a2ed3b93d2736c
Robben Wang <350053002@qq.com>
2023-09-28 17:25:14 +0800

    Make error message format more robust. (#642)
    # Description
    
    If we set parameter does not align with `message_format`, show <Param>
    to customer. For now, we will throw KeyError, which is one bad
    experience for customer.
    
    For example:
    ```
    ex = PromptflowException(
            message_format="Test exception message with parameters: {param}, {param1}.",
            param="test_param",
    )
    ```
    Will raise KeyError for ex.message, since we don't have `param1`.
    
    After this PR, ex.message = "Test exception message with parameters:
    test_param, <param>."
    # All Promptflow Contribution checklist:
    - [X] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [X] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [X] Title of the pull request is clear and informative.
    - [X] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [X] Pull request includes test coverage for the included changes.
    
    ---------
    
    Co-authored-by: Robben Wang <robbenwang@microsoft.com>

---
+++ src/promptflow/tests/executor/unittests/executor/test_exceptions.py
@@ -0,0 +1,14 @@
+import pytest
+
+from promptflow.exceptions import PromptflowException
+
+
+@pytest.mark.unittest
+class TestExceptions:
+    def test_exception_message(self):
+        ex = PromptflowException(
+            message_format="Test exception message with parameters: {param}, {param1}.",
+            param="test_param",
+        )
+
+        assert ex.message == "Test exception message with parameters: test_param, <param1>."
