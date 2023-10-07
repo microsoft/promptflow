b83cfc4d0cb2f5eafa1ca95eed8a19d5f6e69057
LuLu Zuo <luluzuo@microsoft.com>
2023-10-07 16:10:21 +0800

    Modify ending process's position

--- src/promptflow/promptflow/executor/_line_execution_process_pool.py
+++ src/promptflow/promptflow/executor/_line_execution_process_pool.py
@@ -231,8 +231,8 @@ class LineExecutionProcessPool:
                 )
                 result_list.append(result)
                 self._completed_idx[line_number] = healthy_ensured_process.format_current_process(line_number, True)
-                healthy_ensured_process.end()
                 if not task_queue.empty():
+                    healthy_ensured_process.end()
                     healthy_ensured_process.start_new(task_queue)
 
             self._processing_idx.pop(line_number)
