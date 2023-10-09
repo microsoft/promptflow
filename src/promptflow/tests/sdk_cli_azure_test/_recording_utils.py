e51fe76df2285cfd4cd5204ef7d2143480650688
Zhengfei Wang <zhengfei.wang@microsoft.com>
2023-10-09 12:53:16 +0800

    add utils for recording

--- src/promptflow/tests/sdk_cli_azure_test/_recording_utils.py
+++ src/promptflow/tests/sdk_cli_azure_test/_recording_utils.py
@@ -4,7 +4,10 @@
 
 import functools
 import inspect
+import os
 import unittest
+
+import vcr
 
 REGISTERED_FIXTURES = [
     "ml_client",
@@ -35,8 +38,37 @@ def fixture_provider(testcase_func):
 
 
 class PFAzureIntegrationTestCase(unittest.TestCase):
+    FILTER_HEADERS = [
+        "authorization",
+        "client-request-id",
+        "retry-after",
+        "x-ms-client-request-id",
+        "x-ms-correlation-request-id",
+        "x-ms-ratelimit-remaining-subscription-reads",
+        "x-ms-request-id",
+        "x-ms-routing-request-id",
+        "x-ms-gateway-service-instanceid",
+        "x-ms-ratelimit-remaining-tenant-reads",
+        "x-ms-served-by",
+        "x-ms-authorization-auxiliary",
+    ]
+
     def __init__(self, method_name: str) -> None:
         super(PFAzureIntegrationTestCase, self).__init__(method_name)
 
+        test_file_path = inspect.getfile(self.__class__)
+        recording_dir = os.path.join(os.path.dirname(test_file_path), "recordings")
+        self.vcr = vcr.VCR(
+            cassette_library_dir=recording_dir,
+            record_mode="all",
+            # filter_headers=self.FILTER_HEADERS,
+        )
+        self.recording_file = os.path.join(recording_dir, f"{method_name}.yaml")
+
     def setUp(self) -> None:
         super(PFAzureIntegrationTestCase, self).setUp()
+
+        # set up cassette
+        cm = self.vcr.use_cassette(self.recording_file)
+        self.cassette = cm.__enter__()
+        self.addCleanup(cm.__exit__)
