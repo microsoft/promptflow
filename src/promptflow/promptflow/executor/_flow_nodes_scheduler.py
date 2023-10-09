f8a289eb32dc8ce27d5cafb856875a60ed70b6a4
Robben Wang <robbenwang@microsoft.com>
2023-10-09 18:46:59 +0800

    Add testcase for FlowNodesScheduler

--- src/promptflow/promptflow/executor/_flow_nodes_scheduler.py
+++ src/promptflow/promptflow/executor/_flow_nodes_scheduler.py
@@ -9,7 +9,7 @@ from typing import Dict, List, Tuple
 
 from promptflow._core.flow_execution_context import FlowExecutionContext
 from promptflow._core.tools_manager import ToolsManager
-from promptflow._utils.logger_utils import logger
+from promptflow._utils.logger_utils import flow_logger, logger
 from promptflow._utils.utils import set_context
 from promptflow.contracts.flow import Node
 from promptflow.executor._dag_manager import DAGManager
@@ -21,34 +21,40 @@ DEFAULT_CONCURRENCY_FLOW = 16
 
 
 class FlowNodesScheduler:
-    def __init__(self, tools_manager: ToolsManager) -> None:
+    def __init__(
+        self,
+        tools_manager: ToolsManager,
+        inputs: Dict,
+        nodes_from_invoker: List[Node],
+        node_concurrency: int,
+        context: FlowExecutionContext,
+    ) -> None:
         self.tools_manager = tools_manager
+        self.future_to_node: Dict[Future, Node] = {}
+        self._node_concurrency = min(node_concurrency, DEFAULT_CONCURRENCY_FLOW)
+        flow_logger.info(f"Start to run {len(nodes_from_invoker)} nodes with concurrency level {node_concurrency}.")
+        self.dag_manager = DAGManager(nodes_from_invoker, inputs)
+        self.context = context
 
     def execute(
-        self, context: FlowExecutionContext, inputs: Dict, nodes_from_invoker: List[Node], node_concurrency: int
+        self,
     ) -> Tuple[dict, dict]:
-        dag_manager = DAGManager(nodes_from_invoker, inputs)
-        node_concurrency = min(node_concurrency, DEFAULT_CONCURRENCY_FLOW)
-        logger.info(f"Start to run {len(nodes_from_invoker)} nodes with concurrency level {node_concurrency}.")
 
-        self.context = context
-        self.inputs = inputs
         parent_context = contextvars.copy_context()
         with ThreadPoolExecutor(
-            max_workers=node_concurrency, initializer=set_context, initargs=(parent_context,)
+            max_workers=self._node_concurrency, initializer=set_context, initargs=(parent_context,)
         ) as executor:
-            self.future_to_node: Dict[Future, Node] = {}
-            self._execute_nodes(dag_manager, executor)
+            self._execute_nodes(executor)
 
-            while not dag_manager.completed():
+            while not self.dag_manager.completed():
                 try:
                     if not self.future_to_node:
                         raise NoNodeExecutedError("No nodes are ready for execution, but the flow is not completed.")
                     completed_futures, _ = futures.wait(self.future_to_node.keys(), return_when=futures.FIRST_COMPLETED)
-                    dag_manager.complete_nodes(self._collect_outputs(completed_futures))
+                    self.dag_manager.complete_nodes(self._collect_outputs(completed_futures))
                     for each_future in completed_futures:
                         del self.future_to_node[each_future]
-                    self._execute_nodes(dag_manager, executor)
+                    self._execute_nodes(executor)
                 except Exception as e:
                     for unfinished_future in self.future_to_node.keys():
                         node_name = self.future_to_node[unfinished_future].name
@@ -57,19 +63,19 @@ class FlowNodesScheduler:
                         unfinished_future.cancel()
                     # Even we raise exception here, still need to wait all running jobs finish to exit.
                     raise e
-        return dag_manager.completed_nodes_outputs, dag_manager.bypassed_nodes
+        return self.dag_manager.completed_nodes_outputs, self.dag_manager.bypassed_nodes
 
-    def _execute_nodes(self, dag_manager: DAGManager, executor: ThreadPoolExecutor):
+    def _execute_nodes(self, executor: ThreadPoolExecutor):
         # Skip nodes and update node run info until there are no nodes to bypass
-        nodes_to_bypass = dag_manager.pop_bypassable_nodes()
+        nodes_to_bypass = self.dag_manager.pop_bypassable_nodes()
         while nodes_to_bypass:
-            self._bypass_nodes(dag_manager, nodes_to_bypass)
-            nodes_to_bypass = dag_manager.pop_bypassable_nodes()
+            self._bypass_nodes(nodes_to_bypass)
+            nodes_to_bypass = self.dag_manager.pop_bypassable_nodes()
 
         # Submit nodes that are ready to run
-        nodes_to_exec = dag_manager.pop_ready_nodes()
+        nodes_to_exec = self.dag_manager.pop_ready_nodes()
         if nodes_to_exec:
-            self._submit_nodes(executor, dag_manager, nodes_to_exec)
+            self._submit_nodes(executor, nodes_to_exec)
 
     def _collect_outputs(self, completed_futures: List[Future]):
         completed_nodes_outputs = {}
@@ -79,18 +85,18 @@ class FlowNodesScheduler:
             completed_nodes_outputs[each_node.name] = each_node_result
         return completed_nodes_outputs
 
-    def _bypass_nodes(self, dag_manager: DAGManager, nodes: List[Node]):
+    def _bypass_nodes(self, nodes: List[Node]):
         try:
             self.context.start()
             for node in nodes:
-                node_outputs = dag_manager.get_bypassed_node_outputs(node)
+                node_outputs = self.dag_manager.get_bypassed_node_outputs(node)
                 self.context.bypass_node(node, node_outputs)
         finally:
             self.context.end()
 
-    def _submit_nodes(self, executor: ThreadPoolExecutor, dag_manager: DAGManager, nodes):
+    def _submit_nodes(self, executor: ThreadPoolExecutor, nodes):
         for each_node in nodes:
-            future = executor.submit(self._exec_single_node_in_thread, (each_node, dag_manager))
+            future = executor.submit(self._exec_single_node_in_thread, (each_node, self.dag_manager))
             self.future_to_node[future] = each_node
 
     def _exec_single_node_in_thread(self, args: Tuple[Node, DAGManager]):
