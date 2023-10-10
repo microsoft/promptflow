22f686ef6dc65a88f60155c9f846d75ebd7b1191
vim12345 <vk2699945@gmail.com>
2023-10-10 20:17:07 +0530

    Added

--- src/promptflow-tools/setup.py
+++ src/promptflow-tools/setup.py
@@ -20,9 +20,13 @@ def parse_requirements(file_name: str) -> List[str]:
 # Version extraction inspired from 'requests'
 with open(os.path.join(PACKAGE_FOLDER_PATH, "version.txt"), "r") as fd:
     version_content = fd.read()
-    print(version_content)
-    version = cast(Match[Any], re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', version_content, re.MULTILINE)).group(1)
-if not version:
+
+# Use regular expressions to extract the version number
+match = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', version_content, re.MULTILINE)
+if match:
+    version = match.group(1)
+    print(f"Version found: {version}")
+else:
     raise RuntimeError("Cannot find version information")
 
 with open("README.md", encoding="utf-8") as f:
