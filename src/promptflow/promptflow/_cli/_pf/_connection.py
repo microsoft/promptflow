microsoft/promptflow

commit ebba24b95af0086c8b033f70f9e119aca01dd829

Migrate the docstring comments when generate connection template (#602)
# Description

1. Migrate the docstring comments from a connection class, using them to
provide detailed information about each corresponding key

**Condition 1**: If the connection class doesn't contain any docstring
comments, the keys will be listed without any additional information.
``` python
class MyFirstConnection(CustomStrongTypeConnection):
    api_key: Secret
    api_key_2: Secret
    api_base: str
    api_base_2: str
```
``` yaml
name: "to_replace_with_connection_name"
type: custom
custom_type: MyFirstConnection
module: my_tool_package.connections
package: my-tools-package-with-cstc
package_version: 0.0.6
configs:
  api_base: "to_replace_with_api_base"
  api_base_2: "to_replace_with_api_base_2"
secrets:      # must-have
  api_key: "to_replace_with_api_key"
  api_key_2: "to_replace_with_api_key_2"
```
**Condition 2**: If the docstring comments only contain information
about specific keys, then these keys will have their corresponding
descriptions, while the rest will be listed without any additional
information.
``` python
class MyFirstConnection(CustomStrongTypeConnection):
    """Custom connection.

    :param api_key: The api key.
    :type api_key: String
    :param api_base: The api base.
    :type api_base: String
    """
    api_key: Secret
    api_key_2: Secret
    api_base: str
    api_base_2: str
```
``` yaml
name: "to_replace_with_connection_name"
type: custom
custom_type: MyFirstConnection
module: my_tool_package.connections
package: my-tools-package-with-cstc
package_version: 0.0.6
configs:
  api_base: "to_replace_with_api_base"  # String, The api base.
  api_base_2: "to_replace_with_api_base_2"
secrets:      # must-have
  api_key: "to_replace_with_api_key"  # String, The api key.
  api_key_2: "to_replace_with_api_key_2"
```
**Condition 3**: If the docstring comments contain information related

src/promptflow/promptflow/_cli/_pf/_connection.py
@@ -6,7 +6,6 @@ import argparse
 import json
 import logging
 from functools import partial
-
 from promptflow._cli._params import add_param_all_results, add_param_max_results, add_param_set, logging_params
 from promptflow._cli._utils import activate_action, confirm, exception_handler, get_secret_input, print_yellow_warning
 from promptflow._sdk._constants import LOGGER_NAME, MAX_LIST_CLI_RESULTS
@@ -46,15 +45,14 @@ def add_connection_parser(subparsers):
 
 def add_connection_create(subparsers):
     epilog = """
-Examples:
-
-# Creating a connection with yaml file:
-pf connection create -f connection.yaml
-# Creating a connection with yaml file and overrides:
-pf connection create -f connection.yaml --set api_key="my_api_key"
-# Creating a custom connection with .env file, note that overrides specified by --set will be ignored:
-pf connection create -f .env --name custom
-"""
+    Examples:
+    # Creating a connection with yaml file:
+    pf connection create -f connection.yaml
+    # Creating a connection with yaml file and overrides:
+    pf connection create -f connection.yaml --set api_key="my_api_key"
+    # Creating a custom connection with .env file, note that overrides specified by --set will be ignored:
+    pf connection create -f .env --name custom
+    """
     activate_action(
         name="create",
         description="Create a connection.",
@@ -68,11 +66,10 @@ pf connection create -f .env --name custom
 
 def add_connection_update(subparsers):
     epilog = """
-Examples:
-
-# Updating a connection:
-pf connection update -n my_connection --set api_key="my_api_key"
-"""
+    Examples:
+    # Updating a connection:
+    pf connection update -n my_connection --set api_key="my_api_key"
+    """
     activate_action(
         name="update",
         description="Update a connection.",
@@ -86,11 +83,10 @@ pf connection update -n my_connection --set api_key="my_api_key"
 
 def add_connection_show(subparsers):
     epilog = """
-Examples:
-
-# Get and show a connection:
-pf connection show -n my_connection_name
-"""
+    Examples:
+    # Get and show a connection:
+    pf connection show -n my_connection_name
+    """
     activate_action(
         name="show",
         description="Show a connection for promptflow.",
@@ -104,11 +100,10 @@ pf connection show -n my_connection_name
 
 def add_connection_delete(subparsers):
     epilog = """
-Examples:
-
-# Delete a connection:
-pf connection delete -n my_connection_name
-"""
+    Examples:
+    # Delete a connection:
+    pf connection delete -n my_connection_name
+    """
     activate_action(
         name="delete",
         description="Delete a connection with specific name.",
@@ -122,11 +117,10 @@ pf connection delete -n my_connection_name
 
 def add_connection_list(subparsers):
     epilog = """
-Examples:
-
-# List all connections:
-pf connection list
-"""
+    Examples:
+    # List all connections:
+    pf connection list
+    """
     activate_action(
         name="list",
         description="List all connections.",

src/promptflow/promptflow/_core/tools_manager.py
@@ -97,7 +97,9 @@ def collect_package_tools_and_connections(keys: Optional[List[str]] = None) -> d
                 custom_strong_type_connections_classes = [
                     obj
                     for name, obj in inspect.getmembers(module)
-                    if inspect.isclass(obj) and ConnectionType.is_custom_strong_type(obj)
+                    if inspect.isclass(obj)
+                    and ConnectionType.is_custom_strong_type(obj)
+                    and (not ConnectionType.is_connection_class_name(name))
                 ]
 
                 if custom_strong_type_connections_classes:

src/promptflow/promptflow/_sdk/_utils.py
@@ -27,6 +27,7 @@ from filelock import FileLock
 from jinja2 import Template
 from keyring.errors import NoKeyringError
 from marshmallow import ValidationError
+from ruamel.yaml import YAML
 
 import promptflow
 from promptflow._constants import EXTENSION_UA
@@ -808,8 +809,11 @@ def refresh_connections_dir(connection_spec_files, connection_template_yamls):
                 with open(connections_dir / file_name, "w") as f:
                     json.dump(content, f, indent=2)
 
+            # use YAML to dump template file in order to keep the comments
+            yaml = YAML()
+            yaml.preserve_quotes = True
             for connection_name, content in connection_template_yamls.items():
-                yaml_data = yaml.safe_load(content)
+                yaml_data = yaml.load(content)
                 file_name = connection_name + ".template.yaml"
                 with open(connections_dir / file_name, "w") as f:
-                    yaml.dump(yaml_data, f, sort_keys=False)
+                    yaml.dump(yaml_data, f)

src/promptflow/promptflow/_utils/connection_utils.py
@@ -1,8 +1,11 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+import io
+import re
 
 from jinja2 import Template
+from ruamel.yaml import YAML
 
 
 def generate_custom_strong_type_connection_spec(cls, package, package_version):
@@ -34,28 +37,32 @@ def generate_custom_strong_type_connection_spec(cls, package, package_version):
 
 def generate_custom_strong_type_connection_template(cls, connection_spec, package, package_version):
     connection_template_str = """
-    name: <connection-name>
+    name: "to_replace_with_connection_name"
     type: custom
     custom_type: {{ custom_type }}
     module: {{ module }}
     package: {{ package }}
     package_version: {{ package_version }}
-    configs:
-      {% for key, value in configs.items() %}
+    configs:{% for key, value in configs.items() %}
       {{ key }}: "{{ value -}}"{% endfor %}
     secrets:  # must-have{% for key, value in secrets.items() %}
       {{ key }}: "{{ value -}}"{% endfor %}
     """
 
+    connection_template = Template(connection_template_str)
+
+    # Extract configs and secrets
     configs = {}
     secrets = {}
-    connection_template = Template(connection_template_str)
     for spec in connection_spec["configSpecs"]:
         if spec["configValueType"] == "Secret":
-            secrets[spec["name"]] = "<" + spec["name"].replace("_", "-") + ">"
+            secrets[spec["name"]] = "to_replace_with_" + spec["name"].replace("-", "_")
         else:
-            configs[spec["name"]] = "<" + spec["name"].replace("_", "-") + ">"
+            configs[spec["name"]] = getattr(cls, spec["name"], None) or "to_replace_with_" + spec["name"].replace(
+                "-", "_"
+            )
 
+    # Prepare data for template
     data = {
         "custom_type": cls.__name__,
         "module": cls.__module__,
@@ -65,4 +72,55 @@ def generate_custom_strong_type_connection_template(cls, connection_spec, packag
         "secrets": secrets,
     }
 
-    return connection_template.render(data)
+    connection_template_with_data = connection_template.render(data)
+    connection_template_with_comments = render_comments(
+        connection_template_with_data, cls, secrets.keys(), configs.keys()
+    )
+
+    return connection_template_with_comments
+
+
+def render_comments(connection_template, cls, secrets, configs):
+    if cls.__doc__ is not None:
+        yaml = YAML()
+        yaml.preserve_quotes = True
+        data = yaml.load(connection_template)
+        comments_map = extract_comments_mapping(list(secrets) + list(configs), cls.__doc__)
+        # Add comments for secret keys
+        for key in secrets:
+            if key in comments_map.keys():
+                data["secrets"].yaml_add_eol_comment(comments_map[key] + "\n", key)
+
+        # Add comments for config keys
+        for key in configs:
+            if key in comments_map.keys():
+                data["configs"].yaml_add_eol_comment(comments_map[key] + "\n", key)
+
+        # Dump data object back to string
+        buf = io.StringIO()
+        yaml.dump(data, buf)
+        connection_template_with_comments = buf.getvalue()
+
+        return connection_template_with_comments
+
+    return connection_template
+
+
+def extract_comments_mapping(keys, doc):
+    comments_map = {}
+    for key in keys:
+        try:
+            param_pattern = rf":param {key}: (.*)"
+            key_description = " ".join(re.findall(param_pattern, doc))
+            type_pattern = rf":type {key}: (.*)"
+            key_type = " ".join(re.findall(type_pattern, doc)).rstrip(".")
+            if key_type and key_description:
+                comments_map[key] = ", ".join([key_type, key_description])
+            elif key_type:
+                comments_map[key] = key_type
+            elif key_description:
+                comments_map[key] = key_description
+        except re.error:
+            print("An error occurred when extract comments mapping.")
+
+    return comments_map

src/promptflow/setup.py
@@ -45,6 +45,7 @@ REQUIRES = [
     "strictyaml>=1.5.0,<2.0.0",  # used to identify exact location of validation error
     "waitress>=2.1.2,<3.0.0",  # used to serve local service
     "opencensus-ext-azure<2.0.0",  # configure opencensus to send telemetry to azure monitor
+    "ruamel.yaml>=0.17.35,<0.18.0",  # used to generate connection templates with preserved comments
 ]
 
 setup(

src/promptflow/tests/conftest.py
@@ -120,4 +120,4 @@ def install_custom_tool_pkg():
             import subprocess
             import sys
 
-            subprocess.check_call([sys.executable, "-m", "pip", "install", "test-custom-tools==0.0.1"])
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "test-custom-tools==0.0.2"])

src/promptflow/tests/executor/unittests/_core/test_tools_manager.py
@@ -1,7 +1,8 @@
+import textwrap
 from pathlib import Path
 
 import pytest
-import yaml
+from ruamel.yaml import YAML
 
 from promptflow import tool
 from promptflow._core._errors import NotSupported, PackageToolNotFoundError
@@ -136,6 +137,12 @@ class TestToolsManager:
             gen_tool_by_source("fake_name", tool_source, tool_type, working_dir),
         assert str(ex.value) == error_message
 
+    def test(self):
+        tools, specs, templates = collect_package_tools_and_connections()
+        from promptflow._sdk._utils import refresh_connections_dir
+
+        refresh_connections_dir(specs, templates)
+
     def test_collect_package_tools_and_connections(self, install_custom_tool_pkg):
         # Need to reload pkg_resources to get the latest installed tools
         import importlib
@@ -144,6 +151,8 @@ class TestToolsManager:
 
         importlib.reload(pkg_resources)
 
+        yaml = YAML()
+        yaml.preserve_quotes = True
         keys = ["my_tool_package.tools.my_tool_2.MyTool.my_tool"]
         tools, specs, templates = collect_package_tools_and_connections(keys)
         assert len(tools) == 1
@@ -159,18 +168,40 @@ class TestToolsManager:
                 ],
                 "module": "my_tool_package.connections",
                 "package": "test-custom-tools",
-                "package_version": "0.0.1",
+                "package_version": "0.0.2",
             }
         }
+
         expected_template = {
-            "name": "<connection-name>",
+            "name": "to_replace_with_connection_name",
             "type": "custom",
             "custom_type": "MyFirstConnection",
             "module": "my_tool_package.connections",
             "package": "test-custom-tools",
-            "package_version": "0.0.1",
-            "configs": {"api_base": "<api-base>"},
-            "secrets": {"api_key": "<api-key>"},
+            "package_version": "0.0.2",
+            "configs": {"api_base": "This is my first connection."},
+            "secrets": {"api_key": "to_replace_with_api_key"},
         }
-        loaded_yaml = yaml.safe_load(templates["my_tool_package.connections.MyFirstConnection"])
+        loaded_yaml = yaml.load(templates["my_tool_package.connections.MyFirstConnection"])
         assert loaded_yaml == expected_template
+
+        keys = ["my_tool_package.tools.my_tool_with_custom_strong_type_connection.my_tool"]
+        tools, specs, templates = collect_package_tools_and_connections(keys)
+        assert len(templates) == 1
+        expected_template = """
+            name: "to_replace_with_connection_name"
+            type: custom
+            custom_type: MyCustomConnection
+            module: my_tool_package.tools.my_tool_with_custom_strong_type_connection
+            package: test-custom-tools
+            package_version: 0.0.2
+            configs:
+              api_url: "This is a fake api url."  # String, The api url.
+            secrets:      # must-have
+              api_key: "to_replace_with_api_key"  # String, The api key.
+            """
+
+        content = templates["my_tool_package.tools.my_tool_with_custom_strong_type_connection.MyCustomConnection"]
+        expected_template_str = textwrap.dedent(expected_template)
+
+        assert content in expected_template_str

src/promptflow/tests/executor/unittests/_utils/test_connection_utils.py
@@ -1,0 +1,88 @@
+import pytest
+
+from promptflow._sdk.entities import CustomStrongTypeConnection
+from promptflow._utils.connection_utils import (
+    generate_custom_strong_type_connection_spec,
+    generate_custom_strong_type_connection_template,
+)
+from promptflow.contracts.types import Secret
+
+
+class MyCustomConnectionWithNoComments(CustomStrongTypeConnection):
+    api_key: Secret
+    api_base: str
+
+
+class MyCustomConnectionWithDefaultValue(CustomStrongTypeConnection):
+    api_key: Secret
+    api_base: str = "default value of api-base"
+
+
+class MyCustomConnectionWithInvalidComments(CustomStrongTypeConnection):
+    """My custom connection with invalid comments.
+    :param api_key: The api key.
+    :type api_key: String
+    :param api_base: The api base.
+    :type api_base: String
+    :param api_key_2: The api key 2.
+    :type api_key_2: String
+    """
+
+    api_key: Secret
+    api_base: str
+
+
+class MyCustomConnectionMissingTypeComments(CustomStrongTypeConnection):
+    """My custom connection with missing type comments.
+    :param api_key: The api key.
+    """
+
+    api_key: Secret
+    api_base: str
+
+
+class MyCustomConnectionMissingParamComments(CustomStrongTypeConnection):
+    """My custom connection with missing param comments.
+    :type api_key: String
+    """
+
+    api_key: Secret
+    api_base: str
+
+
+@pytest.mark.unittest
+class TestConnectionUtils:
+    @pytest.mark.parametrize(
+        "cls, expected_str_in_template",
+        [
+            (
+                MyCustomConnectionWithNoComments,
+                ['api_base: "to_replace_with_api_base"\n', 'api_key: "to_replace_with_api_key"\n'],
+            ),
+            (
+                MyCustomConnectionWithInvalidComments,
+                [
+                    'api_base: "to_replace_with_api_base"  # String, The api base.\n',
+                    'api_key: "to_replace_with_api_key"  # String, The api key.\n',
+                ],
+            ),
+            (MyCustomConnectionMissingTypeComments, ['api_key: "to_replace_with_api_key"  # The api key.']),
+            (MyCustomConnectionMissingParamComments, ['api_key: "to_replace_with_api_key"  # String']),
+        ],
+    )
+    def test_generate_custom_strong_type_connection_template_with_comments(self, cls, expected_str_in_template):
+        package = "test-package"
+        package_version = "0.0.1"
+        spec = generate_custom_strong_type_connection_spec(cls, package, package_version)
+        template = generate_custom_strong_type_connection_template(cls, spec, package, package_version)
+        for comment in expected_str_in_template:
+            assert comment in template
+
+    def test_generate_custom_strong_type_connection_template_with_default_value(self):
+        package = "test-package"
+        package_version = "0.0.1"
+        spec = generate_custom_strong_type_connection_spec(MyCustomConnectionWithDefaultValue, package, package_version)
+        template = generate_custom_strong_type_connection_template(
+            MyCustomConnectionWithDefaultValue, spec, package, package_version
+        )
+        assert 'api_base: "default value of api-base"' in template

commit ef9cfaf0980b2f5dac9b69980f98be90d9237717

[Bugfix] Fix epilog indent caused by pr #602 (#712)
# Description

Fix epilog indent caused by pr
https://github.com/microsoft/promptflow/pull/602/files

Regression:

![image](https://github.com/microsoft/promptflow/assets/46446115/a1ef62d1-03f7-4b07-be26-2a658bd136a4)


After fix:

![image](https://github.com/microsoft/promptflow/assets/46446115/153d8ebd-58f5-4142-b984-3081430eb421)


# All Promptflow Contribution checklist:
- [X] **The pull request does not introduce [breaking changes].**
- [X] **CHANGELOG is updated for new features, bug fixes or other
significant changes.**
- [X] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
- [ ] **Create an issue and link to the pull request to get dedicated
review from promptflow team. Learn more: [suggested
workflow](../CONTRIBUTING.md#suggested-workflow).**

## General Guidelines and Best Practices
- [X] Title of the pull request is clear and informative.
- [X] There are a small number of commits, each of which have an
informative message. This means that previously merged commits do not
appear in the history of the PR. For more information on cleaning up the
commits in your PR, [see this
page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).

### Testing Guidelines
- [ ] Pull request includes test coverage for the included changes.

---------

Co-authored-by: yalu4 <yalu4@microsoft.com>

src/promptflow/promptflow/_cli/_pf/_connection.py
@@ -6,6 +6,7 @@ import argparse
 import json
 import logging
 from functools import partial
+
 from promptflow._cli._params import add_param_all_results, add_param_max_results, add_param_set, logging_params
 from promptflow._cli._utils import activate_action, confirm, exception_handler, get_secret_input, print_yellow_warning
 from promptflow._sdk._constants import LOGGER_NAME, MAX_LIST_CLI_RESULTS
@@ -44,15 +45,17 @@ def add_connection_parser(subparsers):
 
 
 def add_connection_create(subparsers):
+    # Do not change the indent of epilog
     epilog = """
-    Examples:
-    # Creating a connection with yaml file:
-    pf connection create -f connection.yaml
-    # Creating a connection with yaml file and overrides:
-    pf connection create -f connection.yaml --set api_key="my_api_key"
-    # Creating a custom connection with .env file, note that overrides specified by --set will be ignored:
-    pf connection create -f .env --name custom
-    """
+Examples:
+
+# Creating a connection with yaml file:
+pf connection create -f connection.yaml
+# Creating a connection with yaml file and overrides:
+pf connection create -f connection.yaml --set api_key="my_api_key"
+# Creating a custom connection with .env file, note that overrides specified by --set will be ignored:
+pf connection create -f .env --name custom
+"""
     activate_action(
         name="create",
         description="Create a connection.",
@@ -66,10 +69,11 @@ def add_connection_create(subparsers):
 
 def add_connection_update(subparsers):
     epilog = """
-    Examples:
-    # Updating a connection:
-    pf connection update -n my_connection --set api_key="my_api_key"
-    """
+Examples:
+
+# Updating a connection:
+pf connection update -n my_connection --set api_key="my_api_key"
+"""
     activate_action(
         name="update",
         description="Update a connection.",
@@ -83,10 +87,11 @@ def add_connection_update(subparsers):
 
 def add_connection_show(subparsers):
     epilog = """
-    Examples:
-    # Get and show a connection:
-    pf connection show -n my_connection_name
-    """
+Examples:
+
+# Get and show a connection:
+pf connection show -n my_connection_name
+"""
     activate_action(
         name="show",
         description="Show a connection for promptflow.",
@@ -100,10 +105,11 @@ def add_connection_show(subparsers):
 
 def add_connection_delete(subparsers):
     epilog = """
-    Examples:
-    # Delete a connection:
-    pf connection delete -n my_connection_name
-    """
+Examples:
+
+# Delete a connection:
+pf connection delete -n my_connection_name
+"""
     activate_action(
         name="delete",
         description="Delete a connection with specific name.",
@@ -117,10 +123,11 @@ def add_connection_delete(subparsers):
 
 def add_connection_list(subparsers):
     epilog = """
-    Examples:
-    # List all connections:
-    pf connection list
-    """
+Examples:
+
+# List all connections:
+pf connection list
+"""
     activate_action(
         name="list",
         description="List all connections.",