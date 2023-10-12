0db256696b6312800d86a0f215f1d6b3abdfec3d
Honglin <0mza987@gmail.com>
2023-10-12 14:43:16 +0800

    [SDK/CLI] Fix encoding issue with non-English input (#683)
    # Description
    
    To fix the encoding issue, add encoding when opening files.
    
    https://github.com/microsoft/promptflow/issues/668
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow/tests/sdk_cli_test/e2etests/test_flow_test.py
+++ src/promptflow/tests/sdk_cli_test/e2etests/test_flow_test.py
@@ -139,3 +139,7 @@ class TestFlowTest:
         result = _client._flows._test(flow=flow_path, inputs=inputs)
         assert "calculate_accuracy" in result.node_run_infos
         assert result.run_info.metrics == {"accuracy": 1.0}
+
+    def test_pf_test_with_non_english_input(self):
+        result = _client.test(flow=f"{FLOWS_DIR}/flow_with_non_english_input")
+        assert result["output"] == "Hello 日本語"
