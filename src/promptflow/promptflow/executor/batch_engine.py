fab0e275753ca963e2a87dadcc846f004aeff19f
Peiwen Gao <peiwengao@microsoft.com>
2023-10-13 18:02:05 +0800

    Poc for bacth engine

--- src/promptflow/promptflow/executor/batch_engine.py
+++ src/promptflow/promptflow/executor/batch_engine.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from promptflow.executor.flow_executor import FlowExecutor
 
 
@@ -5,6 +7,23 @@ class BatchEngine:
     def __init__(self, flow_executor: FlowExecutor):
         self.flow_executor = flow_executor
 
-    def run(self):
+    def run(
+        self,
+        input_dirs: Dict[str, str],
+        inputs_mapping: Dict[str, str],
+        output_dir: str,
+        run_id: str = None,
+    ):
+        # 1. load data
+        self._load_data()
+        # 2. apply inputs mapping
+        self._apply_inputs_mapping()
+        # 3. execute batch run
         batch_result = self.flow_executor.exec_bulk()
         return batch_result
+
+    def _load_data(self, input_dirs: Dict[str, str]):
+        pass
+
+    def _apply_inputs_mapping(self, inputs_mapping: Dict[str, str]):
+        pass
