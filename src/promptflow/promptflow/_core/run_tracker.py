aca03d44b0a402fb6688ecbf1b3d708d4849360c
Cheng Liu <51689021+liucheng-ms@users.noreply.github.com>
2023-10-16 22:17:19 +0800

    Enable node level tracing of generator output (#702)
    # Description
    
    This PR enables tracing of generator output of nodes in promptflow.
    Previously, the `Trace` object was serialized as a JSON string in
    `RunInfo` right before node execution ends. However, this did not
    capture the generator output as it was not consumed yet. To fix this
    issue, this PR makes the following changes:
    
    - Instead of generating a JSON string in `RunInfo`, we track a list of
    generator output in `RunInfo`, which can be updated when the generator
    is consumed.
    - We update the node `RunInfo` before the flow ends, at that time, all
    generators are expected to be consumed.
    
    This change will enable generator tracing in node level, which is
    currently not possible in the Promptflow implementation.
    
    # All Promptflow Contribution checklist:
    - [X] **The pull request does not introduce [breaking changes].**
    - [X] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [X] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [X] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [X] Title of the pull request is clear and informative.
    - [X] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [X] Pull request includes test coverage for the included changes.

--- src/promptflow/promptflow/_core/run_tracker.py
+++ src/promptflow/promptflow/_core/run_tracker.py
@@ -360,6 +360,24 @@ class RunTracker(ThreadLocalSingleton):
     def persist_node_run(self, run_info: RunInfo):
         self._storage.persist_node_run(run_info)
 
+    def persist_selected_node_runs(self, run_info: FlowRunInfo, node_names: List[str]):
+        """
+        Persists the node runs for the specified node names.
+
+        :param run_info: The flow run information.
+        :type run_info: FlowRunInfo
+        :param node_names: The names of the nodes to persist.
+        :type node_names: List[str]
+        :returns: None
+        """
+        run_id = run_info.run_id
+
+        selected_node_run_info = (
+            run_info for run_info in self.collect_child_node_runs(run_id) if run_info.node in node_names
+        )
+        for node_run_info in selected_node_run_info:
+            self.persist_node_run(node_run_info)
+
     def persist_flow_run(self, run_info: FlowRunInfo):
         self._storage.persist_flow_run(run_info)
 
