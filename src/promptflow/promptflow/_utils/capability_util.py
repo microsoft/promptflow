8f520e9f58d957d3199cbbc6e367ce2b609a91a1
Peiwen Gao <peiwengao@microsoft.com>
2023-10-16 12:11:42 +0800

    Update capability list implementation

--- src/promptflow/promptflow/_utils/capability_util.py
+++ src/promptflow/promptflow/_utils/capability_util.py
@@ -1,10 +1,51 @@
-from pathlib import Path
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
 
-from promptflow._utils.utils import load_json
 
-CAPABILITY_LIST_FILE = Path(__file__).parent / "capability_list.json"
+class CapabilityState(Enum):
+    """The enum of capability state.
+
+    READY: The feature is ready to use.
+    E2ETEST: The feature is not ready to be shipped to customer and is in e2e testing.
+    """
+
+    READY = "Ready"
+    E2ETEST = "E2ETest"
+
+
+class CapabilityComponent(Enum):
+    """The enum of capability component."""
+
+    EXECUTOR = "executor"
+
+
+@dataclass
+class Capability:
+    name: str
+    description: str
+    components: List[CapabilityComponent]
+    state: CapabilityState
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "components": [component.value for component in self.components],
+            "state": self.state.value,
+        }
+
+
+CAPABILITY_LIST = [
+    Capability(
+        name="Activate",
+        description="Bypass node execution when the node does not meet activate condition.",
+        components=[CapabilityComponent.EXECUTOR],
+        state=CapabilityState.READY,
+    ),
+]
 
 
 def get_capability_list():
-    capability_list = load_json(CAPABILITY_LIST_FILE)
+    capability_list = [capability.to_dict() for capability in CAPABILITY_LIST]
     return capability_list
