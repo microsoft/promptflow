f4849744a90a3d9d46969b5eede9e9d585ebe051
melionel <lmamsat@hotmail.com>
2023-10-16 21:12:16 +0800

    refine codes

--- src/promptflow/promptflow/_core/tool_meta_generator.py
+++ src/promptflow/promptflow/_core/tool_meta_generator.py
@@ -19,9 +19,8 @@ from jinja2.environment import COMMENT_END_STRING, COMMENT_START_STRING
 from promptflow._core._errors import MetaFileNotFound, MetaFileReadError, NotSupported
 from promptflow._core.tool import ToolProvider
 from promptflow._utils.exception_utils import ADDITIONAL_INFO_USER_CODE_STACKTRACE, get_tb_next, last_frame_info
-from promptflow._utils.tool_utils import function_to_interface, get_inputs_for_prompt_template, \
-    get_input_type_mapping_for_prompt_template
-from promptflow.contracts.tool import InputDefinition, Tool, ToolType, ValueType
+from promptflow._utils.tool_utils import function_to_interface, get_inputs_for_prompt_template
+from promptflow.contracts.tool import Tool, ToolType, ValueType
 from promptflow.exceptions import ErrorTarget, UserErrorException
 
 PF_MAIN_MODULE_NAME = "__pf_main__"
@@ -57,7 +56,6 @@ def generate_prompt_tool(name, content, prompt_only=False, source=None):
     tool_type = ToolType.PROMPT if prompt_only else ToolType.LLM
     try:
         inputs = get_inputs_for_prompt_template(content)
-        input_type_mapping = get_input_type_mapping_for_prompt_template(content)
     except Exception as e:
         error_type_and_message = f"({e.__class__.__name__}) {e}"
         raise JinjaParsingError(
@@ -79,7 +77,7 @@ def generate_prompt_tool(name, content, prompt_only=False, source=None):
 
         reserved_keys_to_raise = reserved_keys
 
-    for input in inputs:
+    for input in inputs.keys():
         if input in reserved_keys_to_raise:
             raise ReservedVariableCannotBeUsed(
                 message_format=(
@@ -98,7 +96,7 @@ def generate_prompt_tool(name, content, prompt_only=False, source=None):
         name=name,
         description=description,
         type=tool_type,
-        inputs={i: InputDefinition(type=[get_input_type(i, input_type_mapping)]) for i in inputs},
+        inputs=inputs,
         outputs={},
     )
     if source is None:
--- src/promptflow/promptflow/_utils/tool_utils.py
+++ src/promptflow/promptflow/_utils/tool_utils.py
@@ -159,45 +159,43 @@ def function_to_tool_definition(f: Callable, type=None, initialize_inputs=None) 
 
 
 def get_inputs_for_prompt_template(template_str):
-    """Get all input variable names from a jinja2 template string."""
-    env = Environment()
-    template = env.parse(template_str)
-    return sorted(meta.find_undeclared_variables(template), key=lambda x: template_str.find(x))
+    """Get all input variable names and definitions from a jinja2 template string.
 
-
-def get_prompt_param_name_from_func(f):
-    """Get the param name of prompt template on provider."""
-    return next((k for k, annotation in f.__annotations__.items() if annotation == PromptTemplate), None)
-
-
-def get_input_type_mapping_for_prompt_template(template_str):
-    """Get all tagged input type mapping from a template string
+    : param template_str: template string
+    : type t: str
+    : return: the input name to InputDefinition dict
+    : rtype t: Dict[str, ~promptflow.contracts.tool.InputDefinition]
     Example:
-    >>> get_input_type_mapping_for_prompt_template(
+    >>> get_inputs_for_prompt_template(
         template_str="A simple prompt with no variables"
     )
     {}
 
-    >>> get_input_type_mapping_for_prompt_template(
+    >>> get_inputs_for_prompt_template(
         template_str="Prompt with only one string input {{str_input}}"
     )
-    {}
+    {"str_input": InputDefinition(type=[ValueType.STRING])}
 
-    >>> get_input_type_mapping_for_prompt_template(
-        template_str="Prompt with image input ![image]({{image_input}})"
+    >>> get_inputs_for_prompt_template(
+        template_str="Prompt with image input ![image]({{image_input}}) and string input {{str_input}}"
     )
-    {"image_input": "image"}
+    {"image_input": InputDefinition(type=[ValueType.IMAGE]), "str_input": InputDefinition(type=[ValueType.STRING])
     """
+    env = Environment()
+    template = env.parse(template_str)
+    inputs = sorted(meta.find_undeclared_variables(template), key=lambda x: template_str.find(x))
+    result_dict = {i: InputDefinition(type=[ValueType.STRING]) for i in inputs}
 
     # currently we only support image type
     pattern = r'\!\[(\s*image\s*)\]\(\{\{\s*([^{}]+)\s*\}\}\)'
     matches = re.finditer(pattern, template_str)
 
-    result_dict = {}
-
     for match in matches:
         input_name = match.group(2).strip()
-        if input_name != "" and input_name not in result_dict:
-            result_dict[input_name] = match.group(1).strip()
+        result_dict[input_name] = InputDefinition([ValueType(match.group(1).strip())])
 
     return result_dict
+
+def get_prompt_param_name_from_func(f):
+    """Get the param name of prompt template on provider."""
+    return next((k for k, annotation in f.__annotations__.items() if annotation == PromptTemplate), None)
