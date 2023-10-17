0130217d296e18a0a32a70f6b6645b4f21898079
Peiwen Gao <peiwengao@microsoft.com>
2023-10-17 18:55:51 +0800

    Update tests

--- src/promptflow/tests/executor/unittests/_utils/test_capability_util.py
+++ src/promptflow/tests/executor/unittests/_utils/test_capability_util.py
@@ -4,8 +4,7 @@ from promptflow._utils.capability_util import Capability, get_capability_list
 
 
 @pytest.mark.unittest
-class TestCapabilityUtil:
-    def test_get_capability_list(self):
-        capability_list = get_capability_list()
-        assert isinstance(capability_list, list)
-        assert all(isinstance(capability, Capability) for capability in capability_list)
+def test_get_capability_list():
+    capability_list = get_capability_list()
+    assert isinstance(capability_list, list)
+    assert all(isinstance(capability, Capability) for capability in capability_list)
