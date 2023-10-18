dbf7f21e19060f895bdc299e01089de9361d43af
Lina Tang <linatang@microsoft.com>
2023-10-18 13:41:59 +0800

    Add utils for multimedia data

--- src/promptflow/promptflow/contracts/multimedia.py
+++ src/promptflow/promptflow/contracts/multimedia.py
@@ -1,16 +1,9 @@
 import base64
 import hashlib
-import imghdr
 import os
-import re
-import requests
 import uuid
 from pathlib import Path
 from typing import Callable
-from urllib.parse import urlparse
-
-from promptflow.contracts._errors import InvalidImageInput
-from promptflow.exceptions import ErrorTarget
 
 
 class PFBytes(bytes):
@@ -18,8 +11,6 @@ class PFBytes(bytes):
     It has all the functionalities of a bytes object,
     and also has some additional methods to help with serialization and deserialization.
     """
-
-    MIME_PATTERN = re.compile(r"^data:image/(.*);(path|base64|url)$")
 
     def __new__(cls, value: bytes, *args, **kwargs):
         # Here we must only pass the value to the bytes constructor,
@@ -33,58 +24,10 @@ class PFBytes(bytes):
         self._hash = hashlib.sha1(data).hexdigest()[:8]
         self._mime_type = mime_type.lower()
 
-    @staticmethod
-    def _get_mime_type_from_path(path: Path):
-        ext = path.suffix[1:]
-        return f"image/{ext}" if ext else "image/*"
-
-    @staticmethod
-    def _get_extension_from_mime_type(mime_type: str):
-        ext = mime_type.split("/")[-1]
-        if ext == "*":
-            return None
-        return ext
-
-    @staticmethod
-    def _is_multimedia_dict(multimedia_dict: dict):
-        if len(multimedia_dict) != 1:
-            return False
-        key = list(multimedia_dict.keys())[0]
-        if re.match(PFBytes.MIME_PATTERN, key):
-            return True
-        return False
-
-    @staticmethod
-    def _get_multimedia_info(key: str):
-        match = re.match(PFBytes.MIME_PATTERN, key)
-        if match:
-            return match.group(1), match.group(2)
-        return None, None
-
-    @staticmethod
-    def _is_url(value: str):
-        try:
-            result = urlparse(value)
-            return all([result.scheme, result.netloc])
-        except ValueError:
-            return False
-
-    @staticmethod
-    def _is_base64(value: str):
-        base64_regex = re.compile(r"^([A-Za-z0-9+/]{4})*(([A-Za-z0-9+/]{2})*(==|[A-Za-z0-9+/]=)?)?$")
-        if re.match(base64_regex, value):
-            return True
-        return False
-
-    @staticmethod
-    def default_json_encoder(obj):
-        if isinstance(obj, PFBytes):
-            return str(obj)
-        else:
-            raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
-
     def _save_to_file(self, file_name: str, folder_path: Path, relative_path: Path = None):
-        ext = PFBytes._get_extension_from_mime_type(self._mime_type)
+        from promptflow._utils.multimedia_utils import get_extension_from_mime_type
+
+        ext = get_extension_from_mime_type(self._mime_type)
         file_name = f"{file_name}.{ext}" if ext else file_name
         image_info = {
             f"data:{self._mime_type};path": str(relative_path / file_name) if relative_path else file_name
@@ -105,8 +48,17 @@ class PFBytes(bytes):
             raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
         return pfbytes_file_reference_encoder
 
+    def to_base64(self):
+        """Returns the base64 representation of the PFBytes."""
+
+        return base64.b64encode(self).decode("utf-8")
+
 
 class Image(PFBytes):
+    """This class is used to represent an image in PromptFlow. It is a subclass of
+    ~promptflow.contracts.multimedia.PFBytes.
+    """
+
     def __init__(self, data: bytes, mime_type: str = "image/*"):
         return super().__init__(data, mime_type)
 
@@ -116,92 +68,9 @@ class Image(PFBytes):
     def __repr__(self) -> str:
         return f"Image({self._hash})"
 
-    @staticmethod
-    def _from_file(f: Path, mime_type: str = None):
-        if not mime_type:
-            mime_type = PFBytes._get_mime_type_from_path(f)
-        with open(f, "rb") as fin:
-            return Image(fin.read(), mime_type=mime_type)
+    def serialize(self, encoder: Callable = None):
+        """Serialize the image to a dictionary."""
 
-    @staticmethod
-    def _from_base64(base64_str: str, mime_type: str = None):
-        image_bytes = base64.b64decode(base64_str)
-        if not mime_type:
-            format = imghdr.what(None, image_bytes)
-            mime_type = f"image/{format}" if format else "image/*"
-        return Image(image_bytes, mime_type=mime_type)
-
-    @staticmethod
-    def _from_url(url: str, mime_type: str = None):
-        response = requests.get(url)
-        if response.status_code == 200:
-            if not mime_type:
-                format = imghdr.what(None, response.content)
-                mime_type = f"image/{format}" if format else "image/*"
-            return Image(response.content, mime_type=mime_type)
-        else:
-            raise InvalidImageInput(
-                message_format=f"Error while fetching image from URL: {url}. "
-                "Error code: {response.status_code}. Error message: {response.text}.",
-                target=ErrorTarget.EXECUTOR,
-            )
-
-    @staticmethod
-    def _from_dict(image_dict: dict):
-        for k, v in image_dict.items():
-            format, resource = Image._get_multimedia_info(k)
-            if resource == "path":
-                return Image._from_file(v, mime_type=f"image/{format}")
-            elif resource == "base64":
-                return Image._from_base64(v, mime_type=f"image/{format}")
-            elif resource == "url":
-                return Image._from_url(v, mime_type=f"image/{format}")
-            else:
-                raise InvalidImageInput(
-                    message_format=f"Unsupported image resource: {resource}. "
-                    "Supported Resources are [path, base64, url].",
-                    target=ErrorTarget.EXECUTOR,
-                )
-
-    @staticmethod
-    def _create_from_string(value: str, image_dir: Path = None):
-        if PFBytes._is_base64(value):
-            return Image._from_base64(value)
-        elif PFBytes._is_url(value):
-            return Image._from_url(value)
-        else:
-            image_path = Path(value)
-            if not image_path.is_absolute():
-                if image_dir:
-                    image_path = Path.joinpath(image_dir, image_path)
-            return Image._from_file(image_path)
-
-    @staticmethod
-    def _create(value: any, image_dir: Path = None):
-        if isinstance(value, Image):
-            return value
-        elif isinstance(value, dict):
-            if PFBytes._is_multimedia_dict(value):
-                return Image._from_dict(value)
-            else:
-                raise InvalidImageInput(
-                    message_format="Invalid image input format. The image input should be a dictionary like: "
-                    "{data:image/<image_type>;[path|base64|url]: <image_data>}.",
-                    target=ErrorTarget.EXECUTOR,
-                )
-        elif isinstance(value, str):
-            return Image._create_from_string(value, image_dir)
-        else:
-            raise InvalidImageInput(
-                message_format=f"Unsupported image input type: {type(value)}. "
-                "The image inputs should be a string or a dictionary.",
-                target=ErrorTarget.EXECUTOR,
-            )
-
-    def to_base64(self):
-        return base64.b64encode(self).decode("utf-8")
-
-    def _serialize(self, encoder: Callable = None):
         if encoder is None:
             return self.__str__()
         return encoder(self)
--- src/promptflow/promptflow/storage/_run_storage.py
+++ src/promptflow/promptflow/storage/_run_storage.py
@@ -5,7 +5,7 @@
 from functools import partial
 from pathlib import Path
 
-from promptflow._utils.dataclass_serializer import serialize
+from promptflow._utils.multimedia_utils import recursive_process
 from promptflow.contracts.multimedia import Image, PFBytes
 from promptflow.contracts.run_info import FlowRunInfo, RunInfo as NodeRunInfo
 
@@ -35,25 +35,25 @@ class DefaultRunStorage(AbstractRunStorage):
 
     def persist_node_run(self, run_info: NodeRunInfo):
         if run_info.inputs:
-            run_info.inputs = self._serialize(run_info.inputs)
+            run_info.inputs = self._persist_images(run_info.inputs)
         if run_info.output:
-            serialized_output = self._serialize(run_info.output)
+            serialized_output = self._persist_images(run_info.output)
             run_info.output = serialized_output
             run_info.result = serialized_output
         if run_info.api_calls:
-            run_info.api_calls = self._serialize(run_info.api_calls)
+            run_info.api_calls = self._persist_images(run_info.api_calls)
 
     def persist_flow_run(self, run_info: FlowRunInfo):
         if run_info.inputs:
-            run_info.inputs = self._serialize(run_info.inputs)
+            run_info.inputs = self._persist_images(run_info.inputs)
         if run_info.output:
-            serialized_output = self._serialize(run_info.output)
+            serialized_output = self._persist_images(run_info.output)
             run_info.output = serialized_output
             run_info.result = serialized_output
         if run_info.api_calls:
-            run_info.api_calls = self._serialize(run_info.api_calls)
+            run_info.api_calls = self._persist_images(run_info.api_calls)
 
-    def _serialize(self, value):
+    def _persist_images(self, value):
         if self._base_dir:
             pfbytes_file_reference_encoder = PFBytes._get_file_reference_encoder(
                 folder_path=self._base_dir,
@@ -61,5 +61,5 @@ class DefaultRunStorage(AbstractRunStorage):
             )
         else:
             pfbytes_file_reference_encoder = None
-        serialization_funcs = {Image: partial(Image._serialize, **{"encoder": pfbytes_file_reference_encoder})}
-        return serialize(value, serialization_funcs=serialization_funcs)
+        serialization_funcs = {Image: partial(Image.serialize, **{"encoder": pfbytes_file_reference_encoder})}
+        return recursive_process(value, process_funcs=serialization_funcs)
