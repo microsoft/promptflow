eeb615ce5fed035ac56b3e619bbf46bc6b643129
cs_lucky <si.chen@microsoft.com>
2023-10-19 15:16:02 +0800

    fix flake 8

--- src/promptflow-tools/tests/test_common.py
+++ src/promptflow-tools/tests/test_common.py
@@ -33,7 +33,7 @@ class TestCommon:
             ("123", "function_call parameter '123' must be a dict"),
             ({"name1": "get_current_weather"},
              'function_call parameter {"name1": "get_current_weather"} must '
-                                                 'contain "name" field'),
+             'contain "name" field'),
         ],
     )
     def test_chat_api_invalid_function_call(self, function_call, error_message):
