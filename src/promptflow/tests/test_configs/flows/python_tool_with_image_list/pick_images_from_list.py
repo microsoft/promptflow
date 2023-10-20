70e65be8d8ee072da9fade86041df4a66df0b280
Lina Tang <linatanglily@gmail.com>
2023-10-20 16:06:32 +0800

    Support including images in object type flow inputs (#833)
    # Description
    
    We only support including images in list type flow inputs before, in
    this PR, we support including images in object type flow inputs.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.
    
    Co-authored-by: Lina Tang <linatang@microsoft.com>

--- src/promptflow/tests/test_configs/flows/python_tool_with_image_list/pick_images_from_list.py
+++ src/promptflow/tests/test_configs/flows/python_tool_with_image_list/pick_images_from_list.py
@@ -3,8 +3,8 @@ from promptflow.contracts.multimedia import Image
 
 
 @tool
-def pick_images_from_list(image_list: list[Image], idx_l: int, idx_r: int) -> list[Image]:
+def pick_images_from_list(image_list: list[Image], image_dict: dict, idx_l: int, idx_r: int) -> list[Image]:
     if idx_l <= idx_r and idx_l >= 0 and idx_r < len(image_list):
-        return image_list[idx_l:idx_r + 1]
+        return {"Image list": image_list[idx_l:idx_r + 1], "Image dict": image_dict}
     else:
         raise Exception(f"Invalid index.")
