0d21c2b4dd50ff2684567c0058916b8328f30e3a
cs_lucky <si.chen@microsoft.com>
2023-10-23 22:51:11 +0800

    fix flake 8

--- examples/tools/tool-package-quickstart/tests/test_tool_with_enabled_by_value.py
+++ examples/tools/tool-package-quickstart/tests/test_tool_with_enabled_by_value.py
@@ -4,4 +4,3 @@ from my_tool_package.tools.tool_with_enabled_by_value import my_tool
 def test_my_tool():
     result = my_tool(user_type="student", student_id="123", teacher_id=None)
     assert result == '123'
-
