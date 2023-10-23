ac3d1f666cc17781551101d15085f8f0a84a4fdb
cs_lucky <si.chen@microsoft.com>
2023-10-23 10:07:32 +0800

    fix uts

--- src/promptflow/tests/executor/unittests/_core/test_tools_manager.py
+++ src/promptflow/tests/executor/unittests/_core/test_tools_manager.py
@@ -184,8 +184,6 @@ class TestToolsManager:
         tools, specs, templates = collect_package_tools_and_connections(keys)
         assert len(templates) == 1
         expected_template = """
-            $schema: 
-              https://azuremlschemas.azureedge.net/promptflow/latest/CustomStrongTypeConnection.schema.json
             name: "to_replace_with_connection_name"
             type: custom
             custom_type: MyCustomConnection
@@ -200,4 +198,4 @@ class TestToolsManager:
 
         content = templates["my_tool_package.tools.my_tool_with_custom_strong_type_connection.MyCustomConnection"]
         expected_template_str = textwrap.dedent(expected_template)
-        assert content in expected_template_str
+        assert expected_template_str in content
