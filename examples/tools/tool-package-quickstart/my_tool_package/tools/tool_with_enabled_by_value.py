87f8b3e3a5ebfe41f4c1fe14827b083c9cb47840
cs_lucky <si.chen@microsoft.com>
2023-10-24 14:29:22 +0800

    refine tools

--- examples/tools/tool-package-quickstart/my_tool_package/tools/tool_with_enabled_by_value.py
+++ examples/tools/tool-package-quickstart/my_tool_package/tools/tool_with_enabled_by_value.py
@@ -9,9 +9,8 @@ class UserType(str, Enum):
 
 
 @tool
-def my_tool(user_type: Enum, student_id: str, teacher_id: str) -> str:
+def my_tool(user_type: Enum, student_id: str = "", teacher_id: str = "") -> str:
     """This is a dummy function to support enabled by feature.
-
     :param user_type: user type, student or teacher.
     :param student_id: student id.
     :param teacher_id: teacher id.
--- examples/tools/tool-package-quickstart/tests/test_tool_with_enabled_by_value.py
+++ examples/tools/tool-package-quickstart/tests/test_tool_with_enabled_by_value.py
@@ -2,5 +2,5 @@ from my_tool_package.tools.tool_with_enabled_by_value import my_tool
 
 
 def test_my_tool():
-    result = my_tool(user_type="student", student_id="123", teacher_id=None)
+    result = my_tool(user_type="student", student_id="123")
     assert result == '123'
