4c7a0af7ad49976248dad861effdc241a607c402
Ge Gao <gega@microsoft.com>
2023-10-24 16:16:46 +0800

    refine

--- examples/tools/tool-package-quickstart/my_tool_package/tools/whisper.py
+++ examples/tools/tool-package-quickstart/my_tool_package/tools/whisper.py
@@ -1,10 +1,11 @@
 from enum import Enum
+from typing import Union
 from promptflow.contracts.types import FilePath
 
 import openai
 import requests
 
-from promptflow.connections import AzureOpenAIConnection
+from promptflow.connections import AzureOpenAIConnection, OpenAIConnection
 # Avoid circular dependencies: Use import 'from promptflow._internal' instead of 'from promptflow'
 # since the code here is in promptflow namespace as well
 from promptflow._internal import tool
@@ -25,19 +26,33 @@ class ResponseFormat(str, Enum):
 
 @tool
 @handle_openai_error()
-def whisper(connection: AzureOpenAIConnection, file: FilePath, endpoint: WhisperEndpoint, deployment_name: str, prompt: str, response_format: ResponseFormat):
+def whisper(connection: Union[AzureOpenAIConnection, OpenAIConnection], file: FilePath, endpoint: WhisperEndpoint, deployment_name: str, prompt: str, response_format: ResponseFormat):
     audio_file = open(file, 'rb')
     payload = {'file': audio_file}
-    if endpoint == WhisperEndpoint.Transcription:
+    connection_dict = dict(connection)
+    if isinstance(connection, AzureOpenAIConnection):
             # Request URL
-        api_url = f"{connection.api_case}/openai/deployments/{deployment_name}/audio/transcriptions?api-version={connection.api_version}"
+        api_url = f"{connection.api_base}/openai/deployments/{deployment_name}/audio/transcriptions?api-version={connection.api_version}"
+        print("api_url: ",api_url)
         headers =  {"api-key": connection.api_key,
                     "Content-Type": "multipart/form-data"}
         response = requests.post(api_url, data=payload, headers=headers)
 
-        print("response json: ", response)
+        print("response json: ", response.json())
 
-        return response
+        return response.json()
+    elif isinstance(connection, OpenAIConnection):
+        response = openai.Audio.transcribe(
+            model="whisper-1",
+            file=audio_file,
+            prompt = prompt,
+            response_format = response_format,
+            **connection_dict
+        )
+
+        print("response json: ", response.json())
+
+        return response.json()
     else:
         error_message = f"Not Support endpoint '{endpoint}' for whisper api. " \
                         f"Endpoint should be in [Transcription, Translation]."
