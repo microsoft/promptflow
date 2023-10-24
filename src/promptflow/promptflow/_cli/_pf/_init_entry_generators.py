ed51d70ea171796c27ca45fc979fa5eb18f1d603
zhen <henu_rz@163.com>
2023-10-24 17:54:50 +0800

    fix flow test streamlit failed

--- src/promptflow/promptflow/_cli/_pf/_init_entry_generators.py
+++ src/promptflow/promptflow/_cli/_pf/_init_entry_generators.py
@@ -232,7 +232,7 @@ class StreamlitFileGenerator(BaseGenerator):
         executable = ExecutableFlow.from_yaml(
             flow_file=Path(self.flow_dag_path.name), working_dir=self.flow_dag_path.parent
         )
-        return {flow_input: (value.default, value.type.value) for flow_input, value in executable.inputs.items()}
+        return {flow_input: (value.default, value.type.value) for flow_input, value in executable.inputs.items() if not value.is_chat_history}
 
     @property
     def flow_inputs_params(self):
