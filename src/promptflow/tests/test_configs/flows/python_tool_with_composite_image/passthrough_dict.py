41518953585b3053da652c9988b78ad9fddc43c7
Lina Tang <linatanglily@gmail.com>
2023-10-25 18:59:35 +0800

    Fix python_tool_with_composite_image flow (#898)
    # Description
    
    Fix python_tool_with_composite_image flow.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.
    
    Co-authored-by: Lina Tang <linatang@microsoft.com>

--- src/promptflow/tests/test_configs/flows/python_tool_with_composite_image/passthrough_dict.py
+++ src/promptflow/tests/test_configs/flows/python_tool_with_composite_image/passthrough_dict.py
@@ -2,5 +2,5 @@ from promptflow import tool
 
 
 @tool
-def passthrough_dict(image_list: list,image_dict: dict) -> list:
+def passthrough_dict(image_list: list, image_dict: dict):
     return image_dict
