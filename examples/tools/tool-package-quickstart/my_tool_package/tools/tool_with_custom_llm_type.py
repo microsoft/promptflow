599bbbe1e00f49a644b74d88c59d7cfbfc6254cd
jiazeng <jiazeng@microsoft.com>
2023-10-26 09:24:32 +0800

    resolve comments

--- examples/tools/tool-package-quickstart/my_tool_package/tools/tool_with_custom_llm_type.py
+++ examples/tools/tool-package-quickstart/my_tool_package/tools/tool_with_custom_llm_type.py
@@ -9,5 +9,5 @@ def my_tool(connection: CustomConnection, prompt: PromptTemplate, **kwargs) -> s
     # Replace with your tool code, customise your own code to handle and use the prompt here.
     # Usually connection contains configs to connect to an API.
     # Not all tools need a connection. You can remove it if you don't need it.
-    message = Template(prompt, trim_blocks=True, keep_trailing_newline=True).render(**kwargs)
-    return message
+    rendered_prompt = Template(prompt, trim_blocks=True, keep_trailing_newline=True).render(**kwargs)
+    return rendered_prompt
