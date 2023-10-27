f7c66cb988f668d92243ea3e26a2955d86a8bba2
Min Shi <minshi@microsoft.com>
2023-10-27 16:13:58 +0800

    add log_metric

--- src/promptflow/tests/test_configs/flows/conditional_flow_with_aggregate_bypassed/aggregation_node.py
+++ src/promptflow/tests/test_configs/flows/conditional_flow_with_aggregate_bypassed/aggregation_node.py
@@ -1,9 +1,6 @@
 from promptflow import tool
 from promptflow import log_metric
 
-# The inputs section will change based on the arguments of the tool function, after you save the code
-# Adding type to arguments and return value will help the system show the types properly
-# Please update the function name/signature per need
 @tool
 def average(input: list):
   avg, cnt = 0, 0
@@ -13,4 +10,5 @@ def average(input: list):
       cnt += 1
   if len(input) > 0:
     avg = avg/cnt
+  log_metric("average", avg)
   return avg
--- src/promptflow/tests/test_configs/flows/conditional_flow_with_aggregate_bypassed/square.py
+++ src/promptflow/tests/test_configs/flows/conditional_flow_with_aggregate_bypassed/square.py
@@ -1,8 +1,5 @@
 from promptflow import tool
 
-# The inputs section will change based on the arguments of the tool function, after you save the code
-# Adding type to arguments and return value will help the system show the types properly
-# Please update the function name/signature per need
 @tool
 def square(input: int) -> int:
   return input*input
