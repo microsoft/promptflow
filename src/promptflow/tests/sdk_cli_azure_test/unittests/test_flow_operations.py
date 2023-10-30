ab847050421e35e819ec441146d171e6ac32dc75
Honglin Du <0mza987@gmail.com>
2023-10-30 11:05:43 +0800

    Fix tests

--- src/promptflow/tests/sdk_cli_azure_test/unittests/test_flow_operations.py
+++ src/promptflow/tests/sdk_cli_azure_test/unittests/test_flow_operations.py
@@ -18,20 +18,20 @@ data_dir = tests_root_dir / "test_configs/datas"
 class TestFlow:
     def test_create_flow_with_invalid_parameters(self, remote_client):
         with pytest.raises(ValueError, match=r"Flow file .*? does not exist."):
-            remote_client.flows.create_or_update(source="fake_source")
+            remote_client.flows.create_or_update(flow="fake_source")
 
         flow_source = flow_test_dir / "web_classification/"
         with pytest.raises(FlowOperationError, match="Flow name must be a string"):
-            remote_client.flows.create_or_update(source=flow_source, flow_name=object())
+            remote_client.flows.create_or_update(flow=flow_source, name=object())
 
         with pytest.raises(FlowOperationError, match="Flow type 'fake_flow_type' is not supported"):
-            remote_client.flows.create_or_update(source=flow_source, flow_type="fake_flow_type")
+            remote_client.flows.create_or_update(flow=flow_source, type="fake_flow_type")
 
         with pytest.raises(FlowOperationError, match="Description must be a string"):
-            remote_client.flows.create_or_update(source=flow_source, description=object())
+            remote_client.flows.create_or_update(flow=flow_source, description=object())
 
         with pytest.raises(FlowOperationError, match="got non-dict or non-string key/value in tags"):
-            remote_client.flows.create_or_update(source=flow_source, tags={"key": object()})
+            remote_client.flows.create_or_update(flow=flow_source, tags={"key": object()})
 
     def test_create_flow_when_flow_name_already_exist(self, remote_client, mocker: MockerFixture):
         mocker.patch(
@@ -40,7 +40,7 @@ class TestFlow:
         )
         flow_source = flow_test_dir / "web_classification/"
         with pytest.raises(FlowOperationError, match="Please change the flow folder name"):
-            remote_client.flows.create_or_update(source=flow_source)
+            remote_client.flows.create_or_update(flow=flow_source)
 
     def test_parse_flow_portal_url(self, remote_client):
         flow_resource_id = (
