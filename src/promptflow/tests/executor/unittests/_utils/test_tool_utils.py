ccaf535e45721947982ae7dce8ffe2084f3d1129
Robben Wang <robbenwang@microsoft.com>
2023-11-06 15:03:43 +0800

    Add `enable_kwargs` tag in tools.json for customer python tool

--- src/promptflow/tests/executor/unittests/_utils/test_tool_utils.py
+++ src/promptflow/tests/executor/unittests/_utils/test_tool_utils.py
@@ -55,11 +55,12 @@ class TestToolUtils:
         def func(conn: [AzureOpenAIConnection, CustomConnection], input: [str, int]):
             pass
 
-        input_defs, _, connection_types = function_to_interface(func)
+        input_defs, _, connection_types, enable_kwargs = function_to_interface(func)
         assert len(input_defs) == 2
         assert input_defs["conn"].type == ["AzureOpenAIConnection", "CustomConnection"]
         assert input_defs["input"].type == [ValueType.OBJECT]
         assert connection_types == [["AzureOpenAIConnection", "CustomConnection"]]
+        assert enable_kwargs is False
 
     def test_function_to_interface_with_invalid_initialize_inputs(self):
         def func(input_str: str):
@@ -68,6 +69,13 @@ class TestToolUtils:
         with pytest.raises(Exception) as exec_info:
             function_to_interface(func, {"input_str": "test"})
         assert "Duplicate inputs found from" in exec_info.value.args[0]
+
+    def test_function_to_interface_with_kwargs(self):
+        def func(input_str: str, **kwargs):
+            pass
+
+        _, _, _, enable_kwargs = function_to_interface(func)
+        assert enable_kwargs is True
 
     def test_param_to_definition(self):
         from promptflow._sdk.entities import CustomStrongTypeConnection
