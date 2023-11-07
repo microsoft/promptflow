b81901e85be24f38a6fc2dd68b5f7e25dbbd7e9a
Heyi Tang <heta@microsoft.com>
2023-11-07 17:33:35 +0800

    Add tests

---
+++ src/promptflow/tests/executor/unittests/contracts/test_multimedia.py
@@ -0,0 +1,27 @@
+import pytest
+import yaml
+from pathlib import Path
+
+from promptflow.contracts.multimedia import PFBytes, Image
+
+PACKAGE_TOOL_BASE = Path(__file__).parent.parent.parent / "package_tools"
+
+
+@pytest.mark.e2etest
+class TestMultimediaContract:
+    def test_constructors(self):
+        content = b"test"
+        mime_type = "image/*"
+        bs = [
+            PFBytes(content, mime_type),
+            Image(content, mime_type),
+            PFBytes(content, mime_type=mime_type),
+            Image(content, mime_type=mime_type),
+            PFBytes(value=content, mime_type=mime_type),
+            Image(value=content, mime_type=mime_type),
+        ]
+        for b in bs:
+            assert b._mime_type == "image/*"
+            assert b._hash == "a94a8fe5"
+            assert b.to_base64() == "dGVzdA=="
+            assert bytes(b) == content
