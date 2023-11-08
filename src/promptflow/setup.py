4b95d13b9ff15bad01226efb38c726ca89642a55
Lina Tang <linatang@microsoft.com>
2023-11-08 18:49:35 +0800

    Use python-magic to detect image type

--- src/promptflow/setup.py
+++ src/promptflow/setup.py
@@ -48,6 +48,7 @@ REQUIRES = [
     "ruamel.yaml>=0.17.35,<0.18.0",  # used to generate connection templates with preserved comments
     "pyarrow>=9.0.0,<14.0.0",  # used to read parquet file with pandas.read_parquet
     "pillow>=10.1.0,<11.0.0",  # used to generate icon data URI for package tool
+    "python-magic",  # used to detect mime type of mulitmedia input
 ]
 
 setup(
