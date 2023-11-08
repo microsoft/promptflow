167c20068c557cb3052f3ae5d8474623dc5bf63e
Yuhang Yu <yuhangyu@microsoft.com>
2023-11-08 17:54:38 +0800

    revert conftest

--- src/promptflow-tools/tests/conftest.py
+++ src/promptflow-tools/tests/conftest.py
@@ -73,11 +73,6 @@ def gpt2_custom_connection():
 
 
 @pytest.fixture
-def llama_chat_custom_connection():
-    return ConnectionManager().get("llama_chat_connection")
-
-
-@pytest.fixture
 def open_source_llm_ws_service_connection() -> bool:
     try:
         creds_custom_connection: CustomConnection = ConnectionManager().get("open_source_llm_ws_service_connection")
