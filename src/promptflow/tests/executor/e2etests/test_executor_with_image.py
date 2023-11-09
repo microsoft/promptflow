f9efd029cd5d9d24883ff9e7dad91f0c47146d99
Peiwen Gao <peiwengao@microsoft.com>
2023-11-09 14:49:49 +0800

    update tests

--- src/promptflow/tests/executor/e2etests/test_executor_with_image.py
+++ src/promptflow/tests/executor/e2etests/test_executor_with_image.py
@@ -181,7 +181,7 @@ class TestExecutorWithImage:
             ),
             (
                 COMPOSITE_IMAGE_FLOW,
-                {"data": "."},
+                {"data": "inputs.jsonl"},
                 {"image_list": "${data.image_list}", "image_dict": "${data.image_dict}"},
                 2,
             ),
