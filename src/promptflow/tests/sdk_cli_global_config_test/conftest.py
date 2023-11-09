070a2341ebf81944a151f2feca3f801e9a70987a
Zhengfei Wang <zhengfei.wang@microsoft.com>
2023-11-09 19:21:33 +0800

    update fixture not session

--- src/promptflow/tests/sdk_cli_global_config_test/conftest.py
+++ src/promptflow/tests/sdk_cli_global_config_test/conftest.py
@@ -9,12 +9,12 @@ from promptflow import PFClient
 from promptflow._sdk._configuration import Configuration
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def pf() -> PFClient:
     return PFClient()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def global_config():
     config = Configuration.get_instance()
     if Configuration.CONNECTION_PROVIDER in config._config:
--- src/promptflow/tests/sdk_cli_global_config_test/e2etests/test_global_config.py
+++ src/promptflow/tests/sdk_cli_global_config_test/e2etests/test_global_config.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import pytest
 
+from promptflow import PFClient
+
 FLOWS_DIR = Path(__file__).parent.parent.parent / "test_configs" / "flows"
 DATAS_DIR = Path(__file__).parent.parent.parent / "test_configs" / "datas"
 
@@ -9,7 +11,7 @@ DATAS_DIR = Path(__file__).parent.parent.parent / "test_configs" / "datas"
 @pytest.mark.usefixtures("global_config")
 @pytest.mark.e2etest
 class TestGlobalConfig:
-    def test_basic_flow_bulk_run(self, pf) -> None:
+    def test_basic_flow_bulk_run(self, pf: PFClient) -> None:
         data_path = f"{DATAS_DIR}/webClassification3.jsonl"
 
         run = pf.run(flow=f"{FLOWS_DIR}/web_classification", data=data_path)
@@ -18,7 +20,7 @@ class TestGlobalConfig:
         run = pf.run(flow=f"{FLOWS_DIR}/web_classification", data=data_path)
         assert run.status == "Completed"
 
-    def test_connection_operations(self, pf):
+    def test_connection_operations(self, pf: PFClient):
         connections = pf.connections.list()
         assert len(connections) > 0, f"No connection found. Provider: {pf._connection_provider}"
         # Assert create/update/delete not supported.
