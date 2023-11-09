8b81ea94dbe0f4f4a2a31595dd7810a0ab05d3c5
Peiwen Gao <peiwengao@microsoft.com>
2023-11-09 14:50:44 +0800

    Remove constant

--- src/promptflow/promptflow/_constants.py
+++ src/promptflow/promptflow/_constants.py
@@ -14,5 +14,4 @@ CONTENT_SAFETY_API_KEY = "content-safety-api-key"
 ERROR_RESPONSE_COMPONENT_NAME = "promptflow"
 EXTENSION_UA = "prompt-flow-extension"
 
-PROMPT_FLOW_DIR_NAME = ".promptflow"
 DEFAULT_ENCODING = "utf-8"
