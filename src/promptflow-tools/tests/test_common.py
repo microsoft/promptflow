cb1c81806aca7aa849b8d077ef8c68bd9101addb
Meng Lan <mengla@microsoft.com>
2023-11-10 19:38:36 +0800

    add test cases for parse_chat with images

--- src/promptflow-tools/tests/test_common.py
+++ src/promptflow-tools/tests/test_common.py
@@ -1,5 +1,6 @@
 import pytest
 
+from promptflow.contracts.multimedia import Image
 from promptflow.tools.common import ChatAPIInvalidFunctions, validate_functions, \
     process_function_call, parse_chat
 
@@ -44,32 +45,39 @@ class TestCommon:
         assert exc_info.value.error_codes == error_codes.split("/")
 
     @pytest.mark.parametrize(
-        "chat_str, expected_result",
+        "chat_str, images, expected_result",
         [
-            ("system:\nthis is my function:\ndef hello", [
+            ("system:\nthis is my function:\ndef hello", None, [
                 {'role': 'system', 'content': 'this is my function:\ndef hello'}]),
-            ("#system:\nthis is my ##function:\ndef hello", [
+            ("#system:\nthis is my ##function:\ndef hello", None, [
                 {'role': 'system', 'content': 'this is my ##function:\ndef hello'}]),
-            (" \n system:\nthis is my function:\ndef hello", [
+            (" \n system:\nthis is my function:\ndef hello", None, [
                 {'role': 'system', 'content': 'this is my function:\ndef hello'}]),
-            (" \n # system:\nthis is my function:\ndef hello", [
+            (" \n # system:\nthis is my function:\ndef hello", None, [
                 {'role': 'system', 'content': 'this is my function:\ndef hello'}]),
-            ("user:\nhi\nassistant:\nanswer\nfunction:\nname:\nn\ncontent:\nc", [
+            ("user:\nhi\nassistant:\nanswer\nfunction:\nname:\nn\ncontent:\nc", None, [
                 {'role': 'user', 'content': 'hi'},
                 {'role': 'assistant', 'content': 'answer'},
                 {'role': 'function', 'name': 'n', 'content': 'c'}]),
-            ("#user :\nhi\n #assistant:\nanswer\n# function:\n##name:\nn\n##content:\nc", [
+            ("#user :\nhi\n #assistant:\nanswer\n# function:\n##name:\nn\n##content:\nc", None, [
                 {'role': 'user', 'content': 'hi'},
                 {'role': 'assistant', 'content': 'answer'},
                 {'role': 'function', 'name': 'n', 'content': 'c'}]),
-            ("\nsystem:\nfirst\n\nsystem:\nsecond", [
+            ("\nsystem:\nfirst\n\nsystem:\nsecond", None, [
                 {'role': 'system', 'content': 'first'}, {'role': 'system', 'content': 'second'}]),
-            ("\n#system:\nfirst\n\n#system:\nsecond", [
-                {'role': 'system', 'content': 'first'}, {'role': 'system', 'content': 'second'}])
+            ("\n#system:\nfirst\n\n#system:\nsecond", None, [
+                {'role': 'system', 'content': 'first'}, {'role': 'system', 'content': 'second'}]),
+            ("#user:\ntell me about the images\nImage(1edf82c2)\nImage(9b65b0f4)", [
+                Image("image1".encode()), Image("image2".encode(), "image/png", "https://image_url")], [
+                {'role': 'user', 'content': [
+                    {'type': 'text', 'text': 'tell me about the images'},
+                    {'type': 'image_url', 'image_url': {'url': 'data:image/*;base64,aW1hZ2Ux'}},
+                    {'type': 'image_url', 'image_url': 'https://image_url'}]},
+            ])
         ]
     )
-    def test_success_parse_role_prompt(self, chat_str, expected_result):
-        actual_result = parse_chat(chat_str)
+    def test_success_parse_role_prompt(self, chat_str, images, expected_result):
+        actual_result = parse_chat(chat_str, images)
         assert actual_result == expected_result
 
     @pytest.mark.parametrize(
@@ -82,7 +90,7 @@ class TestCommon:
             ("\nsystem:\nname:\n\n content:\nfirst", [
                 {'role': 'system', 'content': 'name:\n\n content:\nfirst'}]),
             ("\nsystem:\nname:\n\n", [
-                {'role': 'system', 'content': 'name:\n\n'}])
+                {'role': 'system', 'content': 'name:'}])
         ]
     )
     def test_parse_chat_with_name_in_role_prompt(self, chat_str, expected_result):
