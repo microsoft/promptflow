a8bbd67d3a2e5d51ffb911a7e7deed2174ec1241
Zhengfei Wang <zhengfei.wang@microsoft.com>
2023-11-13 18:24:57 +0800

    remove some files

--- scripts/runtime_mgmt/constants.py
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-from pathlib import Path
-
-COMPUTE_INSTANCE_YAML = Path(__file__).parent / "compute-instance.yaml"
-
-ENVIRONMENT_YAML = Path(__file__).parent / "runtime-env" / "env.yaml"
