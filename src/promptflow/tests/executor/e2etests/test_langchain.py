c35b1cb6c29982ddf868cbc9def39b232dcd0f42
Peiwen Gao <peiwengao@microsoft.com>
2023-11-14 16:49:17 +0800

    Remove exec_bulk in test_langchain

--- src/promptflow/tests/executor/e2etests/test_langchain.py
+++ src/promptflow/tests/executor/e2etests/test_langchain.py
@@ -1,49 +1,34 @@
+from pathlib import Path
+from tempfile import mkdtemp
+
 import pytest
 
+from promptflow.batch import BatchEngine
 from promptflow.contracts.run_info import Status
-from promptflow.executor import FlowExecutor
 from promptflow.executor._result import BulkResult, LineResult
 
-from ..utils import get_flow_sample_inputs, get_yaml_file
+from ..utils import get_flow_folder, get_flow_inputs_file, get_yaml_file
 
 
 @pytest.mark.usefixtures("use_secrets_config_file", "dev_connections")
 @pytest.mark.e2etest
 class TestLangchain:
-    def get_line_inputs(self, flow_folder=""):
-        if flow_folder:
-            inputs = self.get_bulk_inputs(flow_folder)
-            return inputs[0]
-        return {
-            "url": "https://www.apple.com/shop/buy-iphone/iphone-14",
-            "text": "some_text",
-        }
-
-    def get_bulk_inputs(self, nlinee=4, flow_folder=""):
-        if flow_folder:
-            inputs = get_flow_sample_inputs(flow_folder)
-            if isinstance(inputs, list) and len(inputs) > 0:
-                return inputs
-            elif isinstance(inputs, dict):
-                return [inputs]
-            else:
-                raise Exception(f"Invalid type of bulk input: {inputs}")
-        return [self.get_line_inputs() for _ in range(nlinee)]
-
     @pytest.mark.parametrize(
-        "flow_folder",
+        "flow_folder, inputs_mapping",
         [
-            "flow_with_langchain_traces",
-            "openai_chat_api_flow",
-            "openai_completion_api_flow"
-        ]
+            ("flow_with_langchain_traces", {"question": "${data.question}"}),
+            ("openai_chat_api_flow", {"question": "${data.question}", "chat_history": "${data.chat_history}"}),
+            ("openai_completion_api_flow", {"prompt": "${data.prompt}"}),
+        ],
     )
-    def test_executor_exec_bulk_with_langchain(self, flow_folder, dev_connections):
-        executor = FlowExecutor.create(get_yaml_file(flow_folder), dev_connections, raise_ex=True)
-        bulk_inputs = self.get_bulk_inputs(flow_folder=flow_folder)
-        bulk_results = executor.exec_bulk(bulk_inputs)
+    def test_executor_exec_bulk_with_langchain(self, flow_folder, inputs_mapping, dev_connections):
+        batch_engine = BatchEngine(
+            get_yaml_file(flow_folder), get_flow_folder(flow_folder), connections=dev_connections
+        )
+        input_dirs = {"data": get_flow_inputs_file(flow_folder)}
+        output_dir = Path(mkdtemp())
+        bulk_results = batch_engine.run(input_dirs, inputs_mapping, output_dir)
         assert isinstance(bulk_results, BulkResult)
-        assert len(bulk_results.outputs) == len(bulk_inputs)
         for _, line_result in enumerate(bulk_results.line_results):
             assert isinstance(line_result, LineResult)
             assert line_result.run_info.status == Status.Completed
