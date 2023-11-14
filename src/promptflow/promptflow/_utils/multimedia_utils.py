08314bb9067673046b8473da4044984219020f4e
Lina Tang <linatang@microsoft.com>
2023-11-14 18:26:06 +0800

    Serialize image to url if its source is url

--- src/promptflow/promptflow/_utils/multimedia_utils.py
+++ src/promptflow/promptflow/_utils/multimedia_utils.py
@@ -167,6 +167,8 @@ def _save_image_to_file(
 def get_file_reference_encoder(folder_path: Path, relative_path: Path = None, *, use_absolute_path=False) -> Callable:
     def pfbytes_file_reference_encoder(obj):
         """Dumps PFBytes to a file and returns its reference."""
+        if obj.source_url:
+            return {f"data:{obj._mime_type};url": obj.source_url}
         if isinstance(obj, PFBytes):
             file_name = str(uuid.uuid4())
             # If use_absolute_path is True, the image file path in image dictionary will be absolute path.
