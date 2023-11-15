a3de86d4951bdff2f593a860b06671b7b22cf098
Ming Gu <minggu.thuee@gmail.com>
2023-11-15 17:45:49 +0800

    Add bugbash sample flows

---
+++ examples/flows/evaluation/eval-description/line_process.py
@@ -0,0 +1,17 @@
+from promptflow import tool
+
+
+@tool
+def line_process(groundtruth: str, prediction: str):
+    """
+    This tool processes the prediction of a single line and returns the processed result.
+
+    :param groundtruth: the groundtruth of a single line.
+    :param prediction: the prediction of a single line.
+    """
+
+    processed_result = ""
+
+    # Add your line processing logic here
+
+    return processed_result
