60d4a48d956c3beee15ac48a38794597c05f8f6f
Peiwen Gao <peiwengao@microsoft.com>
2023-11-15 18:11:23 +0800

    Add PythonExecutorProxy to __init__

--- src/promptflow/promptflow/batch/__init__.py
+++ src/promptflow/promptflow/batch/__init__.py
@@ -5,8 +5,6 @@
 # flake8: noqa
 from ._base_executor_proxy import AbstractExecutorProxy
 from ._batch_engine import BatchEngine
+from ._python_executor_proxy import PythonExecutorProxy
 
-__all__ = [
-    "AbstractExecutorProxy",
-    "BatchEngine",
-]
+__all__ = ["AbstractExecutorProxy", "BatchEngine", "PythonExecutorProxy"]
