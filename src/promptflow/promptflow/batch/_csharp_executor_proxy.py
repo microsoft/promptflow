b3a54c0a35d4c751f70e7a468f4a8bb428e3c085
Peiwen Gao <peiwengao@microsoft.com>
2023-11-17 17:08:14 +0800

    Update some codes

--- src/promptflow/promptflow/batch/_csharp_executor_proxy.py
+++ src/promptflow/promptflow/batch/_csharp_executor_proxy.py
@@ -1,3 +1,4 @@
+import subprocess
 from pathlib import Path
 from typing import Any, Mapping, Optional
 
@@ -5,8 +6,13 @@ from promptflow.batch._base_executor_proxy import APIBasedExecutorProxy
 from promptflow.executor._result import AggregationResult
 from promptflow.storage._run_storage import AbstractRunStorage
 
+EXECUTOR_PORT = 8080
+
 
 class CSharpExecutorProxy(APIBasedExecutorProxy):
+    def __init__(self, process: subprocess.Popen):
+        self._process = process
+
     @classmethod
     def create(
         cls,
@@ -14,18 +20,25 @@ class CSharpExecutorProxy(APIBasedExecutorProxy):
         working_dir: Optional[Path] = None,
         *,
         connections: Optional[dict] = None,
-        storage: Optional[AbstractRunStorage] = None
+        storage: Optional[AbstractRunStorage] = None,
     ) -> "CSharpExecutorProxy":
         """Create a new executor"""
-        raise NotImplementedError()
+        command = f'dotnet Promptflow.dll -p {EXECUTOR_PORT} -y {flow_file} -a "." -c "" -l ""'
+        process = subprocess.Popen(command, shell=True)
+        return cls(process)
 
     def destroy(self):
         """Destroy the executor"""
-        pass
+        if self._process and self._process.poll() is None:
+            self._process.terminate()
+            try:
+                self._process.wait(timeout=5)
+            except subprocess.TimeoutExpired:
+                self._process.kill()
 
     @property
     def api_endpoint(self) -> str:
-        raise NotImplementedError()
+        return EXECUTOR_PORT
 
     async def exec_aggregation_async(
         self,
