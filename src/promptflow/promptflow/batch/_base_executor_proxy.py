6d711dae5a1993a7e42849141f177eb9c38554d9
Peiwen Gao <peiwengao@microsoft.com>
2023-11-17 11:22:06 +0800

    Add api based executor proxy

--- src/promptflow/promptflow/batch/_base_executor_proxy.py
+++ src/promptflow/promptflow/batch/_base_executor_proxy.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from typing import Any, Mapping, Optional
 
+import httpx
+
+from promptflow._constants import LINE_TIMEOUT_SEC
 from promptflow.executor._result import AggregationResult, LineResult
 from promptflow.storage._run_storage import AbstractRunStorage
 
@@ -39,3 +42,31 @@ class AbstractExecutorProxy:
     ) -> AggregationResult:
         """Execute aggregation nodes"""
         raise NotImplementedError()
+
+
+class APIBasedExecutorProxy(AbstractExecutorProxy):
+    @property
+    def api_endpoint(self) -> str:
+        raise NotImplementedError()
+
+    async def exec_line_async(
+        self,
+        inputs: Mapping[str, Any],
+        index: Optional[int] = None,
+        run_id: Optional[str] = None,
+    ) -> LineResult:
+        async with httpx.AsyncClient() as client:
+            payload = {"run_id": run_id, "line_number": index, "inputs": inputs}
+            response = await client.post(self.api_endpoint, json=payload, timeout=LINE_TIMEOUT_SEC)
+        return LineResult.deserialize(response.json())
+
+    async def exec_aggregation_async(
+        self,
+        batch_inputs: Mapping[str, Any],
+        aggregation_inputs: Mapping[str, Any],
+        run_id: Optional[str] = None,
+    ) -> AggregationResult:
+        async with httpx.AsyncClient() as client:
+            payload = {"run_id": run_id, "batch_inputs": batch_inputs, "aggregation_inputs": aggregation_inputs}
+            response = await client.post(self.api_endpoint, json=payload, timeout=LINE_TIMEOUT_SEC)
+        return AggregationResult.deserialize(response.json())
--- src/promptflow/setup.py
+++ src/promptflow/setup.py
@@ -22,6 +22,7 @@ with open("CHANGELOG.md", encoding="utf-8") as f:
 
 REQUIRES = [
     "psutil",  # get process information when bulk run
+    "httpx>=0.25.1",  # used to send http requests asynchronously
     "openai>=0.27.8,<0.28.0",  # promptflow.core.api_injector
     "flask>=2.2.3,<3.0.0",  # Serving endpoint requirements
     "dataset>=1.6.0,<2.0.0",  # promptflow.storage
