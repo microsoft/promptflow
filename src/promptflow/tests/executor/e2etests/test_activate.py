7a9857439776aa2fba7a0f26b17a996e8f22df37
Peiwen Gao <peiwengao@microsoft.com>
2023-11-21 16:17:21 +0800

    Update tets cases

--- src/promptflow/tests/executor/e2etests/test_activate.py
+++ src/promptflow/tests/executor/e2etests/test_activate.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 from tempfile import mkdtemp
+from typing import Dict
 
 import pytest
 
 from promptflow._utils.logger_utils import LogContext
-from promptflow.batch import BatchEngine
+from promptflow.batch._batch_engine import OUTPUT_FILE_NAME, BatchEngine
+from promptflow.batch._result import BatchResult
+from promptflow.contracts.run_info import FlowRunInfo
+from promptflow.contracts.run_info import RunInfo as NodeRunInfo
 from promptflow.contracts.run_info import Status
 from promptflow.executor import FlowExecutor
-from promptflow.executor._result import BatchResult, LineResult
+from promptflow.storage import AbstractRunStorage
 
 from ..utils import (
     get_flow_expected_result,
@@ -16,6 +20,7 @@ from ..utils import (
     get_flow_inputs,
     get_flow_inputs_file,
     get_yaml_file,
+    load_jsonl,
 )
 
 ACTIVATE_FLOW_TEST_CASES = [
@@ -24,6 +29,20 @@ ACTIVATE_FLOW_TEST_CASES = [
     "all_depedencies_bypassed_with_activate_met",
     "activate_condition_always_met",
 ]
+
+
+class MemoryRunStorage(AbstractRunStorage):
+    def __init__(self):
+        self._node_runs = {}
+        self._flow_runs = {}
+
+    def persist_flow_run(self, run_info: FlowRunInfo):
+        run_info.result = None
+        self._flow_runs[run_info.run_id] = run_info
+
+    def persist_node_run(self, run_info: NodeRunInfo):
+        run_info.result = None
+        self._node_runs[run_info.run_id] = run_info
 
 
 @pytest.mark.usefixtures("dev_connections")
@@ -36,12 +55,17 @@ class TestExecutorActivate:
         # Assert the flow result
         expected_result = get_flow_expected_result(flow_folder)
         expected_result = expected_result[0] if isinstance(expected_result, list) else get_flow_expected_result
-        self.assert_activate_flow_run_result(results, expected_result)
+        self.assert_activate_flow_run_result(results.run_info, expected_result)
+        self.assert_activate_node_run_result(results.node_run_infos, expected_result)
 
     def test_batch_run_activate(self, dev_connections):
         flow_folder = "conditional_flow_with_activate"
+        mem_run_storage = MemoryRunStorage()
         batch_engine = BatchEngine(
-            get_yaml_file(flow_folder), get_flow_folder(flow_folder), connections=dev_connections
+            get_yaml_file(flow_folder),
+            get_flow_folder(flow_folder),
+            connections=dev_connections,
+            storage=mem_run_storage,
         )
         input_dirs = {"data": get_flow_inputs_file(flow_folder, file_name="inputs.json")}
         inputs_mapping = {"incident_id": "${data.incident_id}", "incident_content": "${data.incident_content}"}
@@ -50,7 +74,9 @@ class TestExecutorActivate:
 
         expected_result = get_flow_expected_result(flow_folder)
         expected_status_summary = get_flow_expected_status_summary(flow_folder)
-        self.assert_activate_bulk_run_result(batch_results, expected_result, expected_status_summary)
+        self.assert_activate_bulk_run_result(
+            output_dir, mem_run_storage, batch_results, expected_result, expected_status_summary
+        )
 
     def test_all_nodes_bypassed(self, dev_connections):
         flow_folder = "all_nodes_bypassed"
@@ -65,7 +91,10 @@ class TestExecutorActivate:
 
     def test_aggregate_bypassed_nodes(self):
         flow_folder = "conditional_flow_with_aggregate_bypassed"
-        batch_engine = BatchEngine(get_yaml_file(flow_folder), get_flow_folder(flow_folder), connections={})
+        mem_run_storage = MemoryRunStorage()
+        batch_engine = BatchEngine(
+            get_yaml_file(flow_folder), get_flow_folder(flow_folder), connections={}, storage=mem_run_storage
+        )
         input_dirs = {"data": get_flow_inputs_file(flow_folder, file_name="inputs.json")}
         inputs_mapping = {"case": "${data.case}", "value": "${data.value}"}
         output_dir = Path(mkdtemp())
@@ -73,44 +102,66 @@ class TestExecutorActivate:
 
         expected_result = get_flow_expected_result(flow_folder)
         expected_status_summary = get_flow_expected_status_summary(flow_folder)
-        self.assert_activate_bulk_run_result(batch_results, expected_result, expected_status_summary)
+        self.assert_activate_bulk_run_result(
+            output_dir, mem_run_storage, batch_results, expected_result, expected_status_summary
+        )
 
         # Validate the aggregate result
-        assert batch_results.aggr_results.node_run_infos["aggregation_double"].output == 3
-        assert batch_results.aggr_results.node_run_infos["aggregation_square"].output == 12.5
+        for node_run_info in mem_run_storage._node_runs.values():
+            if node_run_info.node == "aggregation_double":
+                assert node_run_info.status == Status.Completed
+                assert node_run_info.output == 3
+            elif node_run_info.node == "aggregation_square":
+                assert node_run_info.status == Status.Completed
+                assert node_run_info.output == 12.5
 
-    def assert_activate_bulk_run_result(self, result: BatchResult, expected_result, expected_status_summary):
+    def assert_activate_bulk_run_result(
+        self,
+        output_dir: Path,
+        mem_run_storage: MemoryRunStorage,
+        batch_result: BatchResult,
+        expected_result,
+        expected_status_summary,
+    ):
         # Validate the flow outputs
-        for i, output in enumerate(result.outputs):
+        outputs = load_jsonl(output_dir / OUTPUT_FILE_NAME)
+        for i, output in enumerate(outputs):
             expected_outputs = expected_result[i]["expected_outputs"].copy()
             expected_outputs.update({"line_number": i})
             assert output == expected_outputs
 
         # Validate the flow line results
-        for i, line_result in enumerate(result.line_results):
-            self.assert_activate_flow_run_result(line_result, expected_result[i])
+        for i, (flow_run_id, flow_run_info) in enumerate(mem_run_storage._flow_runs.items()):
+            self.assert_activate_flow_run_result(flow_run_info, expected_result[i])
+            node_run_infos = {
+                node_run_info.node: node_run_info
+                for node_run_info in mem_run_storage._node_runs.values()
+                if node_run_info.parent_run_id == flow_run_id
+            }
+            self.assert_activate_node_run_result(node_run_infos, expected_result[i])
 
         # Validate the flow status summary
-        status_summary = result.get_status_summary()
-        assert status_summary == expected_status_summary
+        assert batch_result.total_lines == batch_result.completed_lines
+        assert batch_result.node_status == expected_status_summary
 
-    def assert_activate_flow_run_result(self, result: LineResult, expected_result):
+    def assert_activate_flow_run_result(self, flow_run_info: FlowRunInfo, expected_result):
         # Validate the flow status
-        assert result.run_info.status == Status.Completed
+        assert flow_run_info.status == Status.Completed
 
         # Validate the flow output
-        assert isinstance(result.output, dict)
-        assert result.output == expected_result["expected_outputs"]
+        assert isinstance(flow_run_info.output, dict)
+        assert flow_run_info.output == expected_result["expected_outputs"]
 
+    def assert_activate_node_run_result(self, node_run_infos: Dict[str, NodeRunInfo], expected_result):
         # Validate the flow node run infos for the completed nodes
-        assert len(result.node_run_infos) == expected_result["expected_node_count"]
+        assert len(node_run_infos) == expected_result["expected_node_count"]
         expected_bypassed_nodes = expected_result["expected_bypassed_nodes"]
         completed_nodes_run_infos = [
-            run_info for i, run_info in result.node_run_infos.items() if i not in expected_bypassed_nodes
+            run_info for i, run_info in node_run_infos.items() if i not in expected_bypassed_nodes
         ]
         assert all([node.status == Status.Completed for node in completed_nodes_run_infos])
 
         # Validate the flow node run infos for the bypassed nodes
-        bypassed_nodes_run_infos = [result.node_run_infos[i] for i in expected_bypassed_nodes]
+        bypassed_nodes_run_infos = [node_run_infos[i] for i in expected_bypassed_nodes]
         assert all([node.status == Status.Bypassed for node in bypassed_nodes_run_infos])
         assert all([node.output is None for node in bypassed_nodes_run_infos])
