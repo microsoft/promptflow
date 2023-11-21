89de4d21bf35cf57a162a7bcc21d00be7cb63e53
zhen <henu_rz@163.com>
2023-11-21 13:31:27 +0800

    run operation

--- src/promptflow/promptflow/_sdk/_service/apis/run.py
+++ src/promptflow/promptflow/_sdk/_service/apis/run.py
@@ -1,19 +1,27 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
-
-from dataclasses import asdict
+import json
 
 from flask import jsonify, request
 from flask_restx import Namespace, Resource
 
-from promptflow._sdk._constants import FlowRunProperties, get_list_view_type
+from promptflow._sdk._constants import get_list_view_type
 from promptflow._sdk._errors import RunNotFoundError
 from promptflow._sdk.operations._local_storage_operations import LocalStorageOperations
 from promptflow._sdk.operations._run_operations import RunOperations
-from promptflow.contracts._run_management import RunMetadata
 
 api = Namespace("Runs", description="Runs Management")
+
+# Define get connection request parsing
+update_run_parser = api.parser()
+update_run_parser.add_argument("display_name", type=str, location="args", required=False)
+update_run_parser.add_argument("description", type=str, location="args", required=False)
+update_run_parser.add_argument("tags", type=str, location="args", required=False)
+update_run_parser.add_argument("display_name", type=str, location="args", required=False)
+
+# Response model of connection operation
+dict_field = api.schema_model("RunDict", {"additionalProperties": True, "type": "object"})
 
 
 @api.errorhandler(RunNotFoundError)
@@ -24,6 +32,7 @@ def handle_run_not_found_exception(error):
 
 @api.route("/")
 class RunList(Resource):
+    @api.marshal_with(dict_field, as_list=True)
     @api.doc(description="List all runs")
     def get(self):
         # parse query parameters
@@ -42,41 +51,54 @@ class RunList(Resource):
         return jsonify(runs_dict)
 
 
+@api.route("/submit")
+class RunSubmit(Resource):
+    @api.response(code=200, description="Submit run info", model=dict_field)
+    @api.doc(description="Submit run")
+    def post(self):
+        # TODO
+        pass
+
+
 @api.route("/<string:name>")
 class Run(Resource):
-    def get(self, name: str):
-        op = RunOperations()
-        run = op.get(name=name)
-        run_dict = run._to_dict()
-        return jsonify(run_dict)
+    @api.response(code=200, description="Update run info", model=dict_field)
+    @api.doc(parser=update_run_parser, description="Update run")
+    def put(self, name: str):
+        args = update_run_parser.parse_args()
+        run_op = RunOperations()
+        tags = json.loads(args.tags) if args.tags else None
+        run = run_op.update(name=name, display_name=args.display_name, description=args.description, tags=tags)
+        return jsonify(run._to_dict())
 
-
-@api.route("/<string:name>/metadata")
-class MetaData(Resource):
+    @api.response(code=200, description="Get run info", model=dict_field)
+    @api.doc(description="Get run")
     def get(self, name: str):
         run_op = RunOperations()
         run = run_op.get(name=name)
-        local_storage_op = LocalStorageOperations(run=run)
-        metadata = RunMetadata(
-            name=run.name,
-            display_name=run.display_name,
-            create_time=run.created_on,
-            flow_path=run.properties[FlowRunProperties.FLOW_PATH],
-            output_path=run.properties[FlowRunProperties.OUTPUT_PATH],
-            tags=run.tags,
-            lineage=run.run,
-            metrics=local_storage_op.load_metrics(),
-            dag=local_storage_op.load_dag_as_string(),
-            flow_tools_json=local_storage_op.load_flow_tools_json(),
-        )
-        return jsonify(asdict(metadata))
+        return jsonify(run._to_dict())
 
 
-@api.route("/<string:name>/detail")
-class Detail(Resource):
+@api.route("/<string:name>/childRuns")
+class FlowChildRuns(Resource):
+    @api.marshal_with(dict_field, as_list=True)
+    @api.doc(description="Get child runs")
     def get(self, name: str):
         run_op = RunOperations()
         run = run_op.get(name=name)
         local_storage_op = LocalStorageOperations(run=run)
         detail_dict = local_storage_op.load_detail()
-        return jsonify(detail_dict)
+        return jsonify(detail_dict["flow_runs"])
+
+
+@api.route("/<string:name>/nodeRuns/<string:node_name>")
+class FlowNodeRuns(Resource):
+    @api.marshal_with(dict_field, as_list=True)
+    @api.doc(description="Get node runs info")
+    def get(self, name: str, node_name: str):
+        run_op = RunOperations()
+        run = run_op.get(name=name)
+        local_storage_op = LocalStorageOperations(run=run)
+        detail_dict = local_storage_op.load_detail()
+        node_runs = [item for item in detail_dict["node_runs"] if item["node"] == node_name]
+        return jsonify(node_runs)
