66c5c072084f4578954682e53d5b619ca1f0354d
Zhengfei Wang <zhengfei.wang@microsoft.com>
2023-11-21 20:21:08 +0800

    add comment

--- src/promptflow/promptflow/_sdk/operations/_local_storage_operations.py
+++ src/promptflow/promptflow/_sdk/operations/_local_storage_operations.py
@@ -223,7 +223,11 @@ class LocalStorageOperations(AbstractRunStorage):
         shutil.copytree(
             flow.code.as_posix(),
             self._snapshot_folder_path,
-            ignore=shutil.ignore_patterns("__pycache__", self._run._output_path.parent.name),
+            ignore=shutil.ignore_patterns(
+                "__pycache__",
+                # ignore current output parent folder to avoid potential recursive copy
+                self._run._output_path.parent.name,
+            ),
             dirs_exist_ok=True,
         )
         # replace DAG file with the overwrite one
