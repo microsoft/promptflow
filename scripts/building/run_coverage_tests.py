8821d8910f1e9f368c3ee10b1c17736628edaaba
Philip Gao <yigao@microsoft.com>
2023-11-22 03:22:57 +0000

    Fix

--- scripts/building/run_coverage_tests.py
+++ scripts/building/run_coverage_tests.py
@@ -1,8 +1,9 @@
 import argparse
 import os
+import sys
 from pathlib import Path
 
-from utils import Color, run_command
+from utils import Color, run_command, print_red
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=Color.RED + "Test Coverage for Promptflow!" + Color.END + "\n")
@@ -15,6 +16,12 @@ if __name__ == "__main__":
         required=True,
         help="Pytest marker to identify the tests to run",
         default="all",
+    )
+    parser.add_argument(
+        "-o",
+        required=False,
+        help="Pytest output file name",
+        default="test-results.xml",
     )
     parser.add_argument("-n", help="Pytest number of process to run the tests", default="auto")
     parser.add_argument(
@@ -35,6 +42,12 @@ if __name__ == "__main__":
         action="store_true",
         help="Whether to enable branch coverage calculation",
     )
+    parser.add_argument(
+        "--ignore-glob",
+        help="The path of ignored test file",
+        type=str,
+        default="",
+    )
 
     args = parser.parse_args()
     print("Working directory: " + str(os.getcwd()))
@@ -43,9 +56,11 @@ if __name__ == "__main__":
     print("Args.l: " + str(args.l))
     print("Args.m: " + str(args.m))
     print("Args.n: " + str(args.n))
+    print("Args.o: " + str(args.o))
     print("Args.model-name: " + str(args.model_name))
     print("Args.timeout: " + str(args.timeout))
     print("Args.coverage-config: " + str(args.coverage_config))
+    print("Args.ignore-glob: " + str(args.ignore_glob))
     print("Args.disable-cov-branch: " + str(args.disable_cov_branch))
 
     test_paths_list = [str(Path(path).absolute()) for path in args.t]
@@ -68,6 +83,8 @@ if __name__ == "__main__":
             "--cov-report=xml",
         ]
         pytest_command = pytest_command + [f"--cov-config={args.coverage_config}"]
+    if args.ignore_glob:
+        pytest_command = pytest_command + [f"--ignore-glob={args.ignore_glob}"]
     pytest_command += [
         "-n",
         args.n,
@@ -96,4 +113,15 @@ if __name__ == "__main__":
         pytest_command = pytest_command + ["--model-name", args.model_name]
 
     # pytest --junit-xml=test-results.xml --cov=azure.ai.ml --cov-report=html --cov-report=xml -ra ./tests/*/unittests/
-    run_command(pytest_command)
+    error_code, _ = run_command(pytest_command, throw_on_retcode=False)
+    if error_code == 1:
+        print_red("Tests were collected and run but some of the tests failed.")
+    elif error_code == 2:
+        print_red("Test execution was interrupted by the user.")
+    elif error_code == 3:
+        print_red("Internal error happened while executing tests.")
+    elif error_code == 4:
+        print_red("pytest command line usage error.")
+    elif error_code == 5:
+        print_red("No tests were collected.")
+    sys.exit(error_code)
