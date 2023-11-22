faacec89e117df3d52f45152bac20b7bb634ae54
Brynn Yin <biyi@microsoft.com>
2023-11-22 19:42:18 +0800

    Refine
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- src/promptflow/promptflow/_sdk/_mlflow.py
+++ src/promptflow/promptflow/_sdk/_mlflow.py
@@ -1,8 +1,3 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-
 # flake8: noqa
 
 """Put some imports here for mlflow promptflow flavor usage."""
