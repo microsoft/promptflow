142557421397baf90c19f1213558ff18fc6cb841
Peiwen Gao <peiwengao@microsoft.com>
2023-11-24 12:14:06 +0800

    Fix bug

--- src/promptflow/promptflow/batch/_python_executor_proxy.py
+++ src/promptflow/promptflow/batch/_python_executor_proxy.py
@@ -22,6 +22,7 @@ class PythonExecutorProxy(AbstractExecutorProxy):
         *,
         connections: Optional[dict] = None,
         storage: Optional[AbstractRunStorage] = None,
+        **kwargs,
     ) -> "PythonExecutorProxy":
         # TODO: Raise error if connections is None
         flow_executor = FlowExecutor.create(flow_file, connections, working_dir, storage=storage)
