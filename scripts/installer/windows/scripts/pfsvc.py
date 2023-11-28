ea8856a44b233bc304709f1f4167eb2cc50209ee
zhen <henu_rz@163.com>
2023-11-28 19:19:37 +0800

    avoid restart compute

--- scripts/installer/windows/scripts/pfsvc.py
+++ scripts/installer/windows/scripts/pfsvc.py
@@ -48,6 +48,9 @@ def init():
         servicemanager.Initialize()
         servicemanager.PrepareToHostSingle(PromptFlowServiceFramework)
         servicemanager.StartServiceCtrlDispatcher()
+    elif len(sys.argv) == 2 and sys.argv[1] == "manual":
+        sys.argv = sys.argv[:1]
+        main()
     else:
         win32serviceutil.HandleCommandLine(PromptFlowServiceFramework)
 
