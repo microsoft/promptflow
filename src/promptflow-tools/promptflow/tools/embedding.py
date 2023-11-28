55ef7caade026494d4debaa3ef19995fd9f94779
chenslucky <75061414+chenslucky@users.noreply.github.com>
2023-11-28 10:24:07 +0800

    Support openai 1.x for promptflow-tools (#1128)
    # Description
    
    ### Background
    OpenAI has upgraded its python version to 1.x which includes breaking
    changes. All documentation has been updated with the new SDK on the
    OpenAI website
    https://platform.openai.com/docs/quickstart?context=python.
    
    Azure OpenAI also provides documentation on how to migrate to the OpenAI
    Python API library version 1.x
    https://learn.microsoft.com/en-us/azure/ai-services/openai/how-to/migration?tabs=python-new%2Cdalle-fix
    
    ### Upgrade Details for Prompt Flow Tools
    The promptflow-tools package will **explicitly require openai>=1.0.0**
    in the requirements. There are three important tasks to support the
    OpenAI Python API library version 1.x. This PR involves the first two
    tasks.
    - **Upgrade all tools that use the OpenAI API to support version 1.x**
    - **Error handling for OpenAI version 1.x**
    - Correct all documents that contain OpenAI API examples.
    
    ### Impact
    - If customers upgrade the promptflow-tools in a regular manner, the
    tool package upgrade will be transparent to them.
    - For some special cases, if after upgrading the promptflow-tools
    package, the OpenAI version in the environment gets downgraded for some
    reason (for instance, due to the installation of other tool packages
    that still use an old version of OpenAI), then using the built-in tool
    will throw the message "Please upgrade your OpenAI package to version
    1.0.0 or later using the command: pip install --upgrade openai". This
    message serves as a prompt for the user to upgrade the OpenAI version.
    
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.
    
    ---------
    
    Co-authored-by: cs_lucky <si.chen@microsoft.com>

--- src/promptflow-tools/promptflow/tools/embedding.py
+++ src/promptflow-tools/promptflow/tools/embedding.py
@@ -1,14 +1,14 @@
 from enum import Enum
 from typing import Union
 
-import openai
+from openai import AzureOpenAI as AzureOpenAIClient, OpenAI as OpenAIClient
+from promptflow.tools.common import handle_openai_error, normalize_connection_config
+from promptflow.tools.exception import InvalidConnectionType
 
-from promptflow.connections import AzureOpenAIConnection, OpenAIConnection
 # Avoid circular dependencies: Use import 'from promptflow._internal' instead of 'from promptflow'
 # since the code here is in promptflow namespace as well
 from promptflow._internal import tool
-from promptflow.tools.common import handle_openai_error
-from promptflow.tools.exception import InvalidConnectionType
+from promptflow.connections import AzureOpenAIConnection, OpenAIConnection
 
 
 class EmbeddingModel(str, Enum):
@@ -21,20 +21,19 @@ class EmbeddingModel(str, Enum):
 @handle_openai_error()
 def embedding(connection: Union[AzureOpenAIConnection, OpenAIConnection], input: str, deployment_name: str = "",
               model: EmbeddingModel = EmbeddingModel.TEXT_EMBEDDING_ADA_002):
-    connection_dict = dict(connection)
     if isinstance(connection, AzureOpenAIConnection):
-        return openai.Embedding.create(
+        client = AzureOpenAIClient(**normalize_connection_config(connection))
+        return client.embeddings.create(
             input=input,
-            engine=deployment_name,
-            headers={"ms-azure-ai-promptflow-called-from": "aoai-tool"},
-            **connection_dict,
-        )["data"][0]["embedding"]
+            model=deployment_name,
+            extra_headers={"ms-azure-ai-promptflow-called-from": "aoai-tool"}
+        ).data[0].embedding
     elif isinstance(connection, OpenAIConnection):
-        return openai.Embedding.create(
+        client = OpenAIClient(**normalize_connection_config(connection))
+        return client.embeddings.create(
             input=input,
-            model=model,
-            **connection_dict,
-        )["data"][0]["embedding"]
+            model=model
+        ).data[0].embedding
     else:
         error_message = f"Not Support connection type '{type(connection).__name__}' for embedding api. " \
                         f"Connection type should be in [AzureOpenAIConnection, OpenAIConnection]."
--- src/promptflow-tools/promptflow/tools/openai_gpt4v.py
+++ src/promptflow-tools/promptflow/tools/openai_gpt4v.py
@@ -1,35 +1,39 @@
-import openai
+try:
+    from openai import OpenAI as OpenAIClient
+except Exception:
+    raise Exception(
+        "Please upgrade your OpenAI package to version 1.0.0 or later using the command: pip install --upgrade openai.")
 
 from promptflow.connections import OpenAIConnection
 from promptflow.contracts.types import PromptTemplate
 from promptflow._internal import ToolProvider, tool
 from promptflow.tools.common import render_jinja_template, handle_openai_error, \
     parse_chat, post_process_chat_api_response, preprocess_template_string, \
-    find_referenced_image_set, convert_to_chat_list
+    find_referenced_image_set, convert_to_chat_list, normalize_connection_config
 
 
 class OpenAI(ToolProvider):
     def __init__(self, connection: OpenAIConnection):
         super().__init__()
-        self.connection = connection
-        self._connection_dict = dict(self.connection)
+        self._connection_dict = normalize_connection_config(connection)
+        self._client = OpenAIClient(**self._connection_dict)
 
     @tool(streaming_option_parameter="stream")
     @handle_openai_error()
     def chat(
-            self,
-            prompt: PromptTemplate,
-            model: str = "gpt-4-vision-preview",
-            temperature: float = 1.0,
-            top_p: float = 1.0,
-            n: int = 1,
-            # stream is a hidden to the end user, it is only supposed to be set by the executor.
-            stream: bool = False,
-            stop: list = None,
-            max_tokens: int = None,
-            presence_penalty: float = 0,
-            frequency_penalty: float = 0,
-            **kwargs,
+        self,
+        prompt: PromptTemplate,
+        model: str = "gpt-4-vision-preview",
+        temperature: float = 1.0,
+        top_p: float = 1.0,
+        n: int = 1,
+        # stream is a hidden to the end user, it is only supposed to be set by the executor.
+        stream: bool = False,
+        stop: list = None,
+        max_tokens: int = None,
+        presence_penalty: float = 0,
+        frequency_penalty: float = 0,
+        **kwargs,
     ) -> [str, dict]:
         # keep_trailing_newline=True is to keep the last \n in the prompt to avoid converting "user:\t\n" to "user:".
         prompt = preprocess_template_string(prompt)
@@ -56,5 +60,5 @@ class OpenAI(ToolProvider):
         if max_tokens:
             params["max_tokens"] = max_tokens
 
-        completion = openai.ChatCompletion.create(**{**self._connection_dict, **params})
+        completion = self._client.chat.completions.create(**params)
         return post_process_chat_api_response(completion, stream, None)
