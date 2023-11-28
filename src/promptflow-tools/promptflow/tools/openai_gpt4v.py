839eed5336e0d9397b5b8090e59302d9461478af
melionel <lmamsat@hotmail.com>
2023-11-28 17:29:44 +0800

    [Tools] add document for openai gpt4v tool (#1276)
    # Description
    
    - remove parameter 'n' from openai gpt4v tool
    - add reference document for openai gpt4v tool
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.
    
    ---------
    
    Co-authored-by: Meng Lan <mengla@microsoft.com>

--- src/promptflow-tools/promptflow/tools/openai_gpt4v.py
+++ src/promptflow-tools/promptflow/tools/openai_gpt4v.py
@@ -26,7 +26,6 @@ class OpenAI(ToolProvider):
         model: str = "gpt-4-vision-preview",
         temperature: float = 1.0,
         top_p: float = 1.0,
-        n: int = 1,
         # stream is a hidden to the end user, it is only supposed to be set by the executor.
         stream: bool = False,
         stop: list = None,
@@ -49,7 +48,7 @@ class OpenAI(ToolProvider):
             "messages": messages,
             "temperature": temperature,
             "top_p": top_p,
-            "n": n,
+            "n": 1,
             "stream": stream,
             "presence_penalty": presence_penalty,
             "frequency_penalty": frequency_penalty,
