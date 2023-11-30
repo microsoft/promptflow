4877bd30f1338f9654757570391e1e58d8019df2
Lina Tang <linatang@microsoft.com>
2023-11-30 20:10:09 +0800

    Refine openai metrics calculator to adapt openai v1

--- src/promptflow/tests/test_configs/flows/openai_chat_api_flow/chat.py
+++ src/promptflow/tests/test_configs/flows/openai_chat_api_flow/chat.py
@@ -5,6 +5,9 @@ from typing import List
 from promptflow import tool
 from promptflow.connections import AzureOpenAIConnection
 
+IS_LEGACY_OPENAI = OPENAI_VERSION.startswith("0.")
+
+
 def get_client(connection: AzureOpenAIConnection):
     api_key = connection.api_key
     conn = dict(
@@ -20,6 +23,7 @@ def get_client(connection: AzureOpenAIConnection):
         )
     return Client(**conn)
 
+
 def create_messages(question, chat_history):
     yield {"role": "system", "content": "You are a helpful assistant."}
     for chat in chat_history:
@@ -29,9 +33,8 @@ def create_messages(question, chat_history):
 
 
 @tool
-def chat(connection: AzureOpenAIConnection, question: str, chat_history: List) -> str:
-    stream = True
-    if OPENAI_VERSION.startswith("0."):
+def chat(connection: AzureOpenAIConnection, question: str, chat_history: List, stream: bool) -> str:
+    if IS_LEGACY_OPENAI:
         completion = openai.ChatCompletion.create(
             engine="gpt-35-turbo",
             messages=list(create_messages(question, chat_history)),
@@ -59,7 +62,10 @@ def chat(connection: AzureOpenAIConnection, question: str, chat_history: List) -
         def generator():
             for chunk in completion:
                 if chunk.choices:
-                    yield getattr(chunk.choices[0]["delta"], "content", "")
+                    if IS_LEGACY_OPENAI:
+                        yield getattr(chunk.choices[0]["delta"], "content", "")
+                    else:
+                        yield chunk.choices[0].delta.content or ""
 
         # We must return the generator object, not using yield directly here.
         # Otherwise, the function itself will become a generator, despite whether stream is True or False.
@@ -67,4 +73,7 @@ def chat(connection: AzureOpenAIConnection, question: str, chat_history: List) -
         return "".join(generator())
     else:
         # chat api may return message with no content.
-        return getattr(completion.choices[0].message, "content", "")
+        if IS_LEGACY_OPENAI:
+            return getattr(completion.choices[0].message, "content", "")
+        else:
+            return completion.choices[0].message.content or ""
