edba17f111d4c3a9078b1af362b92152b0275816
Han Wang <phoenix.seek@gmail.com>
2023-12-01 15:45:48 +0800

    add workspace info

--- src/promptflow/promptflow/_cli/_pf_azure/entry.py
+++ src/promptflow/promptflow/_cli/_pf_azure/entry.py
@@ -88,6 +88,18 @@ def get_parser_args(argv):
     return parser.prog, parser.parse_args(argv)
 
 
+def _get_workspace_info(args):
+    try:
+        return {
+            "subscription_id": args.subscription,
+            "resource_group_name": args.resource_group,
+            "workspace_name": args.workspace_name,
+        }
+    except Exception:
+        # fall back to empty dict if workspace info is not available
+        return {}
+
+
 def entry(argv):
     """
     Control plane CLI tools for promptflow cloud version.
@@ -96,7 +108,13 @@ def entry(argv):
     if hasattr(args, "user_agent"):
         setup_user_agent_to_operation_context(args.user_agent)
     logger = get_telemetry_logger()
-    with log_activity(logger, _get_cli_activity_name(cli=prog, args=args), activity_type=ActivityType.PUBLICAPI):
+    custom_dimensions = _get_workspace_info(args)
+    with log_activity(
+        logger,
+        _get_cli_activity_name(cli=prog, args=args),
+        activity_type=ActivityType.PUBLICAPI,
+        custom_dimensions=custom_dimensions,
+    ):
         run_command(args)
 
 
