a06f80a20b2310787732a419f852536d3136e12e
zhen <henu_rz@163.com>
2023-12-01 14:06:02 +0800

    fix msi

--- src/promptflow/promptflow/_sdk/_service/__init__.py
+++ src/promptflow/promptflow/_sdk/_service/__init__.py
@@ -3,3 +3,10 @@
 # ---------------------------------------------------------
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+
+try:
+    from flask_restx import Api, Namespace, Resource, fields  # noqa: F401
+except ImportError as ex:
+    from promptflow.exceptions import UserErrorException
+
+    raise UserErrorException(f"Please try 'pip install promptflow[pfs]' to install dependency, {ex.msg}.")
