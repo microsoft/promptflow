47446a30c5a17d355ec7b1669eda1a8c3a8cd227
minggu <migu@microsoft.com>
2023-12-03 09:57:01 +0800

    Remove image_pass_through flow

--- examples/flows/standard/image-pass-through/pick_an_image.py
+++
@@ -1,9 +0,0 @@
-import random
-
-from promptflow.contracts.multimedia import Image
-from promptflow import tool
-
-
-@tool
-def pick_an_image(image_1: Image, image_2: Image) -> Image:
-    return image_1
