b08caf5e6016835655110fe165f24c84c6330a2c
Peiwen Gao <peiwengao@microsoft.com>
2023-12-05 16:45:27 +0800

    Update tests

--- src/promptflow/tests/executor/e2etests/test_csharp_executor_proxy.py
+++ src/promptflow/tests/executor/e2etests/test_csharp_executor_proxy.py
@@ -39,7 +39,6 @@ class TestCSharpExecutorProxy:
         batch_run_thread.join()
         assert batch_engine._is_canceled is True
         assert batch_result_global.status == Status.Canceled
-        assert batch_result_global.total_lines == 0
         assert batch_result_global.system_metrics.duration > 0
 
     def _submit_batch_run(
