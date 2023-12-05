15332518751b4c62d5467e71deff5ef27133c527
Ying Chen <2601502859@qq.com>
2023-12-05 10:24:06 +0800

    flake8

--- scripts/installer/windows/scripts/main.py
+++ scripts/installer/windows/scripts/main.py
@@ -1,17 +1,21 @@
+import sys
+
 from promptflow._cli._pf.entry import main as pf_main
 from promptflow._cli._pf_azure.entry import main as pfazure_main
 from promptflow._sdk._service.entry import main as pfs_main
 
-import sys
 
 def command_pf():
     pf_main()
 
+
 def command_pfazure():
     pfazure_main()
 
+
 def command_pfs():
     pfs_main()
+
 
 if __name__ == "__main__":
     command = sys.argv[1] if len(sys.argv) > 1 else None
