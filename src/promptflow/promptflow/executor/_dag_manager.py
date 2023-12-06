98b37cee64d2e9e36949cdbbda8ca4792363fbdc
Peiwen Gao <peiwengao@microsoft.com>
2023-12-06 16:42:21 +0800

    Update log

--- src/promptflow/promptflow/executor/_dag_manager.py
+++ src/promptflow/promptflow/executor/_dag_manager.py
@@ -69,8 +69,8 @@ class DAGManager:
                 # If the parameter has no default value, the input will be set to None so that function will not fail.
                 else:
                     logger.warning(
-                        f"The node '{i.value}' referenced by the input '{name}' has been bypassed, "
-                        "and no default value is set. Will use 'None' as the value for this input."
+                        f"The node '{i.value}' referenced by the input '{name}' of the node '{node.name}' "
+                        "has been bypassed, and no default value is set. Will use 'None' as the value for this input."
                     )
                     results[name] = None
             else:
