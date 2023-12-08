33e2a9f15db49ceedd4c5a6c7d4134833a199076
chengbin wang <chw@microsoft.com>
2023-12-08 17:34:35 +0800

    fixing flow_with_langchain_traces

--- src/promptflow/tests/test_configs/flows/flow_with_langchain_traces/test_langchain_traces.py
+++ src/promptflow/tests/test_configs/flows/flow_with_langchain_traces/test_langchain_traces.py
@@ -1,13 +1,10 @@
 import os
 
-from langchain.agents.agent_types import AgentType
-from langchain.agents.initialize import initialize_agent
-from langchain.agents.load_tools import load_tools
-from langchain.llms.openai import AzureOpenAI
+from langchain.chat_models import AzureChatOpenAI
+from langchain_core.messages import HumanMessage, BaseMessage
 
 from promptflow import tool
 from promptflow.connections import AzureOpenAIConnection
-from promptflow.integrations.langchain import PromptFlowCallbackHandler
 
 
 @tool
@@ -16,15 +13,14 @@ def test_langchain_traces(question: str, conn: AzureOpenAIConnection):
     os.environ["OPENAI_API_VERSION"] = conn.api_version
     os.environ["AZURE_OPENAI_ENDPOINT"] = conn.api_base
 
-    llm = AzureOpenAI(
+    model = AzureChatOpenAI(
         temperature=0.7,
-        deployment_name="text-ada-001",
+        azure_deployment="gpt-35-turbo",
     )
-    tools = load_tools(["llm-math"], llm=llm)
-    agent = initialize_agent(
-        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=False,
-        callbacks = [PromptFlowCallbackHandler()])
-    try:
-        return agent.run(question)
-    except Exception as e:
-        return str(e)
+
+    message = HumanMessage(
+        content=question
+    )
+
+    res = model([message])
+    return res.content
