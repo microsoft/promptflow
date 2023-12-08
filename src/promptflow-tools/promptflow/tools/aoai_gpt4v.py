c9781fa63cb9add665be310a966442b17d5cbe47
Meng Lan <mengla@microsoft.com>
2023-12-08 15:47:07 +0800

    refine aoai gptv tool

--- src/promptflow-tools/promptflow/tools/aoai_gpt4v.py
+++ src/promptflow-tools/promptflow/tools/aoai_gpt4v.py
@@ -7,65 +7,13 @@ except Exception:
 from promptflow._internal import ToolProvider, tool
 from promptflow.connections import AzureOpenAIConnection
 from promptflow.contracts.types import PromptTemplate
-from promptflow._core.openai_injector import inject
 
 from promptflow.tools.common import render_jinja_template, handle_openai_error, parse_chat, \
     preprocess_template_string, find_referenced_image_set, convert_to_chat_list, normalize_connection_config, \
     post_process_chat_api_response
 
 
-KEY_BASED_AOAI_TYPE = "azure"
 TOKEN_BASED_AOAI_TYPE = "azure_ad"
-
-def chat_completion(**kwargs):
-    azure_endpoint = kwargs.pop("azure_endpoint", None)
-    api_version = kwargs.pop("api_version", None)
-    api_type = kwargs.pop("api_type", None)
-
-    if api_type == KEY_BASED_AOAI_TYPE:
-        api_key = kwargs.pop("api_key", None)
-        client = AzureOpenAIClient(azure_endpoint=azure_endpoint, api_version=api_version, api_key=api_key)
-    elif api_type == TOKEN_BASED_AOAI_TYPE:
-        token_provider = kwargs.pop("token_provider", None)
-        client = AzureOpenAIClient(
-            azure_endpoint=azure_endpoint,
-            api_version=api_version,
-            azure_ad_token_provider=token_provider)
-
-    deployment_name = kwargs.pop("deployment_name", None)
-
-    extra_headers = kwargs.pop("headers", None)
-    temperature = kwargs.pop("temperature", None)
-    top_p = kwargs.pop("top_p", None)
-    n = kwargs.pop("n", None)
-    stream = kwargs.pop("stream", None)
-    stop = kwargs.pop("stop", None)
-    max_tokens = kwargs.pop("max_tokens", None)
-    presence_penalty = kwargs.pop("presence_penalty", None)
-    frequency_penalty = kwargs.pop("frequency_penalty", None)
-    messages = kwargs.pop("messages", None)
-
-    data = {
-        "messages": messages,
-        "temperature": temperature,
-        "top_p": top_p,
-        "n": n,
-        "stream": stream,
-        "presence_penalty": presence_penalty,
-        "frequency_penalty": frequency_penalty,
-        "extra_headers": extra_headers
-    }
-
-    if stop:
-        data["stop"] = stop
-    if max_tokens and str(max_tokens).lower() != "inf":
-        data["max_tokens"] = int(max_tokens)
-
-    return client.chat.completions.create(model=deployment_name, **data)
-
-
-if not hasattr(chat_completion, "_original"):
-    chat_completion = inject(chat_completion)
 
 
 class AzureOpenAI(ToolProvider):
@@ -73,6 +21,20 @@ class AzureOpenAI(ToolProvider):
         super().__init__()
         self.connection = connection
         self._connection_dict = normalize_connection_config(self.connection)
+
+        azure_endpoint = self._connection_dict.get("azure_endpoint")
+        api_version = self._connection_dict.get("api_version")
+        api_key = self._connection_dict.get("api_key")
+        api_type = self._connection_dict.get("api_type")
+
+        if api_type == TOKEN_BASED_AOAI_TYPE:
+            token_provider = self.connection.get_token
+            self._client = AzureOpenAIClient(
+                azure_endpoint=azure_endpoint,
+                api_version=api_version,
+                azure_ad_token_provider=token_provider)
+        else:
+            self._client = AzureOpenAIClient(azure_endpoint=azure_endpoint, api_version=api_version, api_key=api_key)
 
     @tool(streaming_option_parameter="stream")
     @handle_openai_error()
@@ -99,31 +61,27 @@ class AzureOpenAI(ToolProvider):
         chat_str = render_jinja_template(prompt, trim_blocks=True, keep_trailing_newline=True, **converted_kwargs)
         messages = parse_chat(chat_str, list(referenced_images))
 
-        azure_endpoint = self._connection_dict.get("azure_endpoint")
-        api_version = self._connection_dict.get("api_version")
-        api_key = self._connection_dict.get("api_key")
-        api_type = self._connection_dict.get("api_type")
-
         headers = {
             "Content-Type": "application/json",
             "ms-azure-ai-promptflow-called-from": "aoai-gpt4v-tool"
         }
 
-        completion = chat_completion(
-            azure_endpoint=azure_endpoint,
-            api_version=api_version,
-            api_type=api_type,
-            api_key=api_key,
-            token_provider=self.connection.get_token,
-            deployment_name=deployment_name,
-            headers=headers,
-            stream=stream,
-            messages=messages,
-            temperature=temperature,
-            top_p=top_p,
-            stop=stop,
-            max_tokens=max_tokens,
-            presence_penalty=presence_penalty,
-            frequency_penalty=frequency_penalty)
+        params = {
+            "messages": messages,
+            "temperature": temperature,
+            "top_p": top_p,
+            "n": 1,
+            "stream": stream,
+            "presence_penalty": presence_penalty,
+            "frequency_penalty": frequency_penalty,
+            "extra_headers": headers,
+            "model": deployment_name,
+        }
 
+        if stop:
+            params["stop"] = stop
+        if max_tokens is not None:
+            params["max_tokens"] = max_tokens
+
+        completion = self._client.chat.completions.create(**params)
         return post_process_chat_api_response(completion, stream, None)
