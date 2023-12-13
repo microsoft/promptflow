a8122644853eceb6959cdec1b27d488fb11ae35f
Zhengfei Wang <38847871+zhengfeiwang@users.noreply.github.com>
2023-12-13 11:46:51 +0800

    [example][fundamental] Use extra text file to declare tutorial workflow monitor resources (#1446)
    # Description
    
    This PR targets to make workflow(s) for example(s) can be triggered
    correctly, so that contributor does not need to wait for unnecessary
    workflows not related to him/her.
    
    The expected listen paths should have:
    
    - working directory (where the notebook or markdown file locates)
    - declared resource(s)
    - corresponding workflow YAML file
    - `examples/requirements.txt` - trick that makes release verification
    process convenient
    - `examples/connections/azure_openai.yml` - fall back as it's the most
    basic and common connection
    
    The major change list contains:
    
    - `scripts/readme`: add logic to resolve resources from metadata
    - `examples/tutorials`: declare resources in metadata
    - `.github/workflows`: update workflows following new generation logic
    - `examples/CONTRIBUTING.md`: update example contributing doc
    
    Some side changes:
    
    - Fix wrong workflow listen path for additional include and symbol link
    flows, as they have dependency to flow web classification
    - Fix missing PEP 8 link in example contributing doc
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.

--- scripts/readme/ghactions_driver/readme_step.py
+++ scripts/readme/ghactions_driver/readme_step.py
@@ -288,14 +288,26 @@ class ReadmeStepsManage:
         schedule_hour = (name_hash // 60) % 4 + 19  # 19-22 UTC
 
         if "tutorials" in workflow_name:
-            path_filter = f"[ examples/**, .github/workflows/{workflow_name}.yml, '!examples/flows/integrations/**' ]"
+            # markdown filename has some exceptions, special handle here
+            if "chat_with_pdf" in workflow_name:
+                readme_name = "chat-with-pdf.md"
+            elif "fine_tuning_evaluation_promptflow_quality_improvement" in workflow_name:
+                readme_name = "promptflow-quality-improvement.md"
+            else:
+                readme_name = "README.md"
+            readme_path = Path(ReadmeStepsManage.git_base_dir()) / ReadmeSteps.working_dir / readme_name
+            # local import to avoid circular import
+            from .resource_resolver import resolve_tutorial_resource
+
+            path_filter = resolve_tutorial_resource(workflow_name, readme_path.resolve())
         else:
-            if "web_classification" in workflow_name:
+            if "flow_with_additional_includes" in workflow_name or "flow_with_symlinks" in workflow_name:
+                # these two flows have dependencies on flow web-classification
+                # so corresponding workflows should also listen to changes in web-classification
                 path_filter = (
                     f"[ {ReadmeSteps.working_dir}/**, "
                     + "examples/*requirements.txt, "
-                    + "examples/flows/standard/flow-with-additional-includes/**, "
-                    + "examples/flows/standard/flow-with-symlinks/** ,"
+                    + "examples/flows/standard/web-classification/**, "
                     + f".github/workflows/{workflow_name}.yml ]"
                 )
             else:
