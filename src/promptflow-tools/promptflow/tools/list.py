38a043c4ec804bb38705f637861142010bef90e9
cs_lucky <si.chen@microsoft.com>
2023-12-15 18:41:43 +0800

    support built-in tool uhint index

--- src/promptflow-tools/promptflow/tools/list.py
+++ src/promptflow-tools/promptflow/tools/list.py
@@ -10,6 +10,11 @@ def collect_tools_from_directory(base_dir) -> dict:
             tools_in_file = yaml.safe_load(f)
             for identifier, tool in tools_in_file.items():
                 tools[identifier] = tool
+                if "inputs" in tool:
+                    inputs_dict = tool["inputs"]
+                    inputs_order = list(inputs_dict.keys())
+                    for input, settings in inputs_dict.items():
+                        settings.setdefault("uhint", {}).setdefault("index", inputs_order.index(input))
     return tools
 
 
