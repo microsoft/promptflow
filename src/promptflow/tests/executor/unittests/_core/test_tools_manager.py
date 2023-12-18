5c9fd92d480ee4d49da27d1de72a6548f3ab7b4e
cs_lucky <si.chen@microsoft.com>
2023-12-18 13:04:37 +0800

    support prompt and python tool

--- src/promptflow/tests/executor/unittests/_core/test_tools_manager.py
+++ src/promptflow/tests/executor/unittests/_core/test_tools_manager.py
@@ -162,8 +162,8 @@ class TestToolsManager:
     def test_collect_package_tools_set_defaut_input_index(self):
         tool = "my_tool_package.tools.tool_with_cascading_inputs.my_tool"
         package_tools = collect_package_tools([tool])
-        assert package_tools[tool]['inputs']['user_type']['uhint']['index'] == 0
-        assert package_tools[tool]['inputs']['teacher_id']['uhint']['index'] == 2
+        assert package_tools[tool]['inputs']['user_type']['ui_hints']['index'] == 0
+        assert package_tools[tool]['inputs']['teacher_id']['ui_hints']['index'] == 2
 
     def test_collect_package_tools_and_connections(self, install_custom_tool_pkg):
         keys = ["my_tool_package.tools.my_tool_2.MyTool.my_tool"]
