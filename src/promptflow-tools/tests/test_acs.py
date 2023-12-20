b3483678eacbe79804b88a055577ac4c6ae2ba60
Yuhang Yu <yuhangyu@microsoft.com>
2023-12-20 22:20:49 +0800

    refine ut

--- src/promptflow-tools/tests/test_acs.py
+++ src/promptflow-tools/tests/test_acs.py
@@ -1,10 +1,14 @@
 import pytest
 
 from promptflow.tools.azure_content_safety import analyze_text
+
 @pytest.mark.usefixtures("use_secrets_config_file")
 class TestAzureContentSafety:
-    def test_azure_content_safety_analyze(self, azure_content_safety_connection):
+    def test_azure_content_safety_analyze_happy_path(self, azure_content_safety_connection):
+        text = "I hate you."
         result = analyze_text(
             connection=azure_content_safety_connection,
             text="This is test input",
         )
+        assert "suggested_action" in result
+        assert "action_by_category" in result
