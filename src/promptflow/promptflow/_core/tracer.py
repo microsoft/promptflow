4f2194dbde2620532189729d1278ac182b74610c
Zhidong Zhu <zhizhu@microsoft.com>
2023-12-21 19:05:45 +0800

    add TraceType.FUNCTION

--- src/promptflow/promptflow/_core/tracer.py
+++ src/promptflow/promptflow/_core/tracer.py
@@ -95,7 +95,7 @@ class Tracer(ThreadLocalSingleton):
         all_kwargs.pop("self", None)
         trace = Trace(
             name=f.__qualname__,
-            type=TraceType.FUNC,
+            type=TraceType.FUNCTION,
             start_time=datetime.utcnow().timestamp(),
             inputs=all_kwargs,
         )
