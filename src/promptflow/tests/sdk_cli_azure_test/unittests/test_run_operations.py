015387b2a11dc7ba5fead3505b0a6c2c5399f340
Honglin Du <0mza987@gmail.com>
2023-12-21 18:14:00 +0800

    update

--- src/promptflow/tests/sdk_cli_azure_test/unittests/test_run_operations.py
+++ src/promptflow/tests/sdk_cli_azure_test/unittests/test_run_operations.py
@@ -1,4 +1,4 @@
-from tempfile import TemporaryDirectory
+import tempfile
 from unittest.mock import patch
 
 import pytest
@@ -11,10 +11,10 @@ from promptflow.exceptions import UserErrorException
 class TestRunOperations:
     def test_download_run_with_invalid_workspace_datastore(self, pf: PFClient):
         # test download with invalid workspace datastore
-        with (
-            TemporaryDirectory() as tmp_dir,
-            patch.object(pf.runs, "_validate_for_run_download"),
-            patch.object(pf.runs, "_workspace_default_datastore", "test"),
-        ):
-            with pytest.raises(UserErrorException, match="workspace default datastore is not supported"):
-                pf.runs.download(run="fake_run_name", output=tmp_dir)
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            with (
+                patch.object(pf.runs, "_validate_for_run_download"),
+                patch.object(pf.runs, "_workspace_default_datastore", "test"),
+            ):
+                with pytest.raises(UserErrorException, match="workspace default datastore is not supported"):
+                    pf.runs.download(run="fake_run_name", output=tmp_dir)
