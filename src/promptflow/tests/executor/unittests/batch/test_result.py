c63f7fa1ced5d23e14ea6dcf874bc341e5443baf
Peiwen Gao <peiwengao@microsoft.com>
2023-12-23 20:01:30 +0800

    Update test cases

--- src/promptflow/tests/executor/unittests/batch/test_result.py
+++ src/promptflow/tests/executor/unittests/batch/test_result.py
@@ -9,72 +9,74 @@ from promptflow.contracts.run_info import Status
 from promptflow.executor._result import AggregationResult, LineResult
 
 
+def get_node_run_infos(node_dict: dict, index=None, api_calls=None):
+    return {
+        k: NodeRunInfo(
+            node=k,
+            flow_run_id="flow_run_id",
+            run_id=f"run_id_{k}",
+            status=v,
+            inputs=[],
+            output={},
+            metrics={},
+            error={"code": "UserError", "message": "test message"} if v == Status.Failed else None,
+            parent_run_id="",
+            start_time=None,
+            end_time=None,
+            index=index,
+            api_calls=api_calls,
+        )
+        for k, v in node_dict.items()
+    }
+
+
+def get_flow_run_info(status_dict: dict, index: int):
+    status = Status.Failed if any(status == Status.Failed for status in status_dict.values()) else Status.Completed
+    error = {"code": "UserError", "message": "test message"} if status == Status.Failed else None
+    return FlowRunInfo(
+        run_id="run_id",
+        status=status,
+        error=error,
+        inputs={},
+        output={},
+        metrics={},
+        request=None,
+        parent_run_id="",
+        root_run_id="",
+        source_run_id="",
+        flow_id="",
+        start_time=datetime.utcnow(),
+        end_time=datetime.utcnow(),
+        index=index,
+    )
+
+
+def get_line_results(line_dict: dict, api_calls=None):
+    return [
+        LineResult(
+            output={},
+            aggregation_inputs={},
+            run_info=get_flow_run_info(status_dict=v, index=k),
+            node_run_infos=get_node_run_infos(node_dict=v, index=k, api_calls=api_calls),
+        )
+        for k, v in line_dict.items()
+    ]
+
+
+def get_batch_result(line_dict, aggr_dict, line_api_calls=None, aggr_api_calls=None):
+    line_results = get_line_results(line_dict=line_dict, api_calls=line_api_calls)
+    aggr_result = AggregationResult(
+        output={}, metrics={}, node_run_infos=get_node_run_infos(node_dict=aggr_dict, api_calls=aggr_api_calls)
+    )
+    return BatchResult.create(datetime.utcnow(), datetime.utcnow(), line_results=line_results, aggr_result=aggr_result)
+
+
 def get_api_call(type, name, inputs={}, output={}, children=None):
     return {"type": type, "name": name, "inputs": inputs, "output": output, "children": children}
 
 
 @pytest.mark.unittest
 class TestBatchResult:
-    def get_node_run_infos(self, node_dict, index=None, api_calls=None):
-        return {
-            k: NodeRunInfo(
-                node=k,
-                flow_run_id="flow_run_id",
-                run_id=f"run_id_{k}",
-                status=v,
-                inputs=[],
-                output={},
-                metrics={},
-                error={"code": "UserError", "message": "test message"} if v == Status.Failed else None,
-                parent_run_id="",
-                start_time=None,
-                end_time=None,
-                index=index,
-                api_calls=api_calls,
-            )
-            for k, v in node_dict.items()
-        }
-
-    def get_flow_run_info(self, status_dict: dict, index: int):
-        status = Status.Failed if any(status == Status.Failed for status in status_dict.values()) else Status.Completed
-        error = {"code": "UserError", "message": "test message"} if status == Status.Failed else None
-        return FlowRunInfo(
-            run_id="run_id",
-            status=status,
-            error=error,
-            inputs={},
-            output={},
-            metrics={},
-            request=None,
-            parent_run_id="",
-            root_run_id="",
-            source_run_id="",
-            flow_id="",
-            start_time=datetime.utcnow(),
-            end_time=datetime.utcnow(),
-            index=index,
-        )
-
-    def get_line_results(self, line_dict, api_calls=None):
-        return [
-            LineResult(
-                output={},
-                aggregation_inputs={},
-                run_info=self.get_flow_run_info(status_dict=v, index=k),
-                node_run_infos=self.get_node_run_infos(node_dict=v, index=k, api_calls=api_calls),
-            )
-            for k, v in line_dict.items()
-        ]
-
-    def get_batch_result(self, line_dict, aggr_dict, line_api_calls=None, aggr_api_calls=None):
-        line_results = self.get_line_results(line_dict=line_dict, api_calls=line_api_calls)
-        aggr_result = AggregationResult(
-            output={}, metrics={}, node_run_infos=self.get_node_run_infos(node_dict=aggr_dict, api_calls=aggr_api_calls)
-        )
-        return BatchResult.create(
-            datetime.utcnow(), datetime.utcnow(), line_results=line_results, aggr_result=aggr_result
-        )
-
     def test_node_status(self):
         line_dict = {
             0: {"node_0": Status.Completed, "node_1": Status.Completed, "node_2": Status.Completed},
@@ -82,7 +84,7 @@ class TestBatchResult:
             2: {"node_0": Status.Completed, "node_1": Status.Completed, "node_2": Status.Bypassed},
         }
         aggr_dict = {"aggr_0": Status.Completed, "aggr_1": Status.Failed, "aggr_2": Status.Bypassed}
-        batch_result = self.get_batch_result(line_dict=line_dict, aggr_dict=aggr_dict)
+        batch_result = get_batch_result(line_dict=line_dict, aggr_dict=aggr_dict)
 
         assert batch_result.total_lines == 3
         assert batch_result.completed_lines == 2
@@ -144,13 +146,19 @@ class TestBatchResult:
             },
             output={"choices": [{"message": {"content": "content"}}]},
         )
-        batch_result = self.get_batch_result(
+        batch_result = get_batch_result(
             line_dict=line_dict, aggr_dict=aggr_dict, line_api_calls=[line_api_calls], aggr_api_calls=[aggr_api_call]
         )
         assert batch_result.system_metrics.total_tokens == 42
         assert batch_result.system_metrics.prompt_tokens == 38
         assert batch_result.system_metrics.completion_tokens == 4
         assert int(batch_result.system_metrics.duration) == 0
+        assert batch_result.system_metrics.to_dict() == {
+            "total_tokens": 42,
+            "prompt_tokens": 38,
+            "completion_tokens": 4,
+            "duration": 0.0,
+        }
 
     @pytest.mark.parametrize(
         "api_call",
@@ -165,7 +173,7 @@ class TestBatchResult:
     )
     def test_invalid_api_calls(self, api_call):
         line_dict = {0: {"node_0": Status.Completed}}
-        batch_result = self.get_batch_result(line_dict=line_dict, aggr_dict={}, line_api_calls=[api_call])
+        batch_result = get_batch_result(line_dict=line_dict, aggr_dict={}, line_api_calls=[api_call])
         assert batch_result.system_metrics.total_tokens == 0
         assert batch_result.system_metrics.completion_tokens == 0
         assert batch_result.system_metrics.prompt_tokens == 0
@@ -182,7 +190,7 @@ class TestBatchResult:
             "aggr_2": Status.Bypassed,
             "aggr_4": Status.Failed,
         }
-        batch_result = self.get_batch_result(line_dict=line_dict, aggr_dict=aggr_dict)
+        batch_result = get_batch_result(line_dict=line_dict, aggr_dict=aggr_dict)
         assert batch_result.total_lines == 3
         assert batch_result.failed_lines == 1
         assert batch_result.error_summary.failed_system_error_lines == 0
@@ -190,7 +198,20 @@ class TestBatchResult:
         assert batch_result.error_summary.error_list == [
             LineError(line_number=1, error={"code": "UserError", "message": "test message"}),
         ]
+        assert batch_result.error_summary.error_list[0].to_dict() == {
+            "line_number": 1,
+            "error": {
+                "code": "UserError",
+                "message": "test message",
+            },
+        }
         assert batch_result.error_summary.aggr_error_dict == {
             "aggr_1": {"code": "UserError", "message": "test message"},
             "aggr_4": {"code": "UserError", "message": "test message"},
         }
+
+
+@pytest.mark.unittest
+class TestErrorSummary:
+    def test_create(self):
+        pass
