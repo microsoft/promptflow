6d5f0ca259a9c13754d469c482c761fd366abb33
LuLu Zuo <luluzuo@microsoft.com>
2023-12-25 18:51:49 +0800

    Modify some tests

--- src/promptflow/tests/executor/unittests/processpool/test_healthy_ensured_process.py
+++ src/promptflow/tests/executor/unittests/processpool/test_healthy_ensured_process.py
@@ -1,82 +1,38 @@
 import pytest
 
-from multiprocessing import Queue
-from promptflow.executor._line_execution_process_pool import HealthyEnsuredProcess, get_multiprocessing_context
+from promptflow.executor._line_execution_process_pool import format_current_process
 
 from unittest.mock import patch
-import time
-
-
-def executor_creation_func(storage):
-    pass
-
-
-def executor_creation_func_timeout(storage):
-    time.sleep(60)
-    pass
-
-
-def end_process(healthy_ensured_process):
-    while healthy_ensured_process.process.is_alive():
-        healthy_ensured_process.end()
-        time.sleep(1)
-    return
 
 
 @pytest.mark.unittest
 class TestHealthyEnsuredProcess:
-
-    def test_healthy_ensured_process(self):
-        context = get_multiprocessing_context("spawn")
-        healthy_ensured_process = HealthyEnsuredProcess(executor_creation_func, context)
-        assert healthy_ensured_process.is_ready is False
-        task_queue = Queue()
-        healthy_ensured_process.start_new(task_queue)
-        assert healthy_ensured_process.process.is_alive()
-        assert healthy_ensured_process.is_ready is True
-        end_process(healthy_ensured_process)
-        assert healthy_ensured_process.process.is_alive() is False
-
-    def test_unhealthy_process(self):
-        context = get_multiprocessing_context("spawn")
-        healthy_ensured_process = HealthyEnsuredProcess(executor_creation_func_timeout, context)
-        assert healthy_ensured_process.is_ready is False
-        task_queue = Queue()
-        healthy_ensured_process.start_new(task_queue)
-        assert healthy_ensured_process.is_ready is False
-        end_process(healthy_ensured_process)
-        assert healthy_ensured_process.process.is_alive() is False
-
-    def test_format_current_process(self):
-        context = get_multiprocessing_context("spawn")
-        healthy_ensured_process = HealthyEnsuredProcess(executor_creation_func, context)
-        healthy_ensured_process.process = patch(
-            'promptflow.executor._line_execution_process_pool.Process', autospec=True)
-        healthy_ensured_process.process.name = "process_name"
-        healthy_ensured_process.process.pid = 123
+    @patch('promptflow.executor._line_execution_process_pool.bulk_logger.info', autospec=True)
+    def test_format_current_process(self, mock_logger_info):
+        process_name = "process_name"
+        process_pid = 123
         line_number = 13
-        formatted_message = healthy_ensured_process.format_current_process(line_number)
-        process_name = healthy_ensured_process.process.name
-        process_pid = healthy_ensured_process.process.pid
-        expected_log_message = (
+        formatted_message = format_current_process(process_name, process_pid, line_number)
+        exexpected_during_execution_log_message = (
+            f"Process name: {process_name}, Process id: {process_pid}, Line number: {line_number} start execution."
+        )
+        expected_returned_log_message = (
             f"Process name({process_name})-Process id({process_pid})-Line number({line_number})"
         )
-        assert formatted_message == expected_log_message
+        mock_logger_info.assert_called_once_with(exexpected_during_execution_log_message)
+        assert formatted_message == expected_returned_log_message
 
     @patch('promptflow.executor._line_execution_process_pool.bulk_logger.info', autospec=True)
     def test_format_completed_process(self, mock_logger_info):
-        context = get_multiprocessing_context("spawn")
-        healthy_ensured_process = HealthyEnsuredProcess(executor_creation_func, context)
-        healthy_ensured_process.process = patch(
-            'promptflow.executor._line_execution_process_pool.Process', autospec=True)
-        healthy_ensured_process.process.name = "process_name"
-        healthy_ensured_process.process.pid = 123
+        process_name = "process_name"
+        process_pid = 123
         line_number = 13
-        mock_logger_info.reset_mock()
-        healthy_ensured_process.format_current_process(line_number, True)
-        process_name = healthy_ensured_process.process.name
-        process_pid = healthy_ensured_process.process.pid
-        exexpected_log_message = (
+        formatted_message = format_current_process(process_name, process_pid, line_number, True)
+        exexpected_during_execution_log_message = (
             f"Process name: {process_name}, Process id: {process_pid}, Line number: {line_number} completed."
         )
-        mock_logger_info.assert_called_once_with(exexpected_log_message)
+        expected_returned_log_message = (
+            f"Process name({process_name})-Process id({process_pid})-Line number({line_number})"
+        )
+        mock_logger_info.assert_called_once_with(exexpected_during_execution_log_message)
+        assert formatted_message == expected_returned_log_message
