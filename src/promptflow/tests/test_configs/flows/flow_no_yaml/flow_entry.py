eb7e8dff3fecb547021e7ffc6064ca40116ac835
Zhidong Zhu <zhizhu@microsoft.com>
2023-12-25 17:37:08 +0800

    separate inject_trace

--- src/promptflow/tests/test_configs/flows/flow_no_yaml/flow_entry.py
+++ src/promptflow/tests/test_configs/flows/flow_no_yaml/flow_entry.py
@@ -1,4 +1,5 @@
 from promptflow import tool, trace
+from promptflow._core.tracer import TraceType
 import time
 import random
 
@@ -10,7 +11,7 @@ def sleep(seconds):
     if random.random() < 0.5:
         raise ValueError("Random error")
 
-@trace
+@trace()
 def passthrough_str_and_wait_sync(input1: str, wait_seconds=3) -> str:
     assert isinstance(input1, str), f"input1 should be a string, got {input1}"
     print("Wait for", wait_seconds, "seconds in sync function")
