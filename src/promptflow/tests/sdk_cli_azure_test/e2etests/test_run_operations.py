4459948b8ebc79fba81c61e56bc772ee879b78de
Zhengfei Wang <zhengfei.wang@microsoft.com>
2023-12-26 17:29:19 +0800

    add test for cloud

--- src/promptflow/tests/sdk_cli_azure_test/e2etests/test_run_operations.py
+++ src/promptflow/tests/sdk_cli_azure_test/e2etests/test_run_operations.py
@@ -902,3 +902,43 @@ class TestFlowRunRelatedToAMLToken:
                 column_mapping={"key": {"value": "1"}, "url": "${data.url}"},
                 runtime=runtime,
             )
+
+    def test_get_details_against_partial_completed_run(
+        self, pf: PFClient, runtime: str, randstr: Callable[[str], str]
+    ) -> None:
+        flow_mod2 = f"{FLOWS_DIR}/mod-n/two"
+        flow_mod3 = f"{FLOWS_DIR}/mod-n/three"
+        data_path = f"{DATAS_DIR}/numbers.jsonl"
+        # batch run against data
+        run1 = pf.run(
+            flow=flow_mod2,
+            data=data_path,
+            column_mapping={"number": "${data.value}"},
+            runtime=runtime,
+            name=randstr("run1"),
+        )
+        pf.runs.stream(run1)
+        details1 = pf.get_details(run1)
+        assert len(details1) == 20
+        assert len(details1.loc[details1["outputs.output"] != "(Failed)"]) == 10
+        # assert to ensure inputs and outputs are aligned
+        for _, row in details1.iterrows():
+            if str(row["outputs.output"]) != "(Failed)":
+                assert int(row["inputs.number"]) == int(row["outputs.output"])
+
+        # batch run against previous run
+        run2 = pf.run(
+            flow=flow_mod3,
+            run=run1,
+            column_mapping={"number": "${run.outputs.output}"},
+            runtime=runtime,
+            name=randstr("run2"),
+        )
+        pf.runs.stream(run2)
+        details2 = pf.get_details(run2)
+        assert len(details2) == 10
+        assert len(details2.loc[details2["outputs.output"] != "(Failed)"]) == 4
+        # assert to ensure inputs and outputs are aligned
+        for _, row in details2.iterrows():
+            if str(row["outputs.output"]) != "(Failed)":
+                assert int(row["inputs.number"]) == int(row["outputs.output"])
