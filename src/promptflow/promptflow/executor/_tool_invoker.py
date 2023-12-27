5b443134f9f1845775cbc8dee3d2a1f013d1b5b1
Lina Tang <linatang@microsoft.com>
2023-12-27 16:10:34 +0800

    Apply suggestions from qianlin

--- src/promptflow/promptflow/executor/_tool_invoker.py
+++ src/promptflow/promptflow/executor/_tool_invoker.py
@@ -2,14 +2,15 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-import docutils.nodes
 from dataclasses import dataclass
-from docutils.core import publish_doctree
 from functools import partial
-from typing import Callable
+from typing import Callable, Dict
 
 from promptflow._core.tool import ToolInvoker
 from promptflow.contracts.flow import InputAssignment, Node, ToolSource
+from promptflow.exceptions import ErrorTarget
+from promptflow.executor._docstring_parser import DocstringParser
+from promptflow.executor._errors import UnsupportedAssistantToolType
 from promptflow.executor._tool_resolver import ToolResolver
 
 
@@ -19,78 +20,73 @@ class DefaultToolInvoker(ToolInvoker):
 
 
 @dataclass
-class AssistantTool():
+class AssistantTool:
     name: str
-    description: dict
+    definition: dict
     func: Callable
 
 
-class AssistantToolInvoker():
+class AssistantToolInvoker:
     def __init__(self):
-        self._assistant_tools = {}
+        self._assistant_tools: Dict[str, AssistantTool] = {}
 
     def load_tools(self, tools: list):
-        tool_resolver = ToolResolver.active_instance()
         for tool in tools:
-            if tool["type"] != "function":
-                self._assistant_tools[tool["type"]] = AssistantTool(name=tool["type"], description=tool, func=None)
-                continue
-            inputs = tool.get("predefined_inputs", {})
-            updated_inputs = {}
-            for input_name, value in inputs.items():
-                updated_inputs[input_name] = InputAssignment.deserialize(value)
-            node = Node(
-                name="assistant_node",
-                tool="assistant_tool",
-                inputs=updated_inputs,
-                source=ToolSource.deserialize(tool["source"])
-            )
-            resolved_tool = tool_resolver._resolve_script_node(node, convert_input_types=True)
-            func_name = resolved_tool.definition.function
-            description = self._get_function_description(func_name, resolved_tool.definition.description, inputs.keys())
-            if resolved_tool.node.inputs:
-                inputs = {name: value.value for name, value in resolved_tool.node.inputs.items()}
-                func = partial(resolved_tool.callable, **inputs)
+            if tool["type"] in ("code_interpreter", "retrieval"):
+                self._assistant_tools[tool["type"]] = AssistantTool(name=tool["type"], definition=tool, func=None)
+            elif tool["type"] == "function":
+                function_tool = self._load_function_tool(tool)
+                self._assistant_tools[function_tool.name] = function_tool
             else:
-                func = resolved_tool.callable
-            self._assistant_tools[func_name] = AssistantTool(name=func_name, description=description, func=func)
+                raise UnsupportedAssistantToolType(
+                    message_format="Unsupported assistant tool type: {tool_type}",
+                    tool_type=tool["type"],
+                    target=ErrorTarget.EXECUTOR,
+                )
+
+    def _load_function_tool(self, tool: dict):
+        tool_resolver = ToolResolver.active_instance()
+        predefined_inputs = {}
+        for input_name, value in tool.get("predefined_inputs", {}).items():
+            predefined_inputs[input_name] = InputAssignment.deserialize(value)
+        node = Node(
+            name="assistant_node",
+            tool="assistant_tool",
+            inputs=predefined_inputs,
+            source=ToolSource.deserialize(tool["source"])
+        )
+        resolved_tool = tool_resolver._resolve_script_node(node, convert_input_types=True)
+        func_name = resolved_tool.definition.function
+        definition = self._generate_tool_definition(
+            func_name, resolved_tool.definition.description, predefined_inputs.keys()
+        )
+        if resolved_tool.node.inputs:
+            inputs = {name: value.value for name, value in resolved_tool.node.inputs.items()}
+            func = partial(resolved_tool.callable, **inputs)
+        else:
+            func = resolved_tool.callable
+        return AssistantTool(name=func_name, definition=definition, func=func)
 
     def invoke_tool(self, func_name, kwargs):
         return self._assistant_tools[func_name].func(**kwargs)
 
     def to_openai_tools(self):
-        return [tool.description for _, tool in self._assistant_tools.items()]
+        return [tool.definition for _, tool in self._assistant_tools.items()]
 
-    def _get_function_description(self, func_name: str, description: str, predefined_inputs: list) -> dict:
+    def _generate_tool_definition(self, func_name: str, description: str, predefined_inputs: list) -> dict:
         to_openai_type = {"str": "string", "int": "number"}
-
-        doctree = publish_doctree(description)
-        params = {}
-
-        for field in doctree.traverse(docutils.nodes.field):
-            field_name = field[0].astext()
-            field_body = field[1].astext()
-
-            if field_name.startswith("param"):
-                param_name = field_name.split(' ')[1]
-                if param_name in predefined_inputs:
-                    continue
-                if param_name not in params:
-                    params[param_name] = {}
-                params[param_name]["description"] = field_body
-            if field_name.startswith("type"):
-                param_name = field_name.split(' ')[1]
-                if param_name in predefined_inputs:
-                    continue
-                if param_name not in params:
-                    params[param_name] = {}
-                params[param_name]["type"] = to_openai_type[field_body] if field_body in to_openai_type else field_body
+        description, params = DocstringParser.parse(description)
+        for input in predefined_inputs:
+            if input in params:
+                params.pop(input)
+        for _, param in params.items():
+            param["type"] = to_openai_type[param["type"]] if param["type"] in to_openai_type else param["type"]
 
         return {
             "type": "function",
             "function": {
                 "name": func_name,
-                "description": doctree[0].astext(),
+                "description": description,
                 "parameters": {
                     "type": "object",
                     "properties": params,
