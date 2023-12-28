391badab9314aeabd48b27be7a114e3661eaeb4b
Zhidong Zhu <zhizhu@microsoft.com>
2023-12-28 18:57:13 +0800

    update unittest

--- src/promptflow/tests/executor/unittests/_core/test_tracer.py
+++ src/promptflow/tests/executor/unittests/_core/test_tracer.py
@@ -1,7 +1,7 @@
 import pytest
 
 from promptflow._core.generator_proxy import GeneratorProxy
-from promptflow._core.tracer import Tracer
+from promptflow._core.tracer import Tracer, _create_trace_from_function_call
 from promptflow.connections import AzureOpenAIConnection
 from promptflow.contracts.trace import Trace, TraceType
 
@@ -13,28 +13,6 @@ def generator():
 
 @pytest.mark.unittest
 class TestTracer:
-    def test_push_tool(self):
-        # Define a mock tool function
-        def mock_tool(a, b, c: AzureOpenAIConnection):
-            return a + b + str(c)
-
-        Tracer.start_tracing("test_run_id")
-        # Push the tool to the tracer with some argument
-        conn = AzureOpenAIConnection("test_key", "test_base")
-        trace = Tracer.push_tool(mock_tool, (1, 2), {"c": conn})
-
-        # Assert that the trace has the correct attributes
-        assert trace.name == mock_tool.__qualname__
-        assert trace.type == TraceType.TOOL
-        assert trace.inputs == {"a": 1, "b": 2, "c": "AzureOpenAIConnection"}
-        assert trace.output is None
-        assert trace.start_time is not None
-        assert trace.end_time is None
-        assert trace.children is None
-        assert trace.error is None
-
-        Tracer.end_tracing()
-
     def test_end_tracing(self):
         # Activate the tracer in the current context
         tracer = Tracer("test_run_id")
@@ -134,3 +112,69 @@ class TestTracer:
     @pytest.mark.parametrize("obj", [({"name": "Alice", "age": 25}), ([1, 2, 3]), (GeneratorProxy(generator())), (42)])
     def test_to_serializable(self, obj):
         assert Tracer.to_serializable(obj) == obj
+
+
+def func_with_no_parameters():
+    pass
+
+
+def func_with_args_and_kwargs(arg1, arg2=None, *, kwarg1=None, kwarg2=None):
+    _ = (arg1, arg2, kwarg1, kwarg2)
+
+
+def func_with_connection_parameter(a: int, conn: AzureOpenAIConnection):
+    _, _ = a, conn
+
+
+@pytest.mark.unittest
+class TestCreateTraceFromFunctionCall:
+    def test_basic_fields_are_filled_and_others_are_not(self):
+        trace = _create_trace_from_function_call(func_with_no_parameters)
+
+        # These fields should be filled in this method call.
+        assert trace.name == "func_with_no_parameters"
+        assert trace.type == TraceType.FUNCTION
+        assert trace.inputs == {}
+        # start_time should be a timestamp, which is a float value currently.
+        assert isinstance(trace.start_time, float)
+
+        # These should be left empty in this method call.
+        # They will be filled by the tracer later.
+        assert trace.output is None
+        assert trace.end_time is None
+        assert trace.children is None
+        assert trace.error is None
+
+    def test_trace_type_can_be_set_correctly(self):
+        trace = _create_trace_from_function_call(func_with_no_parameters, trace_type=TraceType.TOOL)
+        assert trace.type == TraceType.TOOL
+
+    def test_args_and_kwargs_are_filled_correctly(self):
+        trace = _create_trace_from_function_call(
+            func_with_args_and_kwargs, args=[1, 2], kwargs={"kwarg1": 3, "kwarg2": 4}
+        )
+        assert trace.inputs == {"arg1": 1, "arg2": 2, "kwarg1": 3, "kwarg2": 4}
+
+    def test_args_called_with_name_should_be_filled_correctly(self):
+        trace = _create_trace_from_function_call(func_with_args_and_kwargs, args=[1], kwargs={"arg2": 2, "kwarg2": 4})
+        assert trace.inputs == {"arg1": 1, "arg2": 2, "kwarg2": 4}
+
+    def test_kwargs_called_without_name_should_be_filled_correctly(self):
+        trace = _create_trace_from_function_call(func_with_args_and_kwargs, args=[1, 2, 3], kwargs={"kwarg2": 4})
+        assert trace.inputs == {"arg1": 1, "arg2": 2, "kwarg1": 3, "kwarg2": 4}
+
+    def test_empty_args_should_be_excluded_from_inputs(self):
+        trace = _create_trace_from_function_call(func_with_args_and_kwargs, args=[1])
+        assert trace.inputs == {"arg1": 1}
+
+    def test_empty_kwargs_should_be_excluded_from_inputs(self):
+        trace = _create_trace_from_function_call(func_with_args_and_kwargs, kwargs={"kwarg1": 1})
+        assert trace.inputs == {"kwarg1": 1}
+        trace = _create_trace_from_function_call(func_with_args_and_kwargs, kwargs={"kwarg2": 2})
+        assert trace.inputs == {"kwarg2": 2}
+
+    def test_args_and_kwargs_should_be_filled_in_called_order(self):
+        trace = _create_trace_from_function_call(
+            func_with_args_and_kwargs, args=[1, 2], kwargs={"kwarg2": 4, "kwarg1": 3}
+        )
+        assert list(trace.inputs.keys()) == ["arg1", "arg2", "kwarg2", "kwarg1"]
