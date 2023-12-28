adea8cfcb2d8b105378fb3efdeda967ce82c43a4
Lina Tang <linatang@microsoft.com>
2023-12-28 17:13:37 +0800

    Refine demo flow

--- src/promptflow/tests/test_configs/flows/assistant_tool/get_or_create_thread.py
+++ src/promptflow/tests/test_configs/flows/assistant_tool/get_or_create_thread.py
@@ -5,7 +5,9 @@ from promptflow.connections import OpenAIConnection
 
 
 @tool
-async def create_thread(conn: OpenAIConnection):
+async def get_or_create_thread(conn: OpenAIConnection, thread_id: str):
+    if thread_id:
+        return thread_id
     cli = AsyncOpenAI(api_key=conn.api_key, organization=conn.organization)
     thread = await cli.beta.threads.create()
     return thread.id
