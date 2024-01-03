9fbce0e4aae0a7d86f82f40320aeb2e9f12faa65
chengbin wang <chw@microsoft.com>
2024-01-03 16:01:21 +0800

    update sample

--- src/promptflow/tests/test_configs/flows/assistant_tool/get_calorie.py
+++ src/promptflow/tests/test_configs/flows/assistant_tool/get_calorie.py
@@ -5,6 +5,7 @@ import random
 @tool
 def get_calorie_by_jogging(duration: float, temperature: float):
     """Estimate the calories burned by jogging based on duration and temperature.
+    
     :param duration: the length of the jogging in hours.
     :type duration: float
     :param temperature: the environment temperature in degrees Celsius.
--- src/promptflow/tests/test_configs/flows/assistant_tool/get_temperature.py
+++ src/promptflow/tests/test_configs/flows/assistant_tool/get_temperature.py
@@ -5,6 +5,7 @@ from promptflow import tool
 @tool
 def get_temperature(location: str, unit: str = "c"):
     """Estimate the current temperature of a given location. The temperature is randomly generated.
+    
     :param location: Location to get the estimated temperature for.
     :type location: str
     :param unit: The unit of the temperature, either 'c' for Celsius or 'f' for Fahrenheit.
