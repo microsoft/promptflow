6eb8f64fd406b261ef323425d839cbe2d957a016
chengbin wang <chw@microsoft.com>
2024-01-03 16:05:34 +0800

    add flow sample

---
+++ examples/flows/standard/flow-with-assistant/get_calorie.py
@@ -0,0 +1,15 @@
+from promptflow import tool
+import random
+
+
+@tool
+def get_calorie_by_jogging(duration: float, temperature: float):
+    """Estimate the calories burned by jogging based on duration and temperature.
+    
+    :param duration: the length of the jogging in hours.
+    :type duration: float
+    :param temperature: the environment temperature in degrees Celsius.
+    :type temperature: float
+    """
+
+    return random.randint(50, 200)
---
+++ examples/flows/standard/flow-with-assistant/get_temperature.py
@@ -0,0 +1,16 @@
+import random
+from promptflow import tool
+
+
+@tool
+def get_temperature(location: str, unit: str = "c"):
+    """Estimate the current temperature of a given location. The temperature is randomly generated.
+    
+    :param location: Location to get the estimated temperature for.
+    :type location: str
+    :param unit: The unit of the temperature, either 'c' for Celsius or 'f' for Fahrenheit.
+                 Defaults to Celsius ('c').
+    :type unit: str
+    """
+
+    return random.uniform(0, 35)
