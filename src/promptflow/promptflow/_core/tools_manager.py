13ef4910d1c12fbc1d5169d731275fb3d7def55f
Peiwen Gao <peiwengao@microsoft.com>
2024-01-05 11:58:45 +0800

    Update code

--- src/promptflow/promptflow/_core/tools_manager.py
+++ src/promptflow/promptflow/_core/tools_manager.py
@@ -46,9 +46,9 @@ PACKAGE_TOOLS_ENTRY = "package_tools"
 
 def collect_tools_from_directory(base_dir) -> dict:
     tools = {}
+    yaml = YAML()
+    yaml.preserve_quotes = True
     for f in Path(base_dir).glob("**/*.yaml"):
-        yaml = YAML()
-        yaml.preserve_quotes = True
         with open(f, "r") as f:
             tools_in_file = yaml.load(f)
             for identifier, tool in tools_in_file.items():
