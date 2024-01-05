2331e1ffc9483d33012265fe97c71191b4d6ee0f
Peiwen Gao <111329184+PeiwenGaoMS@users.noreply.github.com>
2024-01-05 16:20:14 +0800

    [Internal][Executor] Create the executor proxy in batch_engine.run to ensure that the life cycle of it is in the run function (#1614)
    # Description
    
    Create the executor proxy in batch_engine.run and ensure that the life
    cycle of it is in the run function
    
    Main interface changes:
    
    * <a
    href="diffhunk://#diff-f3f345ba5efdfd8e3d9ad093ae9c2a0639925c41f9b90d5376db7dadf1943291R25-L30">`src/promptflow/tests/executor/e2etests/test_batch_timeout.py`</a>:
    Use `MockPythonExecutorProxy` to run python flow to test timeout.
    * <a
    href="diffhunk://#diff-ecf0905f2116abe08b5cf2931b856bf39aa8b38a30fadd9042538d076dbfde80R86-R93">`src/promptflow/promptflow/batch/_batch_engine.py`</a>:
    Create the executor proxy in batch_engine.run and ensure that the life
    cycle of it is in the run function.
    * <a
    href="diffhunk://#diff-44ac8eeb30a630bd24987e92f8bd5a180d57a3ad067db5d937561f21771e14c5L112-R112">`src/promptflow/promptflow/batch/_base_executor_proxy.py`</a>:
    Changed the API path to lowercase for making HTTP POST requests in the
    `BaseExecutorProxy` class.
    
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.

--- src/promptflow/promptflow/batch/_batch_engine.py
+++ src/promptflow/promptflow/batch/_batch_engine.py
@@ -91,27 +91,14 @@ class BatchEngine:
         :param kwargs: The keyword arguments related to creating the executor proxy class
         :type kwargs: Any
         """
+        self._flow_file = flow_file
         self._working_dir = Flow._resolve_working_dir(flow_file, working_dir)
         self._flow = Flow.from_yaml(flow_file, working_dir=self._working_dir)
         FlowValidator.ensure_flow_valid_in_batch_mode(self._flow)
 
-        executor_proxy_cls = self.executor_proxy_classes[self._flow.program_language]
-        with _change_working_dir(self._working_dir):
-            self._executor_proxy: AbstractExecutorProxy = async_run_allowing_running_loop(
-                executor_proxy_cls.create,
-                flow_file,
-                self._working_dir,
-                connections=connections,
-                storage=storage,
-                **kwargs,
-            )
-            # register signal handler for python flow in the main thread
-            # TODO: For all executor proxies that are executed locally, it might be necessary to
-            # register a signal for Ctrl+C in order to customize some actions beyond just killing
-            # the process, such as terminating the executor service.
-            if isinstance(self._executor_proxy, PythonExecutorProxy):
-                signal.signal(signal.SIGINT, signal_handler)
+        self._connections = connections
         self._storage = storage
+        self._kwargs = kwargs
         # set it to True when the batch run is canceled
         self._is_canceled = False
 
@@ -141,21 +128,40 @@ class BatchEngine:
         :return: The result of this batch run
         :rtype: ~promptflow.batch._result.BatchResult
         """
-
         try:
             self._start_time = datetime.utcnow()
-            # set batch input source from input mapping
-            OperationContext.get_instance().set_batch_input_source_from_inputs_mapping(inputs_mapping)
-            # resolve input data from input dirs and apply inputs mapping
-            batch_input_processor = BatchInputsProcessor(self._working_dir, self._flow.inputs, max_lines_count)
-            batch_inputs = batch_input_processor.process_batch_inputs(input_dirs, inputs_mapping)
-            # resolve output dir
-            output_dir = resolve_dir_to_absolute(self._working_dir, output_dir)
-            # run flow in batch mode
             with _change_working_dir(self._working_dir):
-                return async_run_allowing_running_loop(
-                    self._exec_in_task, batch_inputs, run_id, output_dir, raise_on_line_failure
+                # create executor proxy instance according to the flow program language
+                executor_proxy_cls = self.executor_proxy_classes[self._flow.program_language]
+                self._executor_proxy: AbstractExecutorProxy = async_run_allowing_running_loop(
+                    executor_proxy_cls.create,
+                    self._flow_file,
+                    self._working_dir,
+                    connections=self._connections,
+                    storage=self._storage,
+                    **self._kwargs,
                 )
+                try:
+                    # register signal handler for python flow in the main thread
+                    # TODO: For all executor proxies that are executed locally, it might be necessary to
+                    # register a signal for Ctrl+C in order to customize some actions beyond just killing
+                    # the process, such as terminating the executor service.
+                    if isinstance(self._executor_proxy, PythonExecutorProxy):
+                        signal.signal(signal.SIGINT, signal_handler)
+
+                    # set batch input source from input mapping
+                    OperationContext.get_instance().set_batch_input_source_from_inputs_mapping(inputs_mapping)
+                    # resolve input data from input dirs and apply inputs mapping
+                    batch_input_processor = BatchInputsProcessor(self._working_dir, self._flow.inputs, max_lines_count)
+                    batch_inputs = batch_input_processor.process_batch_inputs(input_dirs, inputs_mapping)
+                    # resolve output dir
+                    output_dir = resolve_dir_to_absolute(self._working_dir, output_dir)
+                    # run flow in batch mode
+                    return async_run_allowing_running_loop(
+                        self._exec_in_task, batch_inputs, run_id, output_dir, raise_on_line_failure
+                    )
+                finally:
+                    async_run_allowing_running_loop(self._executor_proxy.destroy)
         except Exception as e:
             bulk_logger.error(f"Error occurred while executing batch run. Exception: {str(e)}")
             if isinstance(e, PromptflowException):
@@ -170,8 +176,6 @@ class BatchEngine:
                     error_type_and_message=f"({e.__class__.__name__}) {e}",
                 )
                 raise unexpected_error from e
-        finally:
-            async_run_allowing_running_loop(self._executor_proxy.destroy)
 
     def cancel(self):
         """Cancel the batch run"""
@@ -212,6 +216,7 @@ class BatchEngine:
         output_dir: Path = None,
         raise_on_line_failure: bool = False,
     ) -> BatchResult:
+        # ensure executor health before execution
         await self._executor_proxy.ensure_executor_health()
         # apply default value in early stage, so we can use it both in line and aggregation nodes execution.
         batch_inputs = [
