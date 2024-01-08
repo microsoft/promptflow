1c9f3e72ce8ad675b0c79facd634e5c72e9522a8
LuLu Zuo <luluzuo@microsoft.com>
2024-01-08 19:16:28 +0800

    Define class 'SpawnedForkProcessManager'

--- src/promptflow/promptflow/executor/_process_manager.py
+++ src/promptflow/promptflow/executor/_process_manager.py
@@ -5,6 +5,7 @@ import queue
 import signal
 from functools import partial
 from dataclasses import dataclass
+from enum import Enum
 import psutil
 from promptflow._core.operation_context import OperationContext
 from promptflow._utils.logger_utils import LogContext, bulk_logger
@@ -15,6 +16,12 @@ from promptflow.executor.flow_executor import FlowExecutor
 class ProcessInfo:
     process_id: str = None
     process_name: str = None
+
+
+class ProcessControlSignal(str, Enum):
+    START = "start"
+    RESTART = "restart"
+    END = "end"
 
 
 class AbstractProcessManager:
@@ -148,7 +155,8 @@ class SpawnProcessManager(AbstractProcessManager):
 
 class ForkProcessManager(AbstractProcessManager):
     '''
-    ForkProcessManager extends AbstractProcessManager to manage processes using the 'fork' method.
+    ForkProcessManager extends AbstractProcessManager to manage processes using the 'fork' method
+    in a spawned process.
 
     :param control_signal_queue: A queue for controlling signals to manage process operations.
     :type control_signal_queue: multiprocessing.Queue
@@ -211,7 +219,7 @@ class ForkProcessManager(AbstractProcessManager):
         :param i: Index of the process to restart.
         :type i: int
         """
-        self._control_signal_queue.put(('restart', i))
+        self._control_signal_queue.put((ProcessControlSignal.RESTART, i))
 
     def end_process(self, i):
         """
@@ -220,7 +228,7 @@ class ForkProcessManager(AbstractProcessManager):
         :param i: Index of the process to terminate.
         :type i: int
         """
-        self._control_signal_queue.put(('end', i))
+        self._control_signal_queue.put((ProcessControlSignal.END, i))
 
     def new_process(self, i):
         """
@@ -229,7 +237,105 @@ class ForkProcessManager(AbstractProcessManager):
         :param i: Index of the new process to start.
         :type i: int
         """
-        self._control_signal_queue.put(('start', i))
+        self._control_signal_queue.put((ProcessControlSignal.START, i))
+
+
+class SpawnedForkProcessManager(AbstractProcessManager):
+    '''
+    SpawnedForkProcessManager extends AbstractProcessManager to manage processes using 'fork' method
+    in a spawned process.
+
+    :param control_signal_queue: A queue for controlling signals to manage process operations.
+    :type control_signal_queue: multiprocessing.Queue
+
+    :param executor_creation_func: Function to create an executor for each process.
+    :type executor_creation_func: Callable
+
+    :param args: Additional positional arguments for the AbstractProcessManager.
+    :param kwargs: Additional keyword arguments for the AbstractProcessManager.
+    '''
+
+    def __init__(
+            self,
+            log_context_initialization_func,
+            current_operation_context,
+            control_signal_queue,
+            executor_creation_func,
+            *args,
+            **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._log_context_initialization_func = log_context_initialization_func
+        self._current_operation_context = current_operation_context
+        self._control_signal_queue = control_signal_queue
+        self._executor_creation_func = executor_creation_func
+        self.context = multiprocessing.get_context("fork")
+
+    def new_process(self, i):
+        """
+        Create and start a new process using the 'fork' context.
+
+        :param i: Index of the input and output queue for the new process.
+        :type i: int
+        """
+        process = self.context.Process(
+            target=self._process_target_func,
+            args=(
+                self._executor_creation_func,
+                self._input_queues[i],
+                self._output_queues[i],
+                self._log_context_initialization_func,
+                self._current_operation_context
+            ),
+            daemon=True
+        )
+        process.start()
+        self._process_info[i] = ProcessInfo(process_id=process.pid, process_name=process.name)
+        return process
+
+    def end_process(self, i):
+        """
+        Terminates a specified process.
+
+        :param i: Index of the process to terminate.
+        :type i: int
+        """
+        try:
+            pid = self._process_info[i].process_id
+            process = psutil.Process(pid)
+            process.terminate()
+            process.wait()
+            self._process_info.pop(i)
+        except psutil.NoSuchProcess:
+            bulk_logger.warning(f"Process {pid} had been terminated")
+
+    def restart_process(self, i):
+        """
+        Restarts a specified process by first terminating it then creating a new one.
+
+        :param i: Index of the process to restart.
+        :type i: int
+        """
+        self.end_process(i)
+        self.new_process(i)
+
+    def handle_signals(self, control_signal, i):
+        """
+        Handles control signals for processes, performing actions such as starting, ending,
+        or restarting them based on the received signal.
+
+        :param control_signal: The control signal indicating the desired action. It can be 'start', 'end', or 'restart'.
+        :type control_signal: str
+
+        :param i: Index of the process to control.
+        :type i: int
+        """
+        if control_signal == ProcessControlSignal.END:
+            self.end_process(i)
+        elif control_signal == ProcessControlSignal.RESTART:
+            self.restart_process(i)
+        elif control_signal == ProcessControlSignal.START:
+            self.new_process(i)
 
 
 def fork_processes_manager(
@@ -252,9 +358,6 @@ def fork_processes_manager(
     from promptflow.executor._line_execution_process_pool import signal_handler, create_executor_fork
     signal.signal(signal.SIGINT, signal_handler)
 
-    # Set the multiprocessing context to 'fork'.
-    context = multiprocessing.get_context("fork")
-
     # Create flow executor.
     executor = FlowExecutor.create(
         flow_file=flow_file,
@@ -265,47 +368,21 @@ def fork_processes_manager(
 
     executor_creation_func = partial(create_executor_fork, flow_executor=executor)
 
-    # Function to create and start a new process.
-    def new_process(i):
-        process = context.Process(
-            target=process_target_func,
-            args=(
-                executor_creation_func,
-                input_queues[i],
-                output_queues[i],
-                log_context_initialization_func,
-                current_operation_context
-            ),
-            daemon=True
-        )
-        process.start()
-        process_info[i] = ProcessInfo(process_id=process.pid, process_name=process.name)
-        return process
+    manager = SpawnedForkProcessManager(
+        log_context_initialization_func,
+        current_operation_context,
+        control_signal_queue,
+        executor_creation_func,
+        input_queues,
+        output_queues,
+        process_info,
+        process_target_func,
+        raise_ex,
+    )
 
     # Initialize processes.
     for i in range(len(input_queues)):
-        new_process(i)
-
-    # Function to terminate a process.
-    def end_process(i):
-        try:
-            pid = process_info[i].process_id
-            process = psutil.Process(pid)
-            process.terminate()
-            process.wait()
-            process_info.pop(i)
-        except psutil.NoSuchProcess:
-            bulk_logger.warning(f"Process {pid} had been terminated")
-
-    # Function to handle control signals for processes.
-    def handle_signals(control_signal, i):
-        if control_signal == "end":
-            end_process(i)
-        elif control_signal == "restart":
-            end_process(i)
-            new_process(i)
-        elif control_signal == 'start':
-            new_process(i)
+        manager.new_process(i)
 
     # Main loop to handle control signals and manage process lifecycle.
     while True:
@@ -328,7 +405,7 @@ def fork_processes_manager(
             break
         try:
             control_signal, i = control_signal_queue.get(timeout=1)
-            handle_signals(control_signal, i)
+            manager.handle_signals(control_signal, i)
         except queue.Empty:
             # Do nothing until the process_queue have not content or process is killed
             pass
