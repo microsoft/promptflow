0c822965dc1212015c178b842bfccf6fc24c9ff1
Zhidong Zhu <zhizhu@microsoft.com>
2024-01-08 17:27:03 +0800

    update trace_test

--- examples/trace_test.py
+++ examples/trace_test.py
@@ -5,52 +5,54 @@ from concurrent.futures import ThreadPoolExecutor
 
 from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
 from opentelemetry import trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.exporter.zipkin.json import ZipkinExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-jaeger_exporter = JaegerExporter(
-    agent_host_name="localhost",
-    agent_port=6831,
-)
+# from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+# from opentelemetry.exporter.zipkin.json import ZipkinExporter
+# jaeger_exporter = JaegerExporter(
+#    agent_host_name="localhost",
+#    agent_port=6831,
+# )
 # Service name is required for most backends,
 # and although it's not necessary for console export,
 # it's good to set service name anyways.
 resource = Resource(attributes={SERVICE_NAME: "your-service-name"})
 
 traceProvider = TracerProvider(resource=resource)
+trace.set_tracer_provider(traceProvider)
 # traceProvider = get_tracer_provider()
 # processor = BatchSpanProcessor(ConsoleSpanExporter())
 # traceProvider.add_span_processor(processor)
 connection_string = os.environ.get("APPINSIGHTS_CONNECTION_STRING")
-if not connection_string:
-    raise ValueError("No connection string provided")
-processor = BatchSpanProcessor(AzureMonitorTraceExporter(connection_string=connection_string))
-traceProvider.add_span_processor(processor)
-processor = BatchSpanProcessor(jaeger_exporter)
-traceProvider.add_span_processor(processor)
-trace.set_tracer_provider(traceProvider)
+if connection_string:
+    # raise ValueError("No connection string provided")
+    processor = BatchSpanProcessor(AzureMonitorTraceExporter(connection_string=connection_string))
+    traceProvider.add_span_processor(processor)
+# processor = BatchSpanProcessor(jaeger_exporter)
+# traceProvider.add_span_processor(processor)
 
-zipkin_exporter = ZipkinExporter(
-    # Optional: configure the endpoint
-    # endpoint="http://localhost:9411/api/v2/spans",
-    # Optional: configure the service name
-    # service_name="my-service",
-)
+# zipkin_exporter = ZipkinExporter(
+# Optional: configure the endpoint
+# endpoint="http://localhost:9411/api/v2/spans",
+# Optional: configure the service name
+# service_name="my-service",
+# )
 
-tracer_provider = trace.get_tracer_provider()
-tracer_provider.add_span_processor(BatchSpanProcessor(zipkin_exporter))
+# tracer_provider = trace.get_tracer_provider()
+# tracer_provider.add_span_processor(BatchSpanProcessor(zipkin_exporter))
 
 # reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
 # meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
 # metrics.set_meter_provider(meterProvider)
 tracer = trace.get_tracer(__name__)
 
+tracer2 = trace.get_tracer("tracer2")
+
 
 async def wait_task(name: str, wait_seconds: int = 1):
-    with tracer.start_as_current_span(name) as span:
+    with tracer2.start_as_current_span(name) as span:
         span.set_attribute("wait_seconds", wait_seconds)
         await asyncio.sleep(wait_seconds)
         print(f"Task {name} finished")
@@ -64,9 +66,10 @@ async def main():
         span.add_event("Start Await", {"name": "value", "my_key": "my_value"})
         await task2
         await task1
-        await wait_task("task3", 3)
+        await wait_task("task3", 1)
         span.add_event("End Await", {"name": "value", "my_key": "my_value"})
 
+    tracer.get_current_span()
     first_span_context = span.get_span_context()
     link_to_first_span = trace.Link(first_span_context)
 
@@ -103,3 +106,5 @@ def sync_main():
 if __name__ == "__main__":
     asyncio.run(main())
     # sync_main()
+    # with tracer2.start_as_current_span("test"):
+    #    print("yes")
