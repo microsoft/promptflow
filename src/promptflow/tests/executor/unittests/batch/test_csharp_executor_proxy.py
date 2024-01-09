558c250af3b71a6fa401040057659e681e8deb76
Peiwen Gao <peiwengao@microsoft.com>
2024-01-09 17:03:17 +0800

    Update test cases

--- src/promptflow/tests/executor/unittests/batch/test_csharp_executor_proxy.py
+++ src/promptflow/tests/executor/unittests/batch/test_csharp_executor_proxy.py
@@ -1,4 +1,5 @@
 import socket
+import subprocess
 from unittest.mock import MagicMock, patch
 
 import pytest
@@ -8,22 +9,66 @@ from promptflow.batch import CSharpExecutorProxy
 from ...utils import get_flow_folder, get_yaml_file
 
 
+async def get_executor_proxy():
+    flow_file = get_yaml_file("csharp_flow")
+    working_dir = get_flow_folder("csharp_flow")
+    with patch.object(CSharpExecutorProxy, "ensure_executor_startup", return_value=None):
+        return await CSharpExecutorProxy.create(flow_file, working_dir)
+
+
 @pytest.mark.unittest
 class TestCSharpExecutorProxy:
     @pytest.mark.asyncio
     async def test_create(self):
-        flow_file = get_yaml_file("csharp_flow")
-        working_dir = get_flow_folder("csharp_flow")
-
         with patch("subprocess.Popen") as mock_popen:
             mock_popen.return_value = MagicMock()
-            with patch.object(CSharpExecutorProxy, "ensure_executor_startup", return_value=None):
-                executor_proxy = await CSharpExecutorProxy.create(flow_file, working_dir)
+            executor_proxy = await get_executor_proxy()
             mock_popen.assert_called_once()
             assert executor_proxy is not None
             assert executor_proxy._process is not None
             assert executor_proxy._port is not None
             assert executor_proxy.api_endpoint == f"http://localhost:{executor_proxy._port}"
+
+    @pytest.mark.asyncio
+    async def test_destroy_with_already_terminated(self):
+        mock_process = MagicMock()
+        mock_process.poll.return_value = 0
+        executor_proxy = await get_executor_proxy()
+        executor_proxy._process = mock_process
+
+        await executor_proxy.destroy()
+
+        mock_process.poll.assert_called_once()
+        mock_process.terminate.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_destroy_with_terminates_gracefully(self):
+        mock_process = MagicMock()
+        mock_process.poll.return_value = None
+        executor_proxy = await get_executor_proxy()
+        executor_proxy._process = mock_process
+
+        await executor_proxy.destroy()
+
+        mock_process.poll.assert_called_once()
+        mock_process.terminate.assert_called_once()
+        mock_process.wait.assert_called_once_with(timeout=5)
+        mock_process.kill.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_destroy_with_force_kill(self):
+        mock_process = MagicMock()
+        mock_process.poll.return_value = None
+        mock_process.wait.side_effect = subprocess.TimeoutExpired(cmd="cmd", timeout=5)
+        executor_proxy = await get_executor_proxy()
+        executor_proxy._process = mock_process
+
+        await executor_proxy.destroy()
+
+        mock_process.poll.assert_called_once()
+        mock_process.terminate.assert_called_once()
+        mock_process.wait.assert_called_once_with(timeout=5)
+        mock_process.kill.assert_called_once()
 
     def test_find_available_port(self):
         port = CSharpExecutorProxy.find_available_port()
