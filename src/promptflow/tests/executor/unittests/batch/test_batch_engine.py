e9cace553c26ab6e30eeec59700b9e0e29d56e87
Peiwen Gao <peiwengao@microsoft.com>
2024-01-10 11:36:39 +0800

    update test cases

--- src/promptflow/tests/executor/unittests/batch/test_batch_engine.py
+++ src/promptflow/tests/executor/unittests/batch/test_batch_engine.py
@@ -1,28 +1,71 @@
 from pathlib import Path
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 import pytest
 
 from promptflow._core._errors import UnexpectedError
-from promptflow.batch import BatchEngine
+from promptflow.batch import (
+    AbstractExecutorProxy,
+    APIBasedExecutorProxy,
+    BatchEngine,
+    CSharpExecutorProxy,
+    PythonExecutorProxy,
+)
 from promptflow.exceptions import ErrorTarget
+from promptflow.executor._errors import ConnectionNotFound
 
 from ...utils import get_yaml_file
 
 
 @pytest.mark.unittest
 class TestBatchEngine:
-    def test_batch_engine_error(self):
-        with pytest.raises(UnexpectedError) as e:
+    @pytest.mark.parametrize(
+        "side_effect, ex_type, ex_target, ex_codes, ex_msg",
+        [
+            (
+                Exception("test error"),
+                UnexpectedError,
+                ErrorTarget.BATCH,
+                ["SystemError", "UnexpectedError"],
+                "Unexpected error occurred while executing the batch run. Error: (Exception) test error.",
+            ),
+            (
+                ConnectionNotFound(messgae="Connection 'aoai_conn' not found"),
+                ConnectionNotFound,
+                ErrorTarget.EXECUTOR,
+                ["UserError", "ValidationError", "InvalidRequest", "ConnectionNotFound"],
+                "Connection 'aoai_conn' not found",
+            ),
+        ],
+    )
+    def test_batch_engine_run_error(self, side_effect, ex_type, ex_target, ex_codes, ex_msg):
+        batch_engine = BatchEngine(get_yaml_file("print_input_flow"))
+        with patch("promptflow.batch._batch_engine.BatchEngine._exec_in_task") as mock_func:
+            mock_func.side_effect = side_effect
             with patch(
-                "promptflow.batch._batch_inputs_processor.BatchInputsProcessor.process_batch_inputs"
-            ) as mock_func:
-                mock_func.side_effect = Exception("test error")
-                batch_engine = BatchEngine(get_yaml_file("print_input_flow"))
-                batch_engine.run({}, {}, Path("."))
-        assert e.value.target == ErrorTarget.BATCH
-        assert isinstance(e.value.inner_exception, Exception)
-        assert e.value.error_codes == ["SystemError", "UnexpectedError"]
-        assert (
-            e.value.message == "Unexpected error occurred while executing the batch run. Error: (Exception) test error."
-        )
+                "promptflow.batch._batch_inputs_processor.BatchInputsProcessor.process_batch_inputs", new=Mock()
+            ):
+                with pytest.raises(ex_type) as e:
+                    batch_engine.run({}, {}, Path("."))
+        assert e.value.target == ex_target
+        assert e.value.error_codes == ex_codes
+        assert e.value.message == ex_msg
+
+    def test_register_executor(self):
+        # assert original values
+        assert BatchEngine.executor_proxy_classes["python"] == PythonExecutorProxy
+        assert BatchEngine.executor_proxy_classes["csharp"] == CSharpExecutorProxy
+        # register new proxy
+        BatchEngine.register_executor("python", MockPythonExecutorProxy)
+        BatchEngine.register_executor("js", MockJSExecutorProxy)
+        assert BatchEngine.executor_proxy_classes["python"] == MockPythonExecutorProxy
+        assert BatchEngine.executor_proxy_classes["js"] == MockJSExecutorProxy
+        assert len(BatchEngine.executor_proxy_classes) == 3
+
+
+class MockPythonExecutorProxy(AbstractExecutorProxy):
+    pass
+
+
+class MockJSExecutorProxy(APIBasedExecutorProxy):
+    pass
