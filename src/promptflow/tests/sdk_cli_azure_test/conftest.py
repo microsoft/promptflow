ab31dd823fab1c34fa6b70bafdcf00654fbdbb07
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-01-10 17:38:08 +0800

    add fixture to mock upload data

--- src/promptflow/tests/sdk_cli_azure_test/conftest.py
+++ src/promptflow/tests/sdk_cli_azure_test/conftest.py
@@ -7,7 +7,7 @@ import os
 import uuid
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from typing import Callable, Optional
+from typing import Callable, Optional, Union
 from unittest.mock import patch
 
 import jwt
@@ -164,12 +164,14 @@ def flow_serving_client_remote_connection(mocker: MockerFixture, remote_workspac
 
 
 @pytest.fixture
-def flow_serving_client_with_prt_config_env(mocker: MockerFixture, subscription_id, resource_group_name, workspace_name):  # noqa: E501
+def flow_serving_client_with_prt_config_env(
+    mocker: MockerFixture, subscription_id, resource_group_name, workspace_name
+):  # noqa: E501
     connections = {
-            "PRT_CONFIG_OVERRIDE": f"deployment.subscription_id={subscription_id},"
-            f"deployment.resource_group={resource_group_name},"
-            f"deployment.workspace_name={workspace_name},"
-            "app.port=8088",
+        "PRT_CONFIG_OVERRIDE": f"deployment.subscription_id={subscription_id},"
+        f"deployment.resource_group={resource_group_name},"
+        f"deployment.workspace_name={workspace_name},"
+        "app.port=8088",
     }
     return create_serving_client_with_connections("basic-with-connection", mocker, connections)
 
@@ -191,7 +193,7 @@ def flow_serving_client_with_aml_resource_id_env(mocker: MockerFixture, remote_w
 def serving_client_with_connection_name_override(mocker: MockerFixture, remote_workspace_resource_id):
     connections = {
         "aoai_connection": "azure_open_ai_connection",
-        "PROMPTFLOW_CONNECTION_PROVIDER": remote_workspace_resource_id
+        "PROMPTFLOW_CONNECTION_PROVIDER": remote_workspace_resource_id,
     }
     return create_serving_client_with_connections("llm_connection_override", mocker, connections)
 
@@ -209,9 +211,7 @@ def serving_client_with_connection_data_override(mocker: MockerFixture, remote_w
     return create_serving_client_with_connections(model_name, mocker, connections)
 
 
-def create_serving_client_with_connections(
-    model_name, mocker: MockerFixture, connections: dict = {}
-):
+def create_serving_client_with_connections(model_name, mocker: MockerFixture, connections: dict = {}):
     from promptflow._sdk._serving.app import create_app as create_serving_app
 
     model_path = (Path(MODEL_ROOT) / model_name).resolve().absolute().as_posix()
@@ -488,3 +488,28 @@ def mock_isinstance_for_mock_datastore() -> None:
 
         with patch("builtins.isinstance", new=mock_isinstance):
             yield
+
+
+@pytest.fixture(autouse=is_replay())
+def mock_upload_and_generate_remote_uri() -> None:
+    from azure.ai.ml._scope_dependent_operations import OperationScope
+    from azure.ai.ml.entities._datastore._constants import WORKSPACE_BLOB_STORE
+    from azure.ai.ml.exceptions import ErrorTarget
+    from azure.ai.ml.operations._datastore_operations import DatastoreOperations
+
+    def _upload_and_generate_remote_uri(
+        operation_scope: OperationScope,
+        datastore_operation: DatastoreOperations,
+        path: Union[str, Path, os.PathLike],
+        artifact_type: str = ErrorTarget.ARTIFACT,
+        datastore_name: str = WORKSPACE_BLOB_STORE,
+        show_progress: bool = True,
+    ) -> str:
+        path = Path(path).resolve()
+        return f"azureml://datastores/{datastore_name}/paths/LocalUpload/00000000000000000000000000000000/{path.name}"
+
+    with patch(
+        "promptflow.azure.operations._run_operations._upload_and_generate_remote_uri",
+        new=_upload_and_generate_remote_uri,
+    ):
+        yield
