b9155302f05d01262a193a2ebe8dab437dceb8c8
jiazeng <jiazeng@microsoft.com>
2024-01-12 11:10:44 +0800

    tool example use gen meta

--- examples/tools/tool-package-quickstart/my_tool_package/tools/tool_with_custom_llm_type_featuring_uihints.py
+++ examples/tools/tool-package-quickstart/my_tool_package/tools/tool_with_custom_llm_type_featuring_uihints.py
@@ -2,9 +2,23 @@ from jinja2 import Template
 from promptflow import tool
 from promptflow.connections import CustomConnection
 from promptflow.contracts.types import PromptTemplate
+from promptflow.entities import InputSetting
 
 
-@tool
+input_settings = {
+    "connection": InputSetting(ui_hints={"text_box_size": "lg"}),
+    "endpoint_name": InputSetting(ui_hints={"text_box_size": "md"}),
+    "api": InputSetting(ui_hints={"text_box_size": "sm"}),
+    "temperature": InputSetting(ui_hints={"text_box_size": "xs"})
+}
+
+
+@tool(
+    name="Custom LLM Tool with UI Hints",
+    description="This is a custom LLM tool with UI hints.",
+    type="custom_llm",
+    input_settings=input_settings
+)
 def my_tool(
     connection: CustomConnection,
     endpoint_name: str,
