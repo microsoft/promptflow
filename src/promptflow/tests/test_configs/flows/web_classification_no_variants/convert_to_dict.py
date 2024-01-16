e7128dd832976f36f92501b966d091a0c2fd7ee9
chengbin wang <chw@microsoft.com>
2024-01-16 19:54:16 +0800

    fixing the timeout issue when recording enabled

--- src/promptflow/tests/test_configs/flows/web_classification_no_variants/convert_to_dict.py
+++ src/promptflow/tests/test_configs/flows/web_classification_no_variants/convert_to_dict.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from promptflow import tool
 
@@ -6,6 +7,8 @@ from promptflow import tool
 @tool
 def convert_to_dict(input_str: str):
     try:
+        # Sleep for 1.2 seconds
+        time.sleep(1.2)
         return json.loads(input_str)
     except Exception as e:
         print("input is not valid, error: {}".format(e))
