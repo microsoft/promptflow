627ebbae39da5e255823663cc6b0700e7bfa9baf
robbenwang <robbenwang@microsoft.com>
2024-01-17 12:12:01 +0000

    For public access url, runtime will not convert it to local path. Andd abstract class for runtime to implement to determine convert or not and do convert operation.

--- src/promptflow/tests/executor/unittests/_utils/test_multimedia_format_converter.py
+++ src/promptflow/tests/executor/unittests/_utils/test_multimedia_format_converter.py
@@ -1,57 +1,64 @@
+from unittest.mock import Mock
+
 import pytest
 
-from promptflow._utils.multimedia_format_converter import MultimediaConverter, MultimediaFormatConverter20231201
+from promptflow._utils.multimedia_format_converter import (
+    AbstractPathToUrlConverter,
+    AbstractUrlToPathConverter,
+    MultimediaConverter,
+    MultimediaFormatConverter20231201,
+)
 
 
 @pytest.mark.unittest
 class TestMultimediaFormatConverter20231201:
     def test_is_image_dict(self):
         converter = MultimediaFormatConverter20231201()
-        assert converter.is_multimedia_format({"data:image/jpg;path": "logo.jpg"})
-        assert converter.is_multimedia_format({"data:image/jpg;url": "https://example.com/logo.jpg"})
-        assert not converter.is_multimedia_format({"data:audio/mp3;path": "audio.mp3"})
-        assert not converter.is_multimedia_format({"data:video/mp4;url": "https://example.com/video.mp4"})
+        assert converter.is_valid_format({"data:image/jpg;path": "logo.jpg"})
+        assert converter.is_valid_format({"data:image/jpg;url": "https://example.com/logo.jpg"})
+        assert not converter.is_valid_format({"data:audio/mp3;path": "audio.mp3"})
+        assert not converter.is_valid_format({"data:video/mp4;url": "https://example.com/video.mp4"})
 
     def test_extract_path(self):
         converter = MultimediaFormatConverter20231201()
-        assert converter.get_path_from_multimedia_data({"data:image/jpg;path": "logo.jpg"}) == "logo.jpg"
-        assert converter.get_path_from_multimedia_data({"data:image/png;path": "images/logo.png"}) == "images/logo.png"
-        assert converter.get_path_from_multimedia_data({"data:image/webp;url": "https://example.com/logo.webp"}) is None
-        assert converter.get_path_from_multimedia_data({"data:audio/mp3;path": "audio.mp3"}) is None
+        assert converter.get_path_from_data({"data:image/jpg;path": "logo.jpg"}) == "logo.jpg"
+        assert converter.get_path_from_data({"data:image/png;path": "images/logo.png"}) == "images/logo.png"
+        assert converter.get_path_from_data({"data:image/webp;url": "https://example.com/logo.webp"}) is None
+        assert converter.get_path_from_data({"data:audio/mp3;path": "audio.mp3"}) is None
 
     def test_extract_url(self):
         converter = MultimediaFormatConverter20231201()
         assert (
-            converter.get_url_from_multimedia_data({"data:image/jpg;url": "https://example.com/logo.jpg"})
+            converter.get_url_from_data({"data:image/jpg;url": "https://example.com/logo.jpg"})
             == "https://example.com/logo.jpg"
         )
         assert (
-            converter.get_url_from_multimedia_data({"data:image/png;url": "https://example.com/images/logo.png"})
+            converter.get_url_from_data({"data:image/png;url": "https://example.com/images/logo.png"})
             == "https://example.com/images/logo.png"
         )
-        assert converter.get_url_from_multimedia_data({"data:image/webp;path": "logo.webp"}) is None
-        assert converter.get_url_from_multimedia_data({"data:audio/mp3;url": "https://example.com/audio.mp3"}) is None
+        assert converter.get_url_from_data({"data:image/webp;path": "logo.webp"}) is None
+        assert converter.get_url_from_data({"data:audio/mp3;url": "https://example.com/audio.mp3"}) is None
 
     def test_generate_path(self):
         converter = MultimediaFormatConverter20231201()
-        assert converter.create_multimedia_data_from_path({"data:image/jpg;path": "logo.jpg"}, "new_logo.jpg") == {
+        assert converter.create_data_from_path({"data:image/jpg;path": "logo.jpg"}, "new_logo.jpg") == {
             "data:image/jpg;path": "new_logo.jpg"
         }
-        assert converter.create_multimedia_data_from_path(
+        assert converter.create_data_from_path(
             {"data:image/png;url": "https://example.com/logo.png"}, "images/new_logo.png"
         ) == {"data:image/png;path": "images/new_logo.png"}
-        assert converter.create_multimedia_data_from_path({"data:audio/mp3;path": "audio.mp3"}, "new_audio.mp3") is None
+        assert converter.create_data_from_path({"data:audio/mp3;path": "audio.mp3"}, "new_audio.mp3") is None
 
     def test_generate_url(self):
         converter = MultimediaFormatConverter20231201()
-        assert converter.create_multimedia_data_from_url(
+        assert converter.create_data_from_url(
             {"data:image/jpg;url": "https://example.com/logo.jpg"}, "https://example.com/new_logo.jpg"
         ) == {"data:image/jpg;url": "https://example.com/new_logo.jpg"}
-        assert converter.create_multimedia_data_from_url(
+        assert converter.create_data_from_url(
             {"data:image/png;path": "logo.png"}, "https://example.com/images/new_logo.png"
         ) == {"data:image/png;url": "https://example.com/images/new_logo.png"}
         assert (
-            converter.create_multimedia_data_from_url(
+            converter.create_data_from_url(
                 {"data:audio/mp3;url": "https://example.com/audio.mp3"}, "https://example.com/new_audio.mp3"
             )
             is None
@@ -77,8 +84,17 @@ class TestMultimediaConverter:
             "images": [{"data:image/jpg;path": "logo.jpg"}, {"data:image/jpg;path": "logo.jpg"}],
             "object": {"image": {"data:image/jpg;path": "random_path"}, "other_data": "other_data"},
         }
-        result = converter.convert_url_to_path_recursively(value, url_to_path_converter=lambda url: "logo.jpg")
+
+        mock_converter = Mock(spec=AbstractUrlToPathConverter)
+        mock_converter.convert.return_value = "logo.jpg"
+        mock_converter.should_process.return_value = True
+        result = converter.convert_url_to_path_recursively(value, url_to_path_converter=mock_converter)
         assert result == expected_value
+
+        mock_converter.should_process.return_value = False
+        # Converter return empty string means refuse to handle this case.
+        result = converter.convert_url_to_path_recursively(value, url_to_path_converter=mock_converter)
+        assert result == value
 
     def test_convert_path_to_url_recursively(self):
         converter = MultimediaConverter(flow_file="path/to/yaml/file")
@@ -95,7 +111,14 @@ class TestMultimediaConverter:
             ],
             "object": {"image": {"data:image/jpg;url": "random_url"}, "other_data": "other_data"},
         }
-        result = converter.convert_path_to_url_recursively(
-            value, path_to_url_converter=lambda path: "https://example.com/logo.jpg"
-        )
+        mock_converter = Mock(spec=AbstractPathToUrlConverter)
+        mock_converter.convert.return_value = "https://example.com/logo.jpg"
+        mock_converter.should_process.return_value = True
+
+        result = converter.convert_path_to_url_recursively(value, path_to_url_converter=mock_converter)
         assert result == expected_value
+
+        mock_converter.should_process.return_value = False
+        # Converter return empty string means refuse to handle this case.
+        result = converter.convert_path_to_url_recursively(value, path_to_url_converter=mock_converter)
+        assert result == value
