733ade3b49982cdcfb75c893e8ff643e1ec4c6c8
Han Wang <phoenix.seek@gmail.com>
2024-01-17 19:07:26 +0800

    update recording

--- src/promptflow/tests/sdk_cli_azure_test/e2etests/test_run_operations.py
+++ src/promptflow/tests/sdk_cli_azure_test/e2etests/test_run_operations.py
@@ -23,6 +23,7 @@ from promptflow._sdk.entities import Run
 from promptflow._utils.flow_utils import get_flow_lineage_id
 from promptflow._utils.yaml_utils import load_yaml
 from promptflow.azure import PFClient
+from promptflow.azure._constants._flow import ENVIRONMENT, PYTHON_REQUIREMENTS_TXT
 from promptflow.azure._entities._flow import Flow
 from promptflow.exceptions import UserErrorException
 
@@ -886,10 +887,10 @@ class TestFlowRun:
         )
         pf.runs.stream(run)
         # # TODO: test snapshot after download can successfully run
-        # with TemporaryDirectory() as tmp_dir:
-        #     pf.runs.download(run=run.name, output=tmp_dir)
-        #     flow_dag = load_yaml(Path(tmp_dir, run.name, "flow.dag.yaml"))
-        #     assert "requirements.txt" in flow_dag[ENVIRONMENT][PYTHON_REQUIREMENTS_TXT]
+        with TemporaryDirectory() as tmp_dir:
+            pf.runs.download(run=run.name, output=tmp_dir)
+            flow_dag = load_yaml(Path(tmp_dir, run.name, "flow.dag.yaml"))
+            assert "requirements.txt" in flow_dag[ENVIRONMENT][PYTHON_REQUIREMENTS_TXT]
 
         local_flow_dag = load_yaml(f"{FLOWS_DIR}/flow_with_requirements_txt/flow.dag.yaml")
         assert "environment" not in local_flow_dag
