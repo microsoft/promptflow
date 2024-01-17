82fbf3ee2039da1f332e82232f33702525ac9e54
Peiwen Gao <peiwengao@microsoft.com>
2024-01-17 11:10:56 +0800

    Update contracts

--- src/promptflow/promptflow/service/apis/execution.py
+++ src/promptflow/promptflow/service/apis/execution.py
@@ -1,3 +1,12 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/execution/flow")
+async def flow_test(flow):
+    pass
--- src/promptflow/promptflow/service/contracts/execution_request.py
+++ src/promptflow/promptflow/service/contracts/execution_request.py
@@ -2,14 +2,19 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from typing import Any, Dict
+from typing import Any, Mapping
 
 from pydantic import BaseModel
 
 
-class BaseRunRequest(BaseModel):
+class BaseExecutionRequest(BaseModel):
     run_id: str
     working_dir: str
     flow_file: str
-    environment_variables: Dict[str, Any] = None
+    environment_variables: Mapping[str, Any] = None
     log_path: str
+
+
+class FlowExecutionRequest(BaseExecutionRequest):
+    inputs: Mapping[str, Any]
+    output_dir: str
