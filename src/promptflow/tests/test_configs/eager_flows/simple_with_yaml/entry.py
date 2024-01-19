19f3c62bc3d1344a5241c43fac2bb07302a93a4d
Zhidong Zhu <zhizhu@microsoft.com>
2024-01-19 21:28:59 +0800

    update sample for eager flow

--- src/promptflow/tests/test_configs/eager_flows/simple_with_yaml/entry.py
+++ src/promptflow/tests/test_configs/eager_flows/simple_with_yaml/entry.py
@@ -2,6 +2,18 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+from promptflow import trace
+import time
+
+
+@trace
+def query():
+    """Query for input."""
+    time.sleep(1)
+    return "gpt"
+
+
 def my_flow(input_val: str = "gpt") -> str:
     """Simple flow without yaml."""
+    query()
     return f"Hello world! {input_val}"
