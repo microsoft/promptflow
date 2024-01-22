1f48676c7a92a0f88286bc0d05768e1a4a08250f
Yuhang Liu <yuhaliu@microsoft.com>
2024-01-22 14:40:08 +0800

    Add comment

--- src/promptflow-tools/setup.py
+++ src/promptflow-tools/setup.py
@@ -47,6 +47,7 @@ setup(
     install_requires=parse_requirements('requirements.txt'),
     extras_require={
         "azure": [
+            # Dependency to list deployment in aoai_gpt4v
             "azure-mgmt-cognitiveservices==13.5.0"
         ]
     },
