9fe543f7024a5c8c30837b7ec47e7fec9bd2dcf1
Lina Tang <linatanglily@gmail.com>
2024-01-23 18:23:22 +0800

    [Internal][Executor] Support batch run for eager flow (#1755)
    # Description
    This pull request supports batch run for eager flow. The most important
    changes include updating the `ScriptExecutor` class to accept additional
    parameters, improving the functionality and flexibility of the batch
    engine, and handling different types of executors correctly.
    
    Main changes:
    
    *
    [`src/promptflow/promptflow/_utils/dataclass_serializer.py`](diffhunk://#diff-980db094b1371a3d81f41f0796a89e95158c7450b98bd3dea2eb5e2d610011d8R118-R139):
    Added a function `convert_eager_flow_output_to_dict` to convert the
    output of `EagerFlow` to a dictionary.
    
    *
    [`src/promptflow/promptflow/batch/_batch_engine.py`](diffhunk://#diff-ecf0905f2116abe08b5cf2931b856bf39aa8b38a30fadd9042538d076dbfde80R32):
    Added several methods to support the execution of `EagerFlow`. These
    include `_is_eager_flow_yaml`, `_parse_eager_flow_yaml`, and changes to
    the `__init__`, `run`, `_exec`, `_exec_aggregation`, and
    `_update_aggr_result` methods.
    
    *
    [`src/promptflow/promptflow/batch/_python_executor_proxy.py`](diffhunk://#diff-8c42522c7186705de9a6651343af6006be02b7342f72467c2785f23f93ea669eR9-R15):
    Added the `get_inputs_definition` method and modified the `create` and
    `_exec_batch` methods to support `EagerFlow`.
    
    *
    [`src/promptflow/promptflow/contracts/tool.py`](diffhunk://#diff-96d10cc01c636338eedcf0287bd0a5ab57c9e7e8d8fac4619aaa51e5d36de9abR301-R311):
    Added the `to_flow_input_definition` method to convert input definition
    to flow input definition for `EagerFlow`.
    
    *
    [`src/promptflow/promptflow/executor/_errors.py`](diffhunk://#diff-1f02407850413baccbb33e0078d9772e15ae1c46c020dc7457529c6f7a054315R266-R269):
    Added the `InvalidFlowFileError` exception to handle unsupported flow
    file types.
    
    *
    [`src/promptflow/promptflow/executor/_line_execution_process_pool.py`](diffhunk://#diff-c14422c33faa9e44145738152120929455da4e4f05d4fc7b59158423fa609bc1R22):
    Modified the `__init__` and `_exec_line` methods, and added the
    `convert_eager_flow_output_to_dict` function to support `EagerFlow`.
    
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.
    
    ---------
    
    Co-authored-by: Lina Tang <linatang@microsoft.com>
    Co-authored-by: Han Wang <phoenix.seek@gmail.com>

--- src/promptflow/tests/executor/utils.py
+++ src/promptflow/tests/executor/utils.py
@@ -11,6 +11,7 @@ from promptflow.storage import AbstractRunStorage
 TEST_ROOT = Path(__file__).parent.parent
 DATA_ROOT = TEST_ROOT / "test_configs/datas"
 FLOW_ROOT = TEST_ROOT / "test_configs/flows"
+EAGER_FLOW_ROOT = TEST_ROOT / "test_configs/eager_flows"
 WRONG_FLOW_ROOT = TEST_ROOT / "test_configs/wrong_flows"
 EAGER_FLOWS_ROOT = TEST_ROOT / "test_configs/eager_flows"
 
@@ -23,6 +24,11 @@ def get_flow_folder(folder_name, root: str = FLOW_ROOT):
 def get_yaml_file(folder_name, root: str = FLOW_ROOT, file_name: str = "flow.dag.yaml"):
     yaml_file = get_flow_folder(folder_name, root) / file_name
     return yaml_file
+
+
+def get_entry_file(folder_name, root: str = EAGER_FLOW_ROOT, file_name: str = "entry.py"):
+    entry_file = get_flow_folder(folder_name, root) / file_name
+    return entry_file
 
 
 def get_flow_from_folder(folder_name, root: str = FLOW_ROOT, file_name: str = "flow.dag.yaml"):
@@ -41,8 +47,13 @@ def get_flow_inputs(folder_name, root: str = FLOW_ROOT, file_name: str = "inputs
     return inputs[0] if isinstance(inputs, list) else inputs
 
 
-def get_bulk_inputs(folder_name):
-    inputs = load_json(get_flow_inputs_file(folder_name, file_name="inputs.json"))
+def get_bulk_inputs_from_jsonl(folder_name, root: str = FLOW_ROOT, file_name: str = "inputs.jsonl"):
+    inputs = load_jsonl(get_flow_inputs_file(folder_name, root, file_name))
+    return inputs
+
+
+def get_bulk_inputs(folder_name, root: str = FLOW_ROOT, file_name: str = "inputs.json"):
+    inputs = load_json(get_flow_inputs_file(folder_name, root=root, file_name=file_name))
     return [inputs] if isinstance(inputs, dict) else inputs
 
 
