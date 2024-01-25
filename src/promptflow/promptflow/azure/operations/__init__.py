6aab09372f6147c7f2a3bfe0edc39df462694e6c
Honglin Du <0mza987@gmail.com>
2024-01-25 15:09:58 +0800

    add flow operations to init

--- src/promptflow/promptflow/azure/operations/__init__.py
+++ src/promptflow/promptflow/azure/operations/__init__.py
@@ -5,6 +5,7 @@
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
 
 
+from ._flow_operations import FlowOperations
 from ._run_operations import RunOperations
 
-__all__ = ["RunOperations"]
+__all__ = ["FlowOperations", "RunOperations"]
