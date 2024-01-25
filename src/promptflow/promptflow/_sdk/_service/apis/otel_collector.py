00e0847e3e4f9b2a0edde513fc7cb348b6dedc1d
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-01-25 16:24:29 +0800

    implement /v1/traces

--- src/promptflow/promptflow/_sdk/_service/apis/otel_collector.py
+++ src/promptflow/promptflow/_sdk/_service/apis/otel_collector.py
@@ -2,14 +2,19 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-import json
-
 from flask import request
+from opentelemetry.proto.collector.trace.v1.trace_service_pb2 import ExportTraceServiceRequest
+
+from promptflow._sdk.entities._otel import Span
 
 
 def trace_collector():
-    trace_data = request.data
-    trace = json.loads(trace_data)
-    print(trace)
+    traces_request = ExportTraceServiceRequest()
+    traces_request.ParseFromString(request.data)
+    for resource_span in traces_request.resource_spans:
+        for scope_span in resource_span.scope_spans:
+            for span in scope_span.spans:
+                entity = Span._from_protobuf_object(span)
+                entity.persist()
 
     return "Traces received", 200
--- src/promptflow/promptflow/_sdk/entities/_otel.py
+++ src/promptflow/promptflow/_sdk/entities/_otel.py
@@ -2,12 +2,55 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+import json
+import typing
+
+from google.protobuf.json_format import MessageToJson
+from opentelemetry.proto.trace.v1.trace_pb2 import Span as PBSpan
+
 from promptflow._sdk._orm.otel import Span as ORMSpan
 
 
 class Span:
-    def __init__(self):
-        ...
+    def __init__(
+        self,
+        span_id: str,
+        trace_id: str,
+        parent_id: typing.Optional[str] = None,
+        experiment_name: typing.Optional[str] = None,
+        run_name: typing.Optional[str] = None,
+        path: typing.Optional[str] = None,
+        content: typing.Optional[str] = None,
+    ):
+        self.span_id = span_id
+        self.trace_id = trace_id
+        self.parent_id = parent_id
+        self.experiment_name = experiment_name
+        self.run_name = run_name
+        self.path = path
+        self.content = content
+
+    def persist(self) -> None:
+        self._to_orm_object().persist()
 
     def _to_orm_object(self) -> ORMSpan:
-        return ORMSpan()
+        return ORMSpan(
+            span_id=self.span_id,
+            trace_id=self.trace_id,
+            parent_id=self.parent_id,
+            experiment_name=self.experiment_name,
+            run_name=self.run_name,
+            path=self.path,
+            content=self.content,
+        )
+
+    @staticmethod
+    def _from_protobuf_object(obj: PBSpan) -> "Span":
+        span_string = MessageToJson(obj)
+        span_dict = json.loads(span_string)
+        return Span(
+            span_id=span_dict["spanId"],
+            trace_id=span_dict["traceId"],
+            parent_id=span_dict.get("parentSpanId"),
+            content=span_string,
+        )
