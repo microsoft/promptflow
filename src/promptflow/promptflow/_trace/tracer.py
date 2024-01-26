780997d07438def1b9190ac2592185afbbbd71ef
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-01-26 15:19:03 +0800

    add some TODO

--- src/promptflow/promptflow/_trace/tracer.py
+++ src/promptflow/promptflow/_trace/tracer.py
@@ -1,3 +1,13 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+
+
+class Tracer:
+    def trace(span) -> None:
+        ...
+
+
+def _get_tracer() -> Tracer:
+    # return the global singleton tracer
+    ...
