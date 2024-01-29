c31861ba09f223ec6abbed1f794a845d023c17c4
Peiwen Gao <peiwengao@microsoft.com>
2024-01-29 18:19:23 +0800

    Add connections and get_run_mode to execution request

--- src/promptflow/promptflow/executor/service/contracts/execution_request.py
+++ src/promptflow/promptflow/executor/service/contracts/execution_request.py
@@ -6,21 +6,33 @@ from typing import Any, Mapping
 
 from pydantic import BaseModel
 
+from promptflow.contracts.run_mode import RunMode
+
 
 class BaseExecutionRequest(BaseModel):
     run_id: str
     working_dir: str
     flow_file: str
     output_dir: str
+    connections: Mapping[str, Any] = None
     environment_variables: Mapping[str, Any] = None
     log_path: str
+
+    def get_run_mode(self):
+        raise NotImplementedError(f"Request type {self.__class__.__name__} is not implemented.")
 
 
 class FlowExecutionRequest(BaseExecutionRequest):
     inputs: Mapping[str, Any] = None
+
+    def get_run_mode(self):
+        return RunMode.Test
 
 
 class NodeExecutionRequest(BaseExecutionRequest):
     node_name: str
     flow_inputs: Mapping[str, Any] = None
     dependency_nodes_outputs: Mapping[str, Any] = None
+
+    def get_run_mode(self):
+        return RunMode.SingleNode
