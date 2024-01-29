c8c24e3674dea501f9e212b02a4e8e8f67e7d818
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-01-29 16:45:01 +0800

    remove GET span

--- src/promptflow/promptflow/_sdk/_service/apis/trace.py
+++ src/promptflow/promptflow/_sdk/_service/apis/trace.py
@@ -10,16 +10,6 @@ from promptflow._sdk._service.utils.utils import get_client_from_request
 api = Namespace("traces", description="Trace Management")
 
 
-@api.route("/<string:span_id>")
-class Trace(Resource):
-    @api.response(code=200, description="Get trace info")
-    @api.doc(description="Get trace")
-    def get(self, span_id: str):
-        trace = get_client_from_request()._traces.get(span_id=span_id)
-        print(trace)
-        return "Trace", 200
-
-
 @api.route("/list")
 class TraceList(Resource):
     @api.response(code=200, description="Traces")
