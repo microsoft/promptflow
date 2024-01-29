7fc8c6f25809eee8f5d84e5a6a30bddfb921ae0c
Peiwen Gao <peiwengao@microsoft.com>
2024-01-29 15:47:31 +0800

    Add exception handler

--- src/promptflow/promptflow/_utils/service_utils.py
+++ src/promptflow/promptflow/_utils/service_utils.py
@@ -2,7 +2,8 @@ import contextlib
 import json
 from multiprocessing import Queue
 
-from promptflow._utils.exception_utils import ExceptionPresenter, JsonSerializedPromptflowException
+from promptflow._utils.exception_utils import ErrorResponse, ExceptionPresenter, JsonSerializedPromptflowException
+from promptflow._utils.logger_utils import logger
 
 
 @contextlib.contextmanager
@@ -19,3 +20,12 @@ def multi_processing_exception_wrapper(exception_queue: Queue):
         exception = JsonSerializedPromptflowException(message=message)
         exception_queue.put(exception)
         raise exception from e
+
+
+def generate_error_response(ex):
+    if isinstance(ex, JsonSerializedPromptflowException):
+        error_dict = json.loads(ex.message)
+    else:
+        error_dict = ExceptionPresenter.create(ex).to_dict(include_debug_info=True)
+    logger.exception(f"Failed to execute the flow: \n{ex}")
+    return ErrorResponse.from_error_dict(error_dict)
--- src/promptflow/promptflow/executor/service/app.py
+++ src/promptflow/promptflow/executor/service/app.py
@@ -4,13 +4,21 @@
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
+from promptflow._utils.service_utils import generate_error_response
 from promptflow.executor.service.apis.common import router as common_router
 from promptflow.executor.service.apis.execution import router as execution_router
 
 app = FastAPI()
 app.include_router(common_router)
 app.include_router(execution_router)
+
+
+@app.exception_handler(Exception)
+async def exception_handler(ex):
+    resp = generate_error_response(ex)
+    return JSONResponse(status_code=resp.to_dict(), content=resp.response_code)
 
 
 if __name__ == "__main__":
