baf3cdba9b2bed834b4e4471d5609c5e637c5771
Peiwen Gao <peiwengao@microsoft.com>
2024-01-29 11:13:48 +0800

    Remove nodes api

--- src/promptflow/promptflow/executor/service/apis/execution.py
+++ src/promptflow/promptflow/executor/service/apis/execution.py
@@ -9,8 +9,8 @@ from fastapi import APIRouter, Request
 # from promptflow.contracts.flow import Flow
 from promptflow._core.operation_context import OperationContext
 from promptflow._utils.logger_utils import LogContext
-from promptflow.executor.flow_executor import FlowExecutor, execute_flow
-from promptflow.executor.service.contracts.execution_request import FlowExecutionRequest, NodeExecutionRequest
+from promptflow.executor.flow_executor import execute_flow
+from promptflow.executor.service.contracts.execution_request import FlowExecutionRequest
 from promptflow.storage._run_storage import DefaultRunStorage
 
 router = APIRouter()
@@ -42,31 +42,3 @@ async def flow_execution(request: Request, flow_request: FlowExecutionRequest):
             run_id=flow_request.run_id,
             storage=storage,
         )
-
-
-@router.post("/execution/node")
-async def node_execution(request: Request, node_request: NodeExecutionRequest):
-    # Operation context???
-    # headers = dict(request.headers)
-    # get connection endpoints
-    connections = {}
-    credential_list = []
-    # resolve environment variables
-    if isinstance(node_request.environment_variables, dict):
-        os.environ.update(node_request.environment_variables)
-
-    with LogContext(
-        file_path=node_request.log_path,
-        credential_list=credential_list,
-    ):
-        storage = DefaultRunStorage(base_dir=node_request.working_dir, sub_dir=node_request.output_dir)
-        result = FlowExecutor.load_and_exec_node(
-            node_request.flow_file,
-            node_request.node_name,
-            flow_inputs=node_request.flow_inputs,
-            dependency_nodes_outputs=node_request.dependency_nodes_outputs,
-            connections=connections,
-            working_dir=node_request.working_dir,
-            storage=storage,
-        )
-        return result
