774687a3aa6fd1fe65eb120c0be05e3889312836
Peiwen Gao <peiwengao@microsoft.com>
2024-01-30 13:25:48 +0800

    Update codes

--- src/promptflow/promptflow/executor/_service/app.py
+++ src/promptflow/promptflow/executor/_service/app.py
@@ -22,4 +22,4 @@ async def exception_handler(request, exc):
 
 
 if __name__ == "__main__":
-    uvicorn.run("promptflow.executor.service.app:app", port=8000, reload=True)
+    uvicorn.run("promptflow.executor._service.app:app", port=8000, reload=True)
