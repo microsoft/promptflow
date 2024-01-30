bd3baa2b5c38dd5cb63903e5636c371b7670d7c3
Peiwen Gao <peiwengao@microsoft.com>
2024-01-30 11:49:42 +0800

    Demo executor server

--- src/promptflow/promptflow/_utils/service_utils.py
+++ src/promptflow/promptflow/_utils/service_utils.py
@@ -1,39 +1,13 @@
-import contextlib
-import json
-from multiprocessing import Queue
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
 
 from promptflow._core.connection_manager import ConnectionManager
-from promptflow._utils.exception_utils import ErrorResponse, ExceptionPresenter, JsonSerializedPromptflowException
-from promptflow._utils.logger_utils import LogContext, logger
+from promptflow._utils.logger_utils import LogContext
 from promptflow.executor.service.contracts.execution_request import BaseExecutionRequest
-
-
-@contextlib.contextmanager
-def multi_processing_exception_wrapper(exception_queue: Queue):
-    """Wrap the exception to a generic exception to avoid the pickle error."""
-    try:
-        yield
-    except Exception as e:
-        # func runs in a child process, any customized exception can't have extra arguments other than message
-        # wrap the exception to a generic exception to avoid the pickle error
-        # Ref: https://bugs.python.org/issue32696
-        exception_dict = ExceptionPresenter.create(e).to_dict(include_debug_info=True)
-        message = json.dumps(exception_dict)
-        exception = JsonSerializedPromptflowException(message=message)
-        exception_queue.put(exception)
-        raise exception from e
 
 
 def get_log_context(request: BaseExecutionRequest):
     run_mode = request.get_run_mode()
     credential_list = ConnectionManager(request.connections).get_secret_list()
     return LogContext(file_path=request.log_path, run_mode=run_mode, credential_list=credential_list)
-
-
-def generate_error_response(ex):
-    if isinstance(ex, JsonSerializedPromptflowException):
-        error_dict = json.loads(ex.message)
-    else:
-        error_dict = ExceptionPresenter.create(ex).to_dict(include_debug_info=True)
-    logger.exception(f"Failed to execute the flow: \n{ex}")
-    return ErrorResponse.from_error_dict(error_dict)
--- src/promptflow/promptflow/executor/service/apis/execution.py
+++ src/promptflow/promptflow/executor/service/apis/execution.py
@@ -2,22 +2,15 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-import asyncio
-import multiprocessing
 import os
-from multiprocessing import Queue
 from pathlib import Path
-from typing import Callable
 
 from fastapi import APIRouter, Request
 
 from promptflow._core.operation_context import OperationContext
-from promptflow._utils.logger_utils import logger
-from promptflow._utils.service_utils import get_log_context, multi_processing_exception_wrapper
-
-# from promptflow.contracts.flow import Flow
-from promptflow.executor.flow_executor import execute_flow
-from promptflow.executor.service.contracts.execution_request import BaseExecutionRequest, FlowExecutionRequest
+from promptflow._utils.service_utils import get_log_context
+from promptflow.executor.flow_executor import FlowExecutor, execute_flow
+from promptflow.executor.service.contracts.execution_request import FlowExecutionRequest, NodeExecutionRequest
 from promptflow.storage._run_storage import DefaultRunStorage
 
 router = APIRouter()
@@ -25,83 +18,40 @@ router = APIRouter()
 
 @router.post("/execution/flow")
 async def flow_execution(request: Request, flow_request: FlowExecutionRequest):
-    return await execute_request_in_child_process(flow_request, request.headers)
-
-
-async def execute_request_in_child_process(flow_request: FlowExecutionRequest, context_dict: dict):
-    with multiprocessing.Manager() as manager:
-        return_dict = manager.dict()
-        exception_queue = manager.Queue()
-        parent_pid = os.getpid()
-        p = multiprocessing.Process(
-            target=execute_request,
-            args=(execute_flow_in_child_process, parent_pid, flow_request, return_dict, exception_queue, context_dict),
-        )
-        p.start()
-        p.join()
-
-        if p.exitcode and p.exitcode > 0:
-            exception = None
-            try:
-                exception = exception_queue.get(timeout=10)
-            except Exception:
-                pass
-            # JsonSerializedPromptflowException will be raised here
-            # no need to change to PromptflowException since it will be handled in app.exception_handler
-            if exception is not None:
-                raise exception
-
-        default_result = {}
-        result = return_dict.get("result", default_result)
-        return result
-
-
-async def execute_flow_in_child_process(flow_request: FlowExecutionRequest):
+    OperationContext.get_instance().update(dict(request.headers))
     # resolve environment variables
     if isinstance(flow_request.environment_variables, dict):
         os.environ.update(flow_request.environment_variables)
     # execute flow
     storage = DefaultRunStorage(base_dir=flow_request.working_dir, sub_dir=flow_request.output_dir)
-    return execute_flow(
-        Path(flow_request.flow_file),
-        Path(flow_request.working_dir),
-        flow_request.output_dir,
-        flow_request.connections,
-        flow_request.inputs,
-        run_id=flow_request.run_id,
-        storage=storage,
-    )
+    with get_log_context(flow_request):
+        return execute_flow(
+            Path(flow_request.flow_file),
+            Path(flow_request.working_dir),
+            flow_request.output_dir,
+            flow_request.connections,
+            flow_request.inputs,
+            run_id=flow_request.run_id,
+            storage=storage,
+        )
 
 
-def execute_request(
-    execute_flow_func: Callable,
-    parent_pid: int,
-    request: BaseExecutionRequest,
-    return_dict: dict,
-    exception_queue: Queue,
-    context_dict: dict,
-):
-    # We need to create the event loop in a new process to run the asynchronous function
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(
-        execute_request_async(execute_flow_func, parent_pid, request, return_dict, exception_queue, context_dict)
-    )
-    loop.close()
-
-
-async def execute_request_async(
-    execute_flow_func: Callable,
-    parent_pid: int,
-    request: BaseExecutionRequest,
-    return_dict: dict,
-    exception_queue: Queue,
-    context_dict: dict,
-):
-    operation_context = OperationContext.get_instance()
-    operation_context.update(context_dict)
-    with multi_processing_exception_wrapper(exception_queue):
-        with get_log_context(request):
-            logger.info("[%s--%s] Start processing request in executor service......", parent_pid, os.getpid())
-            result = await execute_flow_func(request)
-            return_dict["result"] = result
+@router.post("/execution/node")
+async def node_execution(request: Request, node_request: NodeExecutionRequest):
+    OperationContext.get_instance().update(dict(request.headers))
+    # resolve environment variables
+    if isinstance(node_request.environment_variables, dict):
+        os.environ.update(node_request.environment_variables)
+    # execute node
+    with get_log_context(node_request):
+        storage = DefaultRunStorage(base_dir=node_request.working_dir, sub_dir=node_request.output_dir)
+        result = FlowExecutor.load_and_exec_node(
+            Path(node_request.flow_file),
+            node_request.node_name,
+            flow_inputs=node_request.flow_inputs,
+            dependency_nodes_outputs=node_request.dependency_nodes_outputs,
+            connections=node_request.connections,
+            working_dir=node_request.working_dir,
+            storage=storage,
+        )
+        return result
