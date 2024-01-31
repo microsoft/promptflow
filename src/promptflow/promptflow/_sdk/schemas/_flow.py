7fbfcab2a6462ab71d1d146f1f3a00fecd2eb09a
zhangxingzhi <zhangxingzhi@microsoft.com>
2024-01-31 17:35:17 +0800

    fix: ci

--- src/promptflow/promptflow/_sdk/schemas/_flow.py
+++ src/promptflow/promptflow/_sdk/schemas/_flow.py
@@ -71,21 +71,26 @@ class EagerFlowSchema(BaseFlowSchema):
     def validate_entry(self, data, **kwargs):
         """Validate entry."""
         language = data.get(LANGUAGE_KEY, FlowLanguage.Python)
+        entry_regex = None
         if language == FlowLanguage.CSharp:
             entry_regex = r"\((.+)\)[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+"
-        else:
-            entry_regex = None
+        elif language == FlowLanguage.Python:
+            # TODO: remove this after path is removed
+            if data.get("path", None) is None:
+                raise ValidationError(field_name="path", message="Missing data for required field.")
 
         if entry_regex is not None and not re.match(entry_regex, data["entry"]):
-            raise ValidationError(f"Entry function {data['entry']} is not valid.")
+            raise ValidationError(field_name="entry", message=f"Entry function {data['entry']} is not valid.")
 
     @post_load
     def infer_path(self, data: dict, **kwargs):
         """Infer path from entry."""
         # TODO: remove this after path is removed
-        if data.get("path", None) is None:
+        language = data.get(LANGUAGE_KEY, FlowLanguage.Python)
+        if language == FlowLanguage.CSharp and data.get("path", None) is None:
             # for csharp, path to flow entry file will be a dll path inferred from
             # entry by default given customer won't see the dll on authoring
             m = re.match(r"\((.+)\)[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+", data["entry"])
-            data["path"] = m.group(1) + ".dll"
+            if m:
+                data["path"] = m.group(1) + ".dll"
         return data
