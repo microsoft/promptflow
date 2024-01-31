03a9374c2faac70b3047bda7b8311c76a72f4a5e
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-01-31 20:12:31 +0800

    revert last change

--- src/promptflow/promptflow/_sdk/_service/apis/span.py
+++ src/promptflow/promptflow/_sdk/_service/apis/span.py
@@ -5,8 +5,17 @@
 import typing
 from dataclasses import dataclass
 
+from flask_restx import fields
+
+from promptflow._constants import (
+    SpanAttributeFieldName,
+    SpanContextFieldName,
+    SpanFieldName,
+    SpanResourceAttributesFieldName,
+    SpanResourceFieldName,
+    SpanStatusFieldName,
+)
 from promptflow._sdk._service import Namespace, Resource
-from promptflow._sdk._service.models.span import models, span_model
 from promptflow._sdk._service.utils.utils import get_client_from_request
 
 api = Namespace("spans", description="Span Management")
@@ -33,8 +42,64 @@ class ListSpanParser:
 
 
 # span models, for strong type support in Swagger
-for name, model in models:
-    api.model(name, model)
+context_model = api.model(
+    "Context",
+    {
+        SpanContextFieldName.TRACE_ID: fields.String(required=True),
+        SpanContextFieldName.SPAN_ID: fields.String(required=True),
+        SpanContextFieldName.TRACE_STATE: fields.String(required=True),
+    },
+)
+status_model = api.model(
+    "Status",
+    {
+        SpanStatusFieldName.STATUS_CODE: fields.String(required=True),
+    },
+)
+attributes_model = api.model(
+    "Attributes",
+    {
+        SpanAttributeFieldName.FRAMEWORK: fields.String(required=True, default="promptflow"),
+        SpanAttributeFieldName.SPAN_TYPE: fields.String(required=True, default="Function"),
+        SpanAttributeFieldName.FUNCTION: fields.String(required=True),
+        SpanAttributeFieldName.INPUTS: fields.String(required=True),
+        SpanAttributeFieldName.OUTPUT: fields.String(required=True),
+        SpanAttributeFieldName.SESSION_ID: fields.String(required=True),
+        SpanAttributeFieldName.PATH: fields.String,
+        SpanAttributeFieldName.FLOW_ID: fields.String,
+        SpanAttributeFieldName.RUN: fields.String,
+        SpanAttributeFieldName.EXPERIMENT: fields.String,
+    },
+)
+resource_attributes_model = api.model(
+    "ResourceAttributes",
+    {
+        SpanResourceAttributesFieldName.SERVICE_NAME: fields.String(default="promptflow"),
+    },
+)
+resource_model = api.model(
+    "Resource",
+    {
+        SpanResourceFieldName.ATTRIBUTES: fields.Nested(resource_attributes_model, required=True),
+        SpanResourceFieldName.SCHEMA_URL: fields.String,
+    },
+)
+span_model = api.model(
+    "Span",
+    {
+        SpanFieldName.NAME: fields.String(required=True),
+        SpanFieldName.CONTEXT: fields.Nested(context_model, required=True),
+        SpanFieldName.KIND: fields.String(required=True),
+        SpanFieldName.PARENT_ID: fields.String,
+        SpanFieldName.START_TIME: fields.DateTime(dt_format="iso8601"),
+        SpanFieldName.END_TIME: fields.DateTime(dt_format="iso8601"),
+        SpanFieldName.STATUS: fields.Nested(status_model),
+        SpanFieldName.ATTRIBUTES: fields.Nested(attributes_model, required=True),
+        SpanFieldName.EVENTS: fields.List(fields.String),
+        SpanFieldName.LINKS: fields.List(fields.String),
+        SpanFieldName.RESOURCE: fields.Nested(resource_model, required=True),
+    },
+)
 
 
 @api.route("/list")
