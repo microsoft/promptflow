1d57bc1a8307261936f0b52a5feffae2e47cdb4c
Peiwen Gao <peiwengao@microsoft.com>
2024-02-01 17:45:18 +0800

    Add logs for service

--- src/promptflow/promptflow/executor/_service/apis/execution.py
+++ src/promptflow/promptflow/executor/_service/apis/execution.py
@@ -6,8 +6,15 @@ import os
 
 from fastapi import APIRouter, Request
 
+from promptflow._core.operation_context import OperationContext
+from promptflow._utils.logger_utils import service_logger
 from promptflow.executor._service.contracts.execution_request import FlowExecutionRequest
 from promptflow.executor._service.utils.process_utils import invoke_function_in_process
+from promptflow.executor._service.utils.service_utils import (
+    get_executor_version,
+    get_service_log_context,
+    update_operation_context,
+)
 from promptflow.executor.flow_executor import execute_flow
 from promptflow.storage._run_storage import DefaultRunStorage
 
@@ -16,7 +23,24 @@ router = APIRouter()
 
 @router.post("/execution/flow")
 async def flow_execution(request: Request, flow_request: FlowExecutionRequest):
-    return await invoke_function_in_process(flow_request, request.headers, flow_test)
+    update_operation_context(dict(request.headers))
+    request_id = OperationContext.get_instance().request_id
+    executor_version = get_executor_version()
+    with get_service_log_context(flow_request):
+        service_logger.info(
+            f"Received flow execution request, flow run id: {flow_request.run_id}, "
+            f"request id: {request_id}, executor version: {executor_version}."
+        )
+        try:
+            result = await invoke_function_in_process(flow_request, request.headers, flow_test)
+            service_logger.info(f"Completed flow execution request, flow run id: {flow_request.run_id}.")
+            return result
+        except Exception as ex:
+            error_type_and_message = (f"({ex.__class__.__name__}) {ex}",)
+            service_logger.error(
+                f"Failed to execute flow, flow run id: {flow_request.run_id}. Error: {error_type_and_message}"
+            )
+            raise ex
 
 
 async def flow_test(flow_request: FlowExecutionRequest):
