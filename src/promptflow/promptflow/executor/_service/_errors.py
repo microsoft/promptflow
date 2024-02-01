785ec21a48f671bae65d8672b26cb66cbc69f3e9
Peiwen Gao <peiwengao@microsoft.com>
2024-02-01 18:32:50 +0800

    Refine subprocess code

--- src/promptflow/promptflow/executor/_service/_errors.py
+++ src/promptflow/promptflow/executor/_service/_errors.py
@@ -2,8 +2,15 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from promptflow.exceptions import UserErrorException
+from promptflow.exceptions import ErrorTarget, UserErrorException
 
 
 class FlowFilePathInvalid(UserErrorException):
     pass
+
+
+class ExecutionTimeoutError(UserErrorException):
+    """Exception raised when execution timeout"""
+
+    def __init__(self, timeout):
+        super().__init__(message=f"Execution timeout for exceeding {timeout} seconds", target=ErrorTarget.EXECUTOR)
--- src/promptflow/promptflow/executor/_service/utils/process_utils.py
+++ src/promptflow/promptflow/executor/_service/utils/process_utils.py
@@ -7,64 +7,82 @@ import contextlib
 import json
 import multiprocessing
 import os
+from datetime import timedelta
 from multiprocessing import Queue
 from typing import Callable
 
+from promptflow._core._errors import UnexpectedError
 from promptflow._core.operation_context import OperationContext
 from promptflow._utils.exception_utils import ExceptionPresenter, JsonSerializedPromptflowException
-from promptflow._utils.logger_utils import logger
+from promptflow._utils.logger_utils import service_logger
+from promptflow.exceptions import ErrorTarget
+from promptflow.executor._service._errors import ExecutionTimeoutError
 from promptflow.executor._service.contracts.execution_request import BaseExecutionRequest
 from promptflow.executor._service.utils.service_utils import get_log_context
+
+EXECUTION_TIMEOUT = timedelta(days=1).total_seconds()
 
 
 async def invoke_function_in_process(request: BaseExecutionRequest, context_dict: dict, target_function: Callable):
     with multiprocessing.Manager() as manager:
         return_dict = manager.dict()
         exception_queue = manager.Queue()
-        parent_pid = os.getpid()
+
         p = multiprocessing.Process(
             target=execute_function,
-            args=(target_function, parent_pid, request, return_dict, exception_queue, context_dict),
+            args=(target_function, request, return_dict, exception_queue, context_dict),
         )
         p.start()
-        p.join()
+        service_logger.info(f"[{os.getpid()}--{p.pid}] Start process to execute the request.")
 
+        # Wait for the process to finish or timeout
+        p.join(timeout=EXECUTION_TIMEOUT)
+
+        # Terminate the process if it is still alive after timeout
+        if p.is_alive():
+            service_logger.error(f"[{p.pid}] Stop process for exceeding {EXECUTION_TIMEOUT} seconds.")
+            p.terminate()
+            p.join()
+            raise ExecutionTimeoutError(EXECUTION_TIMEOUT)
+
+        # Raise exception if the process exit code is not 0
         if p.exitcode and p.exitcode > 0:
             exception = None
             try:
                 exception = exception_queue.get(timeout=10)
             except Exception:
                 pass
+            if exception is None:
+                raise UnexpectedError(
+                    message="Unexpected error occurred while executing the request",
+                    target=ErrorTarget.EXECUTOR,
+                )
             # JsonSerializedPromptflowException will be raised here
             # no need to change to PromptflowException since it will be handled in app.exception_handler
-            if exception is not None:
-                raise exception
+            raise exception
 
-        default_result = {}
-        result = return_dict.get("result", default_result)
-        return result
+        service_logger.info(f"[{p.pid}] Process finished.")
+        return return_dict.get("result", {})
 
 
 def execute_function(
-    execute_flow_func: Callable,
-    parent_pid: int,
+    target_function: Callable,
     request: BaseExecutionRequest,
     return_dict: dict,
     exception_queue: Queue,
     context_dict: dict,
 ):
-    # We need to create the event loop in a new process to run the asynchronous function
+    # Create the event loop in a new process to run the asynchronous function
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(
-        execute_function_async(execute_flow_func, parent_pid, request, return_dict, exception_queue, context_dict)
+        execute_function_async(target_function, request, return_dict, exception_queue, context_dict)
     )
     loop.close()
 
 
 async def execute_function_async(
-    execute_flow_func: Callable,
-    parent_pid: int,
+    target_function: Callable,
     request: BaseExecutionRequest,
     return_dict: dict,
     exception_queue: Queue,
@@ -73,8 +91,8 @@ async def execute_function_async(
     OperationContext.get_instance().update(context_dict)
     with exception_wrapper(exception_queue):
         with get_log_context(request):
-            logger.info("[%s--%s] Start processing request in executor service......", parent_pid, os.getpid())
-            result = await execute_flow_func(request)
+            service_logger.info("Start processing request in executor service...")
+            result = await target_function(request)
             return_dict["result"] = result
 
 
@@ -84,8 +102,8 @@ def exception_wrapper(exception_queue: Queue):
     try:
         yield
     except Exception as e:
-        # func runs in a child process, any customized exception can't have extra arguments other than message
-        # wrap the exception to a generic exception to avoid the pickle error
+        # Func runs in a child process, any customized exception can't have extra arguments other than message
+        # Wrap the exception to a generic exception to avoid the pickle error
         # Ref: https://bugs.python.org/issue32696
         exception_dict = ExceptionPresenter.create(e).to_dict(include_debug_info=True)
         message = json.dumps(exception_dict)
