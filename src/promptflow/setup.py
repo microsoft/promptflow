3dabb75f429b5d15c8dfef29a177ba03695b5410
Zhengfei Wang <38847871+zhengfeiwang@users.noreply.github.com>
2024-02-01 15:02:49 +0800

    [promptflow][internal] Set session id in `OperationContext` and get inside `@trace` (#1891)
    # Description
    
    This PR targets to refine some details in #1851 
    
    - `start_trace` will set `session_id` in `OperationContext`, then inside
    `@trace` will retrieve this value and set span's attribute.
    - `opentelemetry-exporter-otlp-proto-http` depends on
    `opentelemetry-api`, `opentelemetry-sdk` and `opentelemetry-proto`, so
    try to clean `setup.py` for `prompflow`.
    - update trace example notebook.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.

--- src/promptflow/setup.py
+++ src/promptflow/setup.py
@@ -50,10 +50,7 @@ REQUIRES = [
     "filetype>=1.2.0",  # used to detect the mime type for mulitmedia input
     "jsonschema>=4.0.0,<5.0.0",  # used to validate tool
     "docutils",  # used to generate description for tools
-    "opentelemetry-api>=1.21.0,<2.0.0",  # used to generate trace
-    "opentelemetry-sdk>=1.21.0,<2.0.0",  # used to collect trace
-    "opentelemetry-exporter-otlp-proto-http>=1.22.0,<2.0.0",  # OpenTelemetry exporter
-    "opentelemetry-proto>=1.22.0,<2.0.0",  # OpenTelemetry collector in PFS
+    "opentelemetry-exporter-otlp-proto-http>=1.22.0,<2.0.0",  # trace support
     "flask-restx>=1.2.0,<2.0.0",  # PFS Swagger
 ]
 
