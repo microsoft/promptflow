65ac2fb4a898619c55bbb49ab3220b503c6f22ee
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-02-01 14:13:17 +0800

    remove imports

--- src/promptflow/promptflow/_sdk/_service/__init__.py
+++ src/promptflow/promptflow/_sdk/_service/__init__.py
@@ -3,12 +3,3 @@
 # ---------------------------------------------------------
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-
-from flask_restx import Api, Namespace, Resource, fields  # noqa: F401
-
-__all__ = [
-    "Api",
-    "Namespace",
-    "Resource",
-    "fields",
-]
