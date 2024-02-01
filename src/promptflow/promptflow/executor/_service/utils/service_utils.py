c038e18b1581fcb39cdd1977e0f28e07ce20ead9
Peiwen Gao <peiwengao@microsoft.com>
2024-02-01 17:32:15 +0800

    Add some utils function

--- src/promptflow/promptflow/executor/_service/utils/service_utils.py
+++ src/promptflow/promptflow/executor/_service/utils/service_utils.py
@@ -3,10 +3,13 @@
 # ---------------------------------------------------------
 
 import json
+import os
 
 from promptflow._core.connection_manager import ConnectionManager
+from promptflow._core.operation_context import OperationContext
 from promptflow._utils.exception_utils import ErrorResponse, ExceptionPresenter, JsonSerializedPromptflowException
 from promptflow._utils.logger_utils import LogContext, logger, service_logger
+from promptflow._version import VERSION
 from promptflow.executor._service.contracts.execution_request import BaseExecutionRequest
 
 
@@ -19,6 +22,26 @@ def get_log_context(request: BaseExecutionRequest):
 def get_service_log_context(request: BaseExecutionRequest):
     run_mode = request.get_run_mode()
     return LogContext(file_path=request.log_path, run_mode=run_mode, input_logger=service_logger)
+
+
+def update_operation_context(headers: dict):
+    operation_context = OperationContext.get_instance()
+    # update user agent to operation context
+    operation_context.user_agent = headers.get("context-user-agent", "")
+    executor_user_agent = get_executor_version()
+    operation_context.append_user_agent(executor_user_agent)
+    # update request id to operation context
+    request_id = headers.get("context-request-id", "")
+    operation_context.request_id = request_id
+
+
+def get_executor_version():
+    build_info = os.environ.get("BUILD_INFO", "")
+    try:
+        build_info_dict = json.loads(build_info)
+        return "promptflow-executor/" + build_info_dict["build_number"]
+    except Exception:
+        return "promptflow-executor/" + VERSION
 
 
 def generate_error_response(ex):
