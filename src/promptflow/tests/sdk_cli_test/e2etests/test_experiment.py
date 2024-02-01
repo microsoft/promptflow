5ff4e223c02c12b715c16b4b7dbd0b732c651cd1
Zhen Ruan <zhrua@microsoft.com>
2024-02-01 13:18:04 +0800

    fix failed test case

--- src/promptflow/tests/sdk_cli_test/e2etests/test_experiment.py
+++ src/promptflow/tests/sdk_cli_test/e2etests/test_experiment.py
@@ -32,7 +32,7 @@ yaml = YAML(typ="safe")
 
 
 def mock_start_process_in_background(args, executable_path=None):
-    subprocess.Popen(args, env=os.environ)
+    subprocess.Popen(args, env=os.environ, shell=True)
 
 
 @pytest.mark.e2etest
