31b90a28a70655a82440abf16ac3e9360b739db4
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-02-01 14:17:40 +0800

    Revert "remove imports"
    This reverts commit 65ac2fb4a898619c55bbb49ab3220b503c6f22ee.

--- src/promptflow/promptflow/_sdk/_service/__init__.py
+++ src/promptflow/promptflow/_sdk/_service/__init__.py
@@ -3,3 +3,12 @@
 # ---------------------------------------------------------
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+
+from flask_restx import Api, Namespace, Resource, fields  # noqa: F401
+
+__all__ = [
+    "Api",
+    "Namespace",
+    "Resource",
+    "fields",
+]
