68c461369d9ca053528e7dfcb94997094dcc13ff
Ge Gao <gega@microsoft.com>
2024-02-02 15:06:10 +0800

    refine

--- src/promptflow-tools/promptflow/tools/common.py
+++ src/promptflow-tools/promptflow/tools/common.py
@@ -231,7 +231,7 @@ def handle_openai_error(tries: int = 10, delay: float = 8.0):
                         )
                         print(msg, file=sys.stderr)
                     else:
-                        retry_after_seconds = retry_after_in_header
+                        retry_after_seconds = float(retry_after_in_header)
                         msg = (
                             f"{type(e).__name__} #{i}, Retry-After={retry_after_in_header}, "
                             f"Back off {retry_after_seconds} seconds for retry."
