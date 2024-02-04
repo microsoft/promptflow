888990ea849d86891422d372b74445c2b6b7cbe7
cs_lucky <si.chen@microsoft.com>
2024-02-04 17:23:33 +0800

    add txt

--- examples/gen_test_data/gen_test_data/constants.py
+++ examples/gen_test_data/gen_test_data/constants.py
@@ -1,4 +1,4 @@
 DOCUMENT_NODE = "document_node"
 TEXT_CHUNK = "text_chunk"
 DETAILS_FILE_NAME ="test-data-gen-details.jsonl"
-SUPPORT_FILE_TYPE = [".docx", ".pdf", ".ipynb", ".md"]
+SUPPORT_FILE_TYPE = [".docx", ".pdf", ".ipynb", ".md", ".txt"]
