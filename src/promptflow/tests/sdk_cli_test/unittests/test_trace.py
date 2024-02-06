3a88bb7514184f1a1dbd8f31ff82f7cd4a175f88
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-02-06 17:09:15 +0800

    add UT for setup exporter

--- src/promptflow/tests/sdk_cli_test/unittests/test_trace.py
+++ src/promptflow/tests/sdk_cli_test/unittests/test_trace.py
@@ -2,12 +2,17 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+import os
 import uuid
+from unittest.mock import patch
 
 import pytest
+from opentelemetry import trace
+from opentelemetry.sdk.environment_variables import OTEL_EXPORTER_OTLP_ENDPOINT
+from opentelemetry.sdk.trace import TracerProvider
 
-from promptflow._constants import ResourceAttributeFieldName
-from promptflow._trace._start_trace import _create_resource
+from promptflow._constants import ResourceAttributeFieldName, TraceEnvironmentVariableName
+from promptflow._trace._start_trace import _create_resource, _is_tracer_provider_configured, setup_exporter_from_environ
 
 
 @pytest.mark.sdk_test
@@ -23,3 +28,26 @@ class TestStartTrace:
         resource2 = _create_resource(session_id=session_id, experiment=experiment)
         assert resource2.attributes[ResourceAttributeFieldName.SESSION_ID] == session_id
         assert resource2.attributes[ResourceAttributeFieldName.EXPERIMENT_NAME] == experiment
+
+    def test_setup_exporter_from_environ(self):
+        assert not _is_tracer_provider_configured()
+
+        # set some required environment variables
+        endpoint = "http://localhost:23333/v1/traces"
+        session_id = str(uuid.uuid4())
+        experiment = "test_experiment"
+        with patch.dict(
+            os.environ,
+            {
+                OTEL_EXPORTER_OTLP_ENDPOINT: endpoint,
+                TraceEnvironmentVariableName.SESSION_ID: session_id,
+                TraceEnvironmentVariableName.EXPERIMENT: experiment,
+            },
+            clear=True,
+        ):
+            setup_exporter_from_environ()
+
+        assert _is_tracer_provider_configured()
+        tracer_provider: TracerProvider = trace.get_tracer_provider()
+        assert session_id == tracer_provider._resource.attributes[ResourceAttributeFieldName.SESSION_ID]
+        assert experiment == tracer_provider._resource.attributes[ResourceAttributeFieldName.EXPERIMENT_NAME]
