bb12852421833291c289c261d24c1fb7087da081
Peiwen Gao <peiwengao@microsoft.com>
2024-02-06 11:21:11 +0800

    Add retrieve_tool_func_result apis

--- src/promptflow/promptflow/executor/_service/apis/tool.py
+++ src/promptflow/promptflow/executor/_service/apis/tool.py
@@ -10,7 +10,7 @@ from promptflow._core.tools_manager import collect_package_tools
 from promptflow._utils.context_utils import _change_working_dir, inject_sys_path
 from promptflow.contracts.tool import ToolType
 from promptflow.executor._service._errors import ExecutionTimeoutError, GenerateMetaTimeout
-from promptflow.executor._service.contracts.tool_request import ToolMetaRequest
+from promptflow.executor._service.contracts.tool_request import RetrieveToolFuncResultRequest, ToolMetaRequest
 from promptflow.executor._service.utils.process_utils import SHORT_WAIT_TIMEOUT, invoke_function_in_process
 from promptflow.executor._service.utils.service_utils import generate_error_response
 
@@ -20,6 +20,18 @@ router = APIRouter(prefix="/tool")
 @router.get("/package_tools")
 async def list_package_tools():
     return collect_package_tools()
+
+
+@router.post("/retrieve_tool_func_result")
+async def retrieve_tool_func_result(request: RetrieveToolFuncResultRequest):
+    from promptflow._core.tools_manager import retrieve_tool_func_result
+
+    return retrieve_tool_func_result(
+        func_call_scenario=request.func_call_scenario,
+        func_path=request.func_path,
+        func_input_params_dict=request.func_kwargs,
+        ws_triple_dict=request.ws_triple,
+    )
 
 
 @router.post("/meta")
--- src/promptflow/promptflow/executor/_service/contracts/tool_request.py
+++ src/promptflow/promptflow/executor/_service/contracts/tool_request.py
@@ -3,11 +3,22 @@
 # ---------------------------------------------------------
 
 from pathlib import Path
-from typing import Mapping
+from typing import Any, Mapping
 
 from pydantic import BaseModel
 
 
 class ToolMetaRequest(BaseModel):
+    """Request model for generating tool meta."""
+
     working_dir: Path
     tools: Mapping[str, Mapping[str, str]]
+
+
+class RetrieveToolFuncResultRequest(BaseModel):
+    """Request model for retrieving tool function result."""
+
+    func_path: str
+    func_kwargs: Mapping[str, Any]
+    func_call_scenario: str = None
+    ws_triple: Mapping[str, str] = None
