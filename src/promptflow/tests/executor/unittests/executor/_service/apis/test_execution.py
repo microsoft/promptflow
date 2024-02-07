5f13bdac69c2ed06f6e12a9a75ca73baf97afeb6
Peiwen Gao <peiwengao@microsoft.com>
2024-02-07 14:03:27 +0800

    Add test cases

---
+++ src/promptflow/tests/executor/unittests/executor/_service/apis/test_execution.py
@@ -0,0 +1,40 @@
+import uuid
+from pathlib import Path
+from tempfile import mkdtemp
+
+import pytest
+from fastapi.testclient import TestClient
+
+from promptflow.executor._service.app import app
+
+from .....utils import get_flow_folder
+
+
+def construct_flow_execution_request_json(flow_folder, inputs=None, connections=None):
+    working_dir = get_flow_folder(flow_folder)
+    tmp_dir = Path(mkdtemp())
+    log_path = tmp_dir / "log.txt"
+    return {
+        "run_id": str(uuid.uuid4()),
+        "working_dir": working_dir.as_posix(),
+        "flow_file": "flow.dag.yaml",
+        "output_dir": tmp_dir.as_posix(),
+        "connections": connections,
+        "log_path": log_path.as_posix(),
+        "inputs": inputs,
+    }
+
+
+@pytest.mark.unittest
+class TestExecutionApis:
+    def setup_method(self):
+        self.client = TestClient(app)
+
+    def test_flow_execution(self, dev_connections):
+        flow_execution_request = construct_flow_execution_request_json(
+            flow_folder="print_input_flow",
+            inputs={"text": "text_0"},
+            connections=dev_connections,
+        )
+        response = self.client.post("/execution/flow", json=flow_execution_request)
+        assert response.status_code == 200
