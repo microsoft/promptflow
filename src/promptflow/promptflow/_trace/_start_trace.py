64e4c4c476ab303d63a47c62879814b6bdd152d9
Brynn Yin <biyi@microsoft.com>
2024-02-20 16:42:33 +0800

    Update otel to remove none
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- src/promptflow/promptflow/_trace/_start_trace.py
+++ src/promptflow/promptflow/_trace/_start_trace.py
@@ -60,8 +60,11 @@ def start_trace(*, session: typing.Optional[str] = None, **kwargs):
     env_attributes = json.loads(env_trace_context).get("attributes") if env_trace_context else {}
     experiment = env_attributes.get(ContextAttributeKey.EXPERIMENT, None)
     ref_line_run_id = env_attributes.get(ContextAttributeKey.REFERENCED_LINE_RUN_ID, None)
-    # Set reference line run id even if it's None to avoid stale value set by previous node
-    operation_context._add_otel_attributes(SpanAttributeFieldName.REFERENCED_LINE_RUN_ID, ref_line_run_id)
+    # Remove reference line run id if it's None to avoid stale value set by previous node
+    if ref_line_run_id is None:
+        operation_context._remove_otel_attributes(SpanAttributeFieldName.REFERENCED_LINE_RUN_ID)
+    else:
+        operation_context._add_otel_attributes(SpanAttributeFieldName.REFERENCED_LINE_RUN_ID, ref_line_run_id)
 
     # init the global tracer with endpoint
     _init_otel_trace_exporter(otlp_port=pfs_port, session_id=session_id, experiment=experiment)
