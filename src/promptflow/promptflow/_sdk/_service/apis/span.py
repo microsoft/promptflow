dacd2fdc3da70e48e640b784bcd0cbfb6e889c7b
Zhengfei Wang <38847871+zhengfeiwang@users.noreply.github.com>
2024-02-20 11:08:49 +0800

    [promptflow][internal] Support trace UI page (#2035)
    # Description
    
    This PR targets to support real trace UI page:
    
    
    ![image](https://github.com/microsoft/promptflow/assets/38847871/c1c243c4-8001-424e-bc68-59d4a0b3c662)
    
    **Trace related APIs**
    
    - Spans: `/v1.0/Spans/list`
    - LineRuns: `/v1.0/LineRuns/list`
    
    Both APIs have strong type in Swagger models, however, for `attributes`
    and `resource.attributes`, we still use raw dictionary as marshmallow
    (flask-restx back) cannot parse field name with dot (".").
    
    **Other changes**
    
    - Expose `start_trace`, so that user can use `pf.start_trace` or `from
    promptflow import start_trace` - not announce this until we are fully
    ready.
    - Convert the type of `LineRun` evaluations from dictionary to list of
    dictionary to better support strong type.
    - Support CORS handling for PFS, especially for UX local development.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow/promptflow/_sdk/_service/apis/span.py
+++ src/promptflow/promptflow/_sdk/_service/apis/span.py
@@ -7,19 +7,12 @@ from dataclasses import dataclass
 
 from flask_restx import fields
 
-from promptflow._constants import (
-    SpanAttributeFieldName,
-    SpanContextFieldName,
-    SpanFieldName,
-    SpanResourceAttributesFieldName,
-    SpanResourceFieldName,
-    SpanStatusFieldName,
-)
+from promptflow._constants import SpanContextFieldName, SpanFieldName, SpanResourceFieldName, SpanStatusFieldName
 from promptflow._sdk._constants import PFS_MODEL_DATETIME_FORMAT
 from promptflow._sdk._service import Namespace, Resource
 from promptflow._sdk._service.utils.utils import get_client_from_request
 
-api = Namespace("spans", description="Span Management")
+api = Namespace("Spans", description="Spans Management")
 
 # parsers for query parameters
 list_span_parser = api.parser()
@@ -54,48 +47,29 @@ status_model = api.model(
         SpanStatusFieldName.STATUS_CODE: fields.String(required=True),
     },
 )
-attributes_model = api.model(
-    "Attributes",
-    {
-        SpanAttributeFieldName.FRAMEWORK: fields.String(required=True, default="promptflow"),
-        SpanAttributeFieldName.SPAN_TYPE: fields.String(required=True, default="Function"),
-        SpanAttributeFieldName.FUNCTION: fields.String(required=True),
-        SpanAttributeFieldName.INPUTS: fields.String(required=True),
-        SpanAttributeFieldName.OUTPUT: fields.String(required=True),
-        SpanAttributeFieldName.SESSION_ID: fields.String(required=True),
-        SpanAttributeFieldName.PATH: fields.String,
-        SpanAttributeFieldName.FLOW_ID: fields.String,
-        SpanAttributeFieldName.RUN: fields.String,
-        SpanAttributeFieldName.EXPERIMENT: fields.String,
-    },
-)
-resource_attributes_model = api.model(
-    "ResourceAttributes",
-    {
-        SpanResourceAttributesFieldName.SERVICE_NAME: fields.String(default="promptflow"),
-    },
-)
 resource_model = api.model(
     "Resource",
     {
-        SpanResourceFieldName.ATTRIBUTES: fields.Nested(resource_attributes_model, required=True),
+        SpanResourceFieldName.ATTRIBUTES: fields.Raw(required=True),
         SpanResourceFieldName.SCHEMA_URL: fields.String,
     },
 )
 span_model = api.model(
     "Span",
     {
+        # marshmallow fields cannot parse field name with dot (e.g., referenced.line_run_id)
+        # so for `attributes` and `resource.attributes`, not support strong type with raw dict
         SpanFieldName.NAME: fields.String(required=True),
-        SpanFieldName.CONTEXT: fields.Nested(context_model, required=True),
+        SpanFieldName.CONTEXT: fields.Nested(context_model, required=True, skip_none=True),
         SpanFieldName.KIND: fields.String(required=True),
         SpanFieldName.PARENT_ID: fields.String,
         SpanFieldName.START_TIME: fields.DateTime(dt_format=PFS_MODEL_DATETIME_FORMAT),
         SpanFieldName.END_TIME: fields.DateTime(dt_format=PFS_MODEL_DATETIME_FORMAT),
-        SpanFieldName.STATUS: fields.Nested(status_model),
-        SpanFieldName.ATTRIBUTES: fields.Nested(attributes_model, required=True),
+        SpanFieldName.STATUS: fields.Nested(status_model, skip_none=True),
+        SpanFieldName.ATTRIBUTES: fields.Raw(required=True),
         SpanFieldName.EVENTS: fields.List(fields.String),
         SpanFieldName.LINKS: fields.List(fields.String),
-        SpanFieldName.RESOURCE: fields.Nested(resource_model, required=True),
+        SpanFieldName.RESOURCE: fields.Nested(resource_model, required=True, skip_none=True),
     },
 )
 
