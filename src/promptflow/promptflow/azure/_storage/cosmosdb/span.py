22234dd958e2903f80b46c0d8f4869303bec1d84
robbenwang <robbenwang@microsoft.com>
2024-02-21 07:01:45 +0000

    [WIP] Persist summary item to cosmosDB.

--- src/promptflow/promptflow/azure/_storage/cosmosdb/span.py
+++ src/promptflow/promptflow/azure/_storage/cosmosdb/span.py
@@ -2,11 +2,15 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from typing import Dict, Any
+from typing import Any, Dict
+
+from promptflow._constants import SpanFieldName
+from promptflow._sdk.entities._trace import Span as SpanEntity
+
 from .client import get_client, get_client_with_workspace_info
 
 
-class Span():
+class Span:
     __container__ = "Span"
 
     name: str = None
@@ -23,61 +27,42 @@ class Span():
     id: str = None
     partition_key: str = None
 
-    def __init__(self, 
-                 name: str, 
-                 context: dict, 
-                 kind: str, 
-                 parent_id: str, 
-                 start_time: str, 
-                 end_time: str, 
-                 status: dict, 
-                 attributes: dict, 
-                 events: list = None, 
-                 links: list = None, 
-                 resource: dict = None,
-                 **kwargs):
-        self.name = name
-        self.context = context
-        self.kind = kind
-        self.parent_id = parent_id
-        self.start_time = start_time
-        self.end_time = end_time
-        self.status = status
-        self.attributes = attributes
-        self.events = events
-        self.links = links
-        self.resource = resource
-        self.id = attributes.get("line_run_id", None) if attributes else None
-        self.partition_key = attributes.get("session_id", None) if attributes else None
+    def __init__(self, span: SpanEntity) -> None:
+        self.name = span.name
+        self.context = span._content[SpanFieldName.CONTEXT]
+        self.kind = span._content[SpanFieldName.KIND]
+        self.parent_id = span.parent_span_id
+        self.start_time = span._content[SpanFieldName.START_TIME]
+        self.end_time = span._content[SpanFieldName.END_TIME]
+        self.status = span._content[SpanFieldName.STATUS]
+        self.attributes = span._content[SpanFieldName.ATTRIBUTES]
+        self.events = span._content[SpanFieldName.EVENTS]
+        self.links = span._content[SpanFieldName.LINKS]
+        self.resource = span._content[SpanFieldName.RESOURCE]
+        self.partition_key = span.session_id
+        self.id = span.span_id
 
     def persist(self):
         if self.id is None or self.partition_key is None or self.resource is None:
             return
-        
+
         resource_attributes = self.resource.get("attributes", None)
         if resource_attributes is None:
             return
-        
+
         client = get_client_with_workspace_info(self.__container__, resource_attributes)
-        return client.create_item(body = self.to_dict())
-        
+        return client.create_item(body=self.to_dict())
+
     @classmethod
-    def from_cosmosdb(self, id: str, partition_key: str, 
-                      subscription_id: str,
-                      resource_group_name: str,
-                      workspace_name: str) -> "Span":
+    def from_cosmosdb(
+        self, id: str, partition_key: str, subscription_id: str, resource_group_name: str, workspace_name: str
+    ) -> "Span":
         client = get_client(Span.__container__, subscription_id, resource_group_name, workspace_name)
         data = client.read_item(id, partition_key)
         params = {}
         for key, value in data.items():
             params[key] = value
-        return Span(**params)
+        return params
 
     def to_dict(self) -> Dict[str, Any]:
         return {k: v for k, v in self.__dict__.items() if v}
-    
-    
-
-
-
-    
