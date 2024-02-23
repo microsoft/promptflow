cda4908dc9a8460b3c21d02a366ea2735ed64f68
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-02-23 16:19:05 +0800

    evaluation.name => evaluation.display_name

--- src/promptflow/promptflow/_sdk/entities/_trace.py
+++ src/promptflow/promptflow/_sdk/entities/_trace.py
@@ -177,7 +177,7 @@ class _LineRunData:
     end_time: str
     status: str
     latency: float
-    name: str
+    display_name: str
     kind: str
     cumulative_token_count: typing.Optional[typing.Dict[str, int]]
 
@@ -210,7 +210,7 @@ class _LineRunData:
             end_time=end_time.isoformat(),
             status=span._content[SpanFieldName.STATUS][SpanStatusFieldName.STATUS_CODE],
             latency=(end_time - start_time).total_seconds(),
-            name=span.name,
+            display_name=span.name,
             kind=attributes.get(SpanAttributeFieldName.SPAN_TYPE, span.span_type),
             cumulative_token_count=cumulative_token_count,
         )
@@ -268,7 +268,7 @@ class LineRun:
             end_time=main_line_run_data.end_time,
             status=main_line_run_data.status,
             latency=main_line_run_data.latency,
-            name=main_line_run_data.name,
+            name=main_line_run_data.display_name,
             kind=main_line_run_data.kind,
             cumulative_token_count=main_line_run_data.cumulative_token_count,
             evaluations=evaluations,
