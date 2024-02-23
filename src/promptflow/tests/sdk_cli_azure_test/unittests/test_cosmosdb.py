327a8929ee8ced2a1f60614d6a779e313cfb5414
riddle xu <riddlexu@users.noreply.github.com>
2024-02-23 16:10:48 +0800

    Resolve CosmosResourceExistsError (#2096)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.
    
    ---------
    
    Co-authored-by: Yangtong Xu <yangtongxu@microsoft.com>

--- src/promptflow/tests/sdk_cli_azure_test/unittests/test_cosmosdb.py
+++ src/promptflow/tests/sdk_cli_azure_test/unittests/test_cosmosdb.py
@@ -1,42 +1,31 @@
 import pytest
 from promptflow.azure._storage.cosmosdb.span import Span
+from promptflow._sdk.entities._trace import Span as SpanEntity
+
 
 @pytest.mark.unittest
 class TestSpan:
     def test_to_dict(self):
         span = Span(
-            name="test",
-            context={},
-            kind="test",
-            parent_id="test",
-            start_time="test",
-            end_time="test",
-            status={},
-            attributes={},
-            events=[],
-            links=[],
-            resource={}
-        )
-        assert span.to_dict() == {
-            "name": "test",
-            "kind": "test",
-            "parent_id": "test",
-            "start_time": "test",
-            "end_time": "test"
-        }
+            SpanEntity(
+                name="test",
+                context={
+                    "trace_id": "0xacf2291a630af328da8fabd6bf49f653",
+                    "span_id": "0x9ded7ce65d5f7775",
+                },
+                kind="test",
+                parent_span_id="test",
+                start_time="test",
+                end_time="test",
+                status={},
+                attributes={},
+                events=[],
+                links=[],
+                resource={},
+                span_type=None,
+                session_id=None,
 
-        span = Span(
-            name="test",
-            context={},
-            kind="test",
-            parent_id="test",
-            start_time="test",
-            end_time="test",
-            status={},
-            attributes={"line_run_id": "test_line_run_id", "session_id": "test_session_id"},
-            events=[],
-            links=[],
-            resource={}
+            )
         )
         assert span.to_dict() == {
             "name": "test",
@@ -44,7 +33,45 @@ class TestSpan:
             "parent_id": "test",
             "start_time": "test",
             "end_time": "test",
-            "attributes": {"line_run_id": "test_line_run_id", "session_id": "test_session_id"},
-            "id": "test_line_run_id",
+            "context": {
+                    "trace_id": "0xacf2291a630af328da8fabd6bf49f653",
+                    "span_id": "0x9ded7ce65d5f7775",
+            },
+            "id": "0x9ded7ce65d5f7775",
+        }
+
+        span = Span(
+            SpanEntity(
+                name="test",
+                context={
+                    "trace_id": "0xacf2291a630af328da8fabd6bf49f653",
+                    "span_id": "0x9ded7ce65d5f7775",
+                },
+                kind="test",
+                parent_span_id="test",
+                start_time="test",
+                end_time="test",
+                status={},
+                attributes={"line_run_id": "test_line_run_id"},
+                events=[],
+                links=[],
+                resource={},
+                span_type=None,
+                session_id="test_session_id",
+            )
+        )
+        assert span.to_dict() == {
+            "name": "test",
+            "kind": "test",
+            "parent_id": "test",
+            "start_time": "test",
+            "end_time": "test",
+            "attributes": {"line_run_id": "test_line_run_id"},
+            "partition_key": "test_session_id",
+            "context": {
+                    "trace_id": "0xacf2291a630af328da8fabd6bf49f653",
+                    "span_id": "0x9ded7ce65d5f7775",
+            },
+            "id": "0x9ded7ce65d5f7775",
             "partition_key": "test_session_id",
         }
