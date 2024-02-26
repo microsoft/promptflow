25c4b39adc34d8963d2326620e02c1c99614f1fe
Zhengfei Wang <38847871+zhengfeiwang@users.noreply.github.com>
2024-02-26 11:14:24 +0800

    [internal][trace] Support list line runs with `run` and `experiment` (#2094)
    # Description
    
    **Experience**
    
    - Order tree views with start time **ASC**, line runs/summaries with
    start time **DESC**
    - Larger view space
    - `start_trace` will print different url for different scenarios:
      - All: `http://localhost:<port>/v1.0/ui/traces`
    - Specific experiment:
    `http://localhost:<port>/v1.0/ui/traces?experiment=<exp>`
    - Specific run(s):
    `http://localhost:<port>/v1.0/ui/traces?run=<run1>,<run2>`
    - Specific session:
    `http://localhost:<port>/v1.0/ui/traces?session=<session_id>`
    
    **SDK/PFS change list**
    
    - Support query parameter `run`, `experiment` and none
    - Order line runs by start time **DESC**
    - Refine printed url in `start_trace`
    - Fix eager flow OpenAI injection bug:
    https://github.com/microsoft/promptflow/pull/2094/commits/1ebf539dc7628f289eea6b3c4de7218c0918aa9b
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow/promptflow/_sdk/entities/_trace.py
+++ src/promptflow/promptflow/_sdk/entities/_trace.py
@@ -144,6 +144,9 @@ class Span:
         session_id = resource_attributes[SpanResourceAttributesFieldName.SESSION_ID]
         experiment = resource_attributes.get(SpanResourceAttributesFieldName.EXPERIMENT_NAME, None)
 
+        # if `batch_run_id` exists, record the run
+        run = attributes.get(SpanAttributeFieldName.BATCH_RUN_ID, None)
+
         return Span(
             name=obj.name,
             context=context,
@@ -156,6 +159,7 @@ class Span:
             span_type=span_type,
             session_id=session_id,
             parent_span_id=parent_span_id,
+            run=run,
             experiment=experiment,
         )
 
@@ -173,7 +177,7 @@ class _LineRunData:
     end_time: str
     status: str
     latency: float
-    name: str
+    display_name: str
     kind: str
     cumulative_token_count: typing.Optional[typing.Dict[str, int]]
 
@@ -206,7 +210,7 @@ class _LineRunData:
             end_time=end_time.isoformat(),
             status=span._content[SpanFieldName.STATUS][SpanStatusFieldName.STATUS_CODE],
             latency=(end_time - start_time).total_seconds(),
-            name=span.name,
+            display_name=span.name,
             kind=attributes.get(SpanAttributeFieldName.SPAN_TYPE, span.span_type),
             cumulative_token_count=cumulative_token_count,
         )
@@ -264,7 +268,7 @@ class LineRun:
             end_time=main_line_run_data.end_time,
             status=main_line_run_data.status,
             latency=main_line_run_data.latency,
-            name=main_line_run_data.name,
+            name=main_line_run_data.display_name,
             kind=main_line_run_data.kind,
             cumulative_token_count=main_line_run_data.cumulative_token_count,
             evaluations=evaluations,
