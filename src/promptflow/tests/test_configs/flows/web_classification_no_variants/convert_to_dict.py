92eaa9f826ed1f417df36690bdf68c97864c4e3c
chengbin wang <chw@microsoft.com>
2024-02-26 15:58:37 +0800

    temporary change

--- src/promptflow/tests/test_configs/flows/web_classification_no_variants/convert_to_dict.py
+++ src/promptflow/tests/test_configs/flows/web_classification_no_variants/convert_to_dict.py
@@ -7,8 +7,8 @@ from promptflow import tool
 @tool
 def convert_to_dict(input_str: str):
     try:
-        # Sleep for 1.2 seconds
-        time.sleep(1.2)
+        # Sleep for 1.3 seconds
+        time.sleep(1.3)
         return json.loads(input_str)
     except Exception as e:
         print("input is not valid, error: {}".format(e))
