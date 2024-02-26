ece9c74699e10d58ba9924373db12cd8b8d00b4d
Ming Gu 🙂 <minggu.thuee@outlook.com>
2024-02-26 14:57:04 +0800

    Add resume aggregation test case

---
+++ examples/flows/evaluation/test-resume-aggregation/hello.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+from pathlib import Path
+from promptflow import tool
+
+# The inputs section will change based on the arguments of the tool function, after you save the code
+# Adding type to arguments and return value will help the system show the types properly
+# Please update the function name/signature per need
+
+
+@tool
+def my_python_tool(input1: str) -> str:
+    switch_file = Path("sunday_failure_switch.txt")
+    if "Sunday" in input1:
+        if switch_file.exists() and "Sunday" in input1:
+            switch_file.unlink()
+            raise Exception("Sunday failure")
+        else:
+            switch_file.touch()
+    return "Prompt: " + input1
