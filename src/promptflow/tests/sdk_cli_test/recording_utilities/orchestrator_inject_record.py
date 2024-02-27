5445d65dd9e6b5fb5d60c0d0be4d8017aadc07e6
Zhen Ruan <zhrua@microsoft.com>
2024-02-27 22:04:28 +0800

    fix test case

--- src/promptflow/tests/sdk_cli_test/recording_utilities/orchestrator_inject_record.py
+++ src/promptflow/tests/sdk_cli_test/recording_utilities/orchestrator_inject_record.py
@@ -1,14 +1,14 @@
 import sys
 
 from sdk_cli_test.conftest import setup_recording_injection_if_enabled
-from sdk_cli_test.recording_utilities import is_record
+from sdk_cli_test.recording_utilities import is_replay
 
 from promptflow._sdk._submitter.experiment_orchestrator import main
 from promptflow._sdk._submitter.utils import _start_process_in_background
 
 
 def mock_start_process_in_background(args, executable_path=None):
-    if is_record():
+    if is_replay():
         args[1] = __file__
     _start_process_in_background(args, executable_path)
 
