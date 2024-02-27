d6d8b336e7d8e19e04a741566c44c4cb2c0432ce
Brynn Yin <biyi@microsoft.com>
2024-02-27 10:56:17 +0800

    Update message
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- src/promptflow/tests/test_configs/flows/flow_with_aad_connection/assert_aad_conn.py
+++ src/promptflow/tests/test_configs/flows/flow_with_aad_connection/assert_aad_conn.py
@@ -5,5 +5,5 @@ from promptflow.connections import AzureOpenAIConnection
 @tool
 def assert_aad_conn(conn: AzureOpenAIConnection) -> str:
     assert conn.auth_mode == "meid_token"
-    assert not conn.api_key, f"expected api_key to be empty, but got {conn.api_key}"
+    assert not conn.api_key, f"expected api_key to be empty, but not"
     return conn.auth_mode
