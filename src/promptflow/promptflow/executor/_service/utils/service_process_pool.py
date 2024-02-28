b727c1727ac6ccea2f67e5c23f764570cfc6838a
Peiwen Gao <peiwengao@microsoft.com>
2024-02-28 13:45:41 +0800

    Add ServiceProcessPool

---
+++ src/promptflow/promptflow/executor/_service/utils/service_process_pool.py
@@ -0,0 +1,21 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+
+class ServiceProcessPool:
+    def __init__(self):
+        # create process
+
+        # create thread
+
+        # create other
+        pass
+
+    def submit(self):
+        # submit
+        pass
+
+    def shutdown(self):
+        # shutdown
+        pass
