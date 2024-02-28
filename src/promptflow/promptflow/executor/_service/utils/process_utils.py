26d9e49849d9285892a3642a9ea47d8ed8c22d7d
Peiwen Gao <peiwengao@microsoft.com>
2024-02-28 13:37:30 +0800

    Test function

--- src/promptflow/promptflow/executor/_service/utils/process_utils.py
+++ src/promptflow/promptflow/executor/_service/utils/process_utils.py
@@ -48,7 +48,7 @@ async def invoke_sync_function_in_process(
 
         # Wait for the process to finish or timeout asynchronously
         start_time = datetime.utcnow()
-        while (datetime.utcnow() - start_time).total_seconds() < wait_timeout and _is_process_alive(p.pid):
+        while (datetime.utcnow() - start_time).total_seconds() < wait_timeout and _is_process_alive(p):
             await asyncio.sleep(1)
 
         try:
