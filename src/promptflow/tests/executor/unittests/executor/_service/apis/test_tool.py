6e635e4f0b801df0444a8d5e8fcec7cd5cff3b20
Peiwen Gao <peiwengao@microsoft.com>
2024-02-28 20:07:30 +0800

    Update test cases

--- src/promptflow/tests/executor/unittests/executor/_service/apis/test_tool.py
+++ src/promptflow/tests/executor/unittests/executor/_service/apis/test_tool.py
@@ -31,13 +31,20 @@ class TestToolApis:
         assert all(isinstance(tool, dict) for name, tool in package_tools.items())
 
     def test_gen_tool_meta_all_completed(self):
-        request = construct_tool_meta_request_json()
+        flow_folder = "web_classification"
+        tools = [
+            ("fetch_text_content_from_url.py", "python"),
+            ("prepare_examples.py", "python"),
+            ("classify_with_llm.jinja2", "llm"),
+            ("convert_to_dict.py", "python"),
+        ]
+        request = construct_tool_meta_request_json(flow_folder, tools)
         response = self.client.post(url="/tool/meta", json=request)
         # assert response
         assert response.status_code == 200
         tool_meta = response.json()
-        assert tool_meta["tools"]
-        assert tool_meta["errors"]
+        assert len(tool_meta["tools"]) == len(tools)
+        assert len(tool_meta["errors"]) == 0
 
     def test_gen_tool_meta_partial_failed(self):
         pass
