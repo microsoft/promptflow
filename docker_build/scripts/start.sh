bc4e9d2e250e5de3b3249f16821ff67f6bf6d710
Peiwen Gao <peiwengao@microsoft.com>
2024-02-29 14:14:19 +0800

    Update docker build files

---
+++ docker_build/scripts/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+source $BASEDIR/auto_detect_env.sh
+
+# stop services created by runsv and propagate SIGTERM to child jobs
+sv_stop() {
+    echo "$(date -uIns) - Stopping all runsv services"
+    for s in $(ls -d /var/runit/*); do
+        sv stop $s
+    done
+}
+
+# register SIGTERM handler
+trap sv_stop SIGTERM
+
+# start services in background and wait all child jobs
+runsvdir /var/runit &
+wait
