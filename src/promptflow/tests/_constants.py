e2223c80fccc26cbf45ac4725f6bc325a1c3604f
Brynn Yin <biyi@microsoft.com>
2024-02-29 19:28:06 +0800

    Revert test config
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- src/promptflow/tests/_constants.py
+++ src/promptflow/tests/_constants.py
@@ -10,7 +10,7 @@ ENV_FILE = (PROMOTFLOW_ROOT / ".env").resolve().absolute().as_posix()
 # below constants are used for pfazure and global config tests
 DEFAULT_SUBSCRIPTION_ID = "96aede12-2f73-41cb-b983-6d11a904839b"
 DEFAULT_RESOURCE_GROUP_NAME = "promptflow"
-DEFAULT_WORKSPACE_NAME = "promptflow-canary-dev"
-DEFAULT_COMPUTE_INSTANCE_NAME = "biyi1"
-DEFAULT_RUNTIME_NAME = "brynn-test"
+DEFAULT_WORKSPACE_NAME = "promptflow-eastus2euap"
+DEFAULT_COMPUTE_INSTANCE_NAME = "ci-lin-cpu-sp"
+DEFAULT_RUNTIME_NAME = "test-runtime-ci"
 DEFAULT_REGISTRY_NAME = "promptflow-preview"
