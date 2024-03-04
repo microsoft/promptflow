78e42d8f41fff67894dd50fa452fe9c9b0c35f48
Lina Tang <linatang@microsoft.com>
2024-03-04 17:16:34 +0800

    tmp

--- src/promptflow/tests/test_configs/flows/flow_with_nested_tool/nested_tool.py
+++ src/promptflow/tests/test_configs/flows/flow_with_nested_tool/nested_tool.py
@@ -5,4 +5,10 @@ from promptflow import tool
 def echo(input: str, recursive_call=True) -> str:
     if recursive_call:
         return echo(input, recursive_call=False)
+    
+    from promptflow._core.operation_context import OperationContext
+
+    attrs = OperationContext.get_instance()._get_otel_attributes()
+    raise Exception(f"{attrs}")
+
     return input
