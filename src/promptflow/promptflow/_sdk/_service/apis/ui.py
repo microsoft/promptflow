9bee9da7cf93bd236d65d53c31f8cd30844d18fb
chenyin <chenyin@microsoft.com>
2024-03-04 11:46:25 +0800

    restrict image path

--- src/promptflow/promptflow/_sdk/_service/apis/ui.py
+++ src/promptflow/promptflow/_sdk/_service/apis/ui.py
@@ -9,6 +9,7 @@ import os
 import uuid
 from flask import make_response, request, send_from_directory
 from pathlib import Path
+from werkzeug.utils import safe_join
 
 from promptflow._sdk._constants import HOME_PROMPT_FLOW_DIR
 from promptflow._sdk._service import Namespace, Resource, fields
@@ -63,7 +64,12 @@ class ImageUrl(Resource):
     @api.doc(description="Get image url")
     def get(self):
         args = image_path_parser.parse_args()
-        image_path = args.image_path
+        image_path = Path(args.image_path).as_posix()
+        home_prompt_flow_dir = HOME_PROMPT_FLOW_DIR.as_posix()
+
+        if not image_path.startswith(home_prompt_flow_dir):
+            return make_response(f"Can only access images in the restricted directory: {home_prompt_flow_dir}", 403)
+
         if not os.path.exists(image_path):
             return make_response("The image doesn't exist", 404)
         directory, filename = os.path.split(image_path)
@@ -71,9 +77,19 @@ class ImageUrl(Resource):
         return f"{request.base_url}/{str(directory)}/{filename}"
 
 
-@api.route('/image/<path:directory>/<path:filename>')
+@api.route('/image/<path:filepath>')
 class ImageView(Resource):
     @api.doc(description="Visualize image")
     @api.response(code=200, description="Visualize image", model=fields.String)
-    def get(self, directory, filename):
+    def get(self, filepath):
+        directory, filename = os.path.split(filepath)
+        directory = Path(directory).as_posix()
+        home_prompt_flow_dir = HOME_PROMPT_FLOW_DIR.as_posix()
+        if directory != home_prompt_flow_dir:
+            return make_response(f"Can only access images in the restricted directory: {home_prompt_flow_dir}", 403)
+
+        image_path = safe_join(directory, filename)
+        if image_path is None:
+            return make_response("Invalid path", 400)
+
         return send_from_directory(directory, filename)
