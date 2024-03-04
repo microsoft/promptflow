b94b42a220efafa26069d52f717f72d25e2039a6
Yuhang Liu <yuhaliu@microsoft.com>
2024-03-04 11:56:59 +0800

    update

--- src/promptflow/tests/executor/unittests/_core/test_token_provider.py
+++ src/promptflow/tests/executor/unittests/_core/test_token_provider.py
@@ -4,14 +4,12 @@ from promptflow._core.token_provider import AzureTokenProvider
 
 
 def test_get_token_with_sovereign_credential():
-    from azure.ai.ml._azure_environments import AzureEnvironments
-
     with (
         patch('azure.ai.ml._azure_environments._get_default_cloud_name') as mock_cloud_name,
         patch('azure.ai.ml._azure_environments._get_cloud') as mock_cloud,
         patch('azure.identity.DefaultAzureCredential') as mock_credential,
     ):
-        mock_cloud_name.return_value = AzureEnvironments.ENV_CHINA
+        mock_cloud_name.return_value = "AzureChinaCloud"
         cloud = mock_cloud.return_value
         cloud.get.return_value = "authority"
         mock_token = "mocked_token"
