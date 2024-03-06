2d24b27a1b038dfb5774b4d3b79b3b255fb6b952
Peiwen Gao <peiwengao@microsoft.com>
2024-03-06 12:23:48 +0800

    Fix bug of create folder

--- src/promptflow/promptflow/storage/_queue_run_storage.py
+++ src/promptflow/promptflow/storage/_queue_run_storage.py
@@ -38,12 +38,12 @@ class ServiceQueueRunStorage(QueueRunStorage):
 
     def persist_flow_run(self, run_info: FlowRunInfo):
         super().persist_flow_run(run_info)
-        flow_folder = prepare_folder(self._flow_artifacts_path / run_info.index)
+        flow_folder = prepare_folder(self._flow_artifacts_path / str(run_info.index))
         self._process_multimedia_in_run_info(run_info, flow_folder)
 
     def persist_node_run(self, run_info: NodeRunInfo):
         super().persist_node_run(run_info)
-        node_folder = prepare_folder(self._node_artifacts_path / run_info.index / run_info.node)
+        node_folder = prepare_folder(self._node_artifacts_path / str(run_info.index) / run_info.node)
         self._process_multimedia_in_run_info(run_info, node_folder)
 
     def _process_multimedia_in_run_info(self, run_info: Union[FlowRunInfo, NodeRunInfo], folder_path):
