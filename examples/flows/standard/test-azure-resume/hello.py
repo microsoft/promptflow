5c9ebe7189972c393d0ff1971bd3c55bae4cb69c
Ming Gu 🙂 <minggu.thuee@outlook.com>
2024-03-07 19:16:04 +0800

    Fix bug caused by wrong timezon

--- examples/flows/standard/test-azure-resume/hello.py
+++ examples/flows/standard/test-azure-resume/hello.py
@@ -12,9 +12,13 @@ from promptflow import tool
 @tool
 def my_python_tool(input1: str, fail_wednesday_before: str) -> str:
     from datetime import datetime
-    hour = datetime.now().hour
-    minute = datetime.now().minute
+    import pytz
+
+    tz = pytz.timezone('Asia/Shanghai')
+    hour = datetime.now(tz).hour
+    minute = datetime.now(tz).minute
     hour_minute_str = f"{hour}".zfill(2) + f"{minute}".zfill(2)
+    print(f"Current time: {hour_minute_str}")
     if "Wednesday" in input1:
         if hour_minute_str < fail_wednesday_before:
             raise Exception("Wednesday failure")
