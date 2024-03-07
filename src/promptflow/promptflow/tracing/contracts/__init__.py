b4573c92d3641bd3bcb15302e7114df7b6fafa65
Lina Tang <linatanglily@gmail.com>
2024-03-07 11:48:45 +0800

    [Tracing] Change import path of trace (#2233)
    # Description
    
    In this PR, we change the import path of trace to `promtpflow.tracing`,
    in order to prepare for splitting the promptflow-tracing package from
    promptflow.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.
    
    ---------
    
    Co-authored-by: Lina Tang <linatang@microsoft.com>

--- src/promptflow/promptflow/tracing/contracts/__init__.py
+++ src/promptflow/promptflow/tracing/contracts/__init__.py
@@ -3,8 +3,3 @@
 # ---------------------------------------------------------
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-
-from ._start_trace import start_trace
-from ._trace import trace
-
-__all__ = ["start_trace", "trace"]
