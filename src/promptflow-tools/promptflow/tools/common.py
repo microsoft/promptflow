cbb5f136d43d0846aa6851230bd91068e5a1379e
cs_lucky <si.chen@microsoft.com>
2024-03-07 16:05:18 +0800

    fix comments

--- src/promptflow-tools/promptflow/tools/common.py
+++ src/promptflow-tools/promptflow/tools/common.py
@@ -311,10 +311,10 @@ def list_deployment_connections(
 
 
 def refine_extra_fields_not_permitted_error(connection, deployment_name, model):
-    tsg = "[Please kindly avoid using vision model in LLM tool, " \
+    tsg = "Please kindly avoid using vision model in LLM tool, " \
           "because vision model cannot work with some chat api parameters. " \
           "You can change to use tool 'Azure OpenAI GPT-4 Turbo with Vision' " \
-          "or 'OpenAI GPT-4V' for vision model.]"
+          "or 'OpenAI GPT-4V' for vision model."
     try:
         if isinstance(connection, AzureOpenAIConnection):
             subscription_id, resource_group, workspace_name = get_workspace_triad()
