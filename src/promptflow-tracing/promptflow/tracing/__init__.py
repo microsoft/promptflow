7f7a10d737e5375f3c5cffa3698e4c2ebe9c6d12
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-03-11 11:29:52 +0000

    get version from `importlib.metadata`

--- src/promptflow-tracing/promptflow/tracing/__init__.py
+++ src/promptflow-tracing/promptflow/tracing/__init__.py
@@ -4,11 +4,6 @@
 
 from ._start_trace import start_trace
 from ._trace import trace
-from ._version import __title__, __version__
+from ._version import __version__
 
-__all__ = [
-    "__title__",
-    "__version__",
-    "start_trace",
-    "trace",
-]
+__all__ = ["__version__", "start_trace", "trace"]
