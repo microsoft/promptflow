b2bf0e9b86489f46f2428d21bcfc6e536ad8a60f
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-03-11 09:37:43 +0000

    remove version.py

--- src/promptflow-tracing/promptflow/tracing/version.py
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-from ._version import __version__
-
-VERSION: str = __version__
