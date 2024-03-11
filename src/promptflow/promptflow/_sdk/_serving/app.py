0c2dd490875c3f8644b915c65ac33f1af63eefaf
xiaopwan <xiaopwan@microsoft.com>
2024-03-11 01:51:47 +0000

    update tracing reference

--- src/promptflow/promptflow/_sdk/_serving/app.py
+++ src/promptflow/promptflow/_sdk/_serving/app.py
@@ -225,10 +225,10 @@ def add_default_routes(app: PromptflowServingApp):
     @app.route("/feedback", methods=["POST"])
     def feedback():
         ctx = try_extract_trace_context(logger)
-        from promptflow._core.tracer import open_telemetry_tracer
+        from promptflow.tracing._trace import open_telemetry_tracer
         token = context.attach(ctx) if ctx else None
         try:
-            with open_telemetry_tracer.start_as_current_span('feedback') as span:
+            with open_telemetry_tracer.start_as_current_span('promptflow-feedback') as span:
                 data = request.get_data()
                 should_flattern = request.args.get('flatten', 'false').lower() == 'true'
                 if should_flattern:
