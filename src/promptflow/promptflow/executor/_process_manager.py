6652b4988868c1a0f88da4de4df63b5aae8bec0b
Peiwen Gao <peiwengao@microsoft.com>
2024-03-12 17:21:17 +0800

    Use _kwargs to temporarily store all common kwargs and pass them to SpawnedForkProcessManager

--- src/promptflow/promptflow/executor/_process_manager.py
+++ src/promptflow/promptflow/executor/_process_manager.py
@@ -260,9 +260,10 @@ class ForkProcessManager(AbstractProcessManager):
 
     def __init__(self, control_signal_queue: Queue, flow_create_kwargs, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._kwargs = kwargs
         self._control_signal_queue = control_signal_queue
         self._flow_create_kwargs = flow_create_kwargs
+        # Use _kwargs to temporarily store all common kwargs and pass them to SpawnedForkProcessManager
+        self._kwargs = kwargs
 
     def start_processes(self):
         """
