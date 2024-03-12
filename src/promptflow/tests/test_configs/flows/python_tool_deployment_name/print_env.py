0e5b62cccca56f3cedc21f664ef5066274335845
Han Wang <phoenix.seek@gmail.com>
2024-03-12 17:27:40 +0800

    update test case to enabled by

--- src/promptflow/tests/test_configs/flows/python_tool_deployment_name/print_env.py
+++ src/promptflow/tests/test_configs/flows/python_tool_deployment_name/print_env.py
@@ -1,7 +1,25 @@
+from typing import Union
+ 
+ 
 from promptflow import tool
+from promptflow._core.tool import InputSetting
+from promptflow.connections import AzureOpenAIConnection, OpenAIConnection
 
 
-@tool
-def get_result(key: str, deployment_name: str, model: str):
-    # get from env var
-    return {"key": key, "deployment_name": deployment_name, "model": model}
+@tool(input_settings={
+    "deployment_name": InputSetting(enabled_by="connection", enabled_by_type=["AzureOpenAIConnection"], capabilities={
+            "completion": False,
+            "chat_completion": True,
+            "embeddings": False
+          }),
+    "model": InputSetting(enabled_by="connection", enabled_by_type=["OpenAIConnection"]),
+})
+def generate_question(
+    connection: Union[OpenAIConnection, AzureOpenAIConnection],
+    generate_question_prompt: str,
+    deployment_name: str = "",
+    model: str = "",
+    context: str = None,
+    temperature: float = 0.2
+):
+    return {"deployment_name": deployment_name, "model": model}
