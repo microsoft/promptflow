7d3f54a8a9e78cf408a3bbe6f5b341be202cd00f
Ankit Singhal <anksing@microsoft.com>
2024-03-11 17:02:17 -0700

    Barebone structure for promptflow eval

---
+++ src/promptflow-evals/setup.py
@@ -0,0 +1,71 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+import os
+import re
+from io import open
+from typing import Any, List, Match, cast
+
+from setuptools import find_namespace_packages, setup
+
+PACKAGE_NAME = "promptflow-evals"
+PACKAGE_FOLDER_PATH = "promptflow-evals"
+
+
+def parse_requirements(file_name: str) -> List[str]:
+    with open(file_name) as f:
+        return [
+            require.strip() for require in f
+            if require.strip() and not require.startswith('#')
+        ]
+
+
+# Version extraction inspired from 'requests'
+with open(os.path.join(PACKAGE_FOLDER_PATH, "version.txt"), "r") as fd:
+    version_content = fd.read()
+    print(version_content)
+    version = cast(Match[Any], re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', version_content, re.MULTILINE)).group(1)
+if not version:
+    raise RuntimeError("Cannot find version information")
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+with open("CHANGELOG.md", encoding="utf-8") as f:
+    changelog = f.read()
+
+setup(
+    name=PACKAGE_NAME,
+    version=version,
+    description="Prompt flow evaluation",
+    long_description_content_type="text/markdown",
+    long_description=readme + "\n\n" + changelog,
+    author="Microsoft Corporation",
+    author_email="aml-pt-eng@microsoft.com",
+    url="https://github.com/microsoft/promptflow",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires="<4.0,>=3.8",
+    install_requires=parse_requirements('requirements.txt'),
+    extras_require={
+        "simulator": [
+            # Dependency to list deployment in aoai_gpt4v
+        ]
+    },
+    packages=find_namespace_packages(include=[f"{PACKAGE_FOLDER_PATH}.*"]),
+    include_package_data=True,
+    project_urls={
+        "Bug Reports": "https://github.com/microsoft/promptflow/issues",
+        "Source": "https://github.com/microsoft/promptflow",
+    },
+)
