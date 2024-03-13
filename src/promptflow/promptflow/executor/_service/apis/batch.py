5578846f29ab23654961c821b7625e482de59a61
Peiwen Gao <peiwengao@microsoft.com>
2024-03-13 19:05:26 +0800

    Update codes

--- src/promptflow/promptflow/executor/_service/apis/batch.py
+++ src/promptflow/promptflow/executor/_service/apis/batch.py
@@ -2,13 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-import os
-from pathlib import Path
 
 from fastapi import APIRouter
 
 from promptflow._utils.logger_utils import service_logger
-from promptflow.executor._service._errors import FlowFilePathInvalid
 from promptflow.executor._service.contracts.batch_request import (
     AggregationRequest,
     InitializationRequest,
@@ -29,29 +26,14 @@ def initialize(request: InitializationRequest):
     with get_log_context(request, enable_service_logger=True):
         # validate request and get operation context
         request.validate_request()
-
-        # Ensure that the flow file path is within the working directory
-        working_dir = os.path.normpath(request.working_dir)
-        flow_file = os.path.normpath(request.flow_file)
-        full_path = os.path.normpath(os.path.join(working_dir, flow_file))
-        if not full_path.startswith(working_dir):
-            raise FlowFilePathInvalid(
-                message_format=(
-                    "The flow file path ({flow_file}) is invalid. The path should be in the working directory."
-                ),
-                flow_file=request.flow_file.as_posix(),
-            )
-        working_dir = Path(working_dir)
-        flow_file = Path(flow_file)
-
         operation_context = update_and_get_operation_context(request.operation_context)
         service_logger.info(f"Received batch init request, executor version: {operation_context.get_user_agent()}.")
         # resolve environment variables
         set_environment_variables(request)
         # init batch coordinator to validate flow and create process pool
         batch_coordinator = BatchCoordinator(
-            working_dir,
-            flow_file,
+            request.working_dir,
+            request.flow_file,
             request.output_dir,
             request.connections,
             worker_count=request.worker_count,
