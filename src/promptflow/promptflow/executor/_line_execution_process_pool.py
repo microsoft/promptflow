9e3b6eff2687c903eb163b4aaf08ca3b20c1629a
LuLu Zuo <luluzuo@microsoft.com>
2024-03-14 14:29:39 +0800

    Revert "Revert "Add try catch when delete process log files""
    This reverts commit ef6c94c1784251c78654971a998aa7688fcef7ef.

--- src/promptflow/promptflow/executor/_line_execution_process_pool.py
+++ src/promptflow/promptflow/executor/_line_execution_process_pool.py
@@ -10,6 +10,7 @@ import queue
 import signal
 import sys
 import threading
+import time
 from datetime import datetime
 from functools import partial
 from logging import INFO
@@ -124,15 +125,22 @@ class LineExecutionProcessPool:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # Delete log files to prevent interference from the previous run on the current execution.
-        log_path = PROCESS_LOG_PATH / SPANED_FORK_PROCESS_MANAGER_LOG_NAME
-        if log_path.exists():
-            log_path.unlink()
-        for file in PROCESS_LOG_PATH.glob(f"{PROCESS_LOG_NAME}*"):
-            file.unlink()
-        # Delete the PROCESS_LOG_PATH directory
-        if PROCESS_LOG_PATH.exists() and PROCESS_LOG_PATH.is_dir():
-            PROCESS_LOG_PATH.rmdir()
+        while True:
+            try:
+                # Delete log files to prevent interference from the previous run on the current execution.
+                log_path = PROCESS_LOG_PATH / SPANED_FORK_PROCESS_MANAGER_LOG_NAME
+                if log_path.exists():
+                    log_path.unlink()
+                for file in PROCESS_LOG_PATH.glob(f"{PROCESS_LOG_NAME}*"):
+                    file.unlink()
+                # Delete the PROCESS_LOG_PATH directory
+                if PROCESS_LOG_PATH.exists() and PROCESS_LOG_PATH.is_dir():
+                    PROCESS_LOG_PATH.rmdir()
+                break
+            except PermissionError:
+                # Unable to unlink the file because it is being used by another process. Retrying..."
+                time.sleep(1)
+                pass
         self.close()
 
     @property
