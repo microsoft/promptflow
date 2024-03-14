265396bca11e216db986bf628554fde3ff7e62d2
Honglin Du <0mza987@gmail.com>
2024-03-14 15:26:39 +0800

    refine the chat group sample

--- src/promptflow/tests/test_configs/flows/chat_group_simulation/simulator.py
+++ src/promptflow/tests/test_configs/flows/chat_group_simulation/simulator.py
@@ -3,10 +3,18 @@ from typing import List
 from promptflow import tool
 
 
+def get_answer_from_conversation_history(conversation_history: List) -> str:
+    return "answer from conversation history"
+
+
 @tool
-def simulate(topic: str, persona: str, conversation_history: List) -> str:
-    print(f"topic: {topic}")
-    print(f"persona: {persona}")
+def simulate(question: str, ground_truth: str, conversation_history: List) -> str:
+    print(f"question: {question}")
     print(f"chat_history: {conversation_history}")
-    return f"This is not funny, tell me another joke."
+    answer = get_answer_from_conversation_history(conversation_history)
+    print(f"answer: {answer}")
+    if answer != ground_truth:
+        return "I don't like this answer, give me another one."
+    else:
+        return "[STOP]"
 
