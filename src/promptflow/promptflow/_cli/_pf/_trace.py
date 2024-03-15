d56d5267c65c8c9bc63e4134017782f6d7030779
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-03-15 18:43:35 +0800

    add delete command in CLI

--- src/promptflow/promptflow/_cli/_pf/_trace.py
+++ src/promptflow/promptflow/_cli/_pf/_trace.py
@@ -4,15 +4,61 @@
 
 import argparse
 
+from promptflow._cli._params import base_params
+from promptflow._cli._utils import activate_action
+
 
 def add_trace_parser(subparsers: argparse._SubParsersAction):
-    trace_parser = subparsers.add_parser("trace", description="A CLI tool to manage traces", help="pf trace")
+    trace_parser = subparsers.add_parser(
+        "trace",
+        description="[Experimental] A CLI tool to manage traces for prompt flow.",
+        help="[Experimental] pf trace. This is an experimental feature, and may change at any time.",
+    )
+    subparsers = trace_parser.add_subparsers()
+    add_trace_delete_params(subparsers)
     trace_parser.set_defaults(action="trace")
 
 
 def dispatch_trace_cmds(args: argparse.Namespace):
     if args.sub_action == "delete":
         delete_trace(args)
+
+
+def _add_param_run(parser):
+    parser.add_argument("--run", type=str, help="Name of the run.")
+
+
+def _add_param_session(parser):
+    parser.add_argument("--session", type=str, help="Id of the session.")
+
+
+def _add_param_started_before(parser):
+    parser.add_argument("--started-before", type=str, help="Date and time in ISO 8601 format.")
+
+
+def add_trace_delete_params(subparsers):
+    epilog = """
+Examples:
+
+# Delete traces
+pf trace delete --run <run>
+pf trace delete --session <session>
+pf trace delete --session <session> --started-before <isoformat-string>
+"""
+    add_params = [
+        _add_param_run,
+        _add_param_session,
+        _add_param_started_before,
+    ] + base_params
+    activate_action(
+        name="delete",
+        description=None,
+        epilog=epilog,
+        add_params=add_params,
+        subparsers=subparsers,
+        help_message="Delete traces comes from run, session or by time.",
+        action_param_name="sub_action",
+    )
 
 
 def delete_trace(args: argparse.Namespace):
