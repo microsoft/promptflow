ff7839aacff89a880d8b18307701cd411c0455ba
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-03-15 08:52:53 +0000

    add __version__ in pkg

--- src/promptflow-tracing/promptflow/tracing/__init__.py
+++ src/promptflow-tracing/promptflow/tracing/__init__.py
@@ -6,5 +6,6 @@ __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
 
 from ._start_trace import start_trace
 from ._trace import trace
+from ._version import __version__
 
-__all__ = ["start_trace", "trace"]
+__all__ = ["__version__", "start_trace", "trace"]
