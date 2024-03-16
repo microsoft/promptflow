c66c7965fa880e01ce8abc40a1723f8bcd74a5d6
Ying Chen <2601502859@qq.com>
2024-03-16 14:47:01 +0800

    typo

--- scripts/installer/windows/scripts/pfs.bat
+++ scripts/installer/windows/scripts/pfs.bat
@@ -5,7 +5,6 @@ set MAIN_EXE=%~dp0.\pfcli.exe
 REM Check if the first argument is 'start'
 if "%~1"=="start" (
     cscript %~dp0.\start_pfs.vbs """%MAIN_EXE%"" pfs %*"
-    )
 ) else (
     "%MAIN_EXE%" pfs %*
 )
