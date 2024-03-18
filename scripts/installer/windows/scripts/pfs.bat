8db6d9e475f15e9a0b2b0b88ee92ffcdc7a71897
Ying Chen <2601502859@qq.com>
2024-03-18 11:31:51 +0800

    update

--- scripts/installer/windows/scripts/pfs.bat
+++ scripts/installer/windows/scripts/pfs.bat
@@ -4,7 +4,7 @@ setlocal
 set MAIN_EXE=%~dp0.\pfcli.exe
 REM Check if the first argument is 'start'
 if "%~1"=="start" (
-    cscript %~dp0.\start_pfs.vbs """%MAIN_EXE%"" pfs %*"
+    cscript //nologo %~dp0.\start_pfs.vbs """%MAIN_EXE%"" pfs %*"
 @REM since we won't wait for vbs to finish, we need to wait for the output file to be flushed to disk
     timeout /t 5 >nul
     type "%~dp0output.txt"
