69b9844effd58a11318173b32422464b1072167a
Brynn Yin <biyi@microsoft.com>
2024-03-18 16:07:54 +0800

    Warning for the first time imports from promptflow
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- src/promptflow/promptflow/_sdk/data/executable/main.py
+++ src/promptflow/promptflow/_sdk/data/executable/main.py
@@ -10,11 +10,11 @@ from PIL import Image
 from streamlit_quill import st_quill
 from utils import dict_iter_render_message, parse_image_content, parse_list_from_html, render_single_dict_message
 
-from promptflow import load_flow
 from promptflow._constants import STREAMING_ANIMATION_TIME
 from promptflow._sdk._submitter.utils import resolve_generator, resolve_generator_output_with_cache
 from promptflow._sdk._utils import dump_flow_result
 from promptflow._utils.multimedia_utils import convert_multimedia_data_to_base64, persist_multimedia_data
+from promptflow.client import load_flow
 
 invoker = None
 
