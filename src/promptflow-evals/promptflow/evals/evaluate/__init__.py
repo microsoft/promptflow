f5e8b6ac786a676e7d9fd847b38be45d342c66bb
Ankit Singhal <anksing@microsoft.com>
2024-03-19 12:06:37 -0700

    Evaluate API - First Draft

--- src/promptflow-evals/promptflow/evals/evaluate/__init__.py
+++ src/promptflow-evals/promptflow/evals/evaluate/__init__.py
@@ -2,11 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
-from .evaluate import evaluate
+from ._evaluate import evaluate
 
-__all__ = [
-    "evaluate"
-]
-
+__all__ = ["evaluate"]
