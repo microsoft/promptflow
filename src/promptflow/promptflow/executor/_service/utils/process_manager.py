7a04d165f063db25597bb06e1b12b26f3958fbbd
Peiwen Gao <111329184+PeiwenGaoMS@users.noreply.github.com>
2024-03-19 10:26:58 +0800

    [Internal][Executor] Refine the cancel api to allow the execution server to return partial results when receiving a cancel request (#2378)
    # Description
    
    - Refine the cancel api to allow the execution server to return partial
    results when receiving a cancel request.
    - Use async node scheduler to execute the nodes for flow test in
    executor server.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.

--- src/promptflow/promptflow/executor/_service/utils/process_manager.py
+++ src/promptflow/promptflow/executor/_service/utils/process_manager.py
@@ -2,17 +2,17 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+import os
 import signal
+from multiprocessing import Process
 from typing import Dict
-
-import psutil
 
 from promptflow._utils.logger_utils import service_logger
 
 
 class ProcessManager:
     _instance = None
-    _processes_mapping: Dict[str, int]
+    _processes_mapping: Dict[str, Process]
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
@@ -20,35 +20,31 @@ class ProcessManager:
             cls._instance._processes_mapping = {}
         return cls._instance
 
-    def start_process(self, run_id: str, process_id: int):
-        self._processes_mapping[run_id] = process_id
+    def start_process(self, run_id: str, process: Process):
+        self._processes_mapping[run_id] = process
 
-    def get_process(self, run_id: str):
+    def get_process(self, run_id: str) -> Process:
         return self._processes_mapping.get(run_id, None)
 
-    def remove_process(self, run_id: str):
-        self._processes_mapping.pop(run_id, None)
+    def remove_process(self, run_id: str) -> Process:
+        return self._processes_mapping.pop(run_id, None)
 
     def end_process(self, run_id: str):
-        process_id = self._processes_mapping.pop(run_id, None)
-        if process_id:
+        process = self.remove_process(run_id)
+        if process and process.is_alive():
             try:
-                process = psutil.Process(process_id)
-                if process.is_running():
-                    process.send_signal(signal.SIGINT)
-                    service_logger.info(f"Kill process[{process.pid}] for run[{run_id}] with SIGINT.")
-                    # wait for 30s for executor process to gracefully shutdown
-                    process.wait(timeout=30)
-                    service_logger.info(f"Successfully terminated process[{process.pid}] for run[{run_id}].")
-                else:
-                    service_logger.info(f"Process[{process.pid}] for run[{run_id}] is already terminated.")
-            except psutil.TimeoutExpired:
-                if process.is_running():
-                    # force kill if still alive
-                    process.send_signal(signal.SIGKILL)
+                # Executor will handle SIGINT.
+                os.kill(process.pid, signal.SIGINT)
+                service_logger.info(f"Kill process[{process.pid}] for run[{run_id}] with SIGINT.")
+                # Wait for 30s for executor process to gracefully shutdown
+                process.join(timeout=30)
+                if process.is_alive():
+                    # Force kill if still alive
+                    os.kill(process.pid, signal.SIGKILL)
                     service_logger.info(f"Kill process[{process.pid}] for run[{run_id}] with SIGKILL.")
-            except psutil.NoSuchProcess:
-                service_logger.warning(
+                service_logger.info(f"Successfully terminated process[{process.pid}] for run[{run_id}].")
+            except ProcessLookupError:
+                service_logger.info(
                     f"Process[{process.pid}] for run[{run_id}] not found, it might have already terminated."
                 )
         else:
