09f4d186a023305994249bd086a615fb120746a1
Ankit Singhal <30610298+singankit@users.noreply.github.com>
2024-03-20 13:50:01 -0700

    Adding qa evaluator (#2402)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow-evals/promptflow/evals/evaluators/groundedness/__init__.py
+++ src/promptflow-evals/promptflow/evals/evaluators/groundedness/__init__.py
@@ -9,12 +9,14 @@ from promptflow.entities import AzureOpenAIConnection
 from pathlib import Path
 
 
-def init(model_config: AzureOpenAIConnection):
+def init(model_config: AzureOpenAIConnection, deployment_name: str):
     """
     Initialize an evaluation function configured for a specific Azure OpenAI model.
 
     :param model_config: Configuration for the Azure OpenAI model.
     :type model_config: AzureOpenAIConnection
+    :param deployment_name: Deployment to be used which has Azure OpenAI model.
+    :type deployment_name: AzureOpenAIConnection
     :return: A function that evaluates groundedness.
     :rtype: function
 
@@ -22,12 +24,13 @@ def init(model_config: AzureOpenAIConnection):
 
     .. code-block:: python
 
-        eval_fn = groundedness.init(model_config)
+        eval_fn = groundedness.init(model_config, deployment_name="gpt-4")
         result = eval_fn(
             answer="The capital of Japan is Tokyo.", 
             context="Tokyo is Japan's capital, known for its blend of traditional culture \
                 and technological advancements.")
     """
+
     def eval_fn(answer: str, context: str):
         # Load the flow as function
         current_dir = Path(__file__).resolve().parent
@@ -35,11 +38,18 @@ def init(model_config: AzureOpenAIConnection):
         f = load_flow(source=flow_dir)
 
         # Override the connection
-        f.context.connections = { 
-            "query_llm": { "connection": model_config } 
+        f.context.connections = {
+            "query_llm": {"connection": AzureOpenAIConnection(
+                api_base=model_config.api_base,
+                api_key=model_config.api_key,
+                api_version=model_config.api_version,
+                api_type="azure"
+            ),
+                "deployment_name": deployment_name,
+            }
         }
 
         # Run the evaluation flow
         return f(answer=answer, context=context)
+
     return eval_fn
-    
---
+++ src/promptflow-evals/promptflow/evals/evaluators/qa/__init__.py
@@ -0,0 +1,46 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+
+from promptflow import load_flow
+from promptflow.entities import AzureOpenAIConnection
+from pathlib import Path
+
+
+def init(model_config: AzureOpenAIConnection, deployment_name: str):
+    """
+    Initialize an evaluation function configured for a specific Azure OpenAI model.
+
+    :param model_config: Configuration for the Azure OpenAI model.
+    :type model_config: AzureOpenAIConnection
+    :return: A function that evaluates and generates metrics for "question-answering" scenario.
+    :rtype: function
+
+    **Usage**
+
+    .. code-block:: python
+
+        eval_fn = qa.init(model_config, deployment_name="gpt-4")
+        result = qa_eval(
+        question="Tokyo is the capital of which country?",
+        answer="Japan",
+        context="Tokyo is the capital of Japan.",
+        ground_truth="Japan",
+    )
+    """
+
+    def eval_fn(*, answer: str, question: str = None, context: str = None, ground_truth: str = None, **kwargs):
+        # TODO: How to parallelize metrics calculation
+        from promptflow.evals.evaluators import groundedness, f1_score
+
+        groundedness_eval = groundedness.init(model_config, deployment_name=deployment_name)
+        f1_score_eval = f1_score.init()
+
+        return{
+            **groundedness_eval(answer=answer, context=context),
+            **f1_score_eval(answer=answer, ground_truth=ground_truth)
+        }
+
+    return eval_fn
