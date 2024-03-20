a28c45bd7f8270803c3090cfec1ca85b07f8bc84
Ankit Singhal <30610298+singankit@users.noreply.github.com>
2024-03-20 13:50:01 -0700

    Adding qa evaluator (#2402)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow-evals/samples/built_in_evaluators.py
+++ src/promptflow-evals/samples/built_in_evaluators.py
@@ -9,17 +9,14 @@ from promptflow.evals.evaluators import groundedness
 from promptflow.entities import AzureOpenAIConnection
 
 model_config = AzureOpenAIConnection(
-    name="open_ai_connection",
     api_base=os.environ.get("AZURE_OPENAI_ENDPOINT"),
     api_key=os.environ.get("AZURE_OPENAI_KEY"),
     api_type="azure",
-    model_name="gpt-4",
-    deployment_name="gpt-4",
 )
 
-groundedness_eval = groundedness.init(model_config)
+groundedness_eval = groundedness.init(model_config, deployment_name="gpt-4")
 score = groundedness_eval(
-    answer="The Alpine Explorer Tent is the most waterproof.", 
+    answer="The Alpine Explorer Tent is the most waterproof.",
     context="From the our product list, the alpine explorer tent is the most waterproof. The Adventure Dining Table has higher weight."
 )
 print(score)
@@ -28,9 +25,10 @@ print(score)
 
 # [Quality] F1 Score
 from promptflow.evals.evaluators import f1_score
+
 f1_score_eval = f1_score.init()
 score = f1_score_eval(
-    answer="The capital of Japan is Tokyo.", 
+    answer="The capital of Japan is Tokyo.",
     ground_truth="Tokyo is Japan's capital, known for its blend of traditional culture and technological advancements."
 )
 print(score)
@@ -41,13 +39,27 @@ from promptflow.evals.evaluators.content_safety import violence, sexual
 from azure.identity import DefaultAzureCredential
 
 project_scope = {
-    "subscription_id": "2d385bf4-0756-4a76-aa95-28bf9ed3b625", 
-    "resource_group_name": "rg-ninhuai", 
+    "subscription_id": "2d385bf4-0756-4a76-aa95-28bf9ed3b625",
+    "resource_group_name": "rg-ninhuai",
     "project_name": "ninhu-9214",
 }
 
 violence_eval = violence.init(project_scope, DefaultAzureCredential())
 score = violence_eval(question="What is the capital of France?", answer="Paris.")
 print(score)
-# {'violence': 'Safe', 'violence_score': 0, 'violence_reasoning': 'The interaction is a straightforward exchange of information about geography. There is no mention or implication of violence.'}
+# {'violence': 'Safe', 'violence_score': 0, 'violence_reasoning': 'The interaction is a straightforward exchange of
+# information about geography. There is no mention or implication of violence.'}
 
+# QA Evaluator
+from promptflow.evals.evaluators import qa
+
+qa_eval = qa.init(model_config=model_config, deployment_name="gpt-4-32k")
+
+result = qa_eval(
+    question="Tokyo is the capital of which country?",
+    answer="Japan",
+    context="Tokyo is the capital of Japan.",
+    ground_truth="Japan",
+)
+
+# {'gpt_groundedness': 3.0, 'f1_score': 0.0}
