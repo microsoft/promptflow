c643f57b9a530f6430cf999a6a5c8953fd22b361
Billy Hu <ninhu@microsoft.com>
2024-03-27 08:59:22 -0700

    Convert evaluators to class based

--- src/promptflow-evals/promptflow/evals/evaluators/__init__.py
+++ src/promptflow-evals/promptflow/evals/evaluators/__init__.py
@@ -3,3 +3,23 @@
 # ---------------------------------------------------------
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+
+
+from .coherence import CoherenceEvaluator
+from .f1_score import F1ScoreEvaluator
+from .fluency import FluencyEvaluator
+from .groundedness import GroundednessEvaluator
+from .relevance import RelevanceEvaluator
+from .similarity import SimilarityEvaluator
+from .qa import QAEvaluator
+
+
+__all__ = [
+    "CoherenceEvaluator",
+    "F1ScoreEvaluator",
+    "FluencyEvaluator",
+    "GroundednessEvaluator",
+    "RelevanceEvaluator",
+    "SimilarityEvaluator",
+    "QAEvaluator",
+]
--- src/promptflow-evals/promptflow/evals/evaluators/content_safety/__init__.py
+++ src/promptflow-evals/promptflow/evals/evaluators/content_safety/__init__.py
@@ -3,3 +3,17 @@
 # ---------------------------------------------------------
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+
+
+from .violence import ViolenceEvaluator
+from .sexual import SexualEvaluator
+from.self_harm import SelfHarmEvaluator
+from .hate_unfairness import HateUnfairnessEvaluator
+
+
+__all__ = [
+    "ViolenceEvaluator",
+    "SexualEvaluator",
+    "SelfHarmEvaluator",
+    "HateUnfairnessEvaluator",
+]
