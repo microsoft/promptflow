c8b12f708529abb7ea2675d8e9784be3f20f9829
Ying Chen <2601502859@qq.com>
2024-03-28 17:23:03 +0800

    update

--- scripts/installer/windows/scripts/pf.bat
+++ scripts/installer/windows/scripts/pf.bat
@@ -7,7 +7,7 @@ REM Check if the first argument is 'start'
 if "%~1"=="service" (
     REM Check if the second argument is 'start'
     if "%~2"=="start" (
-        cscript //nologo %~dp0.\start_pfs.vbs """%MAIN_EXE%"" pfs %*"
+        cscript //nologo %~dp0.\start_pfs.vbs """%MAIN_EXE%"" pf %*"
         REM since we won't wait for vbs to finish, we need to wait for the output file to be flushed to disk
         timeout /t 5 >nul
         type "%~dp0output.txt"
