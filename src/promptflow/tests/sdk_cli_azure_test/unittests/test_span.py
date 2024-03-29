55a5e9f30a4bdeadb3d174c611a93d5faeb6b57d
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-03-29 13:52:40 +0800

    fix tests

--- src/promptflow/tests/sdk_cli_azure_test/unittests/test_span.py
+++ src/promptflow/tests/sdk_cli_azure_test/unittests/test_span.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 
 from promptflow._sdk.entities._trace import Span as SpanEntity
@@ -12,43 +14,8 @@ class TestSpan:
     def test_to_dict(self):
         span = Span(
             SpanEntity(
-                name="test",
-                context={
-                    "trace_id": "0xacf2291a630af328da8fabd6bf49f653",
-                    "span_id": "0x9ded7ce65d5f7775",
-                },
-                kind="test",
-                parent_span_id="test",
-                start_time="test",
-                end_time="test",
-                status={},
-                attributes={},
-                events=[],
-                links=[],
-                resource={},
-                span_type=None,
-                session_id=None,
-            ),
-            collection_id=self.FAKE_COLLECTION_ID,
-            created_by=self.FAKE_CREATED_BY,
-        )
-        assert span.to_dict() == {
-            "name": "test",
-            "kind": "test",
-            "parent_id": "test",
-            "start_time": "test",
-            "end_time": "test",
-            "context": {
-                "trace_id": "0xacf2291a630af328da8fabd6bf49f653",
-                "span_id": "0x9ded7ce65d5f7775",
-            },
-            "id": "0x9ded7ce65d5f7775",
-            "collection_id": "fake_collection_id",
-            "created_by": {"oid": "fake_oid"},
-        }
-
-        span = Span(
-            SpanEntity(
+                trace_id="0xacf2291a630af328da8fabd6bf49f653",
+                span_id="0x9ded7ce65d5f7775",
                 name="test",
                 context={
                     "trace_id": "0xacf2291a630af328da8fabd6bf49f653",
@@ -56,15 +23,13 @@ class TestSpan:
                 },
                 kind="test",
                 parent_id="test",
-                start_time="test",
-                end_time="test",
+                start_time=datetime.datetime.fromisoformat("2022-01-01T00:00:00"),
+                end_time=datetime.datetime.fromisoformat("2022-01-01T00:01:00"),
                 status={},
-                attributes={"line_run_id": "test_line_run_id"},
+                attributes={},
                 events=[],
                 links=[],
                 resource={},
-                span_type=None,
-                session_id="test_session_id",
             ),
             collection_id=self.FAKE_COLLECTION_ID,
             created_by=self.FAKE_CREATED_BY,
@@ -73,8 +38,46 @@ class TestSpan:
             "name": "test",
             "kind": "test",
             "parent_id": "test",
-            "start_time": "test",
-            "end_time": "test",
+            "start_time": "2022-01-01T00:00:00",
+            "end_time": "2022-01-01T00:01:00",
+            "context": {
+                "trace_id": "0xacf2291a630af328da8fabd6bf49f653",
+                "span_id": "0x9ded7ce65d5f7775",
+            },
+            "id": "0x9ded7ce65d5f7775",
+            "partition_key": "default",
+            "collection_id": "fake_collection_id",
+            "created_by": {"oid": "fake_oid"},
+        }
+
+        span = Span(
+            SpanEntity(
+                trace_id="0xacf2291a630af328da8fabd6bf49f653",
+                span_id="0x9ded7ce65d5f7775",
+                name="test",
+                context={
+                    "trace_id": "0xacf2291a630af328da8fabd6bf49f653",
+                    "span_id": "0x9ded7ce65d5f7775",
+                },
+                kind="test",
+                parent_id="test",
+                start_time=datetime.datetime.fromisoformat("2022-01-01T00:00:00"),
+                end_time=datetime.datetime.fromisoformat("2022-01-01T00:01:00"),
+                status={},
+                attributes={"line_run_id": "test_line_run_id"},
+                events=[],
+                links=[],
+                resource={"collection": "test_session_id"},
+            ),
+            collection_id=self.FAKE_COLLECTION_ID,
+            created_by=self.FAKE_CREATED_BY,
+        )
+        assert span.to_dict() == {
+            "name": "test",
+            "kind": "test",
+            "parent_id": "test",
+            "start_time": "2022-01-01T00:00:00",
+            "end_time": "2022-01-01T00:01:00",
             "attributes": {"line_run_id": "test_line_run_id"},
             "partition_key": "test_session_id",
             "context": {
@@ -85,4 +88,5 @@ class TestSpan:
             "partition_key": "test_session_id",
             "collection_id": "fake_collection_id",
             "created_by": {"oid": "fake_oid"},
+            "resource": {"collection": "test_session_id"},
         }
