2b5107d0f93428fd66220f31cf6a929ae951e5ce
Yangtong Xu <yangtongxu@microsoft.com>
2024-04-01 18:14:34 +0800

    [Internal] Use data credential to access blob

--- src/promptflow-azure/promptflow/azure/_storage/blob/client.py
+++ src/promptflow-azure/promptflow/azure/_storage/blob/client.py
@@ -8,7 +8,9 @@ from azure.ai.ml import MLClient
 from azure.ai.ml._azure_environments import _get_storage_endpoint_from_metadata
 from azure.ai.ml._restclient.v2022_10_01.models import DatastoreType
 from azure.ai.ml.constants._common import LONG_URI_FORMAT, STORAGE_ACCOUNT_URLS
+from azure.ai.ml.entities._credentials import AccountKeyConfiguration
 from azure.ai.ml.entities._datastore.datastore import Datastore
+from azure.ai.ml.operations import DatastoreOperations
 from azure.storage.blob import ContainerClient
 
 from promptflow.exceptions import UserErrorException
@@ -26,27 +28,28 @@ def get_datastore_container_client(
     credential: Optional[object] = None,
 ) -> Tuple[ContainerClient, str]:
     try:
-        # To write data to blob, user should have "Storage Blob Data Contributor" to the storage account.
         if credential is None:
             from azure.identity import DefaultAzureCredential
 
             credential = DefaultAzureCredential()
 
-        default_datastore = get_default_datastore(subscription_id, resource_group_name, workspace_name, credential)
+        datastore_definition, datastore_credential = _get_default_datastore(
+            subscription_id, resource_group_name, workspace_name, credential
+        )
 
         storage_endpoint = _get_storage_endpoint_from_metadata()
         account_url = STORAGE_ACCOUNT_URLS[DatastoreType.AZURE_BLOB].format(
-            default_datastore.account_name, storage_endpoint
+            datastore_definition.account_name, storage_endpoint
         )
 
         # Datastore is a notion of AzureML, it is not a notion of Blob Storage.
         # So, we cannot get datastore name by blob client.
         # To generate the azureml uri has datastore name, we need to generate the uri here and pass in to db client.
         container_client = ContainerClient(
-            account_url=account_url, container_name=default_datastore.container_name, credential=credential
+            account_url=account_url, container_name=datastore_definition.container_name, credential=datastore_credential
         )
         blob_base_uri = LONG_URI_FORMAT.format(
-            subscription_id, resource_group_name, workspace_name, default_datastore.name, ""
+            subscription_id, resource_group_name, workspace_name, datastore_definition.name, ""
         )
         if not blob_base_uri.endswith("/"):
             blob_base_uri += "/"
@@ -61,31 +64,34 @@ def get_datastore_container_client(
         raise
 
 
-def get_default_datastore(
+def _get_default_datastore(
     subscription_id: str, resource_group_name: str, workspace_name: str, credential: Optional[object]
-) -> Datastore:
+) -> Tuple[Datastore, str]:
 
     datastore_key = _get_datastore_client_key(subscription_id, resource_group_name, workspace_name)
-    datastore = _get_datastore_from_cache(datastore_key=datastore_key)
-    if datastore is None:
+    datastore_definition, datastore_credential = _get_datastore_from_cache(datastore_key=datastore_key)
+    if datastore_definition is None:
         with _thread_lock:
-            datastore = _get_datastore_from_cache(datastore_key=datastore_key)
-            if datastore is None:
-                datastore = _get_default_datastore(subscription_id, resource_group_name, workspace_name, credential)
+            datastore_definition, datastore_credential = _get_datastore_from_cache(datastore_key=datastore_key)
+            if datastore_definition is None:
+                datastore_definition, datastore_credential = _get_aml_default_datastore(
+                    subscription_id, resource_group_name, workspace_name, credential
+                )
                 _datastore_cache[datastore_key] = {
                     "expire_at": datetime.datetime.now() + datetime.timedelta(seconds=_cache_timeout),
-                    "datastore": datastore,
+                    "datastore_definition": datastore_definition,
+                    "datastore_credential": datastore_credential,
                 }
-    return datastore
+    return datastore_definition, datastore_credential
 
 
-def _get_datastore_from_cache(datastore_key: str):
+def _get_datastore_from_cache(datastore_key: str) -> Tuple[Datastore, str]:
     datastore = _datastore_cache.get(datastore_key)
 
     if datastore and datastore["expire_at"] > datetime.datetime.now():
-        return datastore["datastore"]
+        return datastore["datastore_definition"], datastore["datastore_credential"]
 
-    return None
+    return None, None
 
 
 def _get_datastore_client_key(subscription_id: str, resource_group_name: str, workspace_name: str) -> str:
@@ -93,9 +99,9 @@ def _get_datastore_client_key(subscription_id: str, resource_group_name: str, wo
     return f"{subscription_id}@{resource_group_name}@{workspace_name}"
 
 
-def _get_default_datastore(
+def _get_aml_default_datastore(
     subscription_id: str, resource_group_name: str, workspace_name: str, credential: Optional[object]
-) -> Datastore:
+) -> Tuple[Datastore, str]:
 
     ml_client = MLClient(
         credential=credential,
@@ -104,10 +110,22 @@ def _get_default_datastore(
         workspace_name=workspace_name,
     )
 
-    default_datastore = ml_client.datastores.get_default()
+    default_datastore = ml_client.datastores.get_default(include_secrets=True)
     if default_datastore.type != DatastoreType.AZURE_BLOB:
         raise UserErrorException(
             message=f"Default datastore {default_datastore.name} is {default_datastore.type}, not AzureBlob."
         )
 
-    return default_datastore
+    return default_datastore, _get_datastore_credential(default_datastore, ml_client.datastores)
+
+
+def _get_datastore_credential(datastore: Datastore, operations: DatastoreOperations):
+    # Reference the similar logic in azure.ai.ml._artifact._artifact_utilities
+    # https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/ml/azure-ai-ml/azure/ai/ml/_artifacts/_artifact_utilities.py#L103
+    credential = datastore.credentials
+    if isinstance(credential, AccountKeyConfiguration):
+        return credential.account_key
+    elif hasattr(credential, "sas_token"):
+        return credential.sas_token
+    else:
+        return operations._credential
--- src/promptflow/tests/sdk_cli_azure_test/unittests/test_blob_client.py
+++ src/promptflow/tests/sdk_cli_azure_test/unittests/test_blob_client.py
@@ -11,14 +11,20 @@ class TestBlobClient:
         _datastore_cache["test"] = {
             "expire_at": datetime.datetime.now() + datetime.timedelta(0, -1),  # already expire
             "datastore": "test",
+            "credential": "test_credential",
         }
-        assert _get_datastore_from_cache("test") is None
+        datastore_name, credential = _get_datastore_from_cache("test")
+        assert datastore_name is None
+        assert credential is None
 
         _datastore_cache["test"] = {
             "expire_at": datetime.datetime.now() + datetime.timedelta(1, 0),  # expire after 1 day
             "datastore": "test",
+            "credential": "test_credential",
         }
-        assert _get_datastore_from_cache("test") == "test"
+        datastore_name, credential = _get_datastore_from_cache("test")
+        assert datastore_name == "test"
+        assert credential == "test_credential"
 
     def test_get_datastore_client_key(self):
         assert _get_datastore_client_key("sub", "rg", "ws") == "sub@rg@ws"
