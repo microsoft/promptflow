722b339023bdeca53a2a0f9d710ade05fc8b76fa
Philip Gao <yigao@microsoft.com>
2024-04-01 06:07:39 +0000

    Fix

--- src/promptflow-devkit/promptflow/_cli/pf.py
+++ src/promptflow-devkit/promptflow/_cli/pf.py
@@ -3,14 +3,15 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 import os
-import sys
 
 
 def main():
     if os.environ.get("PF_INSTALLER") is None:
         os.environ["PF_INSTALLER"] = "PIP"
 
-    os.execl(sys.executable, sys.executable, "-m", "promptflow._cli._pf.entry", *sys.argv[1:])
+    from promptflow._cli._pf.entry import main as _main
+
+    _main()
 
 
 # this is a compatibility layer for the old CLI which is used for vscode extension
