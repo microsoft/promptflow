6bde8bd9d2fe3b413d506706bcde01c81c7c9528
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-04-01 14:29:40 +0800

    implement traces.delete

--- src/promptflow-devkit/promptflow/_sdk/operations/_trace_operations.py
+++ src/promptflow-devkit/promptflow/_sdk/operations/_trace_operations.py
@@ -2,15 +2,23 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+import datetime
 import typing
 
+from promptflow._sdk._constants import TRACE_DEFAULT_COLLECTION
+from promptflow._sdk._orm.trace import Event as ORMEvent
 from promptflow._sdk._orm.trace import LineRun as ORMLineRun
 from promptflow._sdk._orm.trace import Span as ORMSpan
 from promptflow._sdk._telemetry import ActivityType, monitor_operation
 from promptflow._sdk.entities._trace import Event, LineRun, Span
+from promptflow._utils.logger_utils import get_cli_sdk_logger
+from promptflow.exceptions import UserErrorException
 
 
 class TraceOperations:
+    def __init__(self):
+        self._logger = get_cli_sdk_logger()
+
     def get_event(self, event_id: str) -> typing.Dict:
         return Event.get(event_id=event_id)
 
@@ -88,7 +96,7 @@ class TraceOperations:
         self,
         run: typing.Optional[str] = None,
         collection: typing.Optional[str] = None,
-        started_before: typing.Optional[str] = None,
+        started_before: typing.Optional[typing.Union[str, datetime.datetime]] = None,
     ) -> None:
         """Delete traces permanently.
 
@@ -107,6 +115,58 @@ class TraceOperations:
         :param session: Id of the session.
         :type session: Optional[str]
         :param started_before: ISO 8601 format time string (e.g., "2024-03-19T15:17:23.807563").
-        :type started_before: Optional[str]
+        :type started_before: Optional[Union[str, datetime.datetime]]
         """
-        ...
+        self._logger.debug(
+            "delete traces with parameters, run: %s, collection: %s, started_before: %s",
+            run,
+            collection,
+            started_before,
+        )
+        self._validate_delete_query_params(run=run, collection=collection, started_before=started_before)
+        self._logger.debug("try to delete line run(s)...")
+        if isinstance(started_before, str):
+            started_before = datetime.datetime.fromisoformat(started_before)
+        line_run_cnt, trace_ids = ORMLineRun.delete(
+            run=run,
+            collection=collection,
+            started_before=started_before,
+        )
+        self._logger.debug("deleted %d line runs", line_run_cnt)
+        self._logger.debug("try to delete traces and events for trace_ids: %s", trace_ids)
+        span_cnt = ORMSpan.delete(trace_ids=trace_ids)
+        event_cnt = ORMEvent.delete(trace_ids=trace_ids)
+        self._logger.debug("deleted %d spans and %d events", span_cnt, event_cnt)
+        return
+
+    def _validate_delete_query_params(
+        self,
+        run: typing.Optional[str] = None,
+        collection: typing.Optional[str] = None,
+        started_before: typing.Optional[typing.Union[str, datetime.datetime]] = None,
+    ) -> None:
+        # valid delete queries:
+        #   1. run=xxx
+        #   2. collection=yyy
+        #   3. collection=zz, started_before=zz
+        # this function will directly return for valid cases
+        if run is not None and collection is None and started_before is None:
+            return
+        if collection is not None and run is None:
+            if started_before is not None:
+                # if `started_before` is a time string, need to ensure it's in valid ISO 8601 format
+                if isinstance(started_before, str):
+                    try:
+                        datetime.datetime.fromisoformat(started_before)
+                        return
+                    except ValueError:
+                        pass
+                elif isinstance(started_before, datetime.datetime):
+                    return
+            elif collection != TRACE_DEFAULT_COLLECTION:
+                return
+        error_message = (
+            'Valid delete queries: 1) specify `run`; 2) specify `collection` (not "default"); '
+            "3) specify `collection` and `started_before` (ISO 8601)."
+        )
+        raise UserErrorException(error_message)
