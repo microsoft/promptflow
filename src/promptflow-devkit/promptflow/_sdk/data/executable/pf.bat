ef1efa705c3a60227d78698ff62d6ca15a573dca
Ying Chen <2601502859@qq.com>
2024-04-01 11:53:55 +0800

    support start pf

--- src/promptflow-devkit/promptflow/_sdk/data/executable/pf.bat
+++ src/promptflow-devkit/promptflow/_sdk/data/executable/pf.bat
@@ -7,7 +7,12 @@ REM Check if the first argument is 'start'
 if "%~1"=="service" (
     REM Check if the second argument is 'start'
     if "%~2"=="start" (
-        start /B "" "%MAIN_EXE%" pf %*
+        echo Starting service...
+        echo %*
+        cscript //nologo %~dp0.\start_pfs.vbs """%MAIN_EXE%"" pf %*"
+        REM since we won't wait for vbs to finish, we need to wait for the output file to be flushed to disk
+        timeout /t 5 >nul
+        type "%~dp0output.txt"
     ) else (
         "%MAIN_EXE%" pf %*
     )
