e964e8a4e9d63ae90a5b6d12148adadc38a4a014
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-04-01 16:53:45 +0800

    update test

--- src/promptflow-tracing/tests/unittests/test_start_trace.py
+++ src/promptflow-tracing/tests/unittests/test_start_trace.py
@@ -6,6 +6,7 @@ import uuid
 
 import pytest
 from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from pytest_mock import MockerFixture
 
@@ -28,13 +29,6 @@ class TestStartTrace:
         assert attrs[ResourceAttributesFieldName.SERVICE_NAME] == RESOURCE_ATTRIBUTES_SERVICE_NAME
         assert ResourceAttributesFieldName.COLLECTION not in attrs
 
-    def test_tracer_provider_overwritten(self) -> None:
-        trace.set_tracer_provider(TracerProvider())
-        old_tracer_provider = trace.get_tracer_provider()
-        start_trace()
-        new_tracer_provider = trace.get_tracer_provider()
-        assert id(old_tracer_provider) != id(new_tracer_provider)
-
     def test_tracer_provider_resource_attributes(self) -> None:
         collection = str(uuid.uuid4())
         res_attrs = {"attr1": "value1", "attr2": "value2"}
@@ -44,6 +38,14 @@ class TestStartTrace:
         assert attrs[ResourceAttributesFieldName.COLLECTION] == collection
         assert attrs["attr1"] == "value1"
         assert attrs["attr2"] == "value2"
+
+    def test_tracer_provider_resource_merge(self) -> None:
+        existing_res = {"existing_attr": "existing_value"}
+        trace.set_tracer_provider(TracerProvider(resource=Resource(existing_res)))
+        start_trace()
+        tracer_provider: TracerProvider = trace.get_tracer_provider()
+        assert "existing_attr" in tracer_provider.resource.attributes
+        assert ResourceAttributesFieldName.SERVICE_NAME in tracer_provider.resource.attributes
 
     def test_skip_tracing_local_setup(self, monkeypatch: pytest.MonkeyPatch, mocker: MockerFixture) -> None:
         spy = mocker.spy(promptflow.tracing._start_trace, "_is_devkit_installed")
