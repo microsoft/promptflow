8e35d0fb4ee9fc61bfd6e2b7ac041a4851a82551
Brynn Yin <biyi@microsoft.com>
2024-04-01 21:48:00 +0800

    Revert "Change client __init__ to lazy import to avoid circular imports"
    This reverts commit 5ac1b64592b16598445047d562fc1a6dc66fe3d9.

--- src/promptflow-devkit/promptflow/client/__init__.py
+++ src/promptflow-devkit/promptflow/client/__init__.py
@@ -3,21 +3,9 @@
 # ---------------------------------------------------------
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
 
-
-_imported_attr = {}
-
-
 # control plane sdk functions
-# Use lazy import to avoid circular import
-# Circular path: PFClient -> UserAgent -> client._version -> client.__init__ -> PFClient
-def __getattr__(name):
-    if name in _imported_attr:
-        return _imported_attr[name]
-    if name in ["PFClient", "load_run", "load_flow"]:
-        from promptflow._sdk._load_functions import load_flow, load_run  # noqa: F401
-        from .._sdk._pf_client import PFClient  # noqa: F401
+from promptflow._sdk._load_functions import load_flow, load_run
 
-        _imported_attr[name] = locals()[name]
-        return _imported_attr[name]
+from .._sdk._pf_client import PFClient
 
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+__all__ = ["PFClient", "load_run", "load_flow"]
