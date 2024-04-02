b6881539ed3d0893c6e2aec1e9cbeed83e39fb5e
robbenwang <robbenwang@microsoft.com>
2024-04-02 10:25:40 +0000

    Fix comment, keep original method indention

--- src/promptflow-core/promptflow/executor/flow_executor.py
+++ src/promptflow-core/promptflow/executor/flow_executor.py
@@ -11,6 +11,7 @@ import os
 import signal
 import threading
 import uuid
+from contextlib import contextmanager
 from pathlib import Path
 from threading import current_thread
 from types import GeneratorType
@@ -1336,33 +1337,38 @@ def execute_flow(
     """
     flow_executor = FlowExecutor.create(flow_file, connections, working_dir, raise_ex=False, **kwargs)
     flow_executor.enable_streaming_for_llm_flow(lambda: enable_stream_output)
+    with _change_working_dir(working_dir), _force_flush_tracer_provider():
+        # Execute nodes in the flow except the aggregation nodes
+        # TODO: remove index=0 after UX no longer requires a run id similar to batch runs
+        # (run_id_index, eg. xxx_0) for displaying the interface
+        line_result = flow_executor.exec_line(
+            inputs, index=0, allow_generator_output=allow_generator_output, run_id=run_id
+        )
+        # persist the output to the output directory
+        line_result.output = flow_executor._multimedia_processor.persist_multimedia_data(
+            line_result.output, base_dir=working_dir, sub_dir=output_dir
+        )
+        if run_aggregation and line_result.aggregation_inputs:
+            # Convert inputs of aggregation to list type
+            flow_inputs = {k: [v] for k, v in inputs.items()}
+            aggregation_inputs = {k: [v] for k, v in line_result.aggregation_inputs.items()}
+            aggregation_results = flow_executor.exec_aggregation(
+                flow_inputs, aggregation_inputs=aggregation_inputs, run_id=run_id
+            )
+            line_result.node_run_infos = {**line_result.node_run_infos, **aggregation_results.node_run_infos}
+            line_result.run_info.metrics = aggregation_results.metrics
+            # The aggregation inputs of line results is not utilized in the flow test. So we set it into None.
+            line_result.aggregation_inputs = None
+        if isinstance(line_result.output, dict):
+            # remove line_number from output
+            line_result.output.pop(LINE_NUMBER_KEY, None)
+        return line_result
+
+
+@contextmanager
+def _force_flush_tracer_provider():
     try:
-        with _change_working_dir(working_dir):
-            # Execute nodes in the flow except the aggregation nodes
-            # TODO: remove index=0 after UX no longer requires a run id similar to batch runs
-            # (run_id_index, eg. xxx_0) for displaying the interface
-            line_result = flow_executor.exec_line(
-                inputs, index=0, allow_generator_output=allow_generator_output, run_id=run_id
-            )
-            # persist the output to the output directory
-            line_result.output = flow_executor._multimedia_processor.persist_multimedia_data(
-                line_result.output, base_dir=working_dir, sub_dir=output_dir
-            )
-            if run_aggregation and line_result.aggregation_inputs:
-                # Convert inputs of aggregation to list type
-                flow_inputs = {k: [v] for k, v in inputs.items()}
-                aggregation_inputs = {k: [v] for k, v in line_result.aggregation_inputs.items()}
-                aggregation_results = flow_executor.exec_aggregation(
-                    flow_inputs, aggregation_inputs=aggregation_inputs, run_id=run_id
-                )
-                line_result.node_run_infos = {**line_result.node_run_infos, **aggregation_results.node_run_infos}
-                line_result.run_info.metrics = aggregation_results.metrics
-                # The aggregation inputs of line results is not utilized in the flow test. So we set it into None.
-                line_result.aggregation_inputs = None
-            if isinstance(line_result.output, dict):
-                # remove line_number from output
-                line_result.output.pop(LINE_NUMBER_KEY, None)
-            return line_result
+        yield
     finally:
         try:
             # Force flush the tracer provider to ensure all spans are exported before the process exits.
