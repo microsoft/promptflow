7743440e36f800787289e289812f2a8310c39f04
Xiaodi Bi <bixiaodi@microsoft.com>
2024-04-02 14:44:20 +0800

    update chat role properties

--- src/promptflow-devkit/promptflow/_sdk/entities/_chat_group/_chat_role.py
+++ src/promptflow-devkit/promptflow/_sdk/entities/_chat_group/_chat_role.py
@@ -42,8 +42,7 @@ class ChatRole:
                  **kwargs):
         self._role = role
         self._flow, self._flow_object = self._validate_flow(flow)
-        if inputs is not None:
-            self._inputs, self._outputs = self._build_role_io(flow, inputs)
+        self._inputs, self._outputs = self._build_role_io(flow, inputs)
 
         # Below properties are used for cloud chat group. It may have some duplicate with above ones
         # Will evaluate and refine in the second step.
@@ -124,6 +123,7 @@ class ChatRole:
         flow_path, flow_file = resolve_flow_path(flow, check_flow_exist=False)
         flow_dict = load_yaml(flow_path / flow_file)
         inputs = flow_dict.get("inputs", {})
+        inputs_value = inputs_value or {}
         for key in inputs:
             # fill the inputs with the provided values
             # TODO: Shall we check the value type here or leave it to executor?
