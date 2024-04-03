9236a45a21947207cb58cf6ef4b7ae32e86b83f4
Robben Wang <350053002@qq.com>
2024-04-03 13:10:34 +0800

    Force flush traces for execute_flow (#2606)
    # Description
    
    Add trace force flush for `execute_flow`.
    For multi container scenario, we may fail to export traces because
    process terminated directly.
    
    Why we need to manually invoke force flush:
    Seems like when we create worker process by
    `invoke_sync_function_in_process`, the atexit method registered by
    tracer provider is not invoked. Fix this issue by adding force flush
    first, it's recommended by GPT to handle it manually for multi process
    scenario.
    
    Why use force flush but not shutdown:
    In case of customer execute_flow manually in their code.
    
    # All Promptflow Contribution checklist:
    - [X] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [X] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [X] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [X] Title of the pull request is clear and informative.
    - [X] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.
    
    ---------
    
    Co-authored-by: robbenwang <robbenwang@microsoft.com>

--- src/promptflow-core/promptflow/executor/flow_executor.py
+++ src/promptflow-core/promptflow/executor/flow_executor.py
@@ -11,11 +11,13 @@ import os
 import signal
 import threading
 import uuid
+from contextlib import contextmanager
 from pathlib import Path
 from threading import current_thread
 from types import GeneratorType
 from typing import Any, Callable, Dict, List, Mapping, Optional, Tuple
 
+import opentelemetry.trace as otel_trace
 from opentelemetry.trace.status import StatusCode
 
 from promptflow._constants import LINE_NUMBER_KEY
@@ -1343,7 +1345,7 @@ def execute_flow(
     """
     flow_executor = FlowExecutor.create(flow_file, connections, working_dir, raise_ex=False, **kwargs)
     flow_executor.enable_streaming_for_llm_flow(lambda: enable_stream_output)
-    with _change_working_dir(working_dir):
+    with _change_working_dir(working_dir), _force_flush_tracer_provider():
         # Execute nodes in the flow except the aggregation nodes
         # TODO: remove index=0 after UX no longer requires a run id similar to batch runs
         # (run_id_index, eg. xxx_0) for displaying the interface
@@ -1369,6 +1371,18 @@ def execute_flow(
             # remove line_number from output
             line_result.output.pop(LINE_NUMBER_KEY, None)
         return line_result
+
+
+@contextmanager
+def _force_flush_tracer_provider():
+    try:
+        yield
+    finally:
+        try:
+            # Force flush the tracer provider to ensure all spans are exported before the process exits.
+            otel_trace.get_tracer_provider().force_flush()
+        except Exception as e:
+            flow_logger.warning(f"Error occurred while force flush tracer provider: {e}")
 
 
 def signal_handler(sig, frame):
