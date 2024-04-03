4275976b5d45c5700117a04b8e08e9e845d7a359
Billy Hu <ninhu@microsoft.com>
2024-04-03 14:13:35 -0700

    Update built-in evaluators' inputs validation logic and remove default values (#2645)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow-evals/promptflow/evals/evaluators/coherence/flow/validate_inputs.py
+++ src/promptflow-evals/promptflow/evals/evaluators/coherence/flow/validate_inputs.py
@@ -4,7 +4,9 @@ from promptflow import tool
 @tool
 def validate_inputs(question: str, answer: str):
     # Validate input parameters
-    if not (question and question.strip()) or not (answer and answer.strip()):
+    if not (question and question.strip() and question != "None") or not (
+        answer and answer.strip() and answer != "None"
+    ):
         raise ValueError("Both 'question' and 'answer' must be non-empty strings.")
 
     return True
--- src/promptflow-evals/promptflow/evals/evaluators/f1_score/flow/validate_inputs.py
+++ src/promptflow-evals/promptflow/evals/evaluators/f1_score/flow/validate_inputs.py
@@ -3,7 +3,9 @@ from promptflow import tool
 
 @tool
 def validate_inputs(answer: str, ground_truth: str):
-    if not (answer and answer.strip()) or not (ground_truth and ground_truth.strip()):
+    if not (answer and answer.strip() and answer != "None") or not (
+        ground_truth and ground_truth.strip() and ground_truth != "None"
+    ):
         raise ValueError("Both 'answer' and 'ground_truth' must be non-empty strings.")
 
     return True
