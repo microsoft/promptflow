9c311bf2f8b69fc3fa1b92e6991c78a3526a3209
Billy Hu <ninhu@microsoft.com>
2024-04-03 13:36:13 -0700

    fix

--- src/promptflow-evals/promptflow/evals/evaluators/similarity/flow/validate_inputs.py
+++ src/promptflow-evals/promptflow/evals/evaluators/similarity/flow/validate_inputs.py
@@ -7,7 +7,7 @@ def validate_inputs(question: str, answer: str, ground_truth: str):
     if (
         not (question and question.strip() and question != "None")
         or not (answer and answer.strip() and answer != "None")
-        or not (ground_truth and ground_truth.strip())
+        or not (ground_truth and ground_truth.strip() and ground_truth != "None")
     ):
         raise ValueError("'question', 'answer' and 'ground_truth' must be non-empty strings.")
 
