e2c8aca79fc7e33e8cc4ceb48d7656fcd4a6a8cf
Ge Gao <gega@microsoft.com>
2024-04-03 19:47:42 +0800

    Revert "Update versions for release"
    This reverts commit c323e496bf0c429ceed8fc93bfef76b303d76d6d.

--- src/promptflow/setup.py
+++ src/promptflow/setup.py
@@ -21,9 +21,9 @@ with open("CHANGELOG.md", encoding="utf-8") as f:
     changelog = f.read()
 
 REQUIRES = [
-    "promptflow-tracing==1.1.0rc9",  # tracing capabilities
-    "promptflow-core==1.1.0rc9",  # core capabilities
-    "promptflow-devkit==1.8.0rc9",  # devkit capabilities
+    "promptflow-tracing>=1.0.0",  # tracing capabilities
+    "promptflow-core",  # core capabilities
+    "promptflow-devkit",  # devkit capabilities
 ]
 
 setup(
@@ -51,17 +51,17 @@ setup(
     install_requires=REQUIRES,
     extras_require={
         "all": [
-            "promptflow-core[executor-service]==1.1.0rc9",
-            "promptflow-devkit[all]==1.8.0rc9",
-            "promptflow-azure==1.8.0rc9",
+            "promptflow-core[executor-service]",
+            "promptflow-devkit[all]",
+            "promptflow-azure",
         ],
-        "azure": ["promptflow-azure==1.8.0rc9"],
-        "executable": ["promptflow-devkit[executable]==1.8.0rc9"],
+        "azure": ["promptflow-azure"],
+        "executable": ["promptflow-devkit[executable]"],
         "azureml-serving": [
-            "promptflow-core[azureml-serving]==1.1.0rc9",
+            "promptflow-core[azureml-serving]",
         ],
         "executor-service": [
-            "promptflow-core[executor-service]==1.1.0rc9",  # used to build web executor server
+            "promptflow-core[executor-service]",  # used to build web executor server
         ],
     },
     packages=find_packages(),
