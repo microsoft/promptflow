5c9f38b568f91e42153a071fc39dabea766fe9e7
Ying Chen <2601502859@qq.com>
2024-04-07 18:57:14 +0800

    add comment

--- src/promptflow/tests/sdk_cli_test/unittests/test_utils.py
+++ src/promptflow/tests/sdk_cli_test/unittests/test_utils.py
@@ -529,4 +529,6 @@ class TestRetryUtils:
             "r",
         ) as f:
             executable_all_packages = f.read().splitlines()
+        # check if all packages in requirements.txt are the same with pyproject.toml in devkit/core/tracinf packages.
+        # If not, maybe you need update requirements.txt
         assert all_packages == executable_all_packages
