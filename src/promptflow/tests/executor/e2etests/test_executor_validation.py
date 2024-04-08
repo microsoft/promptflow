870c06398ac18391df68b70df6a45d89e7af50b3
chenyang <chenyzhang@microsoft.com>
2024-04-08 16:16:17 +0800

    Delete DAG_FILE_NAME constant and use the resolve_flow_path function to obtain the flow path and flow file. (#2662)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    
    Base this [PR](https://github.com/microsoft/promptflow/pull/2626):
    Distinguish flex flow and dag flow(use flow.flex.yaml for flex flow,
    flow.dag.yaml for dag flow),
    And next [PR](https://github.com/microsoft/promptflow/pull/2548):
    Migrate the yaml used by the eager flow to flow.flex.yaml, code support
    both flow.flex.yaml and flow.dag.yaml.
    
    To standardize the construction method of flow paths and flow files:
    1. Delete **DAG_FILE_NAME** constant and use the **resolve_flow_path**
    function to obtain the flow path and flow file.
    2. Using **FLOW_DAG_YAML** instead of **DAG_FILE_NAME** .
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow/tests/executor/e2etests/test_executor_validation.py
+++ src/promptflow/tests/executor/e2etests/test_executor_validation.py
@@ -6,7 +6,7 @@ import pytest
 
 from promptflow._core._errors import FlowOutputUnserializable, InvalidSource
 from promptflow._core.tools_manager import APINotFound
-from promptflow._sdk._constants import DAG_FILE_NAME
+from promptflow._utils.flow_utils import resolve_flow_path
 from promptflow._utils.utils import dump_list_to_jsonl
 from promptflow.batch import BatchEngine
 from promptflow.contracts._errors import FailedToImportModule
@@ -339,9 +339,10 @@ class TestValidation:
         self, path_root: str, flow_folder, node_name, line_input, error_class, error_msg, dev_connections
     ):
         # Single Node run - the inputs are from flow_inputs + dependency_nodes_outputs
+        _, flow_file = resolve_flow_path(flow_folder, path_root, check_flow_exist=False)
         with pytest.raises(error_class) as exe_info:
             FlowExecutor.load_and_exec_node(
-                flow_file=DAG_FILE_NAME,
+                flow_file=flow_file,
                 node_name=node_name,
                 flow_inputs=line_input,
                 dependency_nodes_outputs={},
