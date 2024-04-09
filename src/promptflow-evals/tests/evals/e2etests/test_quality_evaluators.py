5767b008da37c92dd2021836fdb89bf488493c5b
Billy Hu <ninhu@microsoft.com>
2024-04-08 20:14:05 -0700

    Add e2e test pipeline for promptflow-evals (#2647)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

---
+++ src/promptflow-evals/tests/evals/e2etests/test_quality_evaluators.py
@@ -0,0 +1,17 @@
+import pytest
+
+from promptflow.evals.evaluators import GroundednessEvaluator
+
+
+@pytest.mark.usefixtures("model_config", "recording_injection")
+@pytest.mark.e2etest
+class TestQualityEvaluators:
+    def test_groundedness_evaluator(self, model_config, deployment_name):
+        groundedness_eval = GroundednessEvaluator(model_config, deployment_name)
+        score = groundedness_eval(
+            answer="The Alpine Explorer Tent is the most waterproof.",
+            context="From the our product list, the alpine explorer tent is the most waterproof. The Adventure Dining "
+            "Table has higher weight.",
+        )
+        assert score is not None
+        assert score["gpt_groundedness"] > 1.0
