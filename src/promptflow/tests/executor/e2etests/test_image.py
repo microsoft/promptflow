18c1b3f54ef569cb458f80b0b3863278a2ef1145
Bin Wang <94442403+codingrabbitt1@users.noreply.github.com>
2024-04-10 16:01:04 +0800

    Add feature_list and e2e test cases for openai_vision image contract feature. (#2724)
    # Description
    
    Add feature_list and e2e test cases for openai_vision image contract
    feature.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow/tests/executor/e2etests/test_image.py
+++ src/promptflow/tests/executor/e2etests/test_image.py
@@ -5,12 +5,17 @@ from tempfile import mkdtemp
 import pytest
 
 from promptflow._constants import OUTPUT_FILE_NAME
-from promptflow._utils.multimedia_utils import MIME_PATTERN, BasicMultimediaProcessor, ImageProcessor
+from promptflow._utils.multimedia_utils import (
+    MIME_PATTERN,
+    BasicMultimediaProcessor,
+    ImageProcessor,
+    OpenaiVisionMultimediaProcessor,
+)
 from promptflow.batch._batch_engine import BatchEngine
 from promptflow.batch._result import BatchResult
 from promptflow.contracts.multimedia import Image
 from promptflow.contracts.run_info import FlowRunInfo, RunInfo, Status
-from promptflow.executor import FlowExecutor
+from promptflow.executor.flow_executor import FlowExecutor
 from promptflow.storage._run_storage import DefaultRunStorage
 
 from ..utils import get_flow_folder, get_yaml_file, is_image_file, is_jsonl_file, load_jsonl
@@ -23,6 +28,9 @@ CHAT_FLOW_WITH_IMAGE = "chat_flow_with_image"
 EVAL_FLOW_WITH_SIMPLE_IMAGE = "eval_flow_with_simple_image"
 EVAL_FLOW_WITH_COMPOSITE_IMAGE = "eval_flow_with_composite_image"
 NESTED_API_CALLS_FLOW = "python_tool_with_image_nested_api_calls"
+
+SAMPLE_IMAGE_FLOW_WITH_OPENAI_VISION_IMAGE = "python_tool_with_openai_vision_image"
+CHAT_FLOW_WITH_OPENAI_VISION_IMAGE = "chat_flow_with_openai_vision_image"
 
 IMAGE_URL = (
     "https://raw.githubusercontent.com/microsoft/promptflow/main/src/promptflow/tests/test_configs/datas/logo.jpg"
@@ -87,30 +95,37 @@ def get_test_cases_for_node_run():
     ]
 
 
-def contain_image_reference(value, parent_path="temp"):
+def contain_image_reference(value, parent_path="temp", multimedia_processor=BasicMultimediaProcessor()):
     if isinstance(value, (FlowRunInfo, RunInfo)):
-        assert contain_image_reference(value.api_calls, parent_path)
-        assert contain_image_reference(value.inputs, parent_path)
-        assert contain_image_reference(value.output, parent_path)
+        assert contain_image_reference(value.api_calls, parent_path, multimedia_processor=multimedia_processor)
+        assert contain_image_reference(value.inputs, parent_path, multimedia_processor=multimedia_processor)
+        assert contain_image_reference(value.output, parent_path, multimedia_processor=multimedia_processor)
         return True
     assert not isinstance(value, Image)
     if isinstance(value, list):
-        return any(contain_image_reference(item, parent_path) for item in value)
+        return any(
+            contain_image_reference(item, parent_path, multimedia_processor=multimedia_processor) for item in value
+        )
     if isinstance(value, dict):
-        if BasicMultimediaProcessor.is_multimedia_dict(value):
-            v = list(value.values())[0]
+        if multimedia_processor.is_multimedia_dict(value):
+            if isinstance(multimedia_processor, BasicMultimediaProcessor):
+                v = list(value.values())[0]
+            else:
+                v = value[value["type"]].get("url", "") or value[value["type"]].get("path", "")
             assert isinstance(v, str)
             assert ImageProcessor.is_url(v) or str(Path(v).parent) == parent_path
             return True
-        return any(contain_image_reference(v, parent_path) for v in value.values())
+        return any(
+            contain_image_reference(v, parent_path, multimedia_processor=multimedia_processor) for v in value.values()
+        )
     return False
 
 
-def contain_image_object(value):
+def contain_image_object(value, multimedia_processor=BasicMultimediaProcessor()):
     if isinstance(value, list):
         return any(contain_image_object(item) for item in value)
     elif isinstance(value, dict):
-        assert not BasicMultimediaProcessor.is_multimedia_dict(value)
+        assert not multimedia_processor.is_multimedia_dict(value)
         return any(contain_image_object(v) for v in value.values())
     else:
         return isinstance(value, Image)
@@ -349,3 +364,157 @@ class TestExecutorWithImage:
             eval_input_dirs, eval_inputs_mapping, eval_output_dir
         )
         assert eval_result.completed_lines == eval_result.total_lines
+
+
+def get_test_cases_for_openai_vision_input(flow_folder):
+    working_dir = get_flow_folder(flow_folder)
+    image = ImageProcessor.create_image_from_file(working_dir / "logo.jpg")
+    inputs = [
+        {"type": "image_file", "image_file": {"path": str(working_dir / "logo.jpg")}},
+        {"type": "image_url", "image_url": {"url": image.to_base64(with_type=True)}},
+        {"type": "image_url", "image_url": {"url": IMAGE_URL}},
+        str(working_dir / "logo.jpg"),
+        image.to_base64(),
+        IMAGE_URL,
+    ]
+    return [(flow_folder, {"image": input}) for input in inputs]
+
+
+def get_test_cases_for_openai_vision_node_run():
+    image = {
+        "type": "image_file",
+        "image_file": {"path": str(get_flow_folder(SAMPLE_IMAGE_FLOW_WITH_OPENAI_VISION_IMAGE) / "logo.jpg")},
+    }
+    simple_image_input = {"image": image}
+
+    return [
+        (SAMPLE_IMAGE_FLOW_WITH_OPENAI_VISION_IMAGE, "python_node", simple_image_input, None),
+        (SAMPLE_IMAGE_FLOW_WITH_OPENAI_VISION_IMAGE, "python_node_2", simple_image_input, {"python_node": image}),
+    ]
+
+
+@pytest.mark.usefixtures("dev_connections")
+@pytest.mark.e2etest
+class TestExecutorWithOpenaiVisionImage:
+    @pytest.mark.parametrize(
+        "flow_folder, inputs",
+        get_test_cases_for_openai_vision_input(SAMPLE_IMAGE_FLOW_WITH_OPENAI_VISION_IMAGE)
+        + [(CHAT_FLOW_WITH_OPENAI_VISION_IMAGE, {})],
+    )
+    def test_executor_exec_line_with_image(self, flow_folder, inputs, dev_connections):
+        working_dir = get_flow_folder(flow_folder)
+        os.chdir(working_dir)
+        storage = DefaultRunStorage(base_dir=working_dir, sub_dir=Path("./temp"))
+        executor = FlowExecutor.create(get_yaml_file(flow_folder), dev_connections, storage=storage)
+        flow_result = executor.exec_line(inputs)
+
+        multimedia_processor = OpenaiVisionMultimediaProcessor()
+        assert isinstance(flow_result.output, dict)
+        assert contain_image_object(flow_result.output, multimedia_processor=multimedia_processor)
+        # Assert output also contains plain text.
+        assert any(isinstance(v, str) for v in flow_result.output)
+        assert flow_result.run_info.status == Status.Completed
+        assert contain_image_reference(flow_result.run_info, multimedia_processor=multimedia_processor)
+        for _, node_run_info in flow_result.node_run_infos.items():
+            assert node_run_info.status == Status.Completed
+            assert contain_image_reference(node_run_info, multimedia_processor=multimedia_processor)
+
+    @pytest.mark.parametrize(
+        "flow_folder, node_name, flow_inputs, dependency_nodes_outputs",
+        get_test_cases_for_openai_vision_node_run(),
+    )
+    def test_executor_exec_node_with_image(
+        self, flow_folder, node_name, flow_inputs, dependency_nodes_outputs, dev_connections
+    ):
+        working_dir = get_flow_folder(flow_folder)
+        os.chdir(working_dir)
+        storage = DefaultRunStorage(base_dir=working_dir, sub_dir=Path("./temp"))
+        run_info = FlowExecutor.load_and_exec_node(
+            get_yaml_file(flow_folder),
+            node_name,
+            flow_inputs=flow_inputs,
+            dependency_nodes_outputs=dependency_nodes_outputs,
+            connections=dev_connections,
+            storage=storage,
+            raise_ex=True,
+        )
+        assert run_info.status == Status.Completed
+        assert contain_image_reference(run_info, multimedia_processor=OpenaiVisionMultimediaProcessor())
+
+    # Assert image could be persisted to the specified path.
+    @pytest.mark.parametrize(
+        "output_sub_dir, assign_storage, expected_path",
+        [
+            ("test_path", True, "test_storage"),
+            ("test_path", False, "test_path"),
+            (None, True, "test_storage"),
+            (None, False, "."),
+        ],
+    )
+    def test_executor_exec_node_with_image_storage_and_path(self, output_sub_dir, assign_storage, expected_path):
+        flow_folder = SAMPLE_IMAGE_FLOW_WITH_OPENAI_VISION_IMAGE
+        node_name = "python_node"
+        image = {
+            "type": "image_file",
+            "image_file": {"path": str(get_flow_folder(SAMPLE_IMAGE_FLOW_WITH_OPENAI_VISION_IMAGE) / "logo.jpg")},
+        }
+        flow_inputs = {"image": image}
+        working_dir = get_flow_folder(flow_folder)
+        os.chdir(working_dir)
+        storage = DefaultRunStorage(base_dir=working_dir, sub_dir=Path("./test_storage"))
+        run_info = FlowExecutor.load_and_exec_node(
+            get_yaml_file(flow_folder),
+            node_name,
+            flow_inputs=flow_inputs,
+            dependency_nodes_outputs=None,
+            connections=None,
+            storage=storage if assign_storage else None,
+            output_sub_dir=output_sub_dir,
+            raise_ex=True,
+        )
+        assert run_info.status == Status.Completed
+        assert contain_image_reference(
+            run_info, parent_path=expected_path, multimedia_processor=OpenaiVisionMultimediaProcessor()
+        )
+
+    @pytest.mark.parametrize(
+        "flow_folder, input_dirs, inputs_mapping, output_key, expected_outputs_number",
+        [
+            (
+                SAMPLE_IMAGE_FLOW_WITH_OPENAI_VISION_IMAGE,
+                {"data": "."},
+                {"image": "${data.image}"},
+                "output",
+                4,
+            ),
+            (
+                CHAT_FLOW_WITH_OPENAI_VISION_IMAGE,
+                {"data": "inputs.jsonl"},
+                {"question": "${data.question}", "chat_history": "${data.chat_history}"},
+                "answer",
+                2,
+            ),
+        ],
+    )
+    def test_batch_engine_with_image(
+        self, flow_folder, input_dirs, inputs_mapping, output_key, expected_outputs_number
+    ):
+        flow_file = get_yaml_file(flow_folder)
+        working_dir = get_flow_folder(flow_folder)
+        output_dir = Path(mkdtemp())
+        batch_result = BatchEngine(flow_file, working_dir).run(
+            input_dirs, inputs_mapping, output_dir, max_lines_count=4
+        )
+
+        assert isinstance(batch_result, BatchResult)
+        assert batch_result.completed_lines == expected_outputs_number
+        assert all(is_jsonl_file(output_file) or is_image_file(output_file) for output_file in output_dir.iterdir())
+
+        outputs = load_jsonl(output_dir / OUTPUT_FILE_NAME)
+        assert len(outputs) == expected_outputs_number
+        for i, output in enumerate(outputs):
+            assert isinstance(output, dict)
+            assert "line_number" in output, f"line_number is not in {i}th output {output}"
+            assert output["line_number"] == i, f"line_number is not correct in {i}th output {output}"
+            result = output[output_key][0] if isinstance(output[output_key], list) else output[output_key]
+            assert OpenaiVisionMultimediaProcessor.is_multimedia_dict(result), f"image is not in {i}th output {output}"
