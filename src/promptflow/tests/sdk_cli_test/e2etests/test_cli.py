2f09e8a0e3f499e0eaa941f59d6331795c86ac86
Xingzhi Zhang <37076709+elliotzh@users.noreply.github.com>
2024-04-10 10:37:11 +0800

    feat: add public API for pf flow save (#2685)
    # Description
    
    Add below public APIs:
    1. `pf flow save`
    2. `pf.flows.save`
    3. `pf.flows.infer_signature`
    
    Add generate meta support for `TypedDict`
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [x] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [x] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow/tests/sdk_cli_test/e2etests/test_cli.py
+++ src/promptflow/tests/sdk_cli_test/e2etests/test_cli.py
@@ -19,7 +19,7 @@ import mock
 import pytest
 
 from promptflow._cli._pf.entry import main
-from promptflow._constants import LINE_NUMBER_KEY, PF_USER_AGENT
+from promptflow._constants import FLOW_FLEX_YAML, LINE_NUMBER_KEY, PF_USER_AGENT
 from promptflow._sdk._constants import LOGGER_NAME, SCRUBBED_VALUE, ExperimentStatus
 from promptflow._sdk._errors import RunNotFoundError
 from promptflow._sdk.operations._local_storage_operations import LocalStorageOperations
@@ -2302,6 +2302,46 @@ class TestCli:
         # check run results
         run = pf.runs.get(run_id)
         assert_batch_run_result(run, pf, assert_func)
+
+    def test_pf_flow_save(self, pf):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            run_pf_command(
+                "flow",
+                "save",
+                "--path",
+                temp_dir,
+                "--entry",
+                "hello:hello_world",
+                "--code",
+                f"{EAGER_FLOWS_DIR}/../functions/hello_world",
+            )
+            assert os.listdir(temp_dir) == [FLOW_FLEX_YAML, "hello.py"]
+            content = load_yaml(Path(temp_dir) / FLOW_FLEX_YAML)
+            assert content == {
+                "entry": "hello:hello_world",
+                "inputs": {
+                    "text": {
+                        "type": "string",
+                    }
+                },
+                "outputs": {
+                    "output": {
+                        "type": "string",
+                    }
+                },
+            }
+            os.unlink(Path(temp_dir) / FLOW_FLEX_YAML)
+            run_pf_command(
+                "flow",
+                "save",
+                "--entry",
+                "hello:hello_world",
+                cwd=temp_dir,
+            )
+            # __pycache__ will be created when inspecting the module
+            assert os.listdir(temp_dir) == [FLOW_FLEX_YAML, "hello.py", "__pycache__"]
+            new_content = load_yaml(Path(temp_dir) / FLOW_FLEX_YAML)
+            assert new_content == content
 
 
 def assert_batch_run_result(run, pf, assert_func):
