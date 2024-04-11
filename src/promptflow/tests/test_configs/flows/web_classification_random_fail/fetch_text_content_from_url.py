451e89cf698a74b6993cffb567f1b7a8902c1661
Min Shi <minshi@microsoft.com>
2024-04-11 20:50:08 +0800

    add random fail node to web_classification_random_fail

--- src/promptflow/tests/test_configs/flows/web_classification_random_fail/fetch_text_content_from_url.py
+++ src/promptflow/tests/test_configs/flows/web_classification_random_fail/fetch_text_content_from_url.py
@@ -6,8 +6,6 @@ from promptflow import tool
 
 @tool
 def fetch_text_content_from_url(url: str):
-    if random.random() < 0.5:
-        raise ValueError("Random failure")
     # Send a request to the URL
     try:
         headers = {
