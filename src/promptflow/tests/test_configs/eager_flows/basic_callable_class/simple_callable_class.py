9d5e5de80ea03d2c45e13ae24e24c2e11010ab23
Han Wang <phoenix.seek@gmail.com>
2024-04-11 13:54:04 +0800

    update annotation

--- src/promptflow/tests/test_configs/eager_flows/basic_callable_class/simple_callable_class.py
+++ src/promptflow/tests/test_configs/eager_flows/basic_callable_class/simple_callable_class.py
@@ -1,12 +1,20 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+import dataclasses
+
+@dataclasses.dataclass
+class FlowOutput:
+    obj_input: str
+    func_input: str
+    obj_id: str
+
 
 class MyFlow:
     def __init__(self, obj_input: str):
         self.obj_input = obj_input
 
-    def __call__(self, func_input: str) -> dict:
+    def __call__(self, func_input: str) -> FlowOutput:
         return {
             "obj_input": self.obj_input,
             "func_input": func_input,
