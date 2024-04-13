f5d2e29b7432724f9da5b174675fc7e14e788383
Kevin Kent <kevinkent@NU-kkent-M.local>
2024-04-13 09:41:20 -0400

    fixed import issues, refactored test to target the warning statement

--- src/promptflow-core/tests/core/unittests/test_metrics.py
+++ src/promptflow-core/tests/core/unittests/test_metrics.py
@@ -1,24 +1,27 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
-
-import pytest
-from promptflow.core._serving.monitor.metrics import MetricsRecorder
-from logging import logger
-from promptflow.contracts.run_info import FlowRunInfo, RunInfo, Status
+import platform
 from datetime import datetime
 
-metrics_enabled = True
+import pytest
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+
+from promptflow.contracts.run_info import FlowRunInfo, RunInfo, Status
+from promptflow.core._serving.extension.extension_type import ExtensionType
+from promptflow.core._serving.extension.otel_exporter_provider_factory import OTelExporterProviderFactory
+from promptflow.core._serving.monitor.metrics import MetricsRecorder
+from promptflow.core._utils import LoggerFactory
+
 
 @pytest.mark.unittest
 class TestMetrics:
     @pytest.mark.parametrize(
-        "metrics_recorder, flow_run, node_run",
+        "flow_run, node_run",
         [
             (
-                MetricsRecorder(logger),
                 FlowRunInfo(
-                    run_id="qrstuv",
+                    run_id="run_id",
                     status=Status.Completed,
                     error=None,
                     inputs={"input": "input"},
@@ -26,10 +29,17 @@ class TestMetrics:
                     metrics=None,
                     request=None,
                     system_metrics=None,
+                    parent_run_id="parent_run_id",
+                    root_run_id="root_run_id",
+                    source_run_id="source_run_id",
+                    start_time=datetime.now(),
+                    end_time=datetime.now(),
+                    flow_id="test_flow",
                 ),
                 RunInfo(
                     node="test",
-                    flow_run_id="a1b2c3",
+                    flow_run_id="flow_run_id",
+                    run_id="run_id",
                     status=Status.Completed,
                     inputs={"question": "What is the meaning of life?", "chat_history": []},
                     output={"answer": "Baseball."},
@@ -42,8 +52,25 @@ class TestMetrics:
             )
         ],
     )
-    def test_metrics_recorder(self, metrics_recorder, flow_run, node_run):
-        try:
-            metrics_recorder.record_tracing_metrics(flow_run, node_run)
-        except Exception:
-            pytest.fail("record_tracing_metrics raised an exception")
+    def test_metrics_recorder(self, caplog, flow_run, node_run):
+        logger = LoggerFactory.get_logger(__name__)
+        logger.propagate = True
+
+        caplog.set_level("WARNING")
+        metric_exporters = OTelExporterProviderFactory.get_metrics_exporters(
+            LoggerFactory.get_logger(__name__), ExtensionType.DEFAULT
+        )
+        readers = []
+        for exporter in metric_exporters:
+            reader = PeriodicExportingMetricReader(exporter=exporter, export_interval_millis=60000)
+            readers.append(reader)
+        metrics_recorder = MetricsRecorder(
+            logger,
+            readers=readers,
+            common_dimensions={
+                "python_version": platform.python_version(),
+                "installation_id": "test_installation_id",
+            },
+        )
+        metrics_recorder.record_tracing_metrics(flow_run, {"run1:": node_run})
+        assert "failed to record metrics:" not in caplog.text
