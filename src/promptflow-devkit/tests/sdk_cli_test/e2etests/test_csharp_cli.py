70e7498444b3a8c02104b1aefe3f3772853f386f
zhangxingzhi <zhangxingzhi@microsoft.com>
2024-04-18 20:38:13 +0800

    refactor: use nuget.config from csharp repo

--- src/promptflow-devkit/tests/sdk_cli_test/e2etests/test_csharp_cli.py
+++ src/promptflow-devkit/tests/sdk_cli_test/e2etests/test_csharp_cli.py
@@ -1,3 +1,5 @@
+import dataclasses
+import json
 import os
 import os.path
 import sys
@@ -8,8 +10,64 @@ import pytest
 from promptflow._cli._pf.entry import main
 
 
-def get_test_cases_root():
-    return os.getenv("CSHARP_TEST_CASES_ROOT", None)
+@dataclasses.dataclass
+class FlowPaths:
+    flow_dir: str
+    data: str
+    init: str
+
+    def __init__(self, root_of_test_cases: Path, flow_name: str):
+        self.flow_dir = (root_of_test_cases / flow_name / "bin" / "Debug" / "net6.0").as_posix()
+        self.data = (root_of_test_cases / flow_name / "data.jsonl").as_posix()
+        self.init = (root_of_test_cases / flow_name / "init.json").as_posix()
+
+        if not os.path.exists(self.flow_dir):
+            raise FileNotFoundError(f"Flow directory not found: {self.flow_dir}\n{os.listdir(root_of_test_cases)}")
+
+
+@dataclasses.dataclass
+class TestCases:
+    basic: FlowPaths
+    function_mode_basic: FlowPaths
+    basic_with_builtin_llm: FlowPaths
+    class_init_flex_flow: FlowPaths
+    basic_chat: FlowPaths
+
+    def __init__(self, root_of_test_cases: Path):
+        self.basic = FlowPaths(root_of_test_cases, "Basic")
+        self.function_mode_basic = FlowPaths(root_of_test_cases, "FunctionModeBasic")
+        self.class_init_flex_flow = FlowPaths(root_of_test_cases, "ClassInitFlexFlow")
+        self.basic_chat = FlowPaths(root_of_test_cases, "BasicChat")
+
+
+@pytest.fixture(scope="session")
+def root_test_cases() -> TestCases:
+    target_path = os.getenv("CSHARP_TEST_CASES_ROOT", None)
+    if target_path is None:
+        pytest.skip("CSHARP_TEST_CASES_ROOT is not set.")
+    target_path = Path(target_path)
+
+    package_root = Path(__file__).parent.parent.parent.parent.parent / "promptflow"
+    dev_connections_path = package_root / "connections.json"
+    if not dev_connections_path.exists():
+        dev_connections_path.write_text(
+            json.dumps(
+                {
+                    "azure_open_ai_connection": {
+                        "type": "AzureOpenAIConnection",
+                        "value": {
+                            "api_key": os.getenv("AZURE_OPENAI_API_KEY", "00000000000000000000000000000000"),
+                            "api_base": os.getenv("AZURE_OPENAI_ENDPOINT", "https://openai.azure.com/"),
+                            "api_type": "azure",
+                            "api_version": "2023-07-01-preview",
+                        },
+                        "module": "promptflow.connections",
+                    }
+                }
+            )
+        )
+        print(f"Using dev connections file: {dev_connections_path}")
+    return TestCases(target_path)
 
 
 # TODO: move this to a shared utility module
@@ -35,43 +93,42 @@ def run_pf_command(*args, cwd=None):
 )
 @pytest.mark.cli_test
 @pytest.mark.e2etest
-@pytest.mark.skipif(get_test_cases_root() is None, reason="available locally only before csharp support go public")
 class TestCSharpCli:
-    def test_pf_flow_test_basic(self):
+    def test_pf_flow_test_basic(self, root_test_cases: TestCases):
         run_pf_command(
             "flow",
             "test",
             "--flow",
-            f"{get_test_cases_root()}\\Basic\\bin\\Debug\\net6.0",
+            root_test_cases.basic.flow_dir,
             "--inputs",
             "question=what is promptflow?",
         )
 
-    def test_flex_flow_test(self):
+    def test_flex_flow_test(self, root_test_cases: TestCases):
         run_pf_command(
             "flow",
             "test",
             "--flow",
-            f"{get_test_cases_root()}\\FunctionModeBasic\\bin\\Debug\\net6.0",
+            root_test_cases.function_mode_basic.flow_dir,
             "--inputs",
             "topic=promptflow",
         )
 
     @pytest.mark.skip(reason="need to update the test case")
-    def test_pf_run_create_with_connection_override(self):
+    def test_pf_run_create_with_connection_override(self, root_test_cases: TestCases):
         run_pf_command(
             "run",
             "create",
             "--flow",
-            f"{get_test_cases_root()}\\examples\\BasicWithBuiltinLLM\\bin\\Debug\\net6.0",
+            root_test_cases.basic_with_builtin_llm.flow_dir,
             "--data",
-            f"{get_test_cases_root()}\\examples\\BasicWithBuiltinLLM\\batchRunData.jsonl",
+            root_test_cases.basic_with_builtin_llm.data,
             "--connections",
             "get_answer.connection=azure_open_ai_connection",
         )
 
-    def test_flow_chat(self, monkeypatch, capsys):
-        flow_dir = f"{get_test_cases_root()}\\BasicChat\\bin\\Debug\\net6.0"
+    def test_flow_chat(self, root_test_cases: TestCases, monkeypatch, capsys):
+        flow_dir = root_test_cases.basic_chat.flow_dir
         # mock user input with pop so make chat list reversed
         chat_list = ["what is chat gpt?", "hi"]
 
@@ -101,12 +158,12 @@ class TestCSharpCli:
         assert "Hello world round 1: what is chat gpt?" in outerr.out
 
     @pytest.mark.skip(reason="need to update the test case")
-    def test_flow_chat_ui_streaming(self):
+    def test_flow_chat_ui_streaming(self, root_test_cases: TestCases):
         """Note that this test won't pass. Instead, it will hang and pop up a web page for user input.
         Leave it here for debugging purpose.
         """
         # The test need to interact with user input in ui
-        flow_dir = f"{get_test_cases_root()}\\examples\\BasicChatFlowWithBuiltinLLM\\bin\\Debug\\net6.0"
+        flow_dir = f"{root_test_cases}\\examples\\BasicChatFlowWithBuiltinLLM\\bin\\Debug\\net6.0"
         run_pf_command(
             "flow",
             "test",
@@ -116,8 +173,8 @@ class TestCSharpCli:
         )
 
     @pytest.mark.skip(reason="need to update the test case")
-    def test_flow_chat_interactive_streaming(self, monkeypatch, capsys):
-        flow_dir = f"{get_test_cases_root()}\\examples\\BasicChatFlowWithBuiltinLLM\\bin\\Debug\\net6.0"
+    def test_flow_chat_interactive_streaming(self, root_test_cases: TestCases, monkeypatch, capsys):
+        flow_dir = f"{root_test_cases}\\examples\\BasicChatFlowWithBuiltinLLM\\bin\\Debug\\net6.0"
         # mock user input with pop so make chat list reversed
         chat_list = ["what is chat gpt?", "hi"]
 
@@ -146,21 +203,19 @@ class TestCSharpCli:
         assert "language model" in outerr.out
 
     @pytest.mark.skip(reason="need to update the test case")
-    def test_flow_run_from_resume(self):
+    def test_flow_run_from_resume(self, root_test_cases: TestCases):
         run_pf_command("run", "create", "--resume-from", "net6_0_variant_0_20240326_163600_356909")
 
-    def test_flow_test_class_init(self):
+    @pytest.mark.skip(reason="need to avoid csharp calling local pfs to fetch connection")
+    def test_flow_test_class_init(self, root_test_cases: TestCases):
         """Note that this test won't pass. Instead, it will hang and pop up a web page for user input.
         Leave it here for debugging purpose.
         """
-        # The test need to interact with user input in ui
-        flow_dir = f"{get_test_cases_root()}\\ClassInitFlexFlow\\bin\\Debug\\net6.0"
-
         run_pf_command(
             "flow",
             "test",
             "--flow",
-            flow_dir,
+            root_test_cases.class_init_flex_flow.flow_dir,
             "--inputs",
             "question=aklhdfqwejk",
             "--init",
@@ -168,14 +223,14 @@ class TestCSharpCli:
             "connection=azure_open_ai_connection",
         )
 
-    def test_pf_run_create_basic(self):
+    def test_pf_run_create_basic(self, root_test_cases: TestCases):
         run_pf_command(
             "run",
             "create",
             "--flow",
-            f"{get_test_cases_root()}\\ClassInitFlexFlow\\bin\\Debug\\net6.0",
+            root_test_cases.basic.flow_dir,
             "--data",
-            f"{get_test_cases_root()}\\ClassInitFlexFlow\\data.jsonl",
+            root_test_cases.class_init_flex_flow.data,
             "--init",
-            f"{get_test_cases_root()}\\ClassInitFlexFlow\\init.json",
+            root_test_cases.class_init_flex_flow.init,
         )
