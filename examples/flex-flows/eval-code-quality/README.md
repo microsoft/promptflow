e6b2d6fb9fe2bf95d1581039df36b3b58efd0eb7
Han Wang <wanhan@microsoft.com>
2024-04-19 18:08:44 +0800

    update evaluator with model config

--- examples/flex-flows/eval-code-quality/README.md
+++ examples/flex-flows/eval-code-quality/README.md
@@ -1,5 +1,5 @@
 # Eval Code Quality
-A example flow defined using function entry which shows how to evaluate the quality of code snippet.
+A example flow defined using class based entry which leverages model config to evaluate the quality of code snippet.
 
 ## Prerequisites
 
@@ -12,13 +12,23 @@ pip install -r requirements.txt
 
 - Prepare your Azure Open AI resource follow this [instruction](https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/create-resource?pivots=web-portal) and get your `api_key` if you don't have one.
 
-- Setup environment variables
+- Setup connection
 
-Ensure you have put your azure open ai endpoint key in [.env](../.env) file. You can create one refer to this [example file](../.env.example).
+Go to "Prompt flow" "Connections" tab. Click on "Create" button, select one of LLM tool supported connection types and fill in the configurations.
+
+Or use CLI to create connection:
 
 ```bash
-cat ../.env
+# Override keys with --set to avoid yaml file changes
+pf connection create --file ../../connections/azure_openai.yml --set api_key=<your_api_key> api_base=<your_api_base> --name open_ai_connection
 ```
+
+Note in [flow.flex.yaml](flow.flex.yaml) we are using connection named `open_ai_connection`.
+```bash
+# show registered connection
+pf connection show --name open_ai_connection
+```
+
 
 - Run as normal Python file
 ```bash
--- examples/flex-flows/eval-code-quality/code_quality.py
+++ examples/flex-flows/eval-code-quality/code_quality.py
@@ -1,14 +1,13 @@
 import json
-import os
 from dataclasses import dataclass
 from pathlib import Path
 
-from dotenv import load_dotenv
 from jinja2 import Template
+from openai import AzureOpenAI
 
 from promptflow.tracing import trace
-from promptflow.connections import AzureOpenAIConnection
-from promptflow.tools.aoai import AzureOpenAI
+from promptflow.core import AzureOpenAIModelConfiguration
+
 
 BASE_DIR = Path(__file__).absolute().parent
 
@@ -29,44 +28,57 @@ class Result:
     explanation: str
 
 
-@trace
-def eval_code(code: str) -> Result:
-    """Evaluate the code based on correctness, readability."""
-    examples = [
-        {
-            "code": 'print("Hello, world!")',
-            "correctness": 5,
-            "readability": 5,
-            "explanation": "The code is correct as it is a simple question and answer format. "
-            "The readability is also good as the code is short and easy to understand.",
-        }
-    ]
+class CodeEvaluator:
+    def __init__(self, model_config: AzureOpenAIModelConfiguration):
+        self.model_config = model_config
+        self.client = AzureOpenAI(
+            azure_endpoint=model_config.azure_endpoint,
+            api_version=model_config.api_version,
+            api_key=model_config.api_key,
+        )
 
-    prompt = load_prompt("prompt.md", code, examples)
+    def __call__(self, code: str) -> Result:
+        """Evaluate the code based on correctness, readability."""
+        examples = [
+            {
+                "code": 'print("Hello, world!")',
+                "correctness": 5,
+                "readability": 5,
+                "explanation": "The code is correct as it is a simple question and answer format. "
+                "The readability is also good as the code is short and easy to understand.",
+            }
+        ]
 
-    if "AZURE_OPENAI_API_KEY" not in os.environ:
-        # load environment variables from .env file
-        load_dotenv()
+        prompt = load_prompt("prompt.md", code, examples)
+        messages = [{"content": prompt, "role": "system"}]
+        response = self.client.chat.completions.create(
+            model=self.model_config.azure_deployment,
+            messages=messages,
+            temperature=2,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None,
+            n=1,
+        )
 
-    if "AZURE_OPENAI_API_KEY" not in os.environ:
-        raise Exception("Please specify environment variables: AZURE_OPENAI_API_KEY")
-
-    connection = AzureOpenAIConnection.from_env()
-
-    output = AzureOpenAI(connection).chat(
-        prompt=prompt,
-        deployment_name="gpt-35-turbo",
-        max_tokens=256,
-        temperature=0.7,
-    )
-    output = Result(**json.loads(output))
-    return output
+        output = response.choices[0].message.content
+        print(output)
+        output = Result(**json.loads(output))
+        return output
 
 
 if __name__ == "__main__":
     from promptflow.tracing import start_trace
+    from promptflow.client import PFClient
 
     start_trace()
-
-    result = eval_code('print("Hello, world!")')
+    pf = PFClient()
+    connection = pf.connections.get("open_ai_connection", with_secrets=True)
+    model_config = AzureOpenAIModelConfiguration.from_connection(
+        connection=connection,
+        azure_deployment="gpt-35-turbo",
+    )
+    evaluator = CodeEvaluator(model_config)
+    result = evaluator('print("Hello, world!")')
     print(result)
