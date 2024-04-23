57ab63f4582cbe3cc59919ce79680749d73c1720
nick863 <30440255+nick863@users.noreply.github.com>
2024-04-23 16:10:16 -0700

    Fix

--- src/promptflow-evals/tests/evals/unittests/test_evaluate.py
+++ src/promptflow-evals/tests/evals/unittests/test_evaluate.py
@@ -1,16 +1,11 @@
 import os
 import pathlib
 
-import pandas as pd
 import pytest
 
-from pandas.testing import assert_frame_equal
-from unittest.mock import patch
-
 from promptflow.evals.evaluate import evaluate
-from promptflow.evals.evaluate._evaluate import _apply_target_to_data, _get_missing_inputs
+
 from promptflow.evals.evaluators import F1ScoreEvaluator, GroundednessEvaluator
-from promptflow.evals.evaluate._utils import save_function_as_flow
 
 
 @pytest.fixture
@@ -23,34 +18,6 @@ def invalid_jsonl_file():
 def missing_columns_jsonl_file():
     data_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "data")
     return os.path.join(data_path, "missing_columns_evaluate_test_data.jsonl")
-
-
-@pytest.fixture
-def questions_file():
-    data_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "data")
-    return os.path.join(data_path, "questions.jsonl")
-
-
-@pytest.fixture
-def questions_wrong_file():
-    data_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "data")
-    return os.path.join(data_path, "questions_wrong.jsonl")
-
-
-@pytest.fixture
-def questions_answers_file():
-    data_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "data")
-    return os.path.join(data_path, "questions_answers.jsonl")
-
-
-def _target_fn(question):
-    """An example target function."""
-    if 'LV-426' in question:
-        return {'answer': 'There is nothing good there.'}
-    if 'central heating' in question:
-        return {'answer': 'There is no central heating on the streets today, but it will be, I promise.'}
-    if 'strange' in question:
-        return {'answer': 'The life is strange...'}
 
 
 @pytest.mark.usefixtures("mock_model_config")
@@ -95,45 +62,3 @@ class TestEvaluate:
             evaluate(data=missing_columns_jsonl_file, evaluators={"g": F1ScoreEvaluator()})
 
         assert "Missing required inputs for evaluator g : ['ground_truth']." in exc_info.value.args[0]
-
-    def test_evaluate_missing_required_inputs_target(self, questions_wrong_file):
-        with pytest.raises(ValueError) as exc_info:
-            evaluate(data=questions_wrong_file,
-                     evaluators={"g": F1ScoreEvaluator()},
-                     target=_target_fn
-                     )
-        assert "Missing required inputs for target : ['question']." in exc_info.value.args[0]
-
-    def test_wrong_target(self, questions_file):
-        """Test error, when target function does not generate required column."""
-        with pytest.raises(ValueError) as exc_info:
-            # target_fn will generate the "answer", but not ground truth.
-            evaluate(data=questions_file, evaluators={"g": F1ScoreEvaluator()}, target=_target_fn)
-
-        assert "Missing required inputs for evaluator g : ['ground_truth']." in exc_info.value.args[0]
-
-    @pytest.mark.parametrize('script_is_file', [True, False])
-    def test_save_fun_as_flow(self, tmpdir, pf_client, script_is_file):
-        """Test saving function as flow."""
-        with patch('promptflow.evals.evaluate._utils.os') as mock_os:
-            mock_os.path.isfile.return_value = script_is_file
-            save_function_as_flow(_target_fn, tmpdir, pf_client)
-        assert os.path.isfile(os.path.join(tmpdir, 'flow.flex.yaml'))
-
-    def test_apply_target_to_data(self, pf_client, questions_file, questions_answers_file):
-        """Test that target was applied correctly."""
-        qa = _apply_target_to_data(_target_fn, questions_file, pf_client)
-        results = pd.read_json(qa, lines=True)
-        ground_truth = pd.read_json(questions_answers_file, lines=True)
-        try:
-            assert_frame_equal(results, ground_truth, check_like=True)
-        finally:
-            os.unlink(qa)
-
-    @pytest.mark.parametrize('columns,expected', [
-        (['answer'], ['ground_truth']),
-        (['answer', 'ground_truth'], []),
-        ])
-    def test_missing_columns(self, columns, expected):
-        """Test that we are returning correct missing columns."""
-        assert _get_missing_inputs(F1ScoreEvaluator(), columns) == expected
---
+++ src/promptflow-evals/tests/unittests/test_evaluate.py
@@ -0,0 +1,96 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+import os
+import pandas as pd
+import pathlib
+import pytest
+
+from pandas.testing import assert_frame_equal
+from unittest.mock import patch
+
+from promptflow.client import PFClient
+from promptflow.evals.evaluate._evaluate import _apply_target_to_data, _get_missing_inputs, \
+    evaluate
+from promptflow.evals.evaluate._utils import save_function_as_flow
+from promptflow.evals.evaluators.f1_score import F1ScoreEvaluator
+
+
+@pytest.fixture
+def pf_client() -> PFClient:
+    """The fixture, returning PRClient"""
+    return PFClient()
+
+
+@pytest.fixture
+def questions_file():
+    data_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "data")
+    return os.path.join(data_path, "questions.jsonl")
+
+
+@pytest.fixture
+def questions_wrong_file():
+    data_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "data")
+    return os.path.join(data_path, "questions_wrong.jsonl")
+
+
+@pytest.fixture
+def questions_answers_file():
+    data_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "data")
+    return os.path.join(data_path, "questions_answers.jsonl")
+
+
+def _target_fn(question):
+    """An example target function."""
+    if 'LV-426' in question:
+        return {'answer': 'There is nothing good there.'}
+    if 'central heating' in question:
+        return {'answer': 'There is no central heating on the streets today, but it will be, I promise.'}
+    if 'strange' in question:
+        return {'answer': 'The life is strange...'}
+
+
+class TestEvaluate:
+    """Test various functions from evaluate."""
+
+    def test_evaluate_missing_required_inputs_target(self, questions_wrong_file):
+        with pytest.raises(ValueError) as exc_info:
+            evaluate(data=questions_wrong_file,
+                     evaluators={"g": F1ScoreEvaluator()},
+                     target=_target_fn
+                     )
+        assert "Missing required inputs for target : ['question']." in exc_info.value.args[0]
+
+    def test_wrong_target(self, questions_file):
+        """Test error, when target function does not generate required column."""
+        with pytest.raises(ValueError) as exc_info:
+            # target_fn will generate the "answer", but not ground truth.
+            evaluate(data=questions_file, evaluators={"g": F1ScoreEvaluator()}, target=_target_fn)
+
+        assert "Missing required inputs for evaluator g : ['ground_truth']." in exc_info.value.args[0]
+
+    @pytest.mark.parametrize('script_is_file', [True, False])
+    def test_save_fun_as_flow(self, tmpdir, pf_client, script_is_file):
+        """Test saving function as flow."""
+        with patch('promptflow.evals.evaluate._utils.os') as mock_os:
+            mock_os.path.isfile.return_value = script_is_file
+            save_function_as_flow(_target_fn, tmpdir, pf_client)
+        assert os.path.isfile(os.path.join(tmpdir, 'flow.flex.yaml'))
+
+    def test_apply_target_to_data(self, pf_client, questions_file, questions_answers_file):
+        """Test that target was applied correctly."""
+        qa = _apply_target_to_data(_target_fn, questions_file, pf_client)
+        results = pd.read_json(qa, lines=True)
+        ground_truth = pd.read_json(questions_answers_file, lines=True)
+        try:
+            assert_frame_equal(results, ground_truth, check_like=True)
+        finally:
+            os.unlink(qa)
+
+    @pytest.mark.parametrize('columns,expected', [
+        (['answer'], ['ground_truth']),
+        (['answer', 'ground_truth'], []),
+        ])
+    def test_missing_columns(self, columns, expected):
+        """Test that we are returning correct missing columns."""
+        assert _get_missing_inputs(F1ScoreEvaluator(), columns) == expected
