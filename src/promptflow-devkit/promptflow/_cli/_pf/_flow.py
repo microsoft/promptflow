3663b3afe7a44d7fd3551559d60bbc9e841a7c56
Ying Chen <2601502859@qq.com>
2024-04-23 18:58:01 +0800

    set internal for no flow in ui

--- src/promptflow-devkit/promptflow/_cli/_pf/_flow.py
+++ src/promptflow-devkit/promptflow/_cli/_pf/_flow.py
@@ -511,19 +511,21 @@ def _test_flow_multi_modal(args, pf_client):
         from promptflow._sdk._tracing import _invoke_pf_svc
 
         # Todo: use base64 encode for now, will consider whether need use encryption or use db to store flow path info
-        def generate_url(flow_path, port):
+        def generate_url(flow_path, port, enable_internal_features=False):
             encrypted_flow_path = encrypt_flow_path(flow_path)
             query_dict = {"flow": encrypted_flow_path}
-            if Configuration.get_instance().is_internal_features_enabled():
+            if Configuration.get_instance().is_internal_features_enabled() or enable_internal_features:
                 query_dict.update({"enable_internal_features": "true"})
             query_params = urlencode(query_dict)
             return urlunparse(("http", f"127.0.0.1:{port}", "/v1.0/ui/chat", "", query_params, ""))
 
         pfs_port = _invoke_pf_svc()
         flow = generate_yaml_entry_core(entry=args.flow)
+        # flex flow without yaml file doesn't support /eval in chat window
+        enable_internal_features = True if flow != args.flow else False
         flow_path_dir, flow_path_file = resolve_flow_path(flow)
         flow_path = str(flow_path_dir / flow_path_file)
-        chat_page_url = generate_url(flow_path, pfs_port)
+        chat_page_url = generate_url(flow_path, pfs_port, enable_internal_features=enable_internal_features)
         print(f"You can begin chat flow on {chat_page_url}")
         if not args.skip_open_browser:
             webbrowser.open(chat_page_url)
