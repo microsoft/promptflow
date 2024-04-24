0d497061066e2ead7afb45e90faad096afca1fb4
Zhen Ruan <zhrua@microsoft.com>
2024-04-24 15:41:49 +0800

    add internal to promptflow

---
+++ src/promptflow-core/promptflow/_internal/__init__.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+
+# flake8: noqa
+
+"""Put some imports here for external calls"""
+from promptflow._utils.flow_utils import is_prompty_flow
