a4a1bcfeaea7af6621a83acf5e4a9d462be9cfdc
Ankit Singhal <anksing@microsoft.com>
2024-04-24 11:03:24 -0700

    Removing path update line from evaluator init file

--- src/promptflow-evals/promptflow/evals/evaluators/chat/__init__.py
+++ src/promptflow-evals/promptflow/evals/evaluators/chat/__init__.py
@@ -2,8 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-
 from ._chat import ChatEvaluator
 
 __all__ = [
