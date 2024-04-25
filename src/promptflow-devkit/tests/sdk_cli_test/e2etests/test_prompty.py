108e0674f4768b0426521ebd6842f6cf65582011
Philip Gao <yigao@microsoft.com>
2024-04-25 15:42:59 +0800

    Fix matrix, using recording even if it doesn't use it. (#2999)
    # Description
    
    Fix matrix
    
    # All Promptflow Contribution checklist:
    - [ ] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [ ] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [ ] Pull request includes test coverage for the included changes.

--- src/promptflow-devkit/tests/sdk_cli_test/e2etests/test_prompty.py
+++ src/promptflow-devkit/tests/sdk_cli_test/e2etests/test_prompty.py
@@ -19,7 +19,6 @@ from promptflow.core._errors import (
 )
 from promptflow.core._model_configuration import AzureOpenAIModelConfiguration
 from promptflow.core._prompty_utils import convert_model_configuration_to_connection
-from promptflow.recording.record_mode import is_live, is_record, is_replay
 
 TEST_ROOT = PROMPTFLOW_ROOT / "tests"
 DATA_DIR = TEST_ROOT / "test_configs/datas"
@@ -242,12 +241,10 @@ class TestPrompty:
         assert isinstance(result, ChatCompletion)
 
     def test_prompty_with_stream(self, pf: PFClient):
-        if is_live():
-            # When running multiple test cases, the type is generator type.
-            # When running alone this case, the type is Stream.
-            stream_type = (types.GeneratorType, Stream)
-        elif is_record() or is_replay():
+        if pytest.is_record or pytest.is_replay:
             stream_type = types.GeneratorType
+        else:
+            stream_type = (types.GeneratorType, Stream)
         # Test text format with stream=true
         prompty = Prompty.load(source=f"{PROMPTY_DIR}/prompty_example.prompty", model={"parameters": {"stream": True}})
         result = prompty(question="what is the result of 1+1?")
