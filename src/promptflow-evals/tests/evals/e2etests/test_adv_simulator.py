1f243e4b2cfad482e4897dac6ae338f6533b4172
Nagkumar Arkalgud <naarkalg@microsoft.com>
2024-04-26 13:58:02 -0700

    e2e test with and without rai_svc_url

--- src/promptflow-evals/tests/evals/e2etests/test_adv_simulator.py
+++ src/promptflow-evals/tests/evals/e2etests/test_adv_simulator.py
@@ -3,16 +3,29 @@ import os
 import pytest
 from azure.identity import DefaultAzureCredential
 
-from promptflow.evals.synthetic import AdversarialSimulator
-
 
 @pytest.mark.usefixtures(
     "model_config", "recording_injection", "ml_client_config", "configure_default_azure_credential"
 )
 @pytest.mark.e2etest
 class TestAdvSimulator:
-    def test_adv_sim_init(self, model_config, ml_client_config):
+    def test_adv_sim_init_with_int_url(self, model_config, ml_client_config):
         os.environ["rai_svc_url"] = "https://int.api.azureml-test.ms"
+        from promptflow.evals.synthetic import AdversarialSimulator
+
+        template = "adv_conversation"
+        project_scope = {
+            "subscription_id": ml_client_config["subscription_id"],
+            "resource_group_name": ml_client_config["resource_group_name"],
+            "workspace_name": ml_client_config["project_name"],
+            "credential": DefaultAzureCredential(),
+        }
+        simulator = AdversarialSimulator(template=template, project_scope=project_scope)
+        assert callable(simulator)
+
+    def test_adv_sim_init_with_prod_url(self, model_config, ml_client_config):
+        from promptflow.evals.synthetic import AdversarialSimulator
+
         template = "adv_conversation"
         project_scope = {
             "subscription_id": ml_client_config["subscription_id"],
