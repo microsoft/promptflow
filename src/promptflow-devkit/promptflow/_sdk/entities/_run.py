2040fe91fee84722b792459a88ed4ceb06384a10
chenyzhang <chenyzhang@microsoft.com>
2024-04-28 16:28:53 +0800

    fix config override tracing.destination

--- src/promptflow-devkit/promptflow/_sdk/entities/_run.py
+++ src/promptflow-devkit/promptflow/_sdk/entities/_run.py
@@ -179,7 +179,7 @@ class Run(YAMLTranslatableMixin):
             # sanitize flow_dir to avoid invalid experiment name
             self._experiment_name = _sanitize_python_variable_name(flow_dir.name)
             self._lineage_id = get_flow_lineage_id(flow_dir=flow_dir)
-            self._output_path = Path(kwargs.get("output_path", self._generate_output_path(config=self._config.config)))
+            self._output_path = Path(kwargs.get("output_path", self._generate_output_path(config=self._config)))
             if is_prompty_flow(self.flow):
                 self._flow_name = Path(self.flow).stem
             else:
