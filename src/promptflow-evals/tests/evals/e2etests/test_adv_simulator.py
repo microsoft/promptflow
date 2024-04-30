340ac6d0d4affc1aaee0c5835d2c1516f7ab75b0
Nagkumar Arkalgud <naarkalg@microsoft.com>
2024-04-30 13:50:56 -0700

    Adding e2e test for adv_conversation

--- src/promptflow-evals/tests/evals/e2etests/test_adv_simulator.py
+++ src/promptflow-evals/tests/evals/e2etests/test_adv_simulator.py
@@ -1,6 +1,6 @@
 import asyncio
 import os
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List
 
 import pytest
 from azure.identity import DefaultAzureCredential
@@ -53,7 +53,6 @@ class TestAdvSimulator:
             messages: List[Dict],
             stream: bool = False,
             session_state: Any = None,
-            context: Optional[Dict[str, Any]] = None,
         ) -> dict:
             question = messages["messages"][0]["content"]
             response_from_acs, temperature = question, 0.0
@@ -65,12 +64,7 @@ class TestAdvSimulator:
                 },
             }
             messages["messages"].append(formatted_response)
-            return {
-                "messages": messages["messages"],
-                "stream": stream,
-                "session_state": session_state,
-                "context": context,
-            }
+            return {"messages": messages["messages"], "stream": stream, "session_state": session_state}
 
         simulator = AdversarialSimulator(template=template, project_scope=project_scope)
 
@@ -86,3 +80,42 @@ class TestAdvSimulator:
             )
         )
         assert len(outputs) == 1
+
+    def test_adv_conversation_sim_responds_with_responses(self, model_config, ml_client_config):
+        from promptflow.evals.synthetic import AdversarialSimulator
+
+        template = "adv_conversation"
+        project_scope = {
+            "subscription_id": ml_client_config["subscription_id"],
+            "resource_group_name": ml_client_config["resource_group_name"],
+            "workspace_name": ml_client_config["project_name"],
+            "credential": DefaultAzureCredential(),
+        }
+
+        async def callback(
+            messages: List[Dict],
+            stream: bool = False,
+            session_state: Any = None,
+        ) -> dict:
+            question = messages["messages"][0]["content"]
+
+            formatted_response = {"content": question, "role": "assistant"}
+            messages["messages"].append(formatted_response)
+            return {"messages": messages["messages"], "stream": stream, "session_state": session_state}
+
+        simulator = AdversarialSimulator(template=template, project_scope=project_scope)
+
+        outputs = asyncio.run(
+            simulator(
+                max_conversation_turns=2,
+                max_simulation_results=1,
+                target=callback,
+                api_call_retry_limit=3,
+                api_call_retry_sleep_sec=1,
+                api_call_delay_sec=30,
+                concurrent_async_task=1,
+            )
+        )
+        assert len(outputs) == 1
+        print(outputs)
+        assert len(outputs[0]["messages"]) == 4
