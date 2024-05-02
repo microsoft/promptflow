2cc03e917cbee716ae2758f261c3fa3ead34b6b9
Ankit Singhal <anksing@microsoft.com>
2024-05-02 13:31:20 -0700

    Removing bug bash folder

--- src/promptflow-evals/samples/bug-bash/evaluate-target/evaluators/blocklist/blocklist.py
+++
@@ -1,8 +0,0 @@
-
-class BlocklistEvaluator:
-    def __init__(self, blocklist):
-        self._blocklist = blocklist
-
-    def __call__(self, *, answer: str, **kwargs):
-        score = any([word in answer for word in self._blocklist])
-        return {"score": score}
--- src/promptflow-evals/samples/bug-bash/evaluate-using-data/custom/answer_length.py
+++
@@ -1,6 +0,0 @@
-class AnswerLengthEvaluator:
-    def __init__(self):
-        pass
-
-    def __call__(self, *, answer: str, **kwargs):
-        return {"answer_length": len(answer)}
