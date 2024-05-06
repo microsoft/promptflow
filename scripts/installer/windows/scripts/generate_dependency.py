3c6c589f4024086ce0527ca4546cca60bb4363bd
Ying Chen <2601502859@qq.com>
2024-05-06 14:38:55 +0800

    update

--- scripts/installer/windows/scripts/generate_dependency.py
+++ scripts/installer/windows/scripts/generate_dependency.py
@@ -103,9 +103,13 @@ if __name__ == '__main__':
     dependencies = list(set(dependencies))
     direct_package_dependencies = get_toml_dependencies(dependencies)
 
-    # get one step furture for dependencies
-    dependencies = list(set(direct_package_dependencies))
-    direct_package_dependencies = get_package_dependencies(dependencies)
+    # Remove this line since some pakages in toml rely on many packages which results in msi package size too large.
+    # We have addd collect_all opentelemetry in spec and no need the line below for now
+
+    # go one step further for direct package dependencies, some packages like opentelemetry-exporter-otlp-proto-http
+    # need this step to get dependencies
+    # dependencies = list(set(direct_package_dependencies))
+    # direct_package_dependencies = get_package_dependencies(dependencies)
 
     # get all dependencies
     all_packages = list(set(dependencies) | set(direct_package_dependencies))
