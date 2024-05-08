b26d68f2dd0f4be498e9806e38449fcfe324423f
Jingyi Zhu <jingyizhu@microsoft.com>
2024-05-08 09:12:31 -0700

    flake 8

--- src/promptflow-rag/promptflow/rag/_get_langchain_retriever.py
+++ src/promptflow-rag/promptflow/rag/_get_langchain_retriever.py
@@ -4,15 +4,16 @@
 from pathlib import Path
 from azureml.rag.mlindex import MLIndex
 from promptflow.rag.constants._common import STORAGE_URI_TO_MLINDEX_PATH_FORMAT
-import re, yaml
+import re
+import yaml
 
 
 def get_langchain_retriever_from_index(path: str):
     if re.match(STORAGE_URI_TO_MLINDEX_PATH_FORMAT, path):
         return MLIndex(path).as_langchain_retriever()
-    
+
     # local path
-    mlindex_path = str(Path(path) / f"MLIndex") if not path.endswith("MLIndex") else path
+    mlindex_path = str(Path(path) / "MLIndex") if not path.endswith("MLIndex") else path
     with open(mlindex_path, "r") as f:
         config = yaml.safe_load(f)
         return MLIndex(mlindex_config=config).as_langchain_retriever()
