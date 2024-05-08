653a9bf67546cd18687f5e12a503c972a34ef244
Ankit Singhal <30610298+singankit@users.noreply.github.com>
2024-05-08 13:56:59 -0700

    Update _pf_evals.py

--- src/promptflow-devkit/promptflow/_dependencies/_pf_evals.py
+++ src/promptflow-devkit/promptflow/_dependencies/_pf_evals.py
@@ -5,5 +5,6 @@
 # This file captures promptflow-evals dependencies on private API of promptflow.
 # In case changes are made please reach out to promptflow-evals team to update the dependencies.
 
+# flake8: noqa
 from promptflow._sdk._constants import LINE_NUMBER, Local2Cloud
 from promptflow._sdk._configuration import Configuration
