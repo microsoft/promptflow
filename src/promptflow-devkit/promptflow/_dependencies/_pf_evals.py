640bb170773d75c24c66d162c44d19a29d2a2e31
Ankit Singhal <anksing@microsoft.com>
2024-05-08 00:16:51 -0700

    Review comments

--- src/promptflow-devkit/promptflow/_dependencies/_pf_evals.py
+++ src/promptflow-devkit/promptflow/_dependencies/_pf_evals.py
@@ -5,12 +5,5 @@
 # This file captures promptflow-evals dependencies on private API of promptflow.
 # In case changes are made please reach out to promptflow-evals team to update the dependencies.
 
-
-def _get_pf_evals_dependencies():
-    from promptflow._sdk._constants import LINE_NUMBER
-    from promptflow._sdk._constants import Local2Cloud
-
-    return {
-        "LINE_NUMBER": LINE_NUMBER,
-        "Local2Cloud": Local2Cloud
-    }
+from promptflow._sdk._constants import LINE_NUMBER, Local2Cloud
+from promptflow._sdk._configuration import Configuration
--- src/promptflow-devkit/tests/unittests/_sdk/_utilities/test_pf_eval_dependencies.py
+++ src/promptflow-devkit/tests/unittests/_sdk/_utilities/test_pf_eval_dependencies.py
@@ -1,13 +1,26 @@
+from unittest.mock import patch
+
 import pytest
-from promptflow._dependencies._pf_evals import _get_pf_evals_dependencies
+from promptflow._dependencies._pf_evals import LINE_NUMBER, Local2Cloud, Configuration
+
+DUMMY_TRACE_DESTINATION = ("azureml://subscriptions/sub_id/resourceGroups/resource_group_name"
+                     "/providers/Microsoft.MachineLearningServices/workspaces/workspace_name")
 
 
 @pytest.mark.unittest
 class TestPromptflowEvalsDependencies:
 
-    def test_pf_eval_dependencies(self):
-        result = _get_pf_evals_dependencies()
-        assert result["LINE_NUMBER"] == "line_number"
-        assert result["Local2Cloud"].FLOW_INSTANCE_RESULTS_FILE_NAME == "instance_results.jsonl"
-        assert result["Local2Cloud"].BLOB_ROOT_PROMPTFLOW == "promptflow"
-        assert result["Local2Cloud"].BLOB_ARTIFACTS == "PromptFlowArtifacts"
+    def test_pf_eval_constants_dependencies(self):
+        assert LINE_NUMBER == "line_number"
+        assert Local2Cloud.FLOW_INSTANCE_RESULTS_FILE_NAME == "instance_results.jsonl"
+        assert Local2Cloud.BLOB_ROOT_PROMPTFLOW == "promptflow"
+        assert Local2Cloud.BLOB_ARTIFACTS == "PromptFlowArtifacts"
+
+    def test_pf_eval_configuration_dependencies(self):
+        with patch("promptflow._sdk._configuration.Configuration._validate", return_value=None):
+            config = Configuration(
+                overrides={
+                    "trace.destination": DUMMY_TRACE_DESTINATION})
+            assert config.get_trace_destination() == DUMMY_TRACE_DESTINATION
+            config = Configuration(overrides={})
+            assert config.get_trace_destination() is None
