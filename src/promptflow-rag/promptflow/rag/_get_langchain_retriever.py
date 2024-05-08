4b0b0ba30c3750f4cb757c0160f6c066e296fdb7
Jingyi Zhu <jingyizhu@microsoft.com>
2024-05-07 21:21:13 -0700

    use Path

--- src/promptflow-rag/promptflow/rag/_get_langchain_retriever.py
+++ src/promptflow-rag/promptflow/rag/_get_langchain_retriever.py
@@ -1,6 +1,7 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+from pathlib import Path
 from azureml.rag.mlindex import MLIndex
 from promptflow.rag.constants._common import STORAGE_URI_TO_MLINDEX_PATH_FORMAT
 import re, yaml
@@ -11,7 +12,7 @@ def get_langchain_retriever_from_index(path: str):
         return MLIndex(path).as_langchain_retriever()
     
     # local path
-    mlindex_path = f"{path}/MLIndex" if not path.endswith("MLIndex") else path
+    mlindex_path = str(Path(path) / f"MLIndex") if not path.endswith("MLIndex") else path
     with open(mlindex_path, "r") as f:
         config = yaml.safe_load(f)
         return MLIndex(mlindex_config=config).as_langchain_retriever()
