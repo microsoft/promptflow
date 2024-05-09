81d752ab1c8c9ca83e9508fd6df2b5bfbe718080
Jingyi Zhu <jingyizhu@microsoft.com>
2024-05-09 16:55:10 -0700

    rename ConnectionConfig parameters

--- src/promptflow-rag/promptflow/rag/config/_connection_config.py
+++ src/promptflow-rag/promptflow/rag/config/_connection_config.py
@@ -21,22 +21,22 @@ class ConnectionConfig:
     def __init__(
         self,
         *,
-        subscription: str,
-        resource_group: str,
-        workspace: str,
+        subscription_id: str,
+        resource_group_name: str,
+        workspace_name: str,
         connection_name: str,
     ) -> None:
-        self.subscription = subscription
-        self.resource_group = resource_group
-        self.workspace = workspace
+        self.subscription_id = subscription_id
+        self.resource_group_name = resource_group_name
+        self.workspace_name = workspace_name
         self.connection_name = connection_name
 
     def build_connection_id(self) -> str:
         """Construct connection id from connection config"""
 
         return CONNECTION_ID_TEMPLATE.format(
-            self.subscription,
-            self.resource_group,
-            self.workspace,
+            self.subscription_id,
+            self.resource_group_name,
+            self.workspace_name,
             self.connection_name
         )
