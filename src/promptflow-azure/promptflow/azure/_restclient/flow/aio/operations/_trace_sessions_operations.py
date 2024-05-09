0f505b4e96e9be571be71e5ea7b5adc10d667879
Zhengfei Wang <38847871+zhengfeiwang@users.noreply.github.com>
2024-05-09 15:15:41 +0800

    [azure] Update Swagger generated REST client code (#3167)
    # Description
    
    Update Swagger generated REST client code for Cosmos DB init (in another
    PR).
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.

--- src/promptflow-azure/promptflow/azure/_restclient/flow/aio/operations/_trace_sessions_operations.py
+++ src/promptflow-azure/promptflow/azure/_restclient/flow/aio/operations/_trace_sessions_operations.py
@@ -15,7 +15,7 @@ from azure.core.tracing.decorator_async import distributed_trace_async
 
 from ... import models as _models
 from ..._vendor import _convert_request
-from ...operations._trace_sessions_operations import build_attach_cosmos_account_request, build_cleanup_trace_session_async_request, build_get_cosmos_resource_token_request, build_init_trace_session_async_request, build_poll_trace_session_status_request
+from ...operations._trace_sessions_operations import build_attach_cosmos_account_request, build_cleanup_trace_session_async_request, build_get_cosmos_resource_token_request, build_get_trace_session_metadata_async_request, build_init_trace_session_async_request, build_poll_trace_session_status_request, build_setup_trace_session_async_request
 T = TypeVar('T')
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, AsyncHttpResponse], T, Dict[str, Any]], Any]]
 
@@ -40,6 +40,76 @@ class TraceSessionsOperations:
         self._serialize = serializer
         self._deserialize = deserializer
         self._config = config
+
+    @distributed_trace_async
+    async def setup_trace_session_async(
+        self,
+        subscription_id: str,
+        resource_group_name: str,
+        workspace_name: str,
+        body: Optional["_models.TraceDbSetupRequest"] = None,
+        **kwargs: Any
+    ) -> Union["_models.TraceCosmosResourceDtos", Any]:
+        """setup_trace_session_async.
+
+        :param subscription_id: The Azure Subscription ID.
+        :type subscription_id: str
+        :param resource_group_name: The Name of the resource group in which the workspace is located.
+        :type resource_group_name: str
+        :param workspace_name: The name of the workspace.
+        :type workspace_name: str
+        :param body:
+        :type body: ~flow.models.TraceDbSetupRequest
+        :keyword callable cls: A custom type or function that will be passed the direct response
+        :return: TraceCosmosResourceDtos or any, or the result of cls(response)
+        :rtype: ~flow.models.TraceCosmosResourceDtos or any
+        :raises: ~azure.core.exceptions.HttpResponseError
+        """
+        cls = kwargs.pop('cls', None)  # type: ClsType[Union["_models.TraceCosmosResourceDtos", Any]]
+        error_map = {
+            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
+        }
+        error_map.update(kwargs.pop('error_map', {}))
+
+        content_type = kwargs.pop('content_type', "application/json")  # type: Optional[str]
+
+        if body is not None:
+            _json = self._serialize.body(body, 'TraceDbSetupRequest')
+        else:
+            _json = None
+
+        request = build_setup_trace_session_async_request(
+            subscription_id=subscription_id,
+            resource_group_name=resource_group_name,
+            workspace_name=workspace_name,
+            content_type=content_type,
+            json=_json,
+            template_url=self.setup_trace_session_async.metadata['url'],
+        )
+        request = _convert_request(request)
+        request.url = self._client.format_url(request.url)
+
+        pipeline_response = await self._client._pipeline.run(request, stream=False, **kwargs)
+        response = pipeline_response.http_response
+
+        if response.status_code not in [200, 202]:
+            map_error(status_code=response.status_code, response=response, error_map=error_map)
+            error = self._deserialize.failsafe_deserialize(_models.ErrorResponse, pipeline_response)
+            raise HttpResponseError(response=response, model=error)
+
+        if response.status_code == 200:
+            deserialized = self._deserialize('TraceCosmosResourceDtos', pipeline_response)
+
+        if response.status_code == 202:
+            deserialized = self._deserialize('object', pipeline_response)
+
+        if cls:
+            return cls(pipeline_response, deserialized, {})
+
+        return deserialized
+
+    setup_trace_session_async.metadata = {'url': '/flow/api/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.MachineLearningServices/workspaces/{workspaceName}/TraceSessions/setup'}  # type: ignore
+
 
     @distributed_trace_async
     async def init_trace_session_async(
@@ -102,6 +172,61 @@ class TraceSessionsOperations:
         return deserialized
 
     init_trace_session_async.metadata = {'url': '/flow/api/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.MachineLearningServices/workspaces/{workspaceName}/TraceSessions/initialize'}  # type: ignore
+
+
+    @distributed_trace_async
+    async def get_trace_session_metadata_async(
+        self,
+        subscription_id: str,
+        resource_group_name: str,
+        workspace_name: str,
+        **kwargs: Any
+    ) -> "_models.TraceCosmosMetaDto":
+        """get_trace_session_metadata_async.
+
+        :param subscription_id: The Azure Subscription ID.
+        :type subscription_id: str
+        :param resource_group_name: The Name of the resource group in which the workspace is located.
+        :type resource_group_name: str
+        :param workspace_name: The name of the workspace.
+        :type workspace_name: str
+        :keyword callable cls: A custom type or function that will be passed the direct response
+        :return: TraceCosmosMetaDto, or the result of cls(response)
+        :rtype: ~flow.models.TraceCosmosMetaDto
+        :raises: ~azure.core.exceptions.HttpResponseError
+        """
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.TraceCosmosMetaDto"]
+        error_map = {
+            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
+        }
+        error_map.update(kwargs.pop('error_map', {}))
+
+        
+        request = build_get_trace_session_metadata_async_request(
+            subscription_id=subscription_id,
+            resource_group_name=resource_group_name,
+            workspace_name=workspace_name,
+            template_url=self.get_trace_session_metadata_async.metadata['url'],
+        )
+        request = _convert_request(request)
+        request.url = self._client.format_url(request.url)
+
+        pipeline_response = await self._client._pipeline.run(request, stream=False, **kwargs)
+        response = pipeline_response.http_response
+
+        if response.status_code not in [200]:
+            map_error(status_code=response.status_code, response=response, error_map=error_map)
+            error = self._deserialize.failsafe_deserialize(_models.ErrorResponse, pipeline_response)
+            raise HttpResponseError(response=response, model=error)
+
+        deserialized = self._deserialize('TraceCosmosMetaDto', pipeline_response)
+
+        if cls:
+            return cls(pipeline_response, deserialized, {})
+
+        return deserialized
+
+    get_trace_session_metadata_async.metadata = {'url': '/flow/api/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.MachineLearningServices/workspaces/{workspaceName}/TraceSessions'}  # type: ignore
 
 
     @distributed_trace_async
