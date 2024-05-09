18efc0c1bd7142671cf0a7525ddf66bc9183483f
Zhengfei Wang <wangzhengfei.chn@outlook.com>
2024-05-09 09:01:40 +0000

    add new cosmos db setup api

---
+++ src/promptflow-azure/promptflow/azure/_constants/_trace.py
@@ -0,0 +1,6 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+COSMOS_DB_SETUP_POLL_TIMEOUT_SECOND = 600  # 10 minutes
+COSMOS_DB_SETUP_POLL_INTERVAL_SECOND = 30  # 30 seconds
