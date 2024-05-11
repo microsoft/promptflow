5bcfe22525a336158f0c180bbfec14b5e872a935
chenyang <chenyzhang@microsoft.com>
2024-05-11 14:21:29 +0800

    add flow type to telemetry (#3197)
    # Description
    
    Please add an informative description that covers that changes made by
    the pull request and link all relevant issues.
    1. add `flow_type `filed to telemetry for` pf.flows.test` and
    `pfazure.flows.create_or_update`.
    2. fix get `trace.destination` from `pf._config` when use`
    PFClient(config={...}) `and `submit a run`.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [ ] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [ ] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [ ] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [ ] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.

--- src/promptflow-devkit/promptflow/_sdk/entities/_run.py
+++ src/promptflow-devkit/promptflow/_sdk/entities/_run.py
@@ -13,7 +13,7 @@ from typing import Any, Dict, List, Optional, Union
 
 from dateutil import parser as date_parser
 
-from promptflow._constants import FlowType, OutputsFolderName, TokenKeys
+from promptflow._constants import OutputsFolderName, TokenKeys
 from promptflow._sdk._configuration import Configuration
 from promptflow._sdk._constants import (
     BASE_PATH_CONTEXT_KEY,
@@ -52,7 +52,13 @@ from promptflow._sdk._utilities.general_utils import (
 )
 from promptflow._sdk.entities._yaml_translatable import YAMLTranslatableMixin
 from promptflow._sdk.schemas._run import RunSchema
-from promptflow._utils.flow_utils import get_flow_lineage_id, is_flex_flow, is_prompty_flow, parse_variant
+from promptflow._utils.flow_utils import (
+    get_flow_lineage_id,
+    get_flow_type,
+    is_flex_flow,
+    is_prompty_flow,
+    parse_variant,
+)
 from promptflow._utils.logger_utils import get_cli_sdk_logger
 from promptflow.exceptions import UserErrorException
 
@@ -874,12 +880,4 @@ class Run(YAMLTranslatableMixin):
     def _flow_type(self) -> str:
         """Get flow type of run."""
 
-        from promptflow._sdk._load_functions import load_flow
-        from promptflow._sdk.entities._flows import FlexFlow
-
-        if is_prompty_flow(self.flow):
-            return FlowType.PROMPTY
-        flow_obj = load_flow(source=self.flow)
-        if isinstance(flow_obj, FlexFlow):
-            return FlowType.FLEX_FLOW
-        return FlowType.DAG_FLOW
+        return get_flow_type(self.flow)
