ed7567250c451bb318f5ee381a8a74ab3ae84a26
Peiwen Gao <peiwengao@microsoft.com>
2024-05-11 22:04:27 +0800

    Add test cases for flow with inf nan

---
+++ src/promptflow/tests/executor/e2etests/test_execution_server.py
@@ -0,0 +1,26 @@
+import math
+
+import pytest
+from fastapi.testclient import TestClient
+
+from promptflow.executor._result import LineResult
+
+from ..utils import construct_flow_execution_request_json
+
+
+@pytest.mark.usefixtures("use_secrets_config_file", "dev_connections", "recording_injection", "executor_client")
+@pytest.mark.e2etest
+class TestExecutionServer:
+    # region /execution
+    def test_execution_flow_with_nan_inf(self, executor_client: TestClient):
+        flow_execution_request = construct_flow_execution_request_json(
+            flow_folder="flow-with-nan-inf", inputs={"number": 1}
+        )
+        response = executor_client.post(url="/execution/flow", json=flow_execution_request)
+        assert response.status_code == 200
+        line_result = LineResult.deserialize(response.json())
+        output = line_result.output["output"]
+        assert math.isnan(output["nan"])
+        assert math.isinf(output["inf"])
+
+    # endregion
