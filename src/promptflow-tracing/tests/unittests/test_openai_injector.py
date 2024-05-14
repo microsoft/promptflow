35040a89e4a9e51bc18bec9e7501e50b26010b27
Ming Gu 🙂 <minggu.thuee@outlook.com>
2024-05-14 11:06:44 +0800

    fix test case

--- src/promptflow-tracing/tests/unittests/test_openai_injector.py
+++ src/promptflow-tracing/tests/unittests/test_openai_injector.py
@@ -1,25 +1,25 @@
 import json
 import logging
+from collections.abc import Iterator
 from importlib.metadata import version
-from types import GeneratorType
 from unittest.mock import MagicMock, patch
 
 import openai
 import pytest
 
 from promptflow.tracing._integrations._openai_injector import (
-    USER_AGENT_HEADER,
     PROMPTFLOW_HEADER,
+    USER_AGENT_HEADER,
     _generate_api_and_injector,
+    _legacy_openai_apis,
     _openai_api_list,
+    _openai_apis,
     get_aoai_telemetry_headers,
     inject_async,
     inject_openai_api,
     inject_operation_headers,
     inject_sync,
     recover_openai_api,
-    _legacy_openai_apis,
-    _openai_apis,
 )
 from promptflow.tracing._operation_context import OperationContext
 from promptflow.tracing._tracer import Tracer
@@ -207,7 +207,7 @@ def test_openai_generator_proxy_sync():
             return_generator = openai.resources.Completions.create(stream=True)
 
         assert return_string == "This is a returned string"
-        assert isinstance(return_generator, GeneratorType)
+        assert isinstance(return_generator, Iterator)
 
         for _ in return_generator:
             pass
@@ -258,7 +258,7 @@ async def test_openai_generator_proxy_async():
             return_generator = await openai.resources.AsyncCompletions.create(stream=True)
 
         assert return_string == "This is a returned string"
-        assert isinstance(return_generator, GeneratorType)
+        assert isinstance(return_generator, Iterator)
 
         for _ in return_generator:
             pass
