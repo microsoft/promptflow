2ed31de768b43be472752c129c5ca942b1ba1167
nick863 <30440255+nick863@users.noreply.github.com>
2024-05-15 13:35:24 -0700

    Fix

--- src/promptflow-azure/promptflow/azure/_version.py
+++ src/promptflow-azure/promptflow/azure/_version.py
@@ -7,6 +7,6 @@ import importlib.metadata
 try:
     __version__ = importlib.metadata.version("promptflow-azure")
 except BaseException:
-    __version__ = 0.0.1dev0
+    __version__ = '0.0.1.dev0'
 
 VERSION: str = __version__
