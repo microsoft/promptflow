fd388654310d24d41cd812218828e8b0afbcbb23
nick863 <30440255+nick863@users.noreply.github.com>
2024-05-15 13:25:38 -0700

    Allow editable install by adding try-catch around versions.

--- src/promptflow-core/promptflow/core/_version.py
+++ src/promptflow-core/promptflow/core/_version.py
@@ -4,6 +4,9 @@
 
 import importlib.metadata
 
-__version__ = importlib.metadata.version("promptflow-core")
+try:
+    __version__ = importlib.metadata.version("promptflow-core")
+except BaseException:
+    __version__ = 0.0.1dev0
 
 VERSION: str = __version__
