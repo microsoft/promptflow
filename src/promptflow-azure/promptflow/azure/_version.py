38fe2978a3a1c60eded6004555e006d95b89ef99
nick863 <30440255+nick863@users.noreply.github.com>
2024-05-15 20:35:16 -0700

    Add try catch around versions (#3284)
    # Description
    
    To allow testing packages without actual installation, I have added the
    try-catch around the versions import.
    
    # All Promptflow Contribution checklist:
    - [x] **The pull request does not introduce [breaking changes].**
    - [x] **CHANGELOG is updated for new features, bug fixes or other
    significant changes.**
    - [x] **I have read the [contribution guidelines](../CONTRIBUTING.md).**
    - [x] **Create an issue and link to the pull request to get dedicated
    review from promptflow team. Learn more: [suggested
    workflow](../CONTRIBUTING.md#suggested-workflow).**
    
    ## General Guidelines and Best Practices
    - [x] Title of the pull request is clear and informative.
    - [x] There are a small number of commits, each of which have an
    informative message. This means that previously merged commits do not
    appear in the history of the PR. For more information on cleaning up the
    commits in your PR, [see this
    page](https://github.com/Azure/azure-powershell/blob/master/documentation/development-docs/cleaning-up-commits.md).
    
    ### Testing Guidelines
    - [x] Pull request includes test coverage for the included changes.

--- src/promptflow-azure/promptflow/azure/_version.py
+++ src/promptflow-azure/promptflow/azure/_version.py
@@ -4,6 +4,9 @@
 
 import importlib.metadata
 
-__version__ = importlib.metadata.version("promptflow-azure")
+try:
+    __version__ = importlib.metadata.version("promptflow-azure")
+except BaseException:
+    __version__ = '0.0.1.dev0'
 
 VERSION: str = __version__
