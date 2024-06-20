338ba26e11639268ef91ddbefef2c5597db6ad96
Nagkumar Arkalgud <naarkalg@microsoft.com>
2024-06-20 12:02:01 -0700

    Revert "Handling the exception while creating jinja template"
    This reverts commit a4b20f1342da9762cfc58ba8be68d1fab5652f90.

--- src/promptflow-evals/promptflow/evals/synthetic/__init__.py
+++ src/promptflow-evals/promptflow/evals/synthetic/__init__.py
@@ -1,5 +1,4 @@
 from .adversarial_scenario import AdversarialScenario
 from .adversarial_simulator import AdversarialSimulator
-from .task_simulator import TaskSimulator
 
-__all__ = ["AdversarialSimulator", "AdversarialScenario", "TaskSimulator"]
+__all__ = ["AdversarialSimulator", "AdversarialScenario"]
