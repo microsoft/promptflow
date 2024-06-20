a4b20f1342da9762cfc58ba8be68d1fab5652f90
Nagkumar Arkalgud <naarkalg@microsoft.com>
2024-06-20 12:00:05 -0700

    Handling the exception while creating jinja template

--- src/promptflow-evals/promptflow/evals/synthetic/__init__.py
+++ src/promptflow-evals/promptflow/evals/synthetic/__init__.py
@@ -1,4 +1,5 @@
 from .adversarial_scenario import AdversarialScenario
 from .adversarial_simulator import AdversarialSimulator
+from .task_simulator import TaskSimulator
 
-__all__ = ["AdversarialSimulator", "AdversarialScenario"]
+__all__ = ["AdversarialSimulator", "AdversarialScenario", "TaskSimulator"]
