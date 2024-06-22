5cfa5f9be65a17f2f5cb4f4aeb1df7af8b8937f4
Ankit Singhal <anksing@microsoft.com>
2024-06-21 23:28:11 -0700

    Removing use of constants from pf core

--- src/promptflow-evals/promptflow/evals/evaluate/_batch_run_client/batch_run_context.py
+++ src/promptflow-evals/promptflow/evals/evaluate/_batch_run_client/batch_run_context.py
@@ -3,13 +3,11 @@
 # ---------------------------------------------------------
 import os
 
-from promptflow._sdk._constants import PF_FLOW_ENTRY_IN_TMP, PF_FLOW_META_LOAD_IN_SUBPROCESS
 from promptflow._utils.user_agent_utils import ClientUserAgentUtil
 from promptflow.tracing._integrations._openai_injector import inject_openai_api, recover_openai_api
 
 from ..._user_agent import USER_AGENT
 from .code_client import CodeClient
-from .proxy_client import ProxyClient
 
 
 class BatchRunContext:
@@ -21,14 +19,6 @@ class BatchRunContext:
             ClientUserAgentUtil.append_user_agent(USER_AGENT)
             inject_openai_api()
 
-        if isinstance(self.client, ProxyClient):
-            os.environ[PF_FLOW_ENTRY_IN_TMP] = "true"
-            os.environ[PF_FLOW_META_LOAD_IN_SUBPROCESS] = "false"
-
     def __exit__(self, exc_type, exc_val, exc_tb):
         if isinstance(self.client, CodeClient):
             recover_openai_api()
-
-        if isinstance(self.client, ProxyClient):
-            os.environ.pop(PF_FLOW_ENTRY_IN_TMP, None)
-            os.environ.pop(PF_FLOW_META_LOAD_IN_SUBPROCESS, None)
