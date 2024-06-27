2eb4e6c653d79049afddb3a1a965f5235336736b
Nagkumar Arkalgud <nagkumar@naarkalg-work-mac.local>
2024-06-27 14:42:31 -0700

    jb sim calls the adv sim

--- src/promptflow-evals/promptflow/evals/synthetic/jailbreak_adversarial_simulator.py
+++ src/promptflow-evals/promptflow/evals/synthetic/jailbreak_adversarial_simulator.py
@@ -2,28 +2,17 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 # noqa: E501
-import asyncio
 import functools
 import logging
-import random
-from typing import Any, Callable, Dict, List
+from typing import Any, Callable, Dict
 
-from tqdm import tqdm
+from azure.identity import DefaultAzureCredential
 
 from promptflow._sdk._telemetry import ActivityType, monitor_operation
 from promptflow.evals.synthetic.adversarial_scenario import AdversarialScenario
 
-from ._conversation import CallbackConversationBot, ConversationBot, ConversationRole
-from ._conversation._conversation import simulate_conversation
-from ._model_tools import (
-    AdversarialTemplateHandler,
-    AsyncHTTPClientWithRetry,
-    ManagedIdentityAPITokenManager,
-    ProxyChatCompletionsModel,
-    RAIClient,
-    TokenScope,
-)
-from ._utils import JsonLineList
+from ._model_tools import AdversarialTemplateHandler, ManagedIdentityAPITokenManager, RAIClient, TokenScope
+from .adversarial_simulator import AdversarialSimulator
 
 logger = logging.getLogger(__name__)
 
@@ -62,25 +51,25 @@ class JailbreakAdversarialSimulator:
     :type azure_ai_project: Dict[str, Any]
     """
 
-    def __init__(self, *, azure_ai_project: Dict[str, Any]):
+    def __init__(self, *, azure_ai_project: Dict[str, Any], credential=None):
         """Constructor."""
         # check if azure_ai_project has the keys: subscription_id, resource_group_name, project_name, credential
-        if not all(
-            key in azure_ai_project for key in ["subscription_id", "resource_group_name", "project_name", "credential"]
-        ):
+        if not all(key in azure_ai_project for key in ["subscription_id", "resource_group_name", "project_name"]):
             raise ValueError(
-                "azure_ai_project must contain keys: subscription_id, resource_group_name, project_name, credential"
+                "azure_ai_project must contain keys: subscription_id, resource_group_name and project_name"
             )
         # check the value of the keys in azure_ai_project is not none
-        if not all(
-            azure_ai_project[key] for key in ["subscription_id", "resource_group_name", "project_name", "credential"]
-        ):
-            raise ValueError("subscription_id, resource_group_name, project_name, and credential must not be None")
+        if not all(azure_ai_project[key] for key in ["subscription_id", "resource_group_name", "project_name"]):
+            raise ValueError("subscription_id, resource_group_name and project_name must not be None")
+        if "credential" not in azure_ai_project and not credential:
+            self.credential = DefaultAzureCredential()
+        elif "credential" in azure_ai_project:
+            self.credential = azure_ai_project["credential"]
         self.azure_ai_project = azure_ai_project
         self.token_manager = ManagedIdentityAPITokenManager(
             token_scope=TokenScope.DEFAULT_AZURE_MANAGEMENT,
             logger=logging.getLogger("AdversarialSimulator"),
-            credential=self.azure_ai_project["credential"],
+            credential=self.credential,
         )
         self.rai_client = RAIClient(azure_ai_project=azure_ai_project, token_manager=self.token_manager)
         self.adversarial_template_handler = AdversarialTemplateHandler(
@@ -181,231 +170,30 @@ class JailbreakAdversarialSimulator:
             }]
         }
         """
-        # validate the inputs
-        if scenario != AdversarialScenario.ADVERSARIAL_CONVERSATION:
-            max_conversation_turns = 2
-        else:
-            max_conversation_turns = max_conversation_turns * 2
         if scenario not in AdversarialScenario.__members__.values():
             raise ValueError("Invalid adversarial scenario")
-        self._ensure_service_dependencies()
-        templates = await self.adversarial_template_handler._get_content_harm_template_collections(scenario.value)
-        concurrent_async_task = min(concurrent_async_task, 1000)
-        semaphore = asyncio.Semaphore(concurrent_async_task)
-        sim_results = []
-        tasks = []
-        total_tasks = sum(len(t.template_parameters) for t in templates)
-        if max_simulation_results > total_tasks:
-            logger.warning(
-                "Cannot provide %s results due to maximum number of adversarial simulations that can be generated: %s."
-                "\n %s simulations will be generated.",
-                max_simulation_results,
-                total_tasks,
-                total_tasks,
-            )
-
-        jailbreak_dataset = await self.rai_client.get_jailbreaks_dataset()
-        total_tasks = min(total_tasks, max_simulation_results) * 2
-
-        progress_bar = tqdm(
-            total=total_tasks,
-            desc="generating simulations",
-            ncols=100,
-            unit="simulations",
+        regular_sim = AdversarialSimulator(azure_ai_project=self.azure_ai_project, credential=self.credential)
+        regular_sim_results = await regular_sim(
+            scenario=scenario,
+            target=target,
+            max_conversation_turns=max_conversation_turns,
+            max_simulation_results=max_simulation_results,
+            api_call_retry_limit=api_call_retry_limit,
+            api_call_retry_sleep_sec=api_call_retry_sleep_sec,
+            api_call_delay_sec=api_call_delay_sec,
+            concurrent_async_task=concurrent_async_task,
+            jailbreak=False,
         )
-        tasks = {"jailbreak": [], "regular": []}
-
-        for template in templates:
-            for parameter in template.template_parameters:
-                tasks["regular"].append(
-                    asyncio.create_task(
-                        self._simulate_async(
-                            target=target,
-                            template=template,
-                            parameters=parameter,
-                            max_conversation_turns=max_conversation_turns,
-                            api_call_retry_limit=api_call_retry_limit,
-                            api_call_retry_sleep_sec=api_call_retry_sleep_sec,
-                            api_call_delay_sec=api_call_delay_sec,
-                            semaphore=semaphore,
-                        )
-                    )
-                )
-                parameter = self._join_conversation_starter(parameter, random.choice(jailbreak_dataset))
-                tasks["jailbreak"].append(
-                    asyncio.create_task(
-                        self._simulate_async(
-                            target=target,
-                            template=template,
-                            parameters=parameter,
-                            max_conversation_turns=max_conversation_turns,
-                            api_call_retry_limit=api_call_retry_limit,
-                            api_call_retry_sleep_sec=api_call_retry_sleep_sec,
-                            api_call_delay_sec=api_call_delay_sec,
-                            semaphore=semaphore,
-                        )
-                    )
-                )
-
-                if len(tasks["jailbreak"]) + len(tasks["regular"]) >= max_simulation_results:
-                    break
-            if len(tasks["jailbreak"]) + len(tasks["regular"]) >= max_simulation_results:
-                break
-
-        sim_results = {"jailbreak": [], "regular": []}
-        for task_type, task_list in tasks.items():
-            for task in asyncio.as_completed(task_list):
-                sim_results[task_type].append(await task)
-                progress_bar.update(1)
-
-        return JsonLineList(sim_results["jailbreak"]), JsonLineList(sim_results["regular"])
-
-    def _to_chat_protocol(self, *, conversation_history, template_parameters):
-        messages = []
-        for i, m in enumerate(conversation_history):  # noqa: F841
-            message = {"content": m.message, "role": m.role.value}
-            if "context" in m.full_response:
-                message["context"] = m.full_response["context"]
-            messages.append(message)
-        template_parameters["metadata"] = {}
-        for key in (
-            "conversation_starter",
-            "group_of_people",
-            "metadata",
-            "target_population",
-            "topic",
-            "ch_template_placeholder",
-        ):
-            template_parameters.pop(key, None)
-
-        return {
-            "template_parameters": template_parameters,
-            "messages": messages,
-            "$schema": "http://azureml/sdk-2-0/ChatConversation.json",
-        }
-
-    async def _simulate_async(
-        self,
-        *,
-        target: Callable,
-        template,
-        parameters,
-        max_conversation_turns,
-        api_call_retry_limit,
-        api_call_retry_sleep_sec,
-        api_call_delay_sec,
-        semaphore,
-    ) -> List[Dict]:
-        user_bot = self._setup_bot(role=ConversationRole.USER, template=template, parameters=parameters)
-        system_bot = self._setup_bot(
-            target=target, role=ConversationRole.ASSISTANT, template=template, parameters=parameters
+        jb_sim = AdversarialSimulator(azure_ai_project=self.azure_ai_project, credential=self.credential)
+        jb_sim_results = await jb_sim(
+            scenario=scenario,
+            target=target,
+            max_conversation_turns=max_conversation_turns,
+            max_simulation_results=max_simulation_results,
+            api_call_retry_limit=api_call_retry_limit,
+            api_call_retry_sleep_sec=api_call_retry_sleep_sec,
+            api_call_delay_sec=api_call_delay_sec,
+            concurrent_async_task=concurrent_async_task,
+            jailbreak=True,
         )
-        bots = [user_bot, system_bot]
-        asyncHttpClient = AsyncHTTPClientWithRetry(
-            n_retry=api_call_retry_limit,
-            retry_timeout=api_call_retry_sleep_sec,
-            logger=logger,
-        )
-        async with semaphore:
-            async with asyncHttpClient.client as session:
-                _, conversation_history = await simulate_conversation(
-                    bots=bots,
-                    session=session,
-                    turn_limit=max_conversation_turns,
-                    api_call_delay_sec=api_call_delay_sec,
-                )
-        return self._to_chat_protocol(conversation_history=conversation_history, template_parameters=parameters)
-
-    def _get_user_proxy_completion_model(self, template_key, template_parameters):
-        return ProxyChatCompletionsModel(
-            name="raisvc_proxy_model",
-            template_key=template_key,
-            template_parameters=template_parameters,
-            endpoint_url=self.rai_client.simulation_submit_endpoint,
-            token_manager=self.token_manager,
-            api_version="2023-07-01-preview",
-            max_tokens=1200,
-            temperature=0.0,
-        )
-
-    def _setup_bot(self, *, role, template, parameters, target: Callable = None):
-        if role == ConversationRole.USER:
-            model = self._get_user_proxy_completion_model(
-                template_key=template.template_name, template_parameters=parameters
-            )
-            return ConversationBot(
-                role=role,
-                model=model,
-                conversation_template=str(template),
-                instantiation_parameters=parameters,
-            )
-
-        if role == ConversationRole.ASSISTANT:
-            dummy_model = lambda: None  # noqa: E731
-            dummy_model.name = "dummy_model"
-            return CallbackConversationBot(
-                callback=target,
-                role=role,
-                model=dummy_model,
-                user_template=str(template),
-                user_template_parameters=parameters,
-                conversation_template="",
-                instantiation_parameters={},
-            )
-        return ConversationBot(
-            role=role,
-            model=model,
-            conversation_template=template,
-            instantiation_parameters=parameters,
-        )
-
-    def _join_conversation_starter(self, parameters, to_join):
-        key = "conversation_starter"
-        if key in parameters.keys():
-            parameters[key] = f"{to_join} {parameters[key]}"
-        else:
-            parameters[key] = to_join
-
-        return parameters
-
-    def call_sync(
-        self,
-        *,
-        max_conversation_turns: int,
-        max_simulation_results: int,
-        target: Callable,
-        api_call_retry_limit: int,
-        api_call_retry_sleep_sec: int,
-        api_call_delay_sec: int,
-        concurrent_async_task: int,
-    ):
-        # Running the async method in a synchronous context
-        loop = asyncio.get_event_loop()
-        if loop.is_running():
-            # If the loop is already running, use run_until_complete
-            # Note: This approach might not be suitable in all contexts, especially with nested async calls
-            future = asyncio.ensure_future(
-                self(
-                    max_conversation_turns=max_conversation_turns,
-                    max_simulation_results=max_simulation_results,
-                    target=target,
-                    api_call_retry_limit=api_call_retry_limit,
-                    api_call_retry_sleep_sec=api_call_retry_sleep_sec,
-                    api_call_delay_sec=api_call_delay_sec,
-                    concurrent_async_task=concurrent_async_task,
-                )
-            )
-            return loop.run_until_complete(future)
-        else:
-            # If no event loop is running, use asyncio.run (Python 3.7+)
-            return asyncio.run(
-                self(
-                    max_conversation_turns=max_conversation_turns,
-                    max_simulation_results=max_simulation_results,
-                    target=target,
-                    api_call_retry_limit=api_call_retry_limit,
-                    api_call_retry_sleep_sec=api_call_retry_sleep_sec,
-                    api_call_delay_sec=api_call_delay_sec,
-                    concurrent_async_task=concurrent_async_task,
-                )
-            )
+        return {"jailbreak": jb_sim_results, "regular": regular_sim_results}
