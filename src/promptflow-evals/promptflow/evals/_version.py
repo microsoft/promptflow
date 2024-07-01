33e984d57b10b7190e573e70acfe1ba4d14ac36a
Ankit Singhal <30610298+singankit@users.noreply.github.com>
2024-07-01 14:51:40 -0700

    Update _version.py

--- src/promptflow-evals/promptflow/evals/_version.py
+++ src/promptflow-evals/promptflow/evals/_version.py
@@ -7,6 +7,6 @@ import importlib.metadata
 try:
     __version__ = importlib.metadata.version("promptflow-evals")
 except BaseException:
-    __version__ = '0.0.1.dev0'
+    __version__ = '0.3.1.rc1'
 
 VERSION: str = __version__
