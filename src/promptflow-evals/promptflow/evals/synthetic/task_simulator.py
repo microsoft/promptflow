b98774f80de9971ee20388eaf4ef6ccee98b6130
Nagkumar Arkalgud <nagkumar@naarkalgworkmac.lan>
2024-07-10 15:47:18 -0700

    Fixing the order of response

--- src/promptflow-evals/promptflow/evals/synthetic/task_simulator.py
+++ src/promptflow-evals/promptflow/evals/synthetic/task_simulator.py
@@ -2,11 +2,51 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+import ast
+import asyncio
 import json
 import os
 from typing import Any, Dict, List
 
 from promptflow.client import load_flow
+
+from ._conversation.constants import ConversationRole
+
+
+class ConvTurn:
+    def __init__(self, role, content, context=None):
+        self.role = role
+        self.content = content
+        self.context = context
+
+    def to_dict(self):
+        return {
+            "role": self.role.value if isinstance(self.role, ConversationRole) else self.role,
+            "content": self.content,
+            "context": self.context,
+        }
+
+    def __repr__(self):
+        return f"ConvTurn(role={self.role}, content={self.content})"
+
+
+class ConvHistory:
+    def __init__(self):
+        self.history = []
+
+    def add_to_history(self, turn):
+        self.history.append(turn)
+
+    def to_conv_history(self):
+        return [turn.to_dict() for turn in self.history]
+
+    def get_length(self):
+        return len(self.history)
+
+    def __repr__(self):
+        for turn in self.history:
+            print(turn)
+        return ""
 
 
 class TaskSimulator:
@@ -42,21 +82,20 @@ class TaskSimulator:
                 raise NotImplementedError("Custom prompty not supported yet")
             _flow = load_flow(source=prompty_path, model={"configuration": self.azure_ai_project})
             response = _flow(user_persona=user_persona, conversation_history=conversation_history)
-            ## TODO: Fix the way the response is formatted
-            fixed_response = response.replace("'", '"')
         except Exception as e:
             print("Something went wrong running the prompty")
             raise e
-        # print(f"Response from user simulator prompty: {fixed_response}")
-        # print(f"Type of sim prompty response: {type(fixed_response)}")
-        # import pdb; pdb.set_trace()
         try:
-            user_simulator_prompty_response = json.loads(fixed_response)
+            response_dict = ast.literal_eval(response)
+            response = json.dumps(response_dict)
+            user_simulator_prompty_response = json.loads(response)
         except Exception as e:
             print("Something went wrong parsing the user_simulator_prompty_response output")
+            import pdb
+
+            pdb.set_trace()
             raise e
-        print(f"User simulator prompty response: {user_simulator_prompty_response}")
-        return user_simulator_prompty_response
+        return user_simulator_prompty_response["content"]
 
     async def __call__(
         self,
@@ -67,6 +106,7 @@ class TaskSimulator:
         num_queries: int = 5,
         query_response_generating_prompty: str = None,
         user_simulator_prompty: str = None,
+        api_call_delay_sec: float = 1,
     ):
         if num_queries != len(user_persona):
             num_queries = len(user_persona)
@@ -97,7 +137,6 @@ class TaskSimulator:
         for query_response_pair in query_response_list:
             query = query_response_pair["q"]
             response = query_response_pair["r"]
-            print(f"Query: {query} Expected Response: {response}")
             user_persona_item = user_persona[i]
             i += 1
             conversation = await self.complete_conversation(
@@ -106,6 +145,7 @@ class TaskSimulator:
                 user_persona=user_persona_item,
                 user_simulator_prompty=user_simulator_prompty,
                 target=target,
+                api_call_delay_sec=api_call_delay_sec,
             )
             all_conversations.append(
                 {
@@ -118,33 +158,45 @@ class TaskSimulator:
         return all_conversations
 
     async def complete_conversation(
-        self, *, conversation_starter, max_conversation_turns, user_persona, user_simulator_prompty, target
+        self,
+        *,
+        conversation_starter,
+        max_conversation_turns,
+        user_persona,
+        user_simulator_prompty,
+        target,
+        api_call_delay_sec,
     ):
-        conversation_history = [
-            {
-                "role": "user",
-                "content": conversation_starter,
-            }
-        ]
-        while len(conversation_history) < max_conversation_turns:
+        conversation_history = ConvHistory()
+        turn = ConvTurn(role=ConversationRole.USER, content=conversation_starter)
+        conversation_history.add_to_history(turn)
+
+        while conversation_history.get_length() < max_conversation_turns:
             response = await target(
-                messages={"messages": conversation_history}, stream=False, session_state=None, context=None
+                messages={"messages": conversation_history.to_conv_history()},
+                stream=False,
+                session_state=None,
+                context=None,
             )
+            await asyncio.sleep(api_call_delay_sec)
             messages_list = response["messages"]
             latest_message = messages_list[-1]
             response_from_target = latest_message["content"]
-            conversation_history.append(
-                {
-                    "role": "assistant",
-                    "content": response_from_target,
-                }
-            )
-            if len(conversation_history) == max_conversation_turns:
+            turn = ConvTurn(role=ConversationRole.ASSISTANT, content=response_from_target)
+            conversation_history.add_to_history(turn)
+            # Check if we have reached max_conversation_turns after appending assistant's response
+            if conversation_history.get_length() >= max_conversation_turns:
                 break
+
+            # Get response from user simulator
             response_from_user_simulating_prompty = await self.build_query(
                 user_persona=user_persona,
-                conversation_history=conversation_history,
+                conversation_history=conversation_history.to_conv_history(),
                 user_simulator_prompty=user_simulator_prompty,
             )
-            conversation_history.append(response_from_user_simulating_prompty)
-        return conversation_history
+            await asyncio.sleep(api_call_delay_sec)
+            # Append user simulator's response
+            turn = ConvTurn(role=ConversationRole.USER, content=response_from_user_simulating_prompty)
+            conversation_history.add_to_history(turn)
+
+        return conversation_history.to_conv_history()
