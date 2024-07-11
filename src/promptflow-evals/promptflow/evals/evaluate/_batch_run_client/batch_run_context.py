8b33305f601e1a382636f564321c03618af32c98
Billy Hu <ninhu@microsoft.com>
2024-07-11 10:58:54 -0700

    Add initial changes for support async-enabled evaluator

--- src/promptflow-evals/promptflow/evals/evaluate/_batch_run_client/batch_run_context.py
+++ src/promptflow-evals/promptflow/evals/evaluate/_batch_run_client/batch_run_context.py
@@ -5,6 +5,7 @@ import os
 
 from promptflow._sdk._constants import PF_FLOW_ENTRY_IN_TMP, PF_FLOW_META_LOAD_IN_SUBPROCESS
 from promptflow._utils.user_agent_utils import ClientUserAgentUtil
+from promptflow.azure._utils.general import set_event_loop_policy
 from promptflow.tracing._integrations._openai_injector import inject_openai_api, recover_openai_api
 
 from ..._user_agent import USER_AGENT
@@ -24,6 +25,10 @@ class BatchRunContext:
         if isinstance(self.client, ProxyClient):
             os.environ[PF_FLOW_ENTRY_IN_TMP] = "true"
             os.environ[PF_FLOW_META_LOAD_IN_SUBPROCESS] = "false"
+
+            # For addressing the issue of asyncio event loop closed on Windows
+            # Reference: https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
+            set_event_loop_policy()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if isinstance(self.client, CodeClient):
