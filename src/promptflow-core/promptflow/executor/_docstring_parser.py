cf36e23634cc3505e189f37fc5a23b90956fb8fe
Lina Tang <linatang@microsoft.com>
2024-07-18 16:09:43 +0800

    Use docstring_parser package

--- src/promptflow-core/promptflow/executor/_docstring_parser.py
+++ src/promptflow-core/promptflow/executor/_docstring_parser.py
@@ -1,36 +1,19 @@
-import re
+from docstring_parser import parse
 
 
 class DocstringParser:
     @staticmethod
     def parse_description(docstring: str):
-        # Remove leading and trailing whitespaces
-        docstring = "\n".join([line.strip() for line in docstring.strip().splitlines()])
+        parsed = parse(docstring)
 
-        idx = DocstringParser._find_param_description_index(docstring)
-        if idx == -1:
-            return docstring, {}
-        description = re.sub(r"\s+", " ", docstring[:idx].strip())
+        description = (
+            (short + " " + long if (long := parsed.long_description) is not None else short)
+            if (short := parsed.short_description) is not None
+            else None
+        )
 
-        param_pattern = re.compile(r":param *(\w+): *(.*?)(:param |:type |$)", re.DOTALL)
-        type_pattern = re.compile(r":type *(\w+): *(\S*)", re.DOTALL)
         params = {}
-        for match in param_pattern.finditer(docstring[idx:]):
-            params[match.group(1)] = {"description": match.group(2).strip(), "type": ""}
-        for match in type_pattern.finditer(docstring[idx:]):
-            if match.group(1) in params:
-                params[match.group(1)]["type"] = match.group(2).strip()
+        for param in parsed.params:
+            params[param.arg_name] = {"description": param.description, "type": param.type_name}
 
         return description, params
-
-    def _find_param_description_index(docstring: str):
-        param_idx = docstring.find(":param ")
-        type_idx = docstring.find(":type ")
-        if param_idx == -1 and type_idx == -1:
-            return -1
-        elif param_idx == -1:
-            return type_idx
-        elif type_idx == -1:
-            return param_idx
-        else:
-            return min(param_idx, type_idx)
