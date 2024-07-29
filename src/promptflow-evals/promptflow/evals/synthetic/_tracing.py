6894e66236e93423a6edbeaa4d6746edf64cb5d8
Nagkumar Arkalgud <nagkumar@naarkalg-work-mac.local>
2024-07-29 14:41:15 -0700

    Added docstrings and copyright

--- src/promptflow-evals/promptflow/evals/synthetic/_tracing.py
+++ src/promptflow-evals/promptflow/evals/synthetic/_tracing.py
@@ -1,12 +1,27 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
 import functools
 
 from promptflow._sdk._telemetry.activity import ActivityType, monitor_operation
 
 
 def monitor_adversarial_scenario(activity_name: str = "adversarial.simulator.call"):
+    """
+    Monitor an adversarial scenario.
+    """
+
     def decorator(func):
+        """
+        Decorator for monitoring an adversarial scenario.
+        """
+
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            """
+            Wrapper for monitoring an adversarial scenario.
+            """
             scenario = str(kwargs.get("scenario", None))
             max_conversation_turns = kwargs.get("max_conversation_turns", None)
             max_simulation_results = kwargs.get("max_simulation_results", None)
@@ -30,8 +45,15 @@ def monitor_adversarial_scenario(activity_name: str = "adversarial.simulator.cal
 
 
 def monitor_task_simulator(func):
+    """
+    Monitor a task simulator.
+    """
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+        """
+        Wrapper for monitoring a task simulator.
+        """
         text_length = len(kwargs.get("text", ""))
         user_persona_length = len(kwargs.get("user_persona", []))
         num_queries = kwargs.get("num_queries", 0)
