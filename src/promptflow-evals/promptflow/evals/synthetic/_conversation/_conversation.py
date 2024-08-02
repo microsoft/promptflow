70861f9a914c0ce778b200ba7d9a2677fff7bb65
Nagkumar Arkalgud <nagkumar@naarkalgworkmac.lan>
2024-08-01 18:45:30 -0700

    change docstring for pylint

--- src/promptflow-evals/promptflow/evals/synthetic/_conversation/_conversation.py
+++ src/promptflow-evals/promptflow/evals/synthetic/_conversation/_conversation.py
@@ -76,22 +76,22 @@ async def simulate_conversation(
     """
     Simulate a conversation between the given bots.
 
-    :param bots: List of ConversationBot instances participating in the conversation.
-    :type bots: List[ConversationBot]
-    :param language: The language in which the conversation should be generated.
-    :type language: promptflow.evals.synthetic._constants.SupportedLanguages
-    :param session: The session to use for making API calls.
-    :type session: RetryClient
-    :param stopping_criteria: A callable that determines when the conversation should stop.
-    :type stopping_criteria: Callable[[str], bool]
-    :param turn_limit: The maximum number of turns in the conversation. Defaults to 10.
-    :type turn_limit: int
-    :param history_limit: The maximum number of turns to keep in the conversation history. Defaults to 5.
-    :type history_limit: int
-    :param api_call_delay_sec: Delay between API calls in seconds. Defaults to 0.
-    :type api_call_delay_sec: float
-    :param logger: The logger to use for logging. Defaults to the logger named after the current module.
-    :type logger: logging.Logger
+    :keyword bots: List of ConversationBot instances participating in the conversation.
+    :paramtype bots: List[ConversationBot]
+    :keyword language: The language in which the conversation should be generated.
+    :paramtype language: promptflow.evals.synthetic._constants.SupportedLanguages
+    :keyword session: The session to use for making API calls.
+    :paramtype session: RetryClient
+    :keyword stopping_criteria: A callable that determines when the conversation should stop.
+    :paramtype stopping_criteria: Callable[[str], bool]
+    :keyword turn_limit: The maximum number of turns in the conversation. Defaults to 10.
+    :paramtype turn_limit: int
+    :keyword history_limit: The maximum number of turns to keep in the conversation history. Defaults to 5.
+    :paramtype history_limit: int
+    :keyword api_call_delay_sec: Delay between API calls in seconds. Defaults to 0.
+    :paramtype api_call_delay_sec: float
+    :keyword logger: The logger to use for logging. Defaults to the logger named after the current module.
+    :paramtype logger: logging.Logger
     :return: Simulation a conversation between the given bots.
     :rtype: Tuple
     """
