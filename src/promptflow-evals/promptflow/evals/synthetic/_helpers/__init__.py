a24eb7811f187dfb8f145ad787a29dc3b17da003
Nagkumar Arkalgud <nagkumar91@users.noreply.github.com>
2024-08-02 16:54:47 -0700

    Update __init__.py to fix imports

--- src/promptflow-evals/promptflow/evals/synthetic/_helpers/__init__.py
+++ src/promptflow-evals/promptflow/evals/synthetic/_helpers/__init__.py
@@ -1,3 +1,3 @@
-from ._simulator_data_classes import ConvHistory, ConvTurn
+from ._simulator_data_classes import Conversation as ConvHistory, Turn as ConvTurn
 
 __all__ = ["ConvHistory", "ConvTurn"]
