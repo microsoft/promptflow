86e52a66a5bff2a0b5e36cb520d99c59dcf2ffd5
kdestin <101366538+kdestin@users.noreply.github.com>
2024-08-21 04:25:11 -0400

    refactor: Replace `aiohttp_retry.RetryClient` with custom azure.core pipeline
        The `RetryClient` from `aiohttp_retry` was used extensively through
        promptflow/evals/synthetic to do async http requests. Both directly
        and as the basis of the `AsyncHTTPClientWithRetry` class.
    
        The two retry strategies used with RetryClient were:
          * Retries with fixed length backoff
          * Retries with jittered exponential backoff
    
        azure.core pipelines essentially covers those two strategies with
        their (Async)RetryPolicy, with the major difference being that
        the exponential flavor doesn't incorporate random jitter.
    
        This commit ends up accomplishing the following:
          * All references to RetryClient, and the two duplicate (?)
            implementation of `AsyncHTTPClientWithRetry` were replaced
            with our "requests-like" azure.core.pipeline. Appropriate retry
            policies were applied to replicate the previous functionality.
          * The dependency on `aiohttp_retry` was removed, since the SDK
            no longer references it

--- src/promptflow-evals/promptflow/evals/synthetic/_model_tools/_identity_manager.py
+++ src/promptflow-evals/promptflow/evals/synthetic/_model_tools/_identity_manager.py
@@ -17,6 +17,7 @@ AZURE_TOKEN_REFRESH_INTERVAL = 600  # seconds
 
 class TokenScope(Enum):
     """Token scopes for Azure endpoints"""
+
     DEFAULT_AZURE_MANAGEMENT = "https://management.azure.com/.default"
 
 
@@ -30,10 +31,12 @@ class APITokenManager(ABC):
     :param credential: Azure credential object
     :type credential: Optional[Union[azure.identity.DefaultAzureCredential, azure.identity.ManagedIdentityCredential]
     """
+
     def __init__(
-        self, logger: logging.Logger,
+        self,
+        logger: logging.Logger,
         auth_header: str = "Bearer",
-        credential: Optional[Union[DefaultAzureCredential, ManagedIdentityCredential]] = None
+        credential: Optional[Union[DefaultAzureCredential, ManagedIdentityCredential]] = None,
     ) -> None:
         self.logger = logger
         self.auth_header = auth_header
@@ -96,6 +99,7 @@ class ManagedIdentityAPITokenManager(APITokenManager):
     :keyword kwargs: Additional keyword arguments
     :paramtype kwargs: Dict
     """
+
     def __init__(self, token_scope: TokenScope, logger: logging.Logger, **kwargs: Dict):
         super().__init__(logger, **kwargs)
         self.token_scope = token_scope
@@ -129,6 +133,7 @@ class PlainTokenManager(APITokenManager):
     :keyword kwargs: Optional keyword arguments
     :paramtype kwargs: Dict
     """
+
     def __init__(self, openapi_key: str, logger: logging.Logger, **kwargs: Dict):
         super().__init__(logger, **kwargs)
         self.token = openapi_key
--- src/promptflow-evals/promptflow/evals/synthetic/_model_tools/_proxy_completion_model.py
+++ src/promptflow-evals/promptflow/evals/synthetic/_model_tools/_proxy_completion_model.py
@@ -8,11 +8,10 @@ import time
 import uuid
 from typing import Dict, List
 
-from aiohttp.web import HTTPException
-from aiohttp_retry import RetryClient
+from azure.core.exceptions import HttpResponseError
 from azure.core.pipeline.policies import AsyncRetryPolicy, RetryMode
 
-from promptflow.evals._http_utils import get_async_http_client
+from promptflow.evals._http_utils import AsyncHttpPipeline, get_async_http_client
 from promptflow.evals._user_agent import USER_AGENT
 
 from .models import OpenAIChatCompletionsModel
@@ -100,7 +99,7 @@ class ProxyChatCompletionsModel(OpenAIChatCompletionsModel):
     async def get_conversation_completion(
         self,
         messages: List[Dict],
-        session: RetryClient,
+        session: AsyncHttpPipeline,
         role: str = "assistant",  # pylint: disable=unused-argument
         **request_params,
     ) -> dict:
@@ -110,8 +109,8 @@ class ProxyChatCompletionsModel(OpenAIChatCompletionsModel):
         :param messages: List of messages to query the model with.
             Expected format: [{"role": "user", "content": "Hello!"}, ...]
         :type messages: List[Dict]
-        :param session: aiohttp RetryClient object to query the model with.
-        :type session: ~promptflow.evals.synthetic._model_tools.RetryClient
+        :param session: AsyncHttpPipeline object to query the model with.
+        :type session: ~promptflow.evals._http_utils.AsyncHttpPipeline
         :param role: The role of the user sending the message. This parameter is not used in this method;
             however, it must be included to match the method signature of the parent class. Defaults to "assistant".
         :type role: str
@@ -131,14 +130,14 @@ class ProxyChatCompletionsModel(OpenAIChatCompletionsModel):
 
     async def request_api(
         self,
-        session: RetryClient,
+        session: AsyncHttpPipeline,
         request_data: dict,
     ) -> dict:
         """
         Request the model with a body of data.
 
         :param session: HTTPS Session for invoking the endpoint.
-        :type session: RetryClient
+        :type session: AsyncHttpPipeline
         :param request_data: Prompt dictionary to query the model with. (Pass {"prompt": prompt} instead of prompt.)
         :type request_data: Dict[str, Any]
         :return: A body of data resulting from the model query.
@@ -179,16 +178,15 @@ class ProxyChatCompletionsModel(OpenAIChatCompletionsModel):
         time_start = time.time()
         full_response = None
 
-        async with session.post(
-            url=self.endpoint_url, headers=proxy_headers, json=sim_request_dto.to_dict()
-        ) as response:
-            if response.status == 202:
-                response = await response.json()
-                self.result_url = response["location"]
-            else:
-                raise HTTPException(
-                    reason=f"Received unexpected HTTP status: {response.status} {await response.text()}"
-                )
+        response = await session.post(url=self.endpoint_url, headers=proxy_headers, json=sim_request_dto.to_dict())
+
+        if response.status_code != 202:
+            raise HttpResponseError(
+                message=f"Received unexpected HTTP status: {response.status} {await response.text()}", response=response
+            )
+
+        response = response.json()
+        self.result_url = response["location"]
 
         retry_policy = AsyncRetryPolicy(  # set up retry configuration
             retry_on_status_codes=[202],  # on which statuses to retry
